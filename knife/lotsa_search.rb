9d68f16d50d63efcc135e668093e6f40a1b872aa
Seth Falcon <seth@opscode.com>
2011-07-13 17:19:12 -0700

    Tweak search load tester
    Use "*:*" query

--- knife/lotsa_search.rb
+++ knife/lotsa_search.rb
@@ -1,12 +1,12 @@
 Chef::Log.level= :fatal
 start = Time.now
 i = 0
-query = (1..20).map { |x| "name:newnode-#{x}" }.join(" ")
-puts query
+query = "*:*"
+puts "query: #{query}"
+puts "running for 30 sec"
 while (Time.now - start) < 30 do
   nodes = search(:node, query)
   i += 1
   puts "#{i}: #{nodes.length}"
-  # nodes.each { |n| puts n.name }
 end
 puts "Time: #{(Time.now - start)}"
