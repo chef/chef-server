51174320da3d7868f47a81d20408e28504b4509e
Seth Falcon <seth@opscode.com>
2011-07-27 08:11:11 -0700

    More crufty chefreq stuff
    Eventually we will refactor into a reusable test tool.
    Really, we will.

--- knife/chefreq.erl
+++ knife/chefreq.erl
@@ -66,4 +66,29 @@ bad_start() ->
     {Url, Headers0} = chef_rest_client:generate_signed_headers(Client, "/organizations/userprimary/search/role?q=ab&start=abc", <<"GET">>),
     Headers = [{"Accept", "application/json"}, {"X-CHEF-VERSION", "0.10.0"} | Headers0],
     ibrowse:send_req(Url, Headers, get, [], [{ssl_options, []}]).
+
+
+user_not_in_org() ->
+    application:start(crypto),
+    ssl:start(),
+    ibrowse:start(),
+    PFile = "/Users/seth/oc/environments/orgs/userprimary/.chef/seth.pem",
+    {ok, PBin} = file:read_file(PFile),
+    Private = chef_authn:extract_private_key(PBin),
+    Client = chef_rest_client:make_chef_rest_client("https://api.opscode.com", "seth", Private),
+    {Url, Headers0} = chef_rest_client:generate_signed_headers(Client, "/organizations/knifetest-org/search/role?q=ab&start=abc", <<"GET">>),
+    Headers = [{"Accept", "application/json"}, {"X-CHEF-VERSION", "0.10.0"} | Headers0],
+    ibrowse:send_req(Url, Headers, get, [], [{ssl_options, []}]).
+
+no_user() ->
+    application:start(crypto),
+    ssl:start(),
+    ibrowse:start(),
+    PFile = "/Users/seth/oc/environments/orgs/userprimary/.chef/seth.pem",
+    {ok, PBin} = file:read_file(PFile),
+    Private = chef_authn:extract_private_key(PBin),
+    Client = chef_rest_client:make_chef_rest_client("https://api.opscode.com", "seth-xxx-yyy", Private),
+    {Url, Headers0} = chef_rest_client:generate_signed_headers(Client, "/organizations/userprimary/search/role?q=ab&start=abc", <<"GET">>),
+    Headers = [{"Accept", "application/json"}, {"X-CHEF-VERSION", "0.10.0"} | Headers0],
+    ibrowse:send_req(Url, Headers, get, [], [{ssl_options, []}]).
     
