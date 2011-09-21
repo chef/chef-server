9d827fcd6b8360cbdf3e98716bd255bf4e130324
Seth Falcon <seth@opscode.com>
2011-09-21 16:07:43 -0700

    Fixup search tests now that chef_req returns binaries instead of strings

--- itests/test/search_integration_test.erl
+++ itests/test/search_integration_test.erl
@@ -3,58 +3,6 @@
 -include("../src/chef_req.hrl").
 -include_lib("eunit/include/eunit.hrl").
 
-create_node_as_user_test_() ->
-    {setup,
-     fun() ->
-             ok = chef_req:start_apps(),
-             KeyPath = "/tmp/opscode-platform-test/clownco-org-admin.pem",
-             chef_req:make_config("http://localhost",
-                                  "clownco-org-admin", KeyPath)
-     end,
-     fun(_X) ->
-             stopping
-     end,
-     fun(ReqConfig) ->
-             [
-              {"empty node search as clownco-org-admin",
-                fun() ->
-                        Path = "/organizations/clownco/nodes",
-                        NodeJson = sample_node(),
-                        {ok, Code, _H, Body} = chef_req:request(post, Path, NodeJson, ReqConfig),
-                       ?debugVal(Code),
-                       ?debugVal(_H),
-                       ?debugVal(Body)
-                end}]
-     end}.
-
-%% create_node_as_client_test_() ->
-%%     {setup,
-%%      fun() ->
-%%              ok = chef_req:start_apps(),
-%%              KeyPath = "/tmp/opscode-platform-test/clownco-org-admin.pem",
-%%              ReqConfig = chef_req:make_config("http://localhost",
-%%                                               "clownco-org-admin", KeyPath),
-%%              {_, ClientConfig} = make_client("clownco", "client01", ReqConfig),
-%%              ClientConfig
-%%      end,
-%%      fun(#req_config{name = Name}=ReqConfig) ->
-%%              delete_client("clownco", Name, ReqConfig)
-%%      end,
-%%      fun(ReqConfig) ->
-%%              [
-%%               {"create a node",
-%%                fun() ->
-%%                        NodeJson = sample_node(),
-%%                        Path = "/organizations/clownco/nodes",
-%%                        {ok, Code, _H, Body} = chef_req:request(post, Path, NodeJson, ReqConfig),
-%%                        ?debugVal(Code),
-%%                        ?debugVal(_H),
-%%                        ?debugVal(Body)
-%%                end}
-
-%%              ]
-%%      end}.
-    
 search_as_client_test_() ->
     {setup,
      fun() ->
@@ -62,11 +10,12 @@ search_as_client_test_() ->
              KeyPath = "/tmp/opscode-platform-test/clownco-org-admin.pem",
              ReqConfig = chef_req:make_config("http://localhost",
                                               "clownco-org-admin", KeyPath),
-             {_, ClientConfig} = make_client("clownco", "client01", ReqConfig),
+             {_, ClientConfig} = chef_req:make_client("clownco", "client01", ReqConfig),
              ClientConfig
      end,
      fun(#req_config{name = Name}=ReqConfig) ->
-             delete_client("clownco", Name, ReqConfig)
+             chef_req:delete_client("clownco", Name, ReqConfig),
+             test_utils:test_cleanup(ignore)
      end,
      fun(ReqConfig) ->
              [
@@ -87,9 +36,9 @@ search_as_client_test_() ->
                        Path = search_path("clownco", "no_such_bag", "*:*"),
                        {ok, "404", _H, Body} = chef_req:request(get, Path,
                                                                 ReqConfig),
-                       Expect = "{\"error\":[\"I don't know "
-                           "how to search for no_such_bag "
-                           "data objects.\"]}",
+                       Expect = <<"{\"error\":[\"I don't know "
+                                  "how to search for no_such_bag "
+                                  "data objects.\"]}">>,
                        ?assertEqual(Expect, Body)
                end},
 
@@ -102,8 +51,8 @@ search_as_client_test_() ->
                        Path = search_path("clownco", "node",
                                           "no_field:not_exist"),
                        {ok, "401", _H, Body} = chef_req:request(get, Path, Config),
-                       ?assertEqual("{\"error\":[\"'client01' not associated with "
-                                    "organization 'clownco'\"]}", Body)
+                       ?assertEqual(<<"{\"error\":[\"'client01' not associated with "
+                                      "organization 'clownco'\"]}">>, Body)
                end}
              ]
      end}.
@@ -117,7 +66,7 @@ search_as_user_test_() ->
                                   "clownco-org-admin", KeyPath)
      end,
      fun(_X) ->
-             stopping
+             test_utils:test_cleanup(ignore)
      end,
      fun(ReqConfig) ->
              [
@@ -138,9 +87,9 @@ search_as_user_test_() ->
                         Path = search_path("clownco", "no_such_bag", "*:*"),
                         {ok, "404", _H, Body} = chef_req:request(get, Path,
                                                                  ReqConfig),
-                        Expect = "{\"error\":[\"I don't know "
-                            "how to search for no_such_bag "
-                            "data objects.\"]}",
+                        Expect = <<"{\"error\":[\"I don't know "
+                                   "how to search for no_such_bag "
+                                   "data objects.\"]}">>,
                         ?assertEqual(Expect, Body)
                 end},
 
@@ -152,9 +101,9 @@ search_as_user_test_() ->
                         Path = search_path("clownco", "node",
                                            "no_field:not_exist"),
                         {ok, "401", _H, Body} = chef_req:request(get, Path, Config),
-                        ?assertEqual("{\"error\":[\"Failed to authenticate as "
-                                     "'no-such-user'. Ensure that your node_name "
-                                     "and client key are correct.\"]}",
+                        ?assertEqual(<<"{\"error\":[\"Failed to authenticate as "
+                                       "'no-such-user'. Ensure that your node_name "
+                                       "and client key are correct.\"]}">>,
                                      Body)
                 end},
 
@@ -166,33 +115,14 @@ search_as_user_test_() ->
                         Path = search_path("clownco", "node",
                                            "no_field:not_exist"),
                         {ok, "401", _H, Body} = chef_req:request(get, Path, Config),
-                        ?assertEqual("{\"error\":[\"'clownco-org-admin' not "
-                                     "associated with organization 'clownco'\"]}", Body)
+                        ?assertEqual(<<"{\"error\":[\"'clownco-org-admin' not "
+                                       "associated with organization 'clownco'\"]}">>,
+                                     Body)
                 end}
              ]
      end
     }.
 
-
 search_path(Org, Type, Query) ->
     "/organizations/" ++ Org ++ "/search/" ++ Type
         ++ "?q=" ++ Query.
-
-make_client(Org, ClientName, Config) ->
-    Path = "/organizations/" ++ Org ++ "/clients",
-    ReqBody = iolist_to_binary([<<"{\"name\":\"">>, ClientName, <<"\"}">>]),
-    {ok, "201", _H, Body} = chef_req:request(post, Path, ReqBody, Config),
-    Client = ejson:decode(Body),
-    ClientConfig = chef_req:clone_config(Config, ClientName,
-                                         ej:get({<<"private_key">>}, Client)),
-    {Client, ClientConfig}.
-
-delete_client(Org, ClientName, Config) ->
-    Path = "/organizations/" ++ Org ++ "/clients/" ++ ClientName,
-    {ok, Code, _H, _Body} = chef_req:request(delete, Path, Config),
-    Code.
-
-sample_node() ->
- <<"{\"normal\":{\"is_anyone\":\"no\"},\"name\":\"n1\",\"override\":{},"
-   "\"default\":{},\"json_class\":\"Chef::Node\",\"automatic\":{},"
-   "\"chef_environment\":\"_default\",\"run_list\":[],\"chef_type\":\"node\"}">>.
