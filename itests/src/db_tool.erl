0d3d9d36b564e55eb894d873f71987c129cb33c4
Seth Falcon <seth@opscode.com>
2011-10-04 11:14:08 -0700

    Add db_tool:metadata_for_node
    Fetches last_updated_by, updated_at, and created_at for a node.  Does
    node lookup based only on node name, to avoid having to find OrgId.
    Works well enough for the integration tests.

--- itests/src/db_tool.erl
+++ itests/src/db_tool.erl
@@ -1,11 +1,17 @@
 -module(db_tool).
 
--export([truncate_nodes_table/0, connect/0]).
+-export([truncate_nodes_table/0,
+         connect/0,
+         metadata_for_node/1]).
 
 -include_lib("emysql/include/emysql.hrl").
 -include_lib("eunit/include/eunit.hrl").
 
 -define(gv(X, L), proplists:get_value(X, L)).
+
+-define(fetch_node_metadata,
+        <<"SELECT `last_updated_by`, `created_at`, `updated_at` FROM nodes "
+          "WHERE (`name` = ?) LIMIT 1">>).
 
 connect() ->
     %% XXX: assume we get run from rebar eunit.
@@ -19,8 +25,21 @@ connect() ->
     error_logger:info_msg("db_tool connecting to MySQL ~s:~b ~s~n",
                           [Host, Port, DbName]),
     emysql:add_pool(db_tool_pool, 10, DbUser, DbPass,
-                    Host, Port, DbName, utf8).
+                    Host, Port, DbName, utf8),
+    emysql:prepare(fetch_node_metadata, ?fetch_node_metadata).
 
 truncate_nodes_table() ->
     #ok_packet{} = emysql:execute(db_tool_pool, <<"DELETE FROM nodes">>),
     ok.
+
+%% XXX: find just by node name to avoid having to obtain org id.
+metadata_for_node(Name) ->
+    case emysql:execute(db_tool_pool, fetch_node_metadata, [Name]) of
+        #result_packet{rows = []} -> not_found;
+        #result_packet{rows = [[Id, {datetime, Created}, {datetime, Updated}]]} ->
+            [{last_updated_by, Id},
+             {created_at, Created},
+             {updated_at, Updated}];
+        Else ->
+            {error, Else}
+    end.
