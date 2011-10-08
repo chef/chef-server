09f44826f7adc877c0c1d53bb4d48ff45c50e506
Kevin Smith <kevin@opscode.com>
2011-10-08 04:29:27 -0400

    Fixing up headers & modelines

--- src/mover.erl
+++ src/mover.erl
@@ -1,4 +1,4 @@
-%% -*- erlang-indent-level: 4;indent-tabs-mode: nil -*-
+%% -*- erlang-indent-level: 4;indent-tabs-mode: nil;fill-column: 92 -*-
 %% ex: ts=4 sw=4 et
 %% @author Seth Falcon <seth@opscode.com>
 %% @copyright 2011 Opscode, Inc.
--- src/node_mover_worker.erl
+++ src/node_mover_worker.erl
@@ -1,3 +1,8 @@
+%% -*- erlang-indent-level: 4;indent-tabs-mode: nil;fill-column: 92 -*-
+%% ex: ts=4 sw=4 et
+%% @author Seth Falcon <seth@opscode.com>
+%% @copyright 2011 Opscode, Inc.
+
 -module(node_mover_worker).
 -behaviour(gen_fsm).
 -define(SERVER, ?MODULE).
