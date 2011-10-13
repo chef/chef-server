552e54a10a16abc0a14dc21442215308e0965e39
Daniel DeLeo <dan@opscode.com>
2011-10-13 14:09:38 -0700

    add sinatra app for nginx reconfig

---
+++ superconductor/config/development.rb
@@ -0,0 +1,3 @@
+Superconductor.configure do |app|
+  app.config.nginx_config = "/tmp/superconductor-nginx.conf"
+end
---
+++ superconductor/config/production.rb
@@ -0,0 +1,3 @@
+Superconductor.configure do |app|
+  app.config.nginx_config = "/etc/nginx/sites-available/sql_nodes_darklaunch.conf"
+end
