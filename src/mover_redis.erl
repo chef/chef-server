3565e4aa618a7a8a6b7ed6366e1aca0303786664
Kevin Smith <kevin@opscode.com>
2011-11-02 13:00:01 -0400

    Fixing borked app.config

--- src/mover_redis.erl
+++ src/mover_redis.erl
@@ -7,29 +7,10 @@
 
 -define(DRY_RUN_CLIENT, dry_run_redis_client).
 
--export([connect/0,
-         disconnect/1,
-         delete_tracking/1,
+-export([delete_tracking/1,
          inflight_requests_for_org/1]).
 
 -define(MIGRATION_DB, 5).
-
-connect() ->
-    case is_dry_run() of
-        true -> dry_run_redis_client;
-        false ->
-            {ok, RedisHost} = application:get_env(mover, redis_host),
-            {ok, RedisPort} = application:get_env(mover, redis_port),
-            {ok, RedisDb} = application:get_env(mover, redis_db),
-            {ok, Client} = erldis:connect(RedisHost, RedisPort),
-            ok = erldis:select(Client, RedisDb),
-            Client
-    end.
-
-disconnect(dry_run_redis_client) ->
-    ok;
-disconnect(Client) ->
-    erldis:quit(Client).
 
 inflight_requests_for_org(OrgName) ->
     inflight_requests_for_org(get_client(), OrgName).
