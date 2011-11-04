40e0e0c37e07d788e76765949c2db686f12589cb
Seth Falcon <seth@userprimary.net>
2011-11-03 21:33:37 -0700

    Fix error reset function

--- src/mover_status.erl
+++ src/mover_status.erl
@@ -25,6 +25,7 @@ set_orgs_migrated(Orgs) ->
 reset_orgs(Orgs) ->
     [ dets:insert(all_orgs, Org#org{active = false,
                                     migrated = false,
+                                    error = false,
                                     worker = undefined})
       || Org <- Orgs].
 
