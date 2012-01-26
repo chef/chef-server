e8dbe7616ad62b65d415f78c5f26c628e801dde4
Stephen Delano <stephen@opscode.com>
2012-01-25 17:28:03 -0800

    add opscode-webui software config

---
+++ config/software/opscode-webui.rb
@@ -0,0 +1,13 @@
+name "opscode-webui"
+
+dependencies ["ruby", "bundler", "libxml2", "libxslt", "curl"]
+
+source :git => "git@github.com:opscode/opscode-webui"
+
+relative_path "opscode-webui"
+
+bundle_env = { "GEM_PATH" => nil, "GEM_HOME" => nil  }
+
+build do
+  command "/opt/opscode/embedded/bin/bundle install --without integration_test test dev hosted_chef", :env => bundle_env
+end
