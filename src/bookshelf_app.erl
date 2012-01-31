4e426915d523d34f0009611e7be9aef178fcefde
Tim Dysinger <timd@opscode.com>
2012-01-30 17:48:01 -1000

    Added the otp application module

---
+++ src/bookshelf_app.erl
@@ -0,0 +1,26 @@
+%% @author Tim Dysinger <timd@opscode.com>
+%% @copyright 2012 Opscode, Inc. All Rights Reserved
+
+-module(bookshelf_app).
+-behaviour(application).
+-export([start/0, start/2, stop/1]).
+
+start() ->
+    application:start(crypto),
+    application:start(public_key),
+    application:start(ssl),
+    application:start(cowboy),
+    application:start(bookshelf).
+
+start(_Type, _Args) ->
+    Dispatch = [{'_', [{'_', bookshelf_bucket, []}]}],
+    cowboy:start_listener(my_http_listener,
+                          100,
+                          cowboy_tcp_transport,
+                          [{port, 8080}],
+                          cowboy_http_protocol,
+                          [{dispatch, Dispatch}]),
+    bookshelf_sup:start_link().
+
+stop(_State) ->
+    ok.
