8b34a494e8802108b0f305a1f34feb712b0a694e
Stephen Delano <stephen@opscode.com>
2012-02-06 20:24:13 -0800

    add opscode-chef software config

---
+++ config/software/opscode-chef.rb
@@ -0,0 +1,20 @@
+name "opscode-chef"
+
+dependencies ["ruby",
+              "bundler",
+              "gecode",
+              "libxml2",
+              "libxslt",
+              "postgresql", # TODO: how do we install just the client library?
+              "curl"]
+
+source :git => "git@github.com:opscode/opscode-chef"
+
+relative_path "opscode-chef"
+
+bundle_env = {"GEM_PATH" => nil, "GEM_HOME" => nil}
+
+build do
+  command "/opt/opscode/embedded/bin/bundle install --without mysql integration_test dev"
+  # TODO: copy the rm, rsync steps from the clojure build
+end
