21e30bd9a21bba48d8d19c0441a79a6f9b8e7c03
Stephen Delano <stephen@opscode.com>
2012-02-07 15:34:23 -0800

    add erlang software config

---
+++ config/software/erlang.rb
@@ -0,0 +1,32 @@
+name "erlang"
+
+dependencies ["zlib", "openssl"]
+
+source :url => "http://www.erlang.org/download/otp_src_R14B03.tar.gz",
+       :md5 => "7979e662d11476b97c462feb7c132fb7"
+
+relative_path "otp_src_R14B03"
+
+env = {
+  "CFLAGS" => "-L/opt/opscode/embedded/lib -I/opt/opscode/embedded/include",
+  "LDFLAGS" => "-Wl,-rpath /opt/opscode/embedded/lib -L/opt/opscode/embedded/lib -I/opt/opscode/embedded/include"
+}
+
+build do
+  # TODO: build cross-platform. this is for linux
+  command(["./configure",
+           "--prefix=/opt/opscode/embedded",
+           "--enable-threads",
+           "--enable-smp-support",
+           "--enable-kernel-poll",
+           "--enable-dynamic-ssl-lib",
+           "--enable-shared-zlib",
+           "--enable-hipe",
+           "--without-javac",
+           "--with-ssl=/opt/opscode/embedded",
+           "--disable-debug"].join(" "),
+          :env => env)
+
+  command "make", :env => env
+  command "make install"
+end
