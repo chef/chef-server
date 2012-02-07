ba9682fb8470d12f5a1100fdd22e3e96cda53748
Stephen Delano <stephen@opscode.com>
2012-02-07 14:02:25 -0800

    add icu software config

---
+++ config/software/icu.rb
@@ -0,0 +1,22 @@
+name "icu"
+
+source :url => "http://download.icu-project.org/files/icu4c/4.8.1.1/icu4c-4_8_1_1-src.tgz",
+:md5 => "ea93970a0275be6b42f56953cd332c17"
+
+relative_path "icu"
+
+working_dir = "#{project_dir}/source"
+
+build do
+  command("./configure --prefix=/opt/opscode/embedded",
+          :env => {
+            "CFLAGS" => "-L/opt/opscode/embedded/lib -I/opt/opscode/embedded/include"
+          },
+          :cwd => working_dir)
+  command("make",
+          :env => {
+            "LD_RUN_PATH" => "/opt/opscode/embedded/lib"
+          },
+          :cwd => working_dir)
+  command "make install", :cwd => working_dir
+end
