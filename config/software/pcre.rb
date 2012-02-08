3be7e54549d596fab971ce63726b28d5ee2aeb97
Stephen Delano <stephen@opscode.com>
2012-02-08 11:31:23 -0800

    add pcre software config

---
+++ config/software/pcre.rb
@@ -0,0 +1,19 @@
+name "pcre"
+
+source :url => "ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre-8.30.tar.gz",
+       :md5 => "d5ee0d9f6d2f0b7489331d04b6c182ef"
+
+relative_path "pcre-8.30"
+
+build do
+  command("./configure --prefix=/opt/opscode/embedded",
+          :env => {
+            "CFLAGS" => "-L/opt/opscode/embedded/lib -I/opt/opscode/embedded/include"
+          })
+  # command "touch alocal.m4"
+  command("make",
+          :env => {
+            "PATH" => "/opt/opscode/embedded/bin:#{ENV["PATH"]}"
+          })
+  command "make install"
+end
