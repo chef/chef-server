0a0e181b57136d77c20f50d71805963468f2d85b
Tim Dysinger <timd@opscode.com>
2012-02-11 15:29:51 -1000

    bookshelf wip

--- src/bookshelf_bkt.erl
+++ src/bookshelf_bkt.erl
@@ -16,10 +16,25 @@
 %% permissions and limitations under the License.
 
 -module(bookshelf_bkt).
+-include("bookshelf.hrl").
 -compile(export_all).
 
+%% ===================================================================
+%% Cowboy functions
+%% ===================================================================
+
 init(_Transport, _Rq, _Opts) ->
+    %% we need to add model to our opts
     {upgrade, protocol, cowboy_http_rest}.
+
+rest_init(Rq, Opts) ->
+    {dir, Dir}     = lists:keyfind(dir, 1, Opts),
+    {model, Model} = lists:keyfind(model, 1, Opts),
+    {ok, ?req(with_amz_request_id, Rq), #state{dir = Dir, model = Model}}.
+
+%% ===================================================================
+%% REST callbacks
+%% ===================================================================
 
 allowed_methods(Rq, St) ->
     {['GET', 'PUT', 'DELETE'], Rq, St}.
@@ -27,11 +42,32 @@ allowed_methods(Rq, St) ->
 content_types_provided(Rq, St) ->
     {[{{<<"text">>, <<"xml">>, []}, to_xml}], Rq, St}.
 
-to_xml(Rq, St) ->
-    {<<"<xml>hello</xml>">>, Rq, St}.
-
 content_types_accepted(Rq, St) ->
     {[{{<<"text">>, <<"xml">>, []}, from_xml}], Rq, St}.
 
-from_xml(Rq, St) ->
+resource_exists(Rq, #state{dir=Dir}=St) ->
+    {filelib:is_dir(Dir), Rq, St}.
+
+to_xml(Rq, #state{dir=Dir, model=Model}=St) ->
+    {bookshelf_xml:list_buckets_xml(buckets(Dir), Model), Rq, St}.
+
+delete_resource(Rq, St) ->
     {true, Rq, St}.
+
+%% ===================================================================
+%% Internal functions
+%% ===================================================================
+
+buckets(Dir) ->
+    {ok, Files} = file:list_dir(Dir), %% crash if no access to base dir
+    Paths   = lists:map(fun(F) -> filename:join(Dir, F) end, Files),
+    Dirs    = lists:filter(fun filelib:is_dir/1, Paths),
+    Buckets = lists:map(fun filename:basename/1, Dirs),
+    Dates   = lists:map(fun(P) ->
+                                %% crash if no access to bucket dir
+                                {ok, #file_info{ctime=Date}} =
+                                    file:read_file_info(P, [{time, universal}]),
+                                iso8601:format(Date)
+                        end,
+                        Dirs),
+    lists:zip(Buckets, Dates).
