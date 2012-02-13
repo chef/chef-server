2931fecc82488e68c9e53bf691bd9527710b9268
Stephen Delano <stephen@opscode.com>
2012-02-13 15:35:55 -0800

    software config for opscode-authz

---
+++ config/software/opscode-authz.rb
@@ -0,0 +1,16 @@
+name "opscode-authz"
+
+dependencies ["erlang", "rsync"]
+
+source :git => "git@github.com:opscode/opscode-authz"
+
+relative_path "opscode-authz"
+
+env = { "PATH" => "/opt/opscode/embedded/bin:#{ENV["PATH"]}" }
+
+build do
+  command "make clean", :env => env
+  command "make", :env => env
+  command "mkdir -p /opt/opscode/embedded/service/opscode-authz"
+  command "/opt/opscode/embedded/bin/rsync -a ./ /opt/opscode/embedded/service/opscode-authz/"
+end
