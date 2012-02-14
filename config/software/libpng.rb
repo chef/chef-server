54ede2afa0bc4ebe553d23cbedad82fe7513e9ff
Stephen Delano <stephen@opscode.com>
2012-02-14 15:54:49 -0800

    add libpng software config

---
+++ config/software/libpng.rb
@@ -0,0 +1,20 @@
+name "libpng"
+
+dependencies ["zlib"]
+
+source :url => "ftp://ftp.simplesystems.org/pub/libpng/png/src/libpng-1.5.8.tar.gz",
+       :md5 => "dc2b84a1c077531ceb5bf9d79ad889a4"
+
+relative_path "libpng-1.5.8"
+
+configure_env = {
+  "LDFLAGS" => "-L/opt/opscode/embedded/lib -I/opt/opscode/embedded/include",
+  "CFLAGS" => "-L/opt/opscode/embedded/lib -I/opt/opscode/embedded/include",
+  "LD_RUN_PATH" => "/opt/opscode/embedded/lib"
+}
+
+build do
+  command "./configure --prefix=/opt/opscode/embedded --with-zlib-prefix=/opt/opscode/embedded", :env => configure_env
+  command "make", :env => {"LD_RUN_PATH" => "/opt/opscode/embedded/lib"}
+  command "make install"
+end
