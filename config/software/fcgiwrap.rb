e1d420eba7382ddbb7fa9628140c5749d92cb08e
Stephen Delano <stephen@opscode.com>
2012-02-15 11:00:41 -0800

    add fcgiwrap software config

---
+++ config/software/fcgiwrap.rb
@@ -0,0 +1,22 @@
+name "fcgiwrap"
+
+dependencies ["autoconf", "fcgi"]
+
+# TODO: deploy from the 1.0.3 tag / SHA
+source :git => "git://github.com/gnosek/fcgiwrap"
+
+relative_path "fcgiwrap"
+
+env = {
+  "LDFLAGS" => "-L/opt/opscode/embedded/lib -I/opt/opscode/embedded/include",
+  "CFLAGS" => "-L/opt/opscode/embedded/lib -I/opt/opscode/embedded/include",
+  "LD_RUN_PATH" => "/opt/opscode/embedded/lib",
+  "PATH" => "/opt/opscode/embedded/bin:#{ENV["PATH"]}"
+}
+
+build do
+  command "autoreconf -i", :env => env
+  command "./configure --prefix=/opt/opscode/embedded", :env => env
+  command "make", :env => {"LD_RUN_PATH" => "/opt/opscode/embedded/lib"}
+  command "make install"
+end
