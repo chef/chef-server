b257399d911252ecef500edc45454ce3c40f117e
Noah Kantrowitz <noah@coderanger.net>
2012-02-17 12:40:05 -0800

    Add the rest of the build for oc-account.

--- config/software/opscode-account.rb
+++ config/software/opscode-account.rb
@@ -12,5 +12,7 @@ bundle_env = {"GEM_PATH" => nil, "GEM_HOME" => nil}
 
 build do
   command "/opt/opscode/embedded/bin/bundle install --without mysql test", :env => bundle_env
-  # TODO: copy the rm, rsync steps from the clojure build
+  command "git log -1 --format=%H > .githash"
+  command "mkdir -p /opt/opscode/embedded/service/opscode-account"
+  command "rsync -a --delete --exclude=.git/*** --exclude=.gitignore ./ /opt/opscode/embedded/service/opscode-account/"
 end
