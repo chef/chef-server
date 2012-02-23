4cce092f3f9c7e942ae17d6b79d6afc1a17ebaaa
Tim Dysinger <timd@opscode.com>
2012-02-22 17:01:11 -1000

    bookshelf_env: Fix the route for bookshelf_obj PUT/GET

--- src/bookshelf_env.erl
+++ src/bookshelf_env.erl
@@ -81,8 +81,8 @@ rule(Env, Domain) ->
     SubDomain = lists:append([bucket], Domain),
     FEnv = filter_env(Env),
     [
-     {Domain,    [{[],    bookshelf_idx, FEnv}]},
-     {SubDomain, [{[],    bookshelf_bkt, FEnv},
+     {Domain,    [{[   ], bookshelf_idx, FEnv}]},
+     {SubDomain, [{[   ], bookshelf_bkt, FEnv},
                   {['*'], bookshelf_obj, FEnv}]}
     ].
 
