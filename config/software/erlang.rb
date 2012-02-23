76662c6bd99c3351633f01aefbb21a682be4c509
Stephen Delano <stephen@opscode.com>
2012-02-23 14:13:02 -0800

    sandbox the included headers for the erlang build

--- config/software/erlang.rb
+++ config/software/erlang.rb
@@ -1,7 +1,7 @@
 name "erlang"
 version "R14B03"
 
-dependencies ["zlib", "openssl"]
+dependencies ["zlib", "openssl", "ncurses"]
 
 source :url => "http://www.erlang.org/download/otp_src_R14B03.tar.gz",
        :md5 => "7979e662d11476b97c462feb7c132fb7"
@@ -9,11 +9,17 @@ source :url => "http://www.erlang.org/download/otp_src_R14B03.tar.gz",
 relative_path "otp_src_R14B03"
 
 env = {
-  "CFLAGS" => "-L/opt/opscode/embedded/lib -I/opt/opscode/embedded/include",
-  "LDFLAGS" => "-Wl,-rpath /opt/opscode/embedded/lib -L/opt/opscode/embedded/lib -I/opt/opscode/embedded/include"
+  "CFLAGS" => "-L/opt/opscode/embedded/lib -I/opt/opscode/embedded/erlang/include",
+  "LDFLAGS" => "-Wl,-rpath /opt/opscode/embedded/lib -L/opt/opscode/embedded/lib -I/opt/opscode/embedded/erlang/include"
 }
 
 build do
+  # set up the erlang include dir
+  command "mkdir -p /opt/opscode/embedded/erlang/include"
+  %w{ncurses openssl zlib.h zconf.h}.each do |link|
+    command "ln -fs /opt/opscode/embedded/include/#{link} /opt/opscode/embedded/erlang/include/#{link}"
+  end
+
   # TODO: build cross-platform. this is for linux
   command(["./configure",
            "--prefix=/opt/opscode/embedded",
