d573640a6af2e89b1cc711c0783a258c4e293672
Stephen Delano <stephen@opscode.com>
2012-02-23 16:10:52 -0800

    add private-chef project config

---
+++ config/projects/private-chef.rb
@@ -0,0 +1,38 @@
+name "private-chef"
+
+# initialize the dependencies
+deps = []
+
+# global
+deps << "chef-pc"
+deps << "private-chef-cookbooks"
+deps << "nginx"
+deps << "runit"
+deps << "unicorn"
+
+# the backend
+deps << "couchdb"
+deps << "postgresql"
+deps << "redis"
+deps << "rabbitmq"
+deps << "opscode-solr"
+deps << "opscode-expander"
+
+# the front-end services
+deps << "opscode-erchef"
+deps << "opscode-chef"
+deps << "opscode-account"
+deps << "opscode-webui"
+deps << "opscode-authz"
+deps << "opscode-org-creator"
+deps << "opscode-certificate"
+deps << "opscode-platform-debug"
+deps << "opscode-test"
+
+# monitoring
+deps << "nagios"
+deps << "nagios-plugins"
+deps << "opscode-nagios-plugins"
+deps << "nrpe"
+
+dependencies deps
