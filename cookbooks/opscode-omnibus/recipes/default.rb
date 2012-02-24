560506b246ddae7bf514439694b67bf78987491b
Noah Kantrowitz <noah@coderanger.net>
2012-02-24 15:59:32 -0800

    Herp to the derp.

--- cookbooks/opscode-omnibus/recipes/default.rb
+++ cookbooks/opscode-omnibus/recipes/default.rb
@@ -42,13 +42,13 @@ end
 
 # gtihub's SSH key
 directory File.expand_path("~#{node["opscode-omnibus"]["build-user"]}/.ssh") do
-  owner "vagrant"
+  owner node["opscode-omnibus"]["build-user"]
   mode "700"
 end
 
 file File.expand_path("~#{node["opscode-omnibus"]["build-user"]}/.ssh/known_hosts") do
   action :create_if_missing
   content "github.com,207.97.227.239 ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ==\n"
-  owner "vagrant"
+  owner node["opscode-omnibus"]["build-user"]
   mode "600"
 end
