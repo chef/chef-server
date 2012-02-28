e08b5c6f5b17cacc5add74e97622c3c00db2f695
Daniel DeLeo <dan@opscode.com>
2012-02-27 16:53:33 -0800

    correct tarball extract location for rabbitmq

--- config/software/rabbitmq.rb
+++ config/software/rabbitmq.rb
@@ -10,13 +10,13 @@ dependencies ["erlang",
 source :url => "http://www.rabbitmq.com/releases/rabbitmq-server/v2.7.1/rabbitmq-server-generic-unix-2.7.1.tar.gz",
        :md5 => "34a5f9fb6f22e6681092443fcc80324f"
 
-relative_path "rabbitmq-server-2.7.1"
+relative_path "rabbitmq_server-2.7.1"
 
 build do
   command "mkdir -p #{install_dir}/embedded/service/rabbitmq"
   command "#{install_dir}/embedded/bin/rsync -a ./ #{install_dir}/embedded/service/rabbitmq/"
 
   %w{rabbitmqctl rabbitmq-env rabbitmq-multi rabbitmq-server}.each do |cmd|
-    command "ln -sf /opt/opsocde/embedded/service/rabbitmq/sbin/#{cmd} #{install_dir}/embedded/bin/#{cmd}"
+    command "ln -sf #{install_dir}/embedded/service/rabbitmq/sbin/#{cmd} #{install_dir}/embedded/bin/#{cmd}"
   end
 end
