cc5acb1d866aa5ad2149172adda054179b60e19f
Daniel DeLeo <dan@opscode.com>
2012-02-28 18:48:23 -0800

    expand cookbooks dir from Omnibus root

--- config/software/private-chef-cookbooks.rb
+++ config/software/private-chef-cookbooks.rb
@@ -1,7 +1,7 @@
 name "private-chef-cookbooks"
 
 build do
-  cookbook_dir = File.expand_path("../files/private-chef-cookbooks", __FILE__)
+  cookbook_dir = File.expand_path("files/private-chef-cookbooks", Omnibus.root)
   command "mkdir -p #{install_dir}/embedded/cookbooks"
   command "cp -ra #{cookbook_dir}/* #{install_dir}/embedded/cookbooks/"
   command "ln -sf #{install_dir}/embedded/cookbooks/bin/private-chef-ctl #{install_dir}/bin/private-chef-ctl"
