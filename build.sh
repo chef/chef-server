bada7d099b6484d908e0bbdd25e936cce7b98447
Noah Kantrowitz <noah@coderanger.net>
2012-03-01 16:12:44 -0800

    Run chef-solo before each build to be safe.

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -xe
+sudo chef-solo -c /srv/opscode-omnibus/shared/solo.rb -j /srv/opscode-omnibus/shared/solo.json
 cp omnibus.rb.example omnibus.rb
 rm pkg/* || true
 bundle install --deployment --without development
