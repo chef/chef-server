cd2baf6e57f92b8acd6ee4ae7ae548410779e754
Daniel DeLeo <dan@opscode.com>
2012-03-05 15:20:43 -0800

    need root to install nrpe to nagios' dir

--- config/software/nrpe.rb
+++ config/software/nrpe.rb
@@ -38,5 +38,5 @@ build do
   # move it
   command "mkdir -p #{install_dir}/embedded/nagios/libexec"
   command "cp ./src/check_nrpe #{install_dir}/embedded/nagios/libexec"
-  command "cp ./src/nrpe #{install_dir}/embedded/nagios/bin"
+  command "sudo cp ./src/nrpe #{install_dir}/embedded/nagios/bin"
 end
