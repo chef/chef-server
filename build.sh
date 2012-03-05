e83369b088925533a11536103d7ceeb000671bf7
Noah Kantrowitz <noah@coderanger.net>
2012-03-05 13:12:17 -0800

    sudo the folder operations.

--- build.sh
+++ build.sh
@@ -3,9 +3,9 @@ if [ ! -e /opt/opscode/.branch ]; then exit 1; fi
 GIT_BRACH_CLEAN="$(git describe --all --match "heads/.*" | sed s/heads\\/// | tr / _)"
 if [ "$GIT_BRANCH_CLEAN" != "$(cat /opt/opscode/.branch)" ]
 then
-  mv /opt/opscode "/opt/opscode-$(cat /opt/opscode/.branch)"
-  if [ ! -d "/opt/opscode-$GIT_BRANCH_CLEAN" ]; then mkdir "/opt/opscode-$GIT_BRANCH_CLEAN"; fi
-  mv "/opt/opscode-$GIT_BRANCH_CLEAN" /opt/opscode
+  sudo mv /opt/opscode "/opt/opscode-$(cat /opt/opscode/.branch)"
+  if [ ! -d "/opt/opscode-$GIT_BRANCH_CLEAN" ]; then sudo mkdir "/opt/opscode-$GIT_BRANCH_CLEAN"; fi
+  sudo mv "/opt/opscode-$GIT_BRANCH_CLEAN" /opt/opscode
 fi
 sudo env CHEF_SOLO_COOKBOOKS="$(pwd)/cookbooks" chef-solo -c /srv/opscode-omnibus/shared/solo.rb -j /srv/opscode-omnibus/shared/solo.json
 cp omnibus.rb.example omnibus.rb
