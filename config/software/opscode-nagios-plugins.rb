6623785f95084a5d1f3e675204a194e694ca4804
Daniel DeLeo <dan@opscode.com>
2012-03-05 14:20:33 -0800

    peg oc-nagios-plugins to git tag

--- config/software/opscode-nagios-plugins.rb
+++ config/software/opscode-nagios-plugins.rb
@@ -2,7 +2,7 @@ name "opscode-nagios-plugins"
 
 dependencies ["rsync"]
 
-version "master"
+version "pc-rel-1.0.0"
 
 source :git => "git@github.com:opscode/opscode-nagios-plugins"
 
@@ -11,4 +11,7 @@ relative_path "opscode-nagios-plugins"
 build do
   command "mkdir -p #{install_dir}/embedded/nagios/libexec"
   command "sudo #{install_dir}/embedded/bin/rsync -a ./plugins/ #{install_dir}/embedded/nagios/libexec/"
+
+  # check_redis needs the redis gem installed to the main gem repo
+  gem "install redis -v 2.2.2"
 end
