aad0f44e08f65873e6fcfd873b6c765f600d4cef
Stephen Delano <stephen@opscode.com>
2012-03-05 16:49:30 -0800

    use new BuildVersion module

--- config/software/pc-version.rb
+++ config/software/pc-version.rb
@@ -4,7 +4,7 @@ description "generates a version manifest file"
 build do
   block do
     File.open("#{install_dir}/pc-version.txt", "w") do |f|
-      f.puts "Opscode Private Chef #{Omnibus.build_version}"
+      f.puts "Opscode Private Chef #{Omnibus::BuildVersion.full}"
       f.puts ""
       f.puts Omnibus::Reports.pretty_version_map
     end
