937af9c56a37cc615845342bdec96fce0bf5dc5a
Daniel DeLeo <dan@opscode.com>
2012-03-08 16:40:17 -0800

    need to sudo when cleaning to remove nagios' root owned crap

--- clean.sh
+++ clean.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -xe
 if [ "$(cat /opt/opscode/.branch)" == "master" ]
 then
-  rm -rf /opt/opscode/*
+  sudo rm -rf /opt/opscode/*
 fi
 if [ -d /opt/opscode-master ]
 then
-  rm -rf /opt/opscode-master/*
+  sudo rm -rf /opt/opscode-master/*
 fi
 rm -rf /var/cache/omnibus/master/*
 
