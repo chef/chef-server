c450423604f0f9b2d8948967fc46c539f64207ec
Lamont Granquist <lamont@opscode.com>
2012-03-09 00:07:39 +0000

    fixes to compile rel-0.3.0 of opscode-authz

--- config/software/opscode-authz.rb
+++ config/software/opscode-authz.rb
@@ -7,11 +7,16 @@ source :git => "git@github.com:opscode/opscode-authz"
 
 relative_path "opscode-authz"
 
-env = { "PATH" => "#{install_dir}/embedded/bin:#{ENV["PATH"]}" }
+env = {
+  "PATH" => "#{install_dir}/embedded/bin:#{ENV["PATH"]}",
+  "LD_FLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
+  "LD_RUN_PATH" => "#{install_dir}/embedded/lib"
+}
 
 build do
-  command "make clean", :env => env
-  command "make", :env => env
+  command "make distclean", :env => env
+  command "make rel", :env => env
   command "mkdir -p #{install_dir}/embedded/service/opscode-authz"
-  command "#{install_dir}/embedded/bin/rsync -a ./ #{install_dir}/embedded/service/opscode-authz/"
+  command "#{install_dir}/embedded/bin/rsync -a ./rel/authz #{install_dir}/embedded/service/opscode-authz/"
+  command "rm -rf #{install_dir}/embedded/service/opscode-authz/log"
 end
