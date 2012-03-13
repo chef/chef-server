f2e261ab8e1aa76c901f30366be0ee17751b681f
Stephen Delano <stephen@opscode.com>
2012-03-12 21:51:00 -0700

    programatically determine which version of python we want to use

--- cookbooks/python/recipes/pip.rb
+++ cookbooks/python/recipes/pip.rb
@@ -28,12 +28,16 @@ remote_file "#{Chef::Config[:file_cache_path]}/distribute_setup.py" do
   not_if "which pip"
 end
 
-use_version = node['python']['distribute_install_py_version']
+py_version = if node['platform'] == 'centos' && node['platform_version'].split('.').first.to_i < 6
+               '26'
+             else
+               nil
+             end
 
 bash "install-pip" do
   cwd Chef::Config[:file_cache_path]
   code <<-EOF
-  python#{use_version} distribute_setup.py
+  python#{py_version} distribute_setup.py
   easy_install pip
   EOF
   not_if "which pip"
