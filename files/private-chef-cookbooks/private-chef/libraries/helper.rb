4832ec56e0c74e2435abf26b617c894cddf7d2c3
Seth Chisamore <schisamo@opscode.com>
2012-03-23 17:00:28 -0400

    move ldap enabled logic into a helper

--- files/private-chef-cookbooks/private-chef/libraries/helper.rb
+++ files/private-chef-cookbooks/private-chef/libraries/helper.rb
@@ -111,7 +111,12 @@ class Chef::Recipe
   def add_nagios_hostgroup(hostgroup)
     if node['private_chef']['nagios']['enable']
       node.default['private_chef']['nagios']['hosts'][node['hostname']]['hostgroups'] << hostgroup
-    end  
+    end
+  end
+
+  def ldap_authentication_enabled?
+    node['private_chef'].attribute?('ldap') &&
+      !(node['private_chef']['ldap'].nil? || node['private_chef']['ldap'].empty?)
   end
 end
 
