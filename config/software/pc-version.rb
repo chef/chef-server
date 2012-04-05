3f83d1b61c297c807b2a3896bc0c91d601e3e8db
Stephen Delano <stephen@opscode.com>
2012-04-05 11:49:56 -0700

    pretty print the versions for a project

--- config/software/pc-version.rb
+++ config/software/pc-version.rb
@@ -6,7 +6,7 @@ build do
     File.open("#{install_dir}/pc-version.txt", "w") do |f|
       f.puts "Opscode Private Chef #{Omnibus::BuildVersion.full}"
       f.puts ""
-      f.puts Omnibus::Reports.pretty_version_map
+      f.puts Omnibus::Reports.pretty_version_map(project)
     end
   end
 end
