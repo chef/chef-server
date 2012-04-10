cc6fc5ea67c2eeec0da74bb549da782c8d30b7cd
Adam Jacob <adam@opscode.com>
2012-04-09 17:14:55 -0700

    Making mysql idempotent, use built-in mysql2 gem

--- files/private-chef-cookbooks/private-chef/recipes/mysql.rb
+++ files/private-chef-cookbooks/private-chef/recipes/mysql.rb
@@ -8,7 +8,6 @@ if node['private_chef']['mysql']['install_libs']
   end
 end
 
-
 bundles = {
   "mixlib-authorization" => false,
   "opscode-account" => "test",
@@ -18,23 +17,31 @@ bundles = {
   "opscode-webui" => "integration_test dev"
 }
 
+execute "/opt/opscode/embedded/bin/gem unpack /opt/opscode/embedded/service/gem/ruby/1.9.1/cache/mysql2-#{node['private_chef']['mysql']['mysql2_version']}.gem" do
+  cwd "/opt/opscode/embedded/service/gem/ruby/1.9.1/gems"
+  not_if { File.directory?("/opt/opscode/embedded/service/gem/ruby/1.9.1/gems/mysql2-#{node['private_chef']['mysql']['mysql2_version']}") }
+end
+
+execute "compile mysql2" do
+  command "/opt/opscode/embedded/bin/rake compile" 
+  cwd "/opt/opscode/embedded/service/gem/ruby/1.9.1/gems/mysql2-#{node['private_chef']['mysql']['mysql2_version']}"
+  not_if { File.directory?("/opt/opscode/embedded/service/gem/ruby/1.9.1/gems/mysql2-#{node['private_chef']['mysql']['mysql2_version']}/lib/mysql2/mysql2.so") }
+end
+
+ruby_block "create mysql2 gemspec" do
+  block do
+    gemspec = Gem::Specification.load("/opt/opscode/embedded/service/gem/ruby/1.9.1/gems/mysql2-#{node['private_chef']['mysql']['mysql2_version']}/mysql2.gemspec").to_ruby_for_cache
+    File.open("/opt/opscode/embedded/service/gem/ruby/1.9.1/specifications/mysql2-#{node['private_chef']['mysql']['mysql2_version']}.gemspec", "w") do |spec_file|
+      spec_file.print gemspec
+    end
+  end
+  not_if { File.exists?("/opt/opscode/embedded/service/gem/ruby/1.9.1/specifications/mysql2-#{node['private_chef']['mysql']['mysql2_version']}.gemspec") }
+end
+
 bundles.each do |name, without_list| 
   execute "sed -i -e 's/mysql://g' /opt/opscode/embedded/service/#{name}/.bundle/config"
   execute "sed -i -e 's/:mysql//g' /opt/opscode/embedded/service/#{name}/.bundle/config"
   execute "sed -i -e 's/mysql//g' /opt/opscode/embedded/service/#{name}/.bundle/config"
-
-  to_run = "/opt/opscode/embedded/bin/bundle update mysql2"
-
-  execute "fix_bundle_#{name}" do
-    command to_run
-    environment({
-      "PATH" => "/opt/opscode/embedded/bin:/opt/opscode/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/usr/local/sbin",
-      "LD_FLAGS" => "-L/opt/opscode/embedded/lib -I/opt/opscode/embedded/include",
-      "LD_RUN_PATH" => "/opt/opscode/embedded/lib"
-    })
-    not_if "cd /opt/opscode/embedded/service/#{name} && /opt/opscode/embedded/bin/bundle show mysql2"
-    cwd "/opt/opscode/embedded/service/#{name}"
-  end
 end
 
 if !File.exists?("/var/opt/opscode/mysql-bootstrap")
