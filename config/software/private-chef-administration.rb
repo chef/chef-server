551fcbf0892d21c81a118c4ad5e7445bfb81f030
Tim Hinderliter <tim@opscode.com>
2012-04-11 12:48:05 -0700

    set private-chef-administration tag to rel-1.1.12

--- config/software/private-chef-administration.rb
+++ config/software/private-chef-administration.rb
@@ -1,5 +1,5 @@
 name "private-chef-administration"
-version "rel-1.1.0" # TODO: pin to version number of private chef build
+version "rel-1.1.12" # TODO: pin to version number of private chef build
 
 dependencies ["rsync"]
 
