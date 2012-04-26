d6b3c1b11e4b1e21411d32d5830e491b09de40b1
jkeiser <jkeiser@opscode.com>
2012-04-26 15:26:09 -0700

    Fix role ACLs

--- files/private-chef-cookbooks/private-chef/attributes/default.rb
+++ files/private-chef-cookbooks/private-chef/attributes/default.rb
@@ -349,7 +349,10 @@ default['private_chef']['dark_launch']["quick_start"] = false
 default['private_chef']['dark_launch']["new_theme"] = true
 default['private_chef']['dark_launch']["private-chef"] = true
 default['private_chef']['dark_launch']["sql_users"] = true
-
+default['private_chef']['dark_launch']["couchdb_roles"] = true
+default['private_chef']['dark_launch']["couchdb_data"] = true
+default['private_chef']['dark_launch']["add_type_and_bag_to_items"] = true
+ 
 ###
 # Opscode Account
 ###
