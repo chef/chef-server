b4d4582752d2c023328662d6116262d9d3d0edd6
Stephen Delano <stephen@opscode.com>
2012-05-02 18:03:20 -0700

    check for db up on backend migrations, not on standalone

--- partybus/lib/partybus/migration_api/v1.rb
+++ partybus/lib/partybus/migration_api/v1.rb
@@ -19,10 +19,16 @@ module Partybus
       def upgrade_schema_to(version)
         role = Partybus.config.private_chef_role
         log("\tPrivate Chef Role: #{role}")
-        if ["backend", "standalone"].include? role
-          log("\tUpgrading Schema to Version #{version}")
-          migrator = Partybus::SchemaMigrator.new
-          migrator.migrate_to(version)
+        if role == "backend"
+          if db_up?
+            log("\tDatabase Up")
+            perform_schema_upgrade(version)
+          else
+            log("\tDatabase Down")
+            log("\tSkipping Schema Upgrade")
+          end
+        elsif role == "standalone"
+          perform_schema_upgrade(version)
         else
           log("\tSkipping Schema Upgrade")
         end
@@ -36,6 +42,21 @@ module Partybus
 
       def migrate
 
+      end
+
+      private # private really doesn't do anything when we #instance_eval
+
+      def db_running?
+        db_service = Partybus.config.db_service_name
+        system("private-chef-ctl #{db_service} status")
+        exit_status = $?.exitstatus
+        exit_status == 0
+      end
+
+      def perform_schema_upgrade(version)
+        log("\tUpgrading Schema to Version #{version}")
+        migrator = Partybus::SchemaMigrator.new
+        migrator.migrate_to(version)
       end
     end
   end
