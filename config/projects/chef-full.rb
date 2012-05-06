cf0b6d2d606fd8e11a631821c9c6bd3922cf703f
Lamont Granquist <lamont@opscode.com>
2012-05-06 10:04:55 -0700

    use to build from git tags: rake projects:chef-full:deb chef_git=master

--- config/projects/chef-full.rb
+++ config/projects/chef-full.rb
@@ -4,5 +4,10 @@ install_path    "/opt/chef"
 build_version   "0.10.8"
 build_iteration "4"
 
-dependencies ["chef"]
+if ENV['chef_git']
+  build_iteration << ".#{ENV['chef_git']}"
+  dependencies ["chef-git"]
+else
+  dependencies ["chef-gem"]
+end
 
