71bb15d3c1a2d2d3d4ae74b85665c3e522b9333d
Stephen Delano <stephen@opscode.com>
2012-06-21 11:54:27 -0700

    partybus: force schema upgrades on bootstrap server

--- partybus/lib/partybus/migration_api/v1.rb
+++ partybus/lib/partybus/migration_api/v1.rb
@@ -19,16 +19,38 @@ module Partybus
       def upgrade_schema_to(version)
         role = Partybus.config.private_chef_role
         log("\tPrivate Chef Role: #{role}")
-        if role == "backend"
+        if role == "backend" && config.bootstrap_server
           if db_up?
             log("\tDatabase Up")
             perform_schema_upgrade(version)
           else
-            log("\tDatabase Down")
-            log("\tSkipping Schema Upgrade")
+            log <<EOF
+****
+ERROR: Database is not running on bootstrap server.
+****
+You must run database schema migraitons on the bootstrap server, and the
+bootstrap server must be the HA-master at the time of the migration.
+If the bootstrap server is not currently the HA-master, please see the
+Private Chef documentation for issuing a graceful transition of the HA
+cluster.
+****
+EOF
+            exit 1
           end
         elsif role == "standalone"
-          perform_schema_upgrade(version)
+          if db_up?
+            perform_schema_upgrade(version)
+          else
+            log <<EOF
+****
+ERROR: Database is not running.
+****
+The database must be running to execute schema migrations. Please start
+the database and re-run private-chef-ctl upgrade.
+****
+EOF
+            exit 1
+          end
         else
           log("\tSkipping Schema Upgrade")
         end
