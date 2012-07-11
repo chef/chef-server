d61a02ad8db70677ae8e78ad48a529b8b352895d
Ho-Sheng Hsiao <hosh@opscode.com>
2012-07-11 15:29:05 -0400

    [OC-1975] Added chef-sql-schema into private-chef

---
+++ config/software/chef-sql-schema.rb
@@ -0,0 +1,18 @@
+name "chef-sql-schema"
+#version "pc-rel-1.6.0"
+version "master"
+
+dependencies ["ruby",
+              "bundler",
+              "postgresql",
+              "rsync"]
+
+source :git => "git@github.com:opscode/chef-sql-schema.git"
+
+relative_path "chef-sql-schema"
+
+build do
+  bundle "install --without mysql --path=/opt/opscode/embedded/service/gem"
+  command "mkdir -p #{install_dir}/embedded/service/chef-sql-schema"
+  command "#{install_dir}/embedded/bin/rsync -a --delete --exclude=.git/*** --exclude=.gitignore ./ #{install_dir}/embedded/service/chef-sql-schema/"
+end
