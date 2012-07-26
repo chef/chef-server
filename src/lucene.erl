cb746acda299b117a005f5cccc5e50298bd58422
doubt72 <doug@opscode.com>
2012-07-26 15:50:56 -0600

    Changed 2012 to 2011-2012 for older updated files

--- src/lucene.erl
+++ src/lucene.erl
@@ -1,7 +1,7 @@
 %% -*- erlang-indent-level: 4;indent-tabs-mode: nil; fill-column: 92 -*-
 %% ex: ts=4 sw=4 et
 %% @author Seth Falcon <seth@opscode.com>
-%% Copyright 2012 Opscode, Inc. All Rights Reserved.
+%% Copyright 2011-2012 Opscode, Inc. All Rights Reserved.
 %%
 %% This file is provided to you under the Apache License,
 %% Version 2.0 (the "License"); you may not use this file
