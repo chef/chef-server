a2570cc9e870303e6f9610ee4e2cdc443f8f59af
doubt72 <doug@opscode.com>
2012-07-26 15:58:02 -0600

    Fixed dates for files that date from 2011

--- src/chef_db_compression.erl
+++ src/chef_db_compression.erl
@@ -2,7 +2,7 @@
 %% ex: ts=4 sw=4 et
 %% @author Kevin Smith <kevin@opscode.com>
 %% @author Seth Falcon <seth@opscode.com>
-%% Copyright 2012 Opscode, Inc. All Rights Reserved.
+%% Copyright 2011-2012 Opscode, Inc. All Rights Reserved.
 %%
 %% This file is provided to you under the Apache License,
 %% Version 2.0 (the "License"); you may not use this file
