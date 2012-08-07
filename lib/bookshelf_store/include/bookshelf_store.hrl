88a66850bf064ea2aa4f800831ef8efbb15942c6
Kevin Smith <kevin@opscode.com>
2012-08-07 12:30:50 -0400

    Bye-bye bookshelf_store

--- lib/bookshelf_store/include/bookshelf_store.hrl
+++
@@ -1,7 +0,0 @@
--record(object, {name ::bookshelf_store:path(),
-                 date :: calendar:datetime1970(),
-                 size :: non_neg_integer(),
-                 digest :: binary()}).
-
--record(bucket, {name::binary(),
-                 date::calendar:datetime1970()}).
