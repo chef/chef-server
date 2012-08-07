253e7731ad5beb881a96a5ae5f409106fad9987d
Seth Falcon <seth@opscode.com>
2012-08-07 14:49:08 -0700

    Use R15 only -callback specs to define chef_wm behavior

--- src/chef_wm.erl
+++ src/chef_wm.erl
@@ -1,10 +1,27 @@
 -module(chef_wm).
 
--export([behaviour_info/1]).
+-include("chef_wm.hrl").
 
-behaviour_info(callbacks) ->
-    [{init, 1},
-     {validate_request, 3},
-     {malformed_request_message, 3},
-     {request_type, 0},
-     {auth_info, 2}].
+-type http_verb() :: 'GET' | 'PUT' | 'POST' | 'DELETE' | 'HEAD' | 'OPTIONS'.
+-type base_state() :: #base_state{}.
+-type container_name() :: cookbook |
+                          node |
+                          role |
+                          sandbox.
+-callback init(list()) ->
+    {ok, base_state()}.
+
+-callback validate_request(http_verb(), wm_req(), base_state()) ->
+    {wm_req(), base_state()}.
+
+-callback malformed_request_message(term(), wm_req(), base_state()) ->
+    term().                                     % return is really EJSON
+
+-callback request_type() ->
+    string().
+
+-callback auth_info(wm_req(), base_state()) ->
+    {{halt, non_neg_integer()}, wm_req(), base_state()} |
+    {{create_in_container, container_name()}, wm_req(), base_state()} |
+    {{container, container_name()}, wm_req(), base_state()} |
+    {{object, object_id()}, wm_req(), base_state()}.
