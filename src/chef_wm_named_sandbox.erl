fab3001d154bde3c2fb6f82874210f3853779342
jkeiser <jkeiser@opscode.com>
2012-09-28 09:04:51 -0700

    Add comment to document non-obvious permissions

--- src/chef_wm_named_sandbox.erl
+++ src/chef_wm_named_sandbox.erl
@@ -62,6 +62,8 @@ allowed_methods(Req, State) ->
     {['PUT'], Req, State}.
 
 auth_info(Req, State) ->
+    % POST /sandboxes and PUT /sandboxes/<id> are two halves of a single upload
+    % operation, so we use the same permission for both (create sandbox perm)
     {[{container, sandbox, create}], Req, State}.
 
 %% Org is checked for in malformed_request/2, sandbox is checked for in forbidden/2;
