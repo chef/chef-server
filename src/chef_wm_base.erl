ab6db335cc37b080e5a2e66ad0514375fa40124b
Christopher Maier <cm@opscode.com>
2012-10-02 00:41:53 -0400

    Extract malformed_request_message/3 into separate module
    That code was getting rather large; hopefully this makes things a
    little more clear.

--- src/chef_wm_base.erl
+++ src/chef_wm_base.erl
@@ -120,15 +120,15 @@ malformed_request(Req, #base_state{resource_mod=Mod,
             Req3 = wrq:set_resp_body(chef_json:encode({[{<<"error">>, [Msg]}]}), Req),
             {{halt, 404}, Req3, State1#base_state{log_msg = org_not_found}};
         throw:bad_clock ->
-            Msg1 = malformed_request_message(bad_clock, Req, State),
+            Msg1 = chef_wm_malformed:malformed_request_message(bad_clock, Req, State),
             Req3 = wrq:set_resp_body(chef_json:encode(Msg1), Req),
             {{halt, 401}, Req3, State1#base_state{log_msg = bad_clock}};
                 throw:bad_headers ->
-            Msg1 = malformed_request_message(bad_headers, Req, State),
+            Msg1 =  chef_wm_malformed:malformed_request_message(bad_headers, Req, State),
             Req3 = wrq:set_resp_body(chef_json:encode(Msg1), Req),
             {{halt, 401}, Req3, State1#base_state{log_msg = bad_headers}};
         throw:bad_sign_desc ->
-            Msg1 = malformed_request_message(bad_sign_desc, Req, State),
+            Msg1 =  chef_wm_malformed:malformed_request_message(bad_sign_desc, Req, State),
             Req3 = wrq:set_resp_body(chef_json:encode(Msg1), Req),
             {{halt, 400}, Req3, State1#base_state{log_msg = bad_sign_desc}};
         throw:{too_big, Msg} ->
@@ -136,180 +136,10 @@ malformed_request(Req, #base_state{resource_mod=Mod,
             Req3 = wrq:set_resp_body(chef_json:encode({[{<<"error">>, Msg}]}), Req),
             {{halt, 413}, Req3, State1#base_state{log_msg = too_big}};
         throw:Why ->
-            Msg = malformed_request_message(Why, Req, State),
+            Msg =  chef_wm_malformed:malformed_request_message(Why, Req, State),
             NewReq = wrq:set_resp_body(chef_json:encode(Msg), Req),
             {true, NewReq, State1#base_state{log_msg = Why}}
     end.
-
-
-%% @doc Handle common malformed request tasks with resource-specific callbacks
-%%
-%% This function does a sanity check on the authn headers (not verifying signing, but does
-%% all checks it can without talking to a db).  It also checks for org existence and halts
-%% with 404 if the org is not found.  This doesn't strictly belong in malformed_request, but
-%% right now all of our resources need this check and end up needing it before
-%% resource_exists will get called so we do it here.
-%%
-%% The caller provides `ValidateFun' which will be given `Req' and `State' as args and
-%% should return a `{Req, State}' tuple or throw.  The `ErrorMsgFun' will be called as
-%% `ErrorMsgFun(Reason, Req, State)' where `Reason' is the term thrown by `ValidateFun'.
-%%
-
-malformed_request_message(bad_clock, Req, State) ->
-    {GetHeader, _State1} = chef_wm_util:get_header_fun(Req, State),
-    User = case GetHeader(<<"X-Ops-UserId">>) of
-               undefined -> <<"">>;
-               UID -> UID
-           end,
-    Msg = iolist_to_binary([<<"Failed to authenticate as ">>, User,
-                            <<". Synchronize the clock on your host.">>]),
-    {[{<<"error">>, [Msg]}]};
-malformed_request_message(bad_sign_desc, _Req, _State) ->
-    Msg = <<"Unsupported authentication protocol version">>,
-    {[{<<"error">>, [Msg]}]};
-malformed_request_message({missing_headers, Missing}, _Req, _State) ->
-    Msg = iolist_to_binary([
-                            <<"missing required authentication header(s) ">>,
-                            bin_str_join(Missing, <<", ">>)]),
-    {[{<<"error">>, [Msg]}]};
-malformed_request_message({bad_headers, Bad}, _Req, _State) ->
-    Msg = iolist_to_binary([
-                            <<"bad header(s) ">>,
-                            bin_str_join(Bad, <<", ">>)]),
-    {[{<<"error">>, [Msg]}]};
-malformed_request_message({error, invalid_json}, _Req, _State) ->
-    %% in theory, there might be some sort of slightly useful error detail from
-    %% chef_json/jiffy, but thus far nothing specific enough to beat out this. Also, would
-    %% not passing internal library error messages out to the user when possible.
-    {[{<<"error">>, [<<"invalid JSON">>]}]};
-malformed_request_message({mismatch, {FieldName, _Pat, _Val}}, _Req, _State) ->
-    {[{<<"error">>, [iolist_to_binary(["Field '", FieldName, "' invalid"])]}]};
-malformed_request_message({missing, FieldName}, _Req, _State) ->
-    {[{<<"error">>, [iolist_to_binary(["Field '", FieldName, "' missing"])]}]};
-malformed_request_message({both_missing, Field1, _Field2}, _Req, _State) ->
-    {[{<<"error">>, [iolist_to_binary(["Field '", Field1, "' missing"])]}]};
-malformed_request_message({client_name_mismatch}, _Req, _State) ->
-    {[{<<"error">>, [<<"name and clientname must match">>]}]};
-malformed_request_message({bad_client_name, Name, Pattern}, _Req, _State) ->
-    {[{<<"error">>, [iolist_to_binary(["Invalid client name '", Name,
-                                       "' using regex: '", Pattern, "'."])]}]};
-
-%% Not sure if we want to be this specific, or just want to fold this into an 'invalid JSON'
-%% case.  At any rate, here it is.
-malformed_request_message({bad_string_list, {Field, _Value}}, _Req, _State) ->
-    {[{<<"error">>, [iolist_to_binary(["Field '", Field, "' is not a list of strings"])]}]};
-malformed_request_message({bad_ejson_proplist, {Field, _Value}}, _Req, _State) ->
-    {[{<<"error">>, [iolist_to_binary(["Field '", Field, "' is not a hash"])]}]};
-malformed_request_message({url_json_name_mismatch, {_UrlName, _Mismatch, Type}}, _Req, _State) ->
-    {[{<<"error">>, [iolist_to_binary([Type, <<" name mismatch.">>])]}]};
-malformed_request_message({bad_run_list, {Field, _Value}}, _Req, _State) ->
-    {[{<<"error">>, [iolist_to_binary(["Field '", Field, "' is not a valid run list"])]}]};
-malformed_request_message({bad_run_lists, {Field, _Value}}, _Req, _State) ->
-    {[{<<"error">>, [iolist_to_binary(["Field '", Field, "' contains invalid run lists"])]}]};
-malformed_request_message(invalid_num_versions, _Req, _State) ->
-    {[{<<"error">>, [<<"You have requested an invalid number of versions (x >= 0 || 'all')">>]}]};
-
-%% This is used by some custom validation in environments that may (or may not!) be pulled up into ej:valid validations
-malformed_request_message({invalid_key, Key}, _Req, _State) ->
-    error_envelope([<<"Invalid key ">>, Key, <<" in request body">>]);
-
-malformed_request_message(invalid_json_body, _Req, _State) ->
-    error_envelope([<<"Incorrect JSON type for request body">>]);
-
-%% General ej_invalid messages
-
-%% Missing fields
-malformed_request_message(#ej_invalid{type=missing,
-                                      key=Key
-                                     }, _Req, _State) ->
-    error_envelope([<<"Field '", Key/binary, "' missing">>]);
-
-%% Exact and string_match failures
-malformed_request_message(#ej_invalid{type=Type,
-                                      key=Key,
-                                      msg=Expected
-                                     }, _Req, _State) when Type =:= exact ;
-                                                           Type =:= string_match ->
-    error_envelope([<<"Field '", Key/binary, "' invalid">>]);
-
-%% Things that should be hashes, but aren't.
-malformed_request_message(#ej_invalid{type=json_type,
-                                      key=Key
-                                     }, _Req, _State) when Key =:= <<"override_attributes">> ;
-                                                           Key =:= <<"default_attributes">> ;
-                                                           Key =:= <<"normal">> ;
-                                                           Key =:= <<"default">> ;
-                                                           Key =:= <<"override">> ;
-                                                           Key =:= <<"automatic">> ;
-                                                           Key =:= <<"cookbook_versions">> ->
-    error_envelope([<<"Field '", Key/binary, "' is not a hash">>]);
-
-%% Entire run list is the wrong type
-malformed_request_message(#ej_invalid{type=json_type,
-                                     key=Key}, _Req, _State) when Key =:= <<"run_list">> ->
-    error_envelope([<<"Field '", Key/binary,"' is not a valid run list">>]);
-
-%% entire env_run_lists is the wrong type
-malformed_request_message(#ej_invalid{type=json_type,
-                                      key=Key
-                                     }, _Req, _State) when Key =:= <<"env_run_lists">> ->
-    error_envelope([<<"Field '", Key/binary, "' contains invalid run lists">>]);
-
-%% All other json_type failures
-malformed_request_message(#ej_invalid{type=json_type,
-                                      key=Key
-                                     }, _Req, _State) ->
-    error_envelope([<<"Field '", Key/binary, "' invalid">>]);
-
-%% Bogus run list items
-%%
-%% In the future, we may wish to add more detailed error messages reflecting whether a run
-%% list item is simply the wrong type (e.g., a number) or an invalid run list item (e.g.,
-%% the string "recipe[").  To do so, we would need to compare the `found_type` and
-%% `expected_type` record fields
-malformed_request_message(#ej_invalid{type=array_elt,
-                                      key=Key}, _Req, _State) when Key =:= <<"run_list">> ->
-    error_envelope([<<"Field '", Key/binary, "' is not a valid run list">>]);
-
-malformed_request_message(#ej_invalid{type = object_key,
-                                      key = Object,
-                                      found = Key}, _Req, _State) ->
-    error_envelope([<<"Invalid key '">>, Key, <<"' for ">>, Object]);
-
-malformed_request_message(#ej_invalid{type = object_value,
-                                      key = Key,
-                                      found = Val}, _Req, _State) when Key =:= <<"env_run_lists">> ->
-    error_envelope([<<"Field '", Key/binary, "' contains invalid run lists">>]);
-
-malformed_request_message(#ej_invalid{type = object_value,
-                                      key = Object,
-                                      found = Val}, _Req, _State) ->
-    error_envelope([<<"Invalid value '">>, Val, <<"' for ">>, Object]);
-
-
-malformed_request_message(Reason, Req, #base_state{resource_mod=Mod}=State) ->
-    Mod:malformed_request_message(Reason, Req, State).
-
--spec to_binary( any() ) -> binary().
-to_binary(A) when is_atom(A) ->
-    atom_to_binary(A, utf8);
-to_binary(I) when is_integer(I)->
-    list_to_binary(integer_to_list(I));
-to_binary(B) when is_binary(B)->
-    B;
-to_binary(O) ->
-    %% Catch-all case
-    list_to_binary(io_lib:format("~p", [O])).
-
--spec binary_message([atom() | string() | binary()]) -> binary().
-binary_message(Parts)  ->
-    iolist_to_binary([to_binary(P) || P <- Parts]).
-
--spec error_envelope(binary() | [binary()]) -> ej:json_object().
-error_envelope(Parts) when is_list(Parts) ->
-    error_envelope(binary_message(Parts));
-error_envelope(Message) when is_binary(Message) ->
-    chef_wm_util:error_message_envelope(Message).
 
 forbidden(Req, #base_state{resource_mod=Mod}=State) ->
     %% For now we call auth_info because currently need the side-effect of looking up the
@@ -635,14 +465,6 @@ log_level(Code) when Code >= 500 ->
     err;
 log_level(_) ->
     info.
-
-bin_str_join(L, Sep) ->
-    bin_str_join(L, Sep, []).
-
-bin_str_join([H], _Sep, Acc) ->
-    lists:reverse([<<"'">>, H, <<"'">>|Acc]);
-bin_str_join([H | T], Sep, Acc) ->
-    bin_str_join(T, Sep, [Sep, <<"'">>, H, <<"'">> | Acc]).
 
 fetch_org_guid(#base_state{organization_guid = Id}) when is_binary(Id) ->
     Id;
