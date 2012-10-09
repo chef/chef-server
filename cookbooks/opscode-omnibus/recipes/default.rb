636b7ba3e67bc0e450ab42f0c6e86234af7da042
Seth Chisamore <schisamo@opscode.com>
2012-10-09 16:08:19 -0400

    leverage the python_pip LWRP to install sphinx and pygments

--- cookbooks/opscode-omnibus/recipes/default.rb
+++ cookbooks/opscode-omnibus/recipes/default.rb
@@ -1,7 +1,14 @@
 
-bash "install python packages" do
-  code <<BASH
-pip install Sphinx==1.1.3
-pip install Pygments==1.4
-BASH
+include_recipe "python"
+
+# We should consider making this proper config/software deps for
+# private-chef-administration
+python_pip "Sphinx" do
+  version "1.1.3"
+  action :install
+end
+
+python_pip "Pygments" do
+  version "1.4"
+  action :install
 end
