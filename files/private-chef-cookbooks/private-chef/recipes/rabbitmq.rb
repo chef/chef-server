009796cefdad994a38acc19c5d36e68083e34073
marc@opscode.com <marc@opscode.com>
2012-10-24 22:09:37 -0400

    rabbitmq config for reporting

--- files/private-chef-cookbooks/private-chef/recipes/rabbitmq.rb
+++ files/private-chef-cookbooks/private-chef/recipes/rabbitmq.rb
@@ -16,11 +16,13 @@
 # limitations under the License.
 #
 
-rabbitmq_dir = node['private_chef']['rabbitmq']['dir']
+rabbitmq = node["private_chef"]["rabbitmq"]
+
+rabbitmq_dir = rabbitmq['dir']
 rabbitmq_etc_dir = File.join(rabbitmq_dir, "etc")
-rabbitmq_data_dir = node['private_chef']['rabbitmq']['data_dir']
+rabbitmq_data_dir = rabbitmq['data_dir']
 rabbitmq_data_dir_symlink = File.join(rabbitmq_dir, "db")
-rabbitmq_log_dir = node['private_chef']['rabbitmq']['log_directory']
+rabbitmq_log_dir = rabbitmq['log_directory']
 
 [ rabbitmq_dir, rabbitmq_etc_dir, rabbitmq_data_dir, rabbitmq_log_dir ].each do |dir_name|
   directory dir_name do
@@ -48,7 +50,7 @@ rabbitmq_service_dir = "/opt/opscode/embedded/service/rabbitmq"
   end
 end
 
-config_file = File.join(node['private_chef']['rabbitmq']['dir'], "etc", "rabbitmq.conf") 
+config_file = File.join(rabbitmq['dir'], "etc", "rabbitmq.conf") 
 
 template "#{rabbitmq_service_dir}/sbin/rabbitmq-env" do
   owner "root"
@@ -63,67 +65,85 @@ template config_file do
   owner "root"
   group "root"
   mode "0644"
-  variables(node['private_chef']['rabbitmq'].to_hash)
+  variables(rabbitmq.to_hash)
 end
 
 runit_service "rabbitmq" do
-  down node['private_chef']['rabbitmq']['ha']
+  down rabbitmq['ha']
   options({
     :log_directory => rabbitmq_log_dir,
-    :svlogd_size => node['private_chef']['rabbitmq']['svlogd_size'],
-    :svlogd_num  => node['private_chef']['rabbitmq']['svlogd_num']
+    :svlogd_size => rabbitmq['svlogd_size'],
+    :svlogd_num  => rabbitmq['svlogd_num']
   }.merge(params))
 end
 
 add_nagios_hostgroup("rabbitmq")
 
 if node['private_chef']['bootstrap']['enable']
-	execute "/opt/opscode/bin/private-chef-ctl rabbitmq start" do
-		retries 20 
-	end
+  rmq_ctl = "/opt/opscode/embedded/bin/rabbitmqctl"
+  opc_ctl = "/opt/opscode/bin/private-chef-ctl"
+  opc_username = node["private_chef"]["user"]["username"]
+  rmq_ctl_chpost = "/opt/opscode/embedded/bin/chpst -u #{opc_username} -U #{opc_username} #{rmq_ctl}"
+
+  execute "/opt/opscode/bin/private-chef-ctl rabbitmq start" do
+    retries 20 
+  end
   
-  execute "/opt/opscode/embedded/bin/chpst -u #{node["private_chef"]["user"]["username"]} -U #{node["private_chef"]["user"]["username"]} /opt/opscode/embedded/bin/rabbitmqctl wait /var/opt/opscode/rabbitmq/db/rabbit@localhost.pid" do
+  execute "#{rmq_ctl_chpost} wait #{rabbitmq_data_dir}/rabbit@localhost.pid" do
     retries 10 
   end
 
-  [ node['private_chef']['rabbitmq']['vhost'], node['private_chef']['rabbitmq']['reindexer_vhost'], node['private_chef']['rabbitmq']['jobs_vhost'] ].each do |vhost|
-    execute "/opt/opscode/embedded/bin/rabbitmqctl add_vhost #{vhost}" do
+  [ rabbitmq['vhost'], rabbitmq['reindexer_vhost'], rabbitmq['jobs_vhost'], rabbitmq['reports_vhost'] ].each do |vhost|
+    execute "#{rmq_ctl} add_vhost #{vhost}" do
       user node['private_chef']['user']['username']
-      not_if "/opt/opscode/embedded/bin/chpst -u #{node["private_chef"]["user"]["username"]} -U #{node["private_chef"]["user"]["username"]} /opt/opscode/embedded/bin/rabbitmqctl list_vhosts| grep #{vhost}"
+      not_if "#{rmq_ctl_chpost} list_vhosts| grep #{vhost}"
       retries 20
     end
   end
-
   # create chef user for the queue
-  execute "/opt/opscode/embedded/bin/rabbitmqctl add_user #{node['private_chef']['rabbitmq']['user']} #{node['private_chef']['rabbitmq']['password']}" do
-    not_if "/opt/opscode/embedded/bin/chpst -u #{node["private_chef"]["user"]["username"]} -U #{node["private_chef"]["user"]["username"]} /opt/opscode/embedded/bin/rabbitmqctl list_users |grep #{node['private_chef']['rabbitmq']['user']}"
+  execute "#{rmq_ctl} add_user #{rabbitmq['user']} #{rabbitmq['password']}" do
+    not_if "#{rmq_ctl_chpost} list_users |grep #{rabbitmq['user']}"
     user node['private_chef']['user']['username']
     retries 10
   end
 
-  execute "/opt/opscode/embedded/bin/rabbitmqctl add_user #{node['private_chef']['rabbitmq']['jobs_user']} #{node['private_chef']['rabbitmq']['jobs_password']}" do
+  execute "#{rmq_ctl} add_user #{rabbitmq['jobs_user']} #{rabbitmq['jobs_password']}" do
     user node['private_chef']['user']['username']
-    not_if "/opt/opscode/embedded/bin/chpst -u #{node["private_chef"]["user"]["username"]} -U #{node["private_chef"]["user"]["username"]} /opt/opscode/embedded/bin/rabbitmqctl list_users |grep #{node['private_chef']['rabbitmq']['jobs_user']}"
+    not_if "#{rmq_ctl_chpost} list_users |grep #{rabbitmq['jobs_user']}"
     retries 10
   end
+  
+  execute "#{rmq_ctl} add_user #{rabbitmq['reports_user']} #{rabbitmq['reports_password']}" do
+    user node['private_chef']['user']['username']
+    not_if "#{rmq_ctl_chpost} list_users |grep #{rabbitmq['reports_user']}"
+    retries 10
+  end
+  #
 
   # grant the mapper user the ability to do anything with the /chef vhost
   # the three regex's map to config, write, read permissions respectively
-  execute "/opt/opscode/embedded/bin/rabbitmqctl set_permissions -p #{node['private_chef']['rabbitmq']['vhost']} #{node['private_chef']['rabbitmq']['user']} \".*\" \".*\" \".*\"" do
+  #
+  execute "#{rmq_ctl} set_permissions -p #{rabbitmq['vhost']} #{rabbitmq['user']} \".*\" \".*\" \".*\"" do
     user node['private_chef']['user']['username']
-    not_if "/opt/opscode/embedded/bin/chpst -u #{node["private_chef"]["user"]["username"]} -U #{node["private_chef"]["user"]["username"]} /opt/opscode/embedded/bin/rabbitmqctl list_user_permissions #{node['private_chef']['rabbitmq']['user']}|grep #{node['private_chef']['rabbitmq']['vhost']}"
+    not_if "#{rmq_ctl_chpost} list_user_permissions #{rabbitmq['user']}|grep #{rabbitmq['vhost']}"
     retries 10
   end
 
-  execute "/opt/opscode/embedded/bin/rabbitmqctl set_permissions -p #{node['private_chef']['rabbitmq']['reindexer_vhost']} #{node['private_chef']['rabbitmq']['user']} \".*\" \".*\" \".*\"" do
+  execute "#{rmq_ctl} set_permissions -p #{rabbitmq['reindexer_vhost']} #{rabbitmq['user']} \".*\" \".*\" \".*\"" do
     user node['private_chef']['user']['username']
-    not_if "/opt/opscode/embedded/bin/chpst -u #{node["private_chef"]["user"]["username"]} -U #{node["private_chef"]["user"]["username"]} /opt/opscode/embedded/bin/rabbitmqctl list_user_permissions #{node['private_chef']['rabbitmq']['user']}|grep #{node['private_chef']['rabbitmq']['reindexer_vhost']}"
+    not_if "#{rmq_ctl_chpost} list_user_permissions #{rabbitmq['user']}|grep #{rabbitmq['reindexer_vhost']}"
     retries 10
   end
 
-  execute "/opt/opscode/embedded/bin/rabbitmqctl set_permissions -p #{node['private_chef']['rabbitmq']['jobs_vhost']} #{node['private_chef']['rabbitmq']['jobs_user']} \".*\" \".*\" \".*\"" do
+  execute "#{rmq_ctl} set_permissions -p #{rabbitmq['jobs_vhost']} #{rabbitmq['jobs_user']} \".*\" \".*\" \".*\"" do
     user node['private_chef']['user']['username']
-    not_if "/opt/opscode/embedded/bin/chpst -u #{node["private_chef"]["user"]["username"]} -U #{node["private_chef"]["user"]["username"]} /opt/opscode/embedded/bin/rabbitmqctl list_user_permissions #{node['private_chef']['rabbitmq']['jobs_user']}|grep #{node['private_chef']['rabbitmq']['jobs_vhost']}"
+    not_if "#{rmq_ctl_chpost} list_user_permissions #{rabbitmq['jobs_user']}|grep #{rabbitmq['jobs_vhost']}"
+    retries 10
+  end
+
+  execute "#{rmq_ctl} set_permissions -p #{rabbitmq['reports_vhost']} #{rabbitmq['reports_user']} \".*\" \".*\" \".*\"" do
+    user node['private_chef']['user']['username']
+    not_if "#{rmq_ctl_chpost} list_user_permissions #{rabbitmq['reports_user']}|grep #{rabbitmq['reports_vhost']}"
     retries 10
   end
 end
