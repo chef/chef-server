96cd96a1a521290d89cc810b449859dfbe7538ab
Seth Falcon <seth@opscode.com>
2012-11-01 15:44:46 -0700

    Update destination path for artifacts in s3

--- jenkins/build.sh
+++ jenkins/build.sh
@@ -7,17 +7,15 @@
 PROJ_NAME=oc_erchef
 
 export PATH=$PATH:/usr/local/bin
-ARTIFACT_BASE=opscode-ci/artifacts/$PROJ_NAME
+ARTIFACT_BASE=opscode-ci/artifacts/$os/$machine_architecture/$PROJ_NAME
 
 make distclean rel || exit 1
-
-BUCKET=oc-erchef-artifacts
 
 # If the string is null, then the git command returned false
 if git describe --tags --match='*-[0-9]*.[0-9]*.[0-9]*' --exact-match
 then
     VERSION=$(git describe --tags --exact-match --match='*-[0-9]*.[0-9].*[0-9]*')
-    PACKAGE=$PROJ_NAME_$VERSION.tar.gz
+    PACKAGE=${PROJ_NAME_}${VERSION}.tar.gz
     cd rel
     tar zcvf $PACKAGE $PROJ_NAME/
     s3cmd put --progress $PACKAGE s3://$ARTIFACT_BASE/$PACKAGE
