df1c64c977d20708a0836384f1c7ff1d1b13e5a1
marc@opscode.com <marc@opscode.com>
2012-11-14 15:35:29 -0500

    oc-reporting-pedant project configuration

---
+++ config/software/oc-reporting-pedant.rb
@@ -0,0 +1,16 @@
+name "oc-chef-pedant"
+version "master"
+
+dependencies ["ruby",
+              "bundler",
+              "rsync"]
+
+source :git => "git@github.com:opscode/oc-reporting-pedant"
+
+relative_path "oc-chef-pedant"
+
+build do
+  bundle "install --path=/opt/opscode/embedded/service/gem"
+  command "mkdir -p #{install_dir}/embedded/service/oc-reporting-pedant"
+  command "#{install_dir}/embedded/bin/rsync -a --delete --exclude=.git/*** --exclude=.gitignore ./ #{install_dir}/embedded/service/oc-reporting-pedant"
+end
