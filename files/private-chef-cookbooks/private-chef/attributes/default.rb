cc8324fb1dd79e40a5138956dc1bd26a65f36be4
mmzyk <mmzyk@opscode.com>
2012-11-15 16:57:05 -0500

    Add server flavor and api_version

--- files/private-chef-cookbooks/private-chef/attributes/default.rb
+++ files/private-chef-cookbooks/private-chef/attributes/default.rb
@@ -8,6 +8,9 @@
 ###
 # High level options
 ###
+default['private_chef']['api_version'] = "11.0.0"
+default['private_chef']['flavor'] = "opc"
+
 default['private_chef']['notification_email'] = "pc-default@opscode.com"
 default['private_chef']['from_email'] = '"Opscode" <donotreply@opscode.com>'
 default['private_chef']['database_type'] = "postgresql"
