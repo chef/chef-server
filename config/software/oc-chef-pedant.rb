4c489071096a2e2566f70d834aa2537f45b2ecab
Christopher Maier <cm@opscode.com>
2012-11-20 10:10:19 -0500

    Use #{install_dir} in oc-chef-pedant recipe

--- config/software/oc-chef-pedant.rb
+++ config/software/oc-chef-pedant.rb
@@ -10,7 +10,7 @@ source :git => "git@github.com:opscode/oc-chef-pedant.git"
 relative_path "oc-chef-pedant"
 
 build do
-  bundle "install --path=/opt/opscode/embedded/service/gem"
+  bundle "install --path=#{install_dir}/embedded/service/gem"
   command "mkdir -p #{install_dir}/embedded/service/oc-chef-pedant"
   command "#{install_dir}/embedded/bin/rsync -a --delete --exclude=.git/*** --exclude=.gitignore ./ #{install_dir}/embedded/service/oc-chef-pedant/"
 end
