32da7ddb7eea3722ecb0a4b7572a6ef96375df8f
Seth Chisamore <schisamo@opscode.com>
2013-01-09 20:13:46 -0500

    override `outside_user`: ensure we are using a bad_user NOT a bad_client
    This value is originally set in chef-pedant and OSC only works correctly 
    with bad_client. More info at:
    https://github.com/opscode/chef-pedant/commit/808d8c633bb94f0b13b1e7c8236ceb40562cba53

--- lib/pedant/multitenant/org_info.rb
+++ lib/pedant/multitenant/org_info.rb
@@ -6,6 +6,9 @@ module Pedant
       included do
         let(:organization) { platform.test_org }
         let(:org)          { organization.name }
+        # Ensure we are not using a bad_client in the place of a bad_user
+        # see https://github.com/opscode/chef-pedant/commit/808d8c633bb94f0b13b1e7c8236ceb40562cba53
+        shared(:outside_user) { platform.bad_user }
       end
     end
   end
