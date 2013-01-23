b43cdacebc1770cb3fbc876b65423f6c2557302c
Seth Falcon <seth@opscode.com>
2013-01-22 21:50:19 -0800

    Add search cache backed by redis
    On startup, if app config entry use_search_cache is true, a pool of
    eredis clients will be started. The number and connection details are
    read from app config as:
    
        redis_host
        redis_port
        redis_db
        eredis_client_pool_size
    
    A pg2 group 'redis_search_cache' is created and the clients are added
    to it on start.
    
    The oc_chef_wm_search_cache module is set as SEARCH_CACHE for use in
    chef_wm.

---
+++ src/oc_chef_wm_eredis_sup.erl
@@ -0,0 +1,44 @@
+%% -*- erlang-indent-level: 4;indent-tabs-mode: nil; fill-column: 92 -*-
+%% ex: ts=4 sw=4 et
+%%
+%% @doc Supervisor for eredis clients.
+-module(oc_chef_wm_eredis_sup).
+
+-behaviour(supervisor).
+
+-export([start_link/0,
+         eredis_start_wrapper/3]).
+
+%% Supervisor callbacks
+-export([init/1]).
+
+-define(SERVER, ?MODULE).
+
+start_link() ->
+    supervisor:start_link({local, ?SERVER}, ?MODULE, []).
+
+init([]) ->
+    {ok, ClientCount} = application:get_env(oc_chef_wm, eredis_client_pool_size),
+    error_logger:info_msg("starting oc_chef_wm_eredis_sup with ~B senders~n", [ClientCount]),
+    ok = pg2:create(redis_search_cache),
+    {ok, Host} = application:get_env(oc_chef_wm, redis_host),
+    {ok, Port} = application:get_env(oc_chef_wm, redis_port),
+    {ok, RedisDb} = application:get_env(oc_chef_wm, redis_db),
+    StartUp = {?MODULE, eredis_start_wrapper, [Host, Port, RedisDb]},
+    Children = [ {make_id(I), StartUp, permanent, brutal_kill, worker, [eredis]}
+                 || I <- lists:seq(1, ClientCount) ],
+    {ok, {{one_for_one, 60, 10}, Children}}.
+
+make_id(I) ->
+    "eredis_" ++ integer_to_list(I).
+
+%% @doc Wrapper around `eredis:start_link/3` that registers the
+%% created client with pg2.
+eredis_start_wrapper(Host, Port, RedisDb) ->
+    case eredis:start_link(Host, Port, RedisDb) of
+        {ok, Pid} ->
+            ok = pg2:join(redis_search_cache, Pid),
+            {ok, Pid};
+        Error ->
+            Error
+    end.
---
+++ src/oc_chef_wm_search_cache.erl
@@ -0,0 +1,91 @@
+%% -*- erlang-indent-level: 4;indent-tabs-mode: nil; fill-column: 92 -*-
+%% ex: ts=4 sw=4 et
+
+-module(oc_chef_wm_search_cache).
+
+-export([
+         get/2,
+         make_key/6,
+         put/3
+        ]).
+
+%% @doc Fetch an entry from the search cache.
+-spec get(_, binary()) -> not_found | {integer(), binary()}.
+get(ReqId, Key) ->
+    get(ReqId, redis_client(), Key).
+
+get(_ReqId, no_redis, _Key) ->
+    not_found;
+get(_ReqId, Redis, Key) ->
+    case eredis:q(Redis, ["GET", Key]) of
+        {ok, undefined} ->
+            not_found;
+        {ok, Value} ->
+            {Count, Gzip} = binary_to_term(Value),
+            {Count, zlib:gunzip(Gzip)}
+    end.
+
+%% @doc Add entry to the search cache
+-spec put(_, binary(), {integer(), binary()}) -> ok | error.
+put(ReqId, Key, {Count, Raw}) ->
+    put(ReqId, redis_client(), Key, {Count, Raw}).
+
+put(_ReqId, no_redis, _Key, _Value) ->
+    ok;
+put(_ReqId, Redis, Key, {Count, Raw}) ->
+    Gzip = zlib:gzip(Raw),
+    Value = term_to_binary({Count, Gzip}),
+    Cmds = [["SET", Key, Value],
+            ["EXPIRE", Key, cache_entry_ttl()]],
+    case eredis:qp(Redis, Cmds) of
+        [{ok, <<"OK">>}, {ok, <<"1">>}] ->
+            ok;
+        _ ->
+            error
+    end.
+
+
+%% @doc Return the TTL in seconds for entries in the search
+%% cache. Will be used with Redis' EXPIRE command to set the
+%% expiration for keys stored with SET.
+-spec cache_entry_ttl() -> non_neg_integer().
+cache_entry_ttl() ->
+    case application:get_env(oc_chef_wm, search_cache_entry_ttl) of
+        undefined ->
+            %% default to 60 seconds
+            60;
+        {ok, Seconds} when is_integer(Seconds) ->
+            Seconds
+    end.
+
+%% @doc Return the cache key for specified inputs that describe a
+%% search request. The key combines request attributes and solr
+%% results to identify unique search queries. The `Ids' are those
+%% returned from Solr. `ReqPath' should be the raw HTTP request path
+%% including query parameters. `Path' is the list of partial search
+%% paths or empty list.
+-spec make_key(binary(), integer(), integer(),
+               [binary()], string(), [binary()]) -> binary().
+make_key(OrgName, BatchSize, Start, Ids, ReqPath, Paths) ->
+    iolist_to_binary([OrgName,
+                      integer_to_list(BatchSize),
+                      integer_to_list(Start),
+                      digest_ids(Ids),
+                      ReqPath, Paths]).
+
+digest_ids(Ids) ->
+    md5_to_hex(crypto:md5(lists:sort(Ids))).
+
+md5_to_hex(<<X:128/big-unsigned-integer>>) ->
+    iolist_to_binary(io_lib:format("~32.16.0b", [X])).
+
+%% Obtain a non-exclusive client from the redis client "pool". If
+%% there are no clients or some other error occurs, returns
+%% `no_redis'.
+redis_client() ->
+    case pg2:get_closest_pid(redis_search_cache) of
+        {error, _} ->
+            no_redis;
+        Pid when is_pid(Pid) ->
+            Pid
+    end.
