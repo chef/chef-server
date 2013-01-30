e90f446c7ac84ee2508aaaceec0b3e7e7cf42bea
Seth Falcon <seth@opscode.com>
2013-01-30 09:41:26 -0800

    Another jenkins build script fix

--- jenkins/build.sh
+++ jenkins/build.sh
@@ -16,7 +16,7 @@ make distclean rel || exit 1
 # If the string is null, then the git command returned false
 if git describe --tags --match='[0-9]*.[0-9]*.[0-9]*' --exact-match
 then
-    VERSION=$(git describe --tags --exact-match --match='[0-9]*.[0-9].*[0-9]*')
+    VERSION=$(git describe --tags --exact-match --match='[0-9]*.[0-9]*.[0-9]*')
     PACKAGE=${PROJ_NAME_}${VERSION}.tar.gz
     cd rel
     tar zcvf $PACKAGE $PROJ_NAME/
