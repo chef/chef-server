92ec19ebc23b4500e0b6b5484c15afc5123a6426
Christopher Maier <cm@opscode.com>
2013-02-08 14:36:29 -0500

    Add serial IDs and use those as PK / FK targets
    A bigserial is an auto-incrementing bigint, which is 8 bytes, as
    opposed to 32 bytes for an authz_id.  This will keep tables smaller,
    which will allow more rows to fit in the server's cache, which will
    improve performance.
    
    Also added a few helper functions that take an Authz ID and returns
    the internal database ID.  These are intended for internal use.

--- schema/sql/authz.sql
+++ schema/sql/authz.sql
@@ -3,22 +3,26 @@ CREATE TYPE auth_permission AS ENUM ('update', 'read', 'grant', 'delete', 'creat
 -- No unique constraint on names now because we don't have
 -- organizations in Authz to do the proper scoping. :(
 CREATE TABLE container(
-    id   CHAR(32) PRIMARY KEY,
+    id bigserial PRIMARY KEY,
+    authz_id  CHAR(32) UNIQUE,
     name TEXT     NOT NULL
 );
 
 CREATE TABLE auth_actor(
-    id CHAR(32) PRIMARY KEY
+    id bigserial PRIMARY KEY,
+    authz_id  CHAR(32) UNIQUE
 );
 
 -- Can't scope Groups to organizations yet, because we have no
 -- organizations!
 CREATE TABLE auth_group(
-       id CHAR(32) PRIMARY KEY
+    id bigserial PRIMARY KEY,
+    authz_id  CHAR(32) UNIQUE
 );
 
 CREATE TABLE auth_object(
-   id CHAR(32) PRIMARY KEY
+    id bigserial PRIMARY KEY,
+    authz_id  CHAR(32) UNIQUE
 );
 
 -- ACL and membership graph information are kept in separate (but
@@ -41,57 +45,57 @@ CREATE TABLE auth_object(
 -- A record in an acl_* table indicates the permission is granted;
 -- lack of a record indicates the permission is denied.
 CREATE TABLE object_acl_group(
-       target CHAR(32) NOT NULL REFERENCES auth_object(id) ON UPDATE CASCADE ON DELETE CASCADE,
-       authorizee CHAR(32) NOT NULL REFERENCES auth_group(id) ON UPDATE CASCADE ON DELETE CASCADE,
+       target bigint NOT NULL REFERENCES auth_object(id) ON UPDATE CASCADE ON DELETE CASCADE,
+       authorizee bigint NOT NULL REFERENCES auth_group(id) ON UPDATE CASCADE ON DELETE CASCADE,
        permission auth_permission NOT NULL,
        PRIMARY KEY (target, authorizee, permission)
 );
 
 CREATE TABLE object_acl_actor(
-       target CHAR(32) NOT NULL REFERENCES auth_object(id) ON UPDATE CASCADE ON DELETE CASCADE,
-       authorizee CHAR(32) NOT NULL REFERENCES auth_actor(id) ON UPDATE CASCADE ON DELETE CASCADE,
+       target bigint NOT NULL REFERENCES auth_object(id) ON UPDATE CASCADE ON DELETE CASCADE,
+       authorizee bigint NOT NULL REFERENCES auth_actor(id) ON UPDATE CASCADE ON DELETE CASCADE,
        permission auth_permission NOT NULL,
        PRIMARY KEY (target, authorizee, permission)
 );
 
 CREATE TABLE actor_acl_group(
-       target CHAR(32) NOT NULL REFERENCES auth_actor(id) ON UPDATE CASCADE ON DELETE CASCADE,
-       authorizee CHAR(32) NOT NULL REFERENCES auth_group(id) ON UPDATE CASCADE ON DELETE CASCADE,
+       target bigint NOT NULL REFERENCES auth_actor(id) ON UPDATE CASCADE ON DELETE CASCADE,
+       authorizee bigint NOT NULL REFERENCES auth_group(id) ON UPDATE CASCADE ON DELETE CASCADE,
        permission auth_permission NOT NULL,
        PRIMARY KEY (target, authorizee, permission)
 );
 
 CREATE TABLE actor_acl_actor(
-       target CHAR(32) NOT NULL REFERENCES auth_actor(id) ON UPDATE CASCADE ON DELETE CASCADE,
-       authorizee CHAR(32) NOT NULL REFERENCES auth_actor(id) ON UPDATE CASCADE ON DELETE CASCADE,
+       target bigint NOT NULL REFERENCES auth_actor(id) ON UPDATE CASCADE ON DELETE CASCADE,
+       authorizee bigint NOT NULL REFERENCES auth_actor(id) ON UPDATE CASCADE ON DELETE CASCADE,
        permission auth_permission NOT NULL,
        PRIMARY KEY (target, authorizee, permission)
 );
 
 CREATE TABLE group_acl_group(
-       target CHAR(32) NOT NULL REFERENCES auth_group(id) ON UPDATE CASCADE ON DELETE CASCADE,
-       authorizee CHAR(32) NOT NULL REFERENCES auth_group(id) ON UPDATE CASCADE ON DELETE CASCADE,
+       target bigint NOT NULL REFERENCES auth_group(id) ON UPDATE CASCADE ON DELETE CASCADE,
+       authorizee bigint NOT NULL REFERENCES auth_group(id) ON UPDATE CASCADE ON DELETE CASCADE,
        permission auth_permission NOT NULL,
        PRIMARY KEY (target, authorizee, permission)
 );
 
 CREATE TABLE group_acl_actor(
-       target CHAR(32) NOT NULL REFERENCES auth_group(id) ON UPDATE CASCADE ON DELETE CASCADE,
-       authorizee CHAR(32) NOT NULL REFERENCES auth_actor(id) ON UPDATE CASCADE ON DELETE CASCADE,
+       target bigint NOT NULL REFERENCES auth_group(id) ON UPDATE CASCADE ON DELETE CASCADE,
+       authorizee bigint NOT NULL REFERENCES auth_actor(id) ON UPDATE CASCADE ON DELETE CASCADE,
        permission auth_permission NOT NULL,
        PRIMARY KEY (target, authorizee, permission)
 );
 
 CREATE TABLE container_acl_group(
-       target CHAR(32) NOT NULL REFERENCES container(id) ON UPDATE CASCADE ON DELETE CASCADE,
-       authorizee CHAR(32) NOT NULL REFERENCES auth_group(id) ON UPDATE CASCADE ON DELETE CASCADE,
+       target bigint NOT NULL REFERENCES container(id) ON UPDATE CASCADE ON DELETE CASCADE,
+       authorizee bigint NOT NULL REFERENCES auth_group(id) ON UPDATE CASCADE ON DELETE CASCADE,
        permission auth_permission NOT NULL,
        PRIMARY KEY (target, authorizee, permission)
 );
 
 CREATE TABLE container_acl_actor(
-       target CHAR(32) NOT NULL REFERENCES container(id) ON UPDATE CASCADE ON DELETE CASCADE,
-       authorizee CHAR(32) NOT NULL REFERENCES auth_actor(id) ON UPDATE CASCADE ON DELETE CASCADE,
+       target bigint NOT NULL REFERENCES container(id) ON UPDATE CASCADE ON DELETE CASCADE,
+       authorizee bigint NOT NULL REFERENCES auth_actor(id) ON UPDATE CASCADE ON DELETE CASCADE,
        permission auth_permission NOT NULL,
        PRIMARY KEY (target, authorizee, permission)
 );
@@ -99,40 +103,54 @@ CREATE TABLE container_acl_actor(
 -- Manage the group hierarchies
 
 CREATE TABLE group_group_relations(
-       parent CHAR(32) REFERENCES auth_group(id) ON UPDATE CASCADE ON DELETE CASCADE,
-       child CHAR(32) REFERENCES auth_group(id) ON UPDATE CASCADE ON DELETE CASCADE,
+       parent bigint REFERENCES auth_group(id) ON UPDATE CASCADE ON DELETE CASCADE,
+       child bigint REFERENCES auth_group(id) ON UPDATE CASCADE ON DELETE CASCADE,
        PRIMARY KEY (parent, child)
 );
 
 CREATE TABLE group_actor_relations(
-       parent CHAR(32) REFERENCES auth_group(id) ON UPDATE CASCADE ON DELETE CASCADE,
-       child CHAR(32) REFERENCES auth_actor(id) ON UPDATE CASCADE ON DELETE CASCADE,
+       parent bigint REFERENCES auth_group(id) ON UPDATE CASCADE ON DELETE CASCADE,
+       child bigint REFERENCES auth_actor(id) ON UPDATE CASCADE ON DELETE CASCADE,
        PRIMARY KEY (parent, child)
 );
 
 
--- Select all the actors in a group, recursively
-CREATE FUNCTION actors_in_group(auth_group.id%TYPE)
-RETURNS TABLE(actor auth_actor.id%TYPE)
-LANGUAGE SQL
+-- These are to help adding data in terms of Authz IDs
+-- TODO: Consider views with rewrite rules instead?
+CREATE FUNCTION actor_id(auth_actor.authz_id%TYPE)
+RETURNS auth_actor.id%TYPE
+LANGUAGE SQL STABLE STRICT
 AS $$
-    WITH RECURSIVE actors(g,a) AS (
-    SELECT parent, child
-    FROM group_actor_relations
-    WHERE parent = $1
-    AND child IS NOT NULL
-
-    UNION
-
-    SELECT gar.parent, gar.child
-    FROM group_actor_relations AS gar
-    JOIN group_group_relations AS ggr
-    ON gar.parent = ggr.child
-    JOIN actors ON actors.g = ggr.parent)
-  SELECT DISTINCT(a) from actors;
+   SELECT id FROM auth_actor WHERE authz_id = $1;
 $$;
 
+CREATE FUNCTION group_id(auth_group.authz_id%TYPE)
+RETURNS auth_group.id%TYPE
+LANGUAGE SQL STABLE STRICT
+AS $$
+   SELECT id FROM auth_group WHERE authz_id = $1;
+$$;
+
+CREATE FUNCTION object_id(auth_object.authz_id%TYPE)
+RETURNS auth_object.id%TYPE
+LANGUAGE SQL STABLE STRICT
+AS $$
+   SELECT id FROM auth_object WHERE authz_id = $1;
+$$;
+
+CREATE FUNCTION container_id(container.authz_id%TYPE)
+RETURNS container.id%TYPE
+LANGUAGE SQL STABLE STRICT
+AS $$
+   SELECT id FROM container WHERE authz_id = $1;
+$$;
+
+
 -- Select all the groups an actor is a member of, recursively
+--
+-- NOTE: This function takes an internal database ID as an argument,
+-- and returns a table of the same.  It is not intended to be called
+-- directly from the outside.
 CREATE FUNCTION groups_for_actor(auth_actor.id%TYPE)
 RETURNS TABLE(auth_group auth_group.id%TYPE)
 LANGUAGE SQL
@@ -158,18 +176,28 @@ $$;
 -- permission, since I don't think that's ever queried for directly
 -- from outside.
 CREATE FUNCTION actor_has_permission_on_object(
-       query_actor auth_actor.id%TYPE,
-       query_object auth_object.id%TYPE,
+       query_actor auth_actor.authz_id%TYPE,
+       query_object auth_object.authz_id%TYPE,
        perm auth_permission)
 RETURNS BOOLEAN
 LANGUAGE plpgsql
+STABLE -- <- this function doesn't alter the database; just queries it
+STRICT -- <- returns NULL immediately if any arguments are NULL
 AS $$
+DECLARE
+  -- Convert Authz IDs into internal database IDs
+  --
+  -- If the Authz IDs don't refer to an existing item, an error will
+  -- be thrown.
+  actor_id  auth_actor.id%TYPE  NOT NULL := actor_id(query_actor);
+  object_id auth_object.id%TYPE NOT NULL := object_id(query_object);
 BEGIN
+
         -- Check to see if the actor has the permission directly
         PERFORM a.*
-        FROM acl_actor AS a
-        WHERE a.auth_object = query_object
-        AND a.authorizee = query_actor
+        FROM object_acl_actor AS a
+        WHERE a.target = object_id
+        AND a.authorizee = actor_id
         AND a.permission = perm;
 
        -- If that returned anything, we're done
@@ -180,10 +208,10 @@ BEGIN
        -- The permission wasn't granted directly to the actor, we need
        -- to check the groups the actor is in
        PERFORM a.*
-       FROM acl_group AS a
-       JOIN groups_for_actor(query_actor)
+       FROM object_acl_group AS a
+       JOIN groups_for_actor(actor_id)
           AS gs(id) ON gs.id = a.authorizee
-       WHERE a.auth_object = query_object
+       WHERE a.target = object_id
        AND a.permission = perm;
 
        -- If anything was found, we're done
