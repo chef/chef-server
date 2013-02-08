8e3c6512efb7717815d0fc60d8de14236a3fcf6e
Christopher Maier <cm@opscode.com>
2013-02-08 14:36:58 -0500

    Add pgTAP tests, with instructions

---
+++ schema/test/actor_is_authorized_on_object_test.sql
@@ -0,0 +1,102 @@
+BEGIN;
+
+SELECT no_plan();
+
+SELECT is_strict('actor_has_permission_on_object', ARRAY['character', 'character', 'auth_permission']);
+SELECT volatility_is('actor_has_permission_on_object', ARRAY['character', 'character', 'auth_permission'], 'stable');
+SELECT function_returns('public', 'actor_has_permission_on_object', ARRAY['character', 'character', 'auth_permission'], 'boolean');
+
+-- Testing Data
+
+INSERT INTO auth_group(authz_id)
+VALUES
+       ('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'),
+       ('bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb'),
+       ('cccccccccccccccccccccccccccccccc');
+
+INSERT INTO auth_actor(authz_id)
+VALUES
+       ('zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz'),
+       ('yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy'),
+       ('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'),
+       ('wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww');
+
+INSERT INTO group_group_relations(parent, child)
+VALUES
+       (group_id('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'), group_id('bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb')),
+       (group_id('bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb'), group_id('cccccccccccccccccccccccccccccccc'));
+
+INSERT INTO group_actor_relations(parent, child)
+VALUES
+       (group_id('bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb'), actor_id('yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy')),
+       (group_id('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'), actor_id('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'));
+
+INSERT INTO auth_object(authz_id)
+VALUES
+        ('oooooooooooooooooooooooooooooooo');
+
+INSERT INTO object_acl_group(target, authorizee, permission)
+VALUES
+        (object_id('oooooooooooooooooooooooooooooooo'), group_id('bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb'), 'read'),
+        (object_id('oooooooooooooooooooooooooooooooo'), group_id('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'), 'delete')
+        ;
+
+INSERT INTO object_acl_actor(target, authorizee, permission)
+VALUES
+        (object_id('oooooooooooooooooooooooooooooooo'), actor_id('zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz'), 'update'),
+        (object_id('oooooooooooooooooooooooooooooooo'), actor_id('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'), 'delete');
+
+
+-- Test the basic cases:
+--
+-- Actor has direct permission
+-- Actor has indirect permission (i.e. is in a group with the permission)
+-- Actor has indirect permission via nested groups (i.e. is in a group in a group with the permission)
+-- Actor has both direct and indirect permission
+-- Actor has no permission
+SELECT is(TRUE,
+       actor_has_permission_on_object('zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz', 'oooooooooooooooooooooooooooooooo', 'update'),
+       'An actor directly granted permission XXX has the XXX permission');
+SELECT is(TRUE,
+       actor_has_permission_on_object('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx', 'oooooooooooooooooooooooooooooooo', 'delete'),
+       'An actor both directly and indirectly granted permission XXX has the XXX permission');
+SELECT is(TRUE,
+       actor_has_permission_on_object('yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy', 'oooooooooooooooooooooooooooooooo', 'read'),
+       'An actor indirectly granted permission XXX has the XXX permission');
+SELECT is(TRUE,
+       actor_has_permission_on_object('yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy', 'oooooooooooooooooooooooooooooooo', 'delete'),
+       'An actor indirectly granted permission XXX via nested groups has the XXX permission');
+SELECT is(FALSE,
+       actor_has_permission_on_object('wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww', 'oooooooooooooooooooooooooooooooo', 'grant'),
+       'An actor neither directly nor indirectly granted permission XXX has the XXX permission');
+
+-- Verify behavior on NULL input
+SELECT is(NULL,
+       actor_has_permission_on_object(NULL, 'oooooooooooooooooooooooooooooooo', 'update'),
+       'actor_has_permission_on_object returns NULL if actor is NULL');
+SELECT is(NULL,
+       actor_has_permission_on_object('zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz', NULL, 'update'),
+       'actor_has_permission_on_object returns NULL if object is NULL');
+SELECT is(NULL,
+       actor_has_permission_on_object('zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz', 'oooooooooooooooooooooooooooooooo', NULL),
+       'actor_has_permission_on_object returns NULL if permission is NULL');
+
+-- Verify behavior when passed non-NULL, but still invalid data (i.e., invalid permission, non-existent actor, non-existent object)
+SELECT throws_ok(
+       $$SELECT actor_has_permission_on_object('zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz', 'qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq', 'update')$$,
+       '22004',
+       'null value cannot be assigned to variable "object_id" declared NOT NULL',
+       'Checking of permission on non-existent object throws an exception');
+SELECT throws_ok(
+       $$SELECT actor_has_permission_on_object('qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq', 'oooooooooooooooooooooooooooooooo', 'update')$$,
+       '22004',
+       'null value cannot be assigned to variable "actor_id" declared NOT NULL',
+       'Checking of permission for non-existent actor throws an exception');
+SELECT throws_ok(
+       $$SELECT actor_has_permission_on_object('zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz', 'oooooooooooooooooooooooooooooooo', 'license_to_kill')$$,
+       '22P02',
+       'invalid input value for enum auth_permission: "license_to_kill"',
+       'Checking a non-existent permission throws an exception');
+
+SELECT finish();
+ROLLBACK;
---
+++ schema/test/schema_test.sql
@@ -0,0 +1,96 @@
+BEGIN;
+
+SELECT no_plan();
+SELECT tables_are(
+       ARRAY[
+             -- Authz's "things"
+             'container',
+             'auth_actor',
+             'auth_group',
+             'auth_object',
+
+             -- Permission tables
+             'object_acl_group',
+             'object_acl_actor',
+             'actor_acl_group',
+             'actor_acl_actor',
+             'group_acl_group',
+             'group_acl_actor',
+             'container_acl_group',
+             'container_acl_actor',
+
+             -- Group membership tables
+             'group_group_relations',
+             'group_actor_relations'
+         ]);
+
+SELECT enums_are(ARRAY['auth_permission']);
+
+-- All authz_id columns should be unique
+SELECT col_is_unique('container', ARRAY['authz_id']);
+SELECT col_is_unique('auth_actor', ARRAY['authz_id']);
+SELECT col_is_unique('auth_group', ARRAY['authz_id']);
+SELECT col_is_unique('auth_object', ARRAY['authz_id']);
+
+-- But the 'id' column is the real PK
+SELECT col_is_pk('container', ARRAY['id']);
+SELECT col_is_pk('auth_actor', ARRAY['id']);
+SELECT col_is_pk('auth_group', ARRAY['id']);
+SELECT col_is_pk('auth_object', ARRAY['id']);
+
+SELECT col_type_is('container','id','bigint');
+SELECT col_type_is('auth_actor','id','bigint');
+SELECT col_type_is('auth_group','id','bigint');
+SELECT col_type_is('auth_object','id','bigint');
+
+SELECT col_type_is('container', 'name', 'text');
+
+-- All permission tables have the exact same columns, and all create the PKs
+SELECT columns_are('object_acl_group', ARRAY['target', 'authorizee', 'permission']);
+SELECT columns_are('object_acl_actor', ARRAY['target', 'authorizee', 'permission']);
+SELECT columns_are('actor_acl_group', ARRAY['target', 'authorizee', 'permission']);
+SELECT columns_are('actor_acl_actor', ARRAY['target', 'authorizee', 'permission']);
+SELECT columns_are('group_acl_group', ARRAY['target', 'authorizee', 'permission']);
+SELECT columns_are('group_acl_actor', ARRAY['target', 'authorizee', 'permission']);
+SELECT columns_are('container_acl_group', ARRAY['target', 'authorizee', 'permission']);
+SELECT columns_are('container_acl_actor', ARRAY['target', 'authorizee', 'permission']);
+
+SELECT col_is_pk('object_acl_group', ARRAY['target', 'authorizee', 'permission']);
+SELECT col_is_pk('object_acl_actor', ARRAY['target', 'authorizee', 'permission']);
+SELECT col_is_pk('actor_acl_group', ARRAY['target', 'authorizee', 'permission']);
+SELECT col_is_pk('actor_acl_actor', ARRAY['target', 'authorizee', 'permission']);
+SELECT col_is_pk('group_acl_group', ARRAY['target', 'authorizee', 'permission']);
+SELECT col_is_pk('group_acl_actor', ARRAY['target', 'authorizee', 'permission']);
+SELECT col_is_pk('container_acl_group', ARRAY['target', 'authorizee', 'permission']);
+SELECT col_is_pk('container_acl_actor', ARRAY['target', 'authorizee', 'permission']);
+
+-- The group membership tables also have the same columns, and all of them make up the PKs
+SELECT columns_are('group_group_relations', ARRAY['parent','child']);
+SELECT columns_are('group_actor_relations', ARRAY['parent','child']);
+
+SELECT col_is_pk('group_group_relations', ARRAY['parent','child']);
+SELECT col_is_pk('group_actor_relations', ARRAY['parent','child']);
+
+-- Test the FKs now
+SELECT fk_ok('group_acl_actor', 'target', 'auth_group', 'id');
+SELECT fk_ok('group_acl_actor', 'authorizee', 'auth_actor', 'id');
+SELECT fk_ok('group_acl_group', 'target', 'auth_group', 'id');
+SELECT fk_ok('group_acl_group', 'authorizee', 'auth_group', 'id');
+
+SELECT fk_ok('object_acl_actor', 'target', 'auth_object', 'id');
+SELECT fk_ok('object_acl_actor', 'authorizee', 'auth_actor', 'id');
+SELECT fk_ok('object_acl_group', 'target', 'auth_object', 'id');
+SELECT fk_ok('object_acl_group', 'authorizee', 'auth_group', 'id');
+
+SELECT fk_ok('actor_acl_actor', 'target', 'auth_actor', 'id');
+SELECT fk_ok('actor_acl_actor', 'authorizee', 'auth_actor', 'id');
+SELECT fk_ok('actor_acl_group', 'target', 'auth_actor', 'id');
+SELECT fk_ok('actor_acl_group', 'authorizee', 'auth_group', 'id');
+
+SELECT fk_ok('container_acl_actor', 'target', 'container', 'id');
+SELECT fk_ok('container_acl_actor', 'authorizee', 'auth_actor', 'id');
+SELECT fk_ok('container_acl_group', 'target', 'container', 'id');
+SELECT fk_ok('container_acl_group', 'authorizee', 'auth_group', 'id');
+
+SELECT finish();
+ROLLBACK;
