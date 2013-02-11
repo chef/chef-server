f01f7aca99eeecbefda335ae940a2129311c652d
Christopher Maier <cm@opscode.com>
2013-02-11 08:36:44 -0500

    Move tests into the 't' directory
    This is also the default for pg_prove

--- schema/shell/test.sh
+++ schema/shell/test.sh
@@ -2,4 +2,5 @@
 
 # Must be run from the 'schema' directory of the oc_authz project!
 
-pg_prove --dbname authz --verbose test/*_test.sql
+# Runs all files in the 't' directory with the 'pg' extension
+pg_prove --dbname authz --verbose
