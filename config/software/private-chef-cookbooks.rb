1db2aac875b2173ec2fd5ec5d46e4a10d52a7908
Seth Chisamore <schisamo@opscode.com>
2013-02-12 21:42:29 -0500

    Ensure `uuidtools` is installed and available to embedded cookbooks
    The embedded `private-chef` cookbook (private-chef-ctl reconfigure)
    require this gem when generating the bootstrap certificates and
    keypairs. Chef 10.x previously shipped with `uuidtools`, but it was
    removed for Chef 11+.

--- config/software/private-chef-cookbooks.rb
+++ config/software/private-chef-cookbooks.rb
@@ -5,6 +5,7 @@ dependencies [ "rsync" ]
 source :path => File.expand_path("files/private-chef-cookbooks", Omnibus.root)
 
 build do
+  gem "install uuidtools --no-rdoc --no-ri -v 2.1.3"
   command "mkdir -p #{install_dir}/embedded/cookbooks"
   command "#{install_dir}/embedded/bin/rsync --delete -a ./ #{install_dir}/embedded/cookbooks/"
 end
