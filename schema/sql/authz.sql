b35bcc615c60619865fbe84765af6f132cd26e32
Christopher Maier <cm@opscode.com>
2013-02-13 09:26:17 -0500

    Disallow group membership cycles
    See comments and documentation in code for more details.

--- schema/sql/authz.sql
+++ schema/sql/authz.sql
@@ -110,7 +110,12 @@ CREATE TABLE container_acl_actor(
 CREATE TABLE group_group_relations(
        parent bigint REFERENCES auth_group(id) ON UPDATE CASCADE ON DELETE CASCADE,
        child bigint REFERENCES auth_group(id) ON UPDATE CASCADE ON DELETE CASCADE,
-       PRIMARY KEY (parent, child)
+       PRIMARY KEY (parent, child),
+       -- We can effectively perform this check in the
+       -- no_long_range_cycles trigger (see below), but there's no
+       -- harm in explicitly coding this simple (and cheap) base-case
+       -- check
+       CONSTRAINT no_trivial_cycles CHECK(parent != child)
 );
 
 CREATE TABLE group_actor_relations(
@@ -119,6 +124,82 @@ CREATE TABLE group_actor_relations(
        PRIMARY KEY (parent, child)
 );
 
+CREATE OR REPLACE FUNCTION forbid_group_cycles()
+RETURNS TRIGGER
+LANGUAGE plpgsql
+AS $$
+DECLARE
+    cycle_root BOOLEAN DEFAULT FALSE;
+BEGIN
+    SELECT INTO cycle_root (
+        WITH RECURSIVE
+            children(id) AS (
+                -- direct children of this current parent group
+                SELECT child
+                FROM group_group_relations
+                WHERE parent = NEW.parent
+
+                UNION
+
+                -- indirect children (recursively)
+                SELECT ggr.child
+                FROM group_group_relations AS ggr
+                JOIN children AS c ON ggr.parent = c.id
+            )
+        SELECT TRUE
+        FROM children
+        WHERE id = NEW.parent
+    );
+
+    IF cycle_root THEN
+      RAISE EXCEPTION 'This would create a group membership cycle, which is not allowed';
+    END IF;
+
+    RETURN NULL;
+END;
+$$;
+
+COMMENT ON FUNCTION forbid_group_cycles() IS
+$$We want to forbid the presence of group membership cycles.  That is,
+paths such as:
+
+  X -member-of-> Y -member-of-> Z -member-of-> X
+
+should be disallowed.  While the recursive group membership queries
+are written to effectively ignore such cycles, forbidding cycles
+allows us to eliminate a class of permission anomalies.
+
+For instance, using the above cycle, if group X was given READ
+permission (but not, say, DELETE) on an object and group Z was given
+DELETE on that same object, by virtue of the cyclical membership
+relationship, group X would inherit the DELETE permission!  If X was
+supposed to be the root of this group hierarchy, it clearly should not
+be inheriting DELETE from Z, its "child".  Alternatively, if Z the
+true root, it would be legitimate for X to inherit DELETE from Z, but
+not for Z to inherit READ!
+
+Since the cyclic nature of such relationships prevents us from saying
+which interpretation is correct, we explicitly forbid such scenarios
+in the first place.
+
+Put another way, by allowing group cycles, we are in effect declaring
+that all actors and groups that participate in a cycle will have the
+__union of all permissions granted to any of the groups__ in the cycle
+(permissions granted directly to actors managed separately, and thus
+are not involved in the present discussion).  Needless to say, this is
+not a good state of affairs.
+
+To implement this, the trigger function operates on each row that is
+inserted.  If insertion of the row creates a cycle containing 'parent'
+(that is, if it creates a path by which 'parent' may be reached from
+itself), an exception is raised.
+$$;
+
+CREATE CONSTRAINT TRIGGER no_long_range_cycles
+    AFTER INSERT OR UPDATE
+    ON group_group_relations
+    FOR EACH ROW
+    EXECUTE PROCEDURE forbid_group_cycles();
 
 -- These are to help adding data in terms of Authz IDs
 -- TODO: Consider views with rewrite rules instead?
