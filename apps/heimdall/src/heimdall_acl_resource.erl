865637951e37fc0eb1db34b68b4f6a9dd282e34d
doubt72 <doug@opscode.com>
2013-02-22 15:03:28 -0700

    GET endpoint for full ACLs

---
+++ apps/heimdall/src/heimdall_acl_resource.erl
@@ -0,0 +1,23 @@
+-module(heimdall_acl_resource).
+
+-include("heimdall_wm_rest_endpoint.hrl").
+
+-export([allowed_methods/2,
+         init/1,
+         to_json/2]).
+
+init(Config) ->
+    heimdall_wm_base:init(?MODULE, Config).
+
+allowed_methods(Req, State) ->
+    {['GET'], Req, State}.
+
+validate_request(Req, State) ->
+    heimdall_wm_base:validate_requestor(Req, State).
+
+auth_info('GET') ->
+    {any}.
+
+to_json(Req, State) ->
+    % TODO: output the ACL populated by forbidden
+    {<<"{}">>, Req, State}
