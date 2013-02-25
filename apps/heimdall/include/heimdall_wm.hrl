90d8a1a6b37cb33e3204280162c7b134ae1c2961
doubt72 <doug@opscode.com>
2013-02-25 11:52:45 -0700

    Added some explanatory comments

--- apps/heimdall/include/heimdall_wm.hrl
+++ apps/heimdall/include/heimdall_wm.hrl
@@ -5,9 +5,21 @@
 -type entity_type() :: actor | group | container | object.
 
 %% Share resource state shared by all heimdall resource modules
+%%
+%% All ACL resources follow the pattern:
+%%
+%% /<type>/<id>/acl/<action>/<member_type>/<member_id>
+%%
+%% Group resources follow this pattern:
+%%
+%% /groups/<id>/<member_type>/<member_id>
+%%
+%% authz_id = <id>, request_type = <type>, otherwise <action>, <member_type> and
+%% <member_id> are the same.
 -record(base_state, {
 
-          %% Who are we working with?
+          %% Who are we working with?  This is the object in question, i.e., for
+          %% /<type>/<id> this is <id>
           authz_id :: binary(),
 
           %% What kind of resource are we talking about here?
@@ -19,7 +31,8 @@
           %% What type of ACE request is this?
           action :: permission(),
 
-          %% What member type are we checking for?
+          %% What member type are we checking for?  This will always be an actor or
+          %% a group
           member_type :: entity_type(),
 
           %% What's the ID of the member
