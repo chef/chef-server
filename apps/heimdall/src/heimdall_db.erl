b108a993a28e901a68bda1d381594e910180cfa4
Christopher Maier <cm@opscode.com>
2013-02-26 11:42:41 -0500

    WIP

--- apps/heimdall/src/heimdall_db.erl
+++ apps/heimdall/src/heimdall_db.erl
@@ -3,9 +3,59 @@
 
 -module(heimdall_db).
 
+-include("heimdall.hrl").
+
 -export([
+         create/2,
+         delete/2,
+         exists/2,
          statements/0
         ]).
+
+create_stmt(actor)     -> insert_actor;
+create_stmt(container) -> insert_container;
+create_stmt(group)     -> insert_group;
+create_stmt(object)    -> insert_object.
+
+-spec create(auth_type(), auth_id()) -> ok | {conflict, term()} | {error, term()}.
+create(Type, AuthzId) ->
+    CreateStatement = create_stmt(Type),
+    case sqerl:statement(CreateStatement, [AuthzId], count) of
+        {ok, 1} ->
+            ok;
+        {conflict, Reason} ->
+            {conflict, Reason};
+        {error, Reason} ->
+            {error, Reason}
+    end.
+
+delete_stmt(actor)     -> delete_actor_by_authz_id;
+delete_stmt(container) -> delete_container_by_authz_id;
+delete_stmt(group)     -> delete_group_by_authz_id;
+delete_stmt(object)    -> delete_object_by_authz_id.
+
+-spec delete(auth_type(), auth_id()) -> ok | {error, term()}.
+delete(Type, AuthzId) ->
+    DeleteStatement = delete_stmt(Type),
+    case sqerl:statement(DeleteStatement, [AuthzId], count) of
+        {ok, 1} ->
+            ok;
+        {error, Reason} ->
+            {error, Reason}
+    end.
+
+exists_query(actor)     -> actor_exists;
+exists_query(container) -> container_exists;
+exists_query(group)     -> group_exists;
+exists_query(object)    -> object_exists.
+
+-spec exists(auth_type(), auth_id()) -> boolean().
+exists(Type, AuthId) ->
+    StatementName = exists_query(Type),
+    {ok, Answer} = sqerl:select(StatementName, [AuthId], first_as_scalar, [exists]),
+    Answer.
+
+
 
 statements() ->
     Path = filename:join([code:priv_dir(heimdall), "pgsql_statements.config"]),
