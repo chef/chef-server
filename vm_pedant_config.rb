f7ca273594792743b1efd203ab7702d510070713
John Keiser <jkeiser@opscode.com>
2013-04-05 11:19:45 -0700

    One more rename

--- vm_pedant_config.rb
+++ vm_pedant_config.rb
@@ -1,5 +1,5 @@
-# This config is handy for testing oc_heimdall running on a VM.
-# Values should match up with oc_heimdall's Vagrantfile
+# This config is handy for testing oc_bifrost running on a VM.
+# Values should match up with oc_bifrost's Vagrantfile
 
 host "http://127.0.0.1"
 port "15959"
