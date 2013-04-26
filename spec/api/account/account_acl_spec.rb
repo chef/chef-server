755ca91a70540d42cf227fa505705385f36dc719
Douglas Triggs <doug@opscode.com>
2013-04-26 15:58:37 -0600

    Added organization ACL tests

--- spec/api/account/account_acl_spec.rb
+++ spec/api/account/account_acl_spec.rb
@@ -1,6 +1,420 @@
 require 'pedant/rspec/common'
 
-describe "ACL API" do
+describe "ACL API", :focus do
+  context "/organization/_acl endpoint" do
+    let(:request_url) { api_url("organization/_acl") }
+
+    context "GET /organization/_acl" do
+      let(:actors) { ["pivotal"] }
+      let(:groups) { ["admins"] }
+      let(:read_groups) { ["admins", "users"] }
+      let(:acl_body) {{
+          "create" => {"actors" => actors, "groups" => groups},
+          "read" => {"actors" => actors, "groups" => read_groups},
+          "update" => {"actors" => actors, "groups" => groups},
+          "delete" => {"actors" => actors, "groups" => groups},
+          "grant" => {"actors" => actors, "groups" => groups}
+        }}
+
+      context "admin user" do
+        it "can get object ACL" do
+          puts request_url
+          get(request_url, platform.admin_user).should look_like({
+              :status => 200,
+              :body_exact => acl_body
+            })
+        end
+      end
+
+      context "default normal user" do
+        it "returns 403" do
+          get(request_url, platform.non_admin_user).should look_like({
+              :status => 403
+            })
+        end
+      end
+
+      context "default client" do
+        it "returns 403" do
+          get(request_url, platform.non_admin_client).should look_like({
+              :status => 403
+            })
+        end
+      end
+
+      context "outside user" do
+        it "returns 403" do
+          get(request_url, outside_user).should look_like({
+              :status => 403
+            })
+        end
+      end
+
+      context "invalid user" do
+        it "returns 401" do
+          get(request_url, invalid_user).should look_like({
+              :status => 401
+            })
+        end
+      end
+
+      context "with modified ACLs" do
+        after :each do
+          ["create", "read", "update", "delete", "grant"].each do |perm|
+            if (perm == "read")
+              reset_body = {perm => {"actors" => actors, "groups" => read_groups}}
+            else
+              reset_body = {perm => {"actors" => actors, "groups" => groups}}
+            end
+            put("#{request_url}/#{perm}", platform.admin_user,
+              :payload => reset_body).should look_like({
+                :status => 200
+              })
+          end
+          # Make sure everything's the same again -- this could really screw up the
+          # rest of the test suite if the permissions aren't right
+          get(request_url, platform.admin_user).should look_like({
+              :status => 200,
+              :body_exact => acl_body
+            })
+        end
+
+        context "when normal user granted all permissions except GRANT" do
+          it "returns 403" do
+            ["create", "read", "update", "delete"].each do |perm|
+              put("#{request_url}/#{perm}", platform.admin_user,
+                :payload => {perm => {
+                    "actors" => [platform.non_admin_user.name, "pivotal"],
+                    "groups" => ["admins"]
+                  }}).should look_like({
+                  :status => 200
+                })
+            end
+            get(request_url, platform.non_admin_user).should look_like({
+                :status => 403
+              })
+          end
+        end
+
+        context "when normal client granted all permisions except GRANT" do
+          it "returns 403" do
+            ["create", "read", "update", "delete"].each do |perm|
+              put("#{request_url}/#{perm}", platform.admin_user,
+                :payload => {perm => {
+                    "actors" => [platform.non_admin_client.name, "pivotal"],
+                    "groups" => ["admins"]
+                  }}).should look_like({
+                  :status => 200
+                })
+            end
+            get(request_url, platform.non_admin_client).should look_like({
+                :status => 403
+              })
+          end
+        end
+
+        context "when normal user granted GRANT permission" do
+          it "can get ACL" do
+            put("#{request_url}/grant", platform.admin_user,
+              :payload => {"grant" => {
+                  "actors" => [platform.non_admin_user.name, "pivotal"],
+                  "groups" => ["admins"]
+                }}).should look_like({
+                :status => 200
+              })
+            get(request_url, platform.non_admin_user).should look_like({
+                :status => 200
+              })
+          end
+        end
+
+        context "when normal client granted GRANT permision" do
+          it "can get ACL" do
+            put("#{request_url}/grant", platform.admin_user,
+              :payload => {"grant" => {
+                  "actors" => [platform.non_admin_client.name, "pivotal"],
+                  "groups" => ["admins"]
+                }}).should look_like({
+                :status => 200
+              })
+            get(request_url, platform.non_admin_client).should look_like({
+                :status => 200
+              })
+          end
+        end
+      end # context with modified ACLs
+    end # context GET /organization/_acl
+
+    context "PUT /organization/_acl" do
+      context "admin user" do
+        # A 405 here would be fine (better, even)
+        it "returns 404" do
+          put(request_url, platform.admin_user).should look_like({
+              :status => 404
+            })
+        end
+      end
+    end # context PUT /organization/_acl
+
+    context "POST /organization/_acl" do
+      context "admin user" do
+        # A 405 here would be fine (better, even)
+        it "returns 404" do
+          post(request_url, platform.admin_user).should look_like({
+              :status => 404
+            })
+        end
+      end
+    end # context POST /organization/_acl
+
+    context "DELETE /organization/_acl" do
+      context "admin user" do
+        # A 405 here would be fine (better, even)
+        it "returns 404" do
+          delete(request_url, platform.admin_user).should look_like({
+              :status => 404
+            })
+        end
+      end
+    end # context DELETE /organization/_acl
+  end # context /organization/_acl endpoint
+
+  ["create", "read", "update", "delete", "grant"].each do |permission|
+    context "/organization/_acl/#{permission} endpoint" do
+      let(:acl_url) { api_url("organization/_acl") }
+      let(:request_url) { api_url("organization/_acl/#{permission}") }
+
+      context "PUT /organization/_acl/#{permission}" do
+        let(:actors) { ["pivotal"] }
+        let(:groups) { ["admins"] }
+        let(:read_groups) { ["admins", "users"] }
+        let(:default_body) {{
+            "create" => {"actors" => actors, "groups" => groups},
+            "read" => {"actors" => actors, "groups" => read_groups},
+            "update" => {"actors" => actors, "groups" => groups},
+            "delete" => {"actors" => actors, "groups" => groups},
+            "grant" => {"actors" => actors, "groups" => groups}
+          }}
+
+        let(:request_body) {{
+            permission => {
+              "actors" => ["pivotal", platform.admin_user.name,
+                platform.non_admin_user.name],
+              "groups" => groups
+            }
+          }}
+
+        after :each do
+          if (permission == "read")
+            reset_body = {permission => {"actors" => actors, "groups" => read_groups}}
+          else
+            reset_body = {permission => {"actors" => actors, "groups" => groups}}
+          end
+          put(request_url, platform.admin_user,
+            :payload => reset_body).should look_like({
+              :status => 200
+            })
+          # Make sure everything's the same again -- this could really screw up the
+          # rest of the test suite if the permissions aren't right
+          get(acl_url, platform.admin_user).should look_like({
+              :status => 200,
+              :body_exact => default_body
+            })
+        end
+
+        context "admin user" do
+          it "can modify ACL" do
+            put(request_url, platform.admin_user,
+              :payload => request_body).should look_like({
+                :status => 200
+              })
+            modified_body = default_body.dup;
+            modified_body[permission] = request_body[permission]
+            get(acl_url, platform.admin_user).should look_like({
+                :status => 200,
+                :body_exact => modified_body
+              })
+          end
+        end
+
+        context "default normal user" do
+          it "returns 403" do
+            put(request_url, platform.non_admin_user,
+              :payload => request_body).should look_like({
+                :status => 403
+              })
+            get(acl_url, platform.admin_user).should look_like({
+                :status => 200,
+                :body_exact => default_body
+              })
+          end
+        end
+
+        context "default normal client" do
+          it "returns 403" do
+            put(request_url, platform.non_admin_client,
+              :payload => request_body).should look_like({
+                :status => 403
+              })
+            get(acl_url, platform.admin_user).should look_like({
+                :status => 200,
+                :body_exact => default_body
+              })
+          end
+        end
+
+        context "outside user" do
+          it "returns 403" do
+            put(request_url, outside_user,
+              :payload => request_body).should look_like({
+                :status => 403
+              })
+            get(acl_url, platform.admin_user).should look_like({
+                :status => 200,
+                :body_exact => default_body
+              })
+          end
+        end
+
+        context "invalid user" do
+          it "returns 401" do
+            put(request_url, invalid_user,
+              :payload => request_body).should look_like({
+                :status => 401
+              })
+            get(acl_url, platform.admin_user).should look_like({
+                :status => 200,
+                :body_exact => default_body
+              })
+          end
+        end
+
+        context "with modified ACLs" do
+          after :each do
+            ["create", "read", "update", "delete", "grant"].each do |perm|
+              if (perm == "read")
+                reset_body = {perm => {"actors" => actors, "groups" => read_groups}}
+              else
+                reset_body = {perm => {"actors" => actors, "groups" => groups}}
+              end
+              put("#{acl_url}/#{perm}", platform.admin_user,
+                :payload => reset_body).should look_like({
+                  :status => 200
+                })
+            end
+            # Make sure everything's the same again -- this could really screw up the
+            # rest of the test suite if the permissions aren't right
+            get(acl_url, platform.admin_user).should look_like({
+                :status => 200,
+                :body_exact => default_body
+              })
+          end
+
+          context "when normal user granted all permissions except GRANT" do
+            it "returns 403" do
+              ["create", "read", "update", "delete"].each do |perm|
+                put("#{acl_url}/#{perm}", platform.admin_user,
+                  :payload => {perm => {
+                      "actors" => [platform.non_admin_user.name, "pivotal"],
+                      "groups" => ["admins"]
+                    }}).should look_like({
+                    :status => 200
+                  })
+              end
+              put(request_url, platform.non_admin_user,
+                :payload => request_body).should look_like({
+                  :status => 403
+                })
+            end
+          end
+
+          context "when normal client granted all permisions except GRANT" do
+            it "returns 403" do
+              ["create", "read", "update", "delete"].each do |perm|
+                put("#{acl_url}/#{perm}", platform.admin_user,
+                  :payload => {perm => {
+                      "actors" => [platform.non_admin_client.name, "pivotal"],
+                      "groups" => ["admins"]
+                    }}).should look_like({
+                    :status => 200
+                  })
+              end
+              put(request_url, platform.non_admin_client,
+                :payload => request_body).should look_like({
+                  :status => 403
+                })
+            end
+          end
+
+          context "when normal user granted GRANT permission" do
+            it "can modify ACL" do
+              put("#{acl_url}/grant", platform.admin_user,
+                :payload => {"grant" => {
+                    "actors" => [platform.non_admin_user.name, "pivotal"],
+                    "groups" => ["admins"]
+                  }}).should look_like({
+                  :status => 200
+                })
+              put(request_url, platform.non_admin_user,
+                :payload => request_body).should look_like({
+                  :status => 200
+                })
+            end
+          end
+
+          context "when normal client granted GRANT permision" do
+            it "can modify ACL" do
+              put("#{acl_url}/grant", platform.admin_user,
+                :payload => {"grant" => {
+                    "actors" => [platform.non_admin_client.name, "pivotal"],
+                    "groups" => ["admins"]
+                  }}).should look_like({
+                  :status => 200
+                })
+              put(request_url, platform.non_admin_client,
+                :payload => request_body).should look_like({
+                  :status => 200
+                })
+            end
+          end
+        end # context with modified ACLs
+      end # context PUT /organization/_acl/<permission>
+
+      context "GET /organization/_acl/#{permission}" do
+        context "admin user" do
+          # A 405 here would be fine (better, even)
+          it "returns 404" do
+            get(request_url, platform.admin_user).should look_like({
+                :status => 404
+              })
+          end
+        end
+      end # context GET /organization/_acl/<permission>
+
+      context "POST /organization/_acl/#{permission}" do
+        context "admin user" do
+          # A 405 here would be fine (better, even)
+          it "returns 404" do
+            post(request_url, platform.admin_user).should look_like({
+                :status => 404
+              })
+          end
+        end
+      end # context POST /organization/_acl/<permission>
+
+      context "DELETE /organization/_acl/#{permission}" do
+        context "admin user" do
+          # A 405 here would be fine (better, even)
+          it "returns 404" do
+            delete(request_url, platform.admin_user).should look_like({
+                :status => 404
+              })
+          end
+        end
+      end # context DELETE /organization/_acl/<permission>
+    end # context /organizations/_acl/<permission> endpoint
+  end # loop (each) over permissions
+
   context "/<type>/<name>/_acl endpoint" do
 
     # TODO: Sanity check: users don't seem to have any ACLs, or at least, nothing is
