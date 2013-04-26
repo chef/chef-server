c06f4afbea939fbc70d31234e309ea1b25b2d398
sdelano <stephen@opscode.com>
2013-04-26 10:09:24 -0700

    load eredis configuration with envy

--- src/mover_eredis_sup.erl
+++ src/mover_eredis_sup.erl
@@ -10,7 +10,7 @@
 
 %% Supervisor callbacks
 -export([init/1,
-         eredis_start_link/0
+         eredis_start_link/2
         ]).
 
 -define(SERVER, ?MODULE).
@@ -19,17 +19,11 @@ start_link() ->
     supervisor:start_link({local, ?SERVER}, ?MODULE, []).
 
 init([]) ->
-    ClientCount = 1, %% envy:get(eredis, client_pool_size, integer),
-    error_logger:info_msg("search cache ENABLED with ~B senders~n", [ClientCount]),
-    %%Host = envy:get(eredis, host, string),
-    %%Port = envy:get(eredis, port, integer),
-    StartUp = {?MODULE, eredis_start_link, []},
-    Children = [ {make_id(I), StartUp, permanent, brutal_kill, worker, [eredis]}
-                 || I <- lists:seq(1, ClientCount) ],
-    {ok, {{one_for_one, 60, 10}, Children}}.
+    Host = envy:get(mover, eredis_host, string),
+    Port = envy:get(mover, eredis_port, integer),
+    StartUp = {?MODULE, eredis_start_link, [Host, Port]},
+    Child = [{eredis, StartUp, permanent, brutal_kill, worker, [eredis]}],
+    {ok, {{one_for_one, 60, 10}, Child}}.
 
-eredis_start_link() ->
-    gen_server:start_link({local, mover_eredis_client}, eredis_client, ["127.0.0.1", 6379, 0, "", 100], []).
-
-make_id(I) ->
-    "eredis_" ++ integer_to_list(I).
+eredis_start_link(Host, Port) ->
+    gen_server:start_link({local, mover_eredis_client}, eredis_client, [Host, Port, 0, "", 100], []).
