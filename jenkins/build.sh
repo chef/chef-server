feaaf697fdcb8f1fcc64cf5d608a013e187a6bd0
Ho-Sheng Hsiao <hosh@opscode.com>
2013-05-06 12:45:54 -0700

    Fix package name built by Jenkins

--- jenkins/build.sh
+++ jenkins/build.sh
@@ -17,7 +17,7 @@ make distclean rel || exit 1
 if git describe --tags --match='[0-9]*.[0-9]*.[0-9]*' --exact-match
 then
     VERSION=$(git describe --tags --exact-match --match='[0-9]*.[0-9]*.[0-9]*')
-    PACKAGE=${PROJ_NAME_}${VERSION}.tar.gz
+    PACKAGE=${PROJ_NAME}-${VERSION}.tar.gz
     cd rel
     tar zcvf $PACKAGE $PROJ_NAME/
     s3cmd put --progress $PACKAGE s3://$ARTIFACT_BASE/$PACKAGE
@@ -25,7 +25,7 @@ else
     REL_VERSION=`cat rel/reltool.config|grep '{rel,.*"oc_erchef"'|cut -d ',' -f 3|sed 's/"//g'`
     GIT_SHA=`git rev-parse --short HEAD`
     VERSION=${REL_VERSION}-${GIT_SHA}
-    PACKAGE=${PROJ_NAME_}${VERSION}.tar.gz
+    PACKAGE=${PROJ_NAME}-${VERSION}.tar.gz
     cd rel
     tar zcvf $PACKAGE $PROJ_NAME/
     s3cmd put --progress $PACKAGE s3://$ARTIFACT_BASE/$PACKAGE
