0b10e338269147552b5d35bb84c957c70655bd9f
Nathan L Smith <nlloyds@gmail.com>
2013-05-29 15:43:10 -0500

    don't need to create the yamlz

--- config/software/opscode-webui2.rb
+++ config/software/opscode-webui2.rb
@@ -1,5 +1,5 @@
 name "opscode-webui2"
-version "master"
+version "release"
 
 dependency "bundler"
 dependency "libxml2"
@@ -12,10 +12,8 @@ source :git => "git@github.com:opscode/opscode-webui2"
 relative_path "opscode-webui2"
 
 build do
-  bundle 'install --without development test --path=/opt/opscode/embedded/service/gem'
-  # Stub this YAML file to allow the rake task to run.
-  command "echo 'production:' > #{source_dir}/opscode-webui2/config/opscode_platform.yml"
-  rake 'assets:precompile'
+  bundle  "install --without development test --path=/opt/opscode/embedded/service/gem"
+  rake    "assets:precompile"
   command "mkdir -p #{install_dir}/embedded/service/opscode-webui2"
   command "#{install_dir}/embedded/bin/rsync -a --delete --delete-excluded --exclude=.git/*** --exclude=.gitignore --exclude=prototype/*** ./ #{install_dir}/embedded/service/opscode-webui2/"
 end
