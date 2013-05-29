4be5735f3bb46837eb983c6c95486e6712a8b51d
Nathan L Smith <nlloyds@gmail.com>
2013-05-29 10:14:18 -0500

    Add node.js as build dependency. Compile assets at build time.

--- config/software/opscode-webui2.rb
+++ config/software/opscode-webui2.rb
@@ -4,15 +4,18 @@ version "master"
 dependency "bundler"
 dependency "libxml2"
 dependency "libxslt"
-dependency "nodejs"
 dependency "rsync"
+dependency "zlib"
 
 source :git => "git@github.com:opscode/opscode-webui2"
 
 relative_path "opscode-webui2"
 
 build do
-  bundle "install --without development test --path=/opt/opscode/embedded/service/gem"
+  bundle 'install --without development test --path=/opt/opscode/embedded/service/gem'
+  # Stub this YAML file to allow the rake task to run.
+  command "echo 'production:' > #{source_dir}/opscode-webui2/config/opscode_platform.yml"
+  rake 'assets:precompile'
   command "mkdir -p #{install_dir}/embedded/service/opscode-webui2"
   command "#{install_dir}/embedded/bin/rsync -a --delete --delete-excluded --exclude=.git/*** --exclude=.gitignore --exclude=prototype/*** ./ #{install_dir}/embedded/service/opscode-webui2/"
 end
