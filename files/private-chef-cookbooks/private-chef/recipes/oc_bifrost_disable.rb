1b43cfa9afd6e8f9bcd24257eb6f8034649b1cbf
Seth Chisamore <schisamo@opscode.com>
2013-05-30 08:43:42 -0400

    replace authz with oc_bifrost in private-chef cookbook

--- files/private-chef-cookbooks/private-chef/recipes/oc_bifrost_disable.rb
+++ files/private-chef-cookbooks/private-chef/recipes/oc_bifrost_disable.rb
@@ -1,11 +1,9 @@
 #
-# Author:: Adam Jacob (<adam@opscode.com>)
 # Copyright:: Copyright (c) 2012 Opscode, Inc.
 #
 # All Rights Reserved
 #
 
-runit_service "opscode-authz" do
+runit_service "oc_bifrost" do
   action :disable
 end
-
