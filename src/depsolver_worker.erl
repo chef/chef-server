ad6765711f4511fb0e611cfadb5b6b0dc579188f
Oliver Ferrigni <oliver@opscode.com>
2013-06-06 10:41:54 -0500

    Correct copyrights.

--- src/depsolver_worker.erl
+++ src/depsolver_worker.erl
@@ -1,6 +1,24 @@
+%% -*- erlang-indent-level: 4; indent-tabs-mode: nil; fill-column: 80 -*-
+%% ex: ts=4 sx=4 et
+%%
+%% Copyright 2012 Opscode, Inc. All Rights Reserved.
+%%
+%% This file is provided to you under the Apache License,
+%% Version 2.0 (the "License"); you may not use this file
+%% except in compliance with the License.  You may obtain
+%% a copy of the License at
+%%
+%%   http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing,
+%% software distributed under the License is distributed on an
+%% "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+%% KIND, either express or implied.  See the License for the
+%% specific language governing permissions and limitations
+%% under the License.
+%%
 %%%-------------------------------------------------------------------
-%%% @author Oliver Ferrigni <oliver@opscode.com>
-%%% @copyright (C) 2013, Oliver Ferrigni
+%%% @author Oliver Ferrigni <oliver@opscode.come>
 %%% @doc
 %%%
 %%% @end
