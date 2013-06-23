fb1daa5a616a273d5feb56c7bbd8484d8261ff50
Seth Falcon <seth@opscode.com>
2013-06-23 11:55:13 -0700

    Add with_new_http_client/1 to oc_chef_authz_http
    * Provide the plumbing needed to give callers a means to issue any
    number of requests on a newly established HTTP connection to the
    upstream authz service which will be cleaned up upon completion of the
    callers actions.
    
    * Refactor oc_chef_authz config format for the authz service. Bundle
    authz service root URL and call timeout into a proplist as:
    
        {authz_service, [{root_url, URL}, {timeout, Timeout}]}
    
    * Update the ping function to use the new configuration and take
    advantage of with_new_http_client so that a new HTTP connection is
    used for each ping request.
    
    * Standardize on binary() format for ibrowse respone body return type.
    
    * Enhance logging for failed ping requests.
    
    * Pass req_id as header to upstream in oc_chef_authz_http if present
    The req_id is an identifier for the request chain. If a value other
    than 'undefined' is provided, it will be passed along to the upstream
    authz service in HTTP header X-Request-Id.

--- src/oc_chef_authz_http.erl
+++ src/oc_chef_authz_http.erl
@@ -3,7 +3,7 @@
 %% @author Kevin Smith <kevin@opscode.com>
 %% @doc oc_chef_authz's interface to the authz server
 %%
-%% Copyright 2011-2012 Opscode, Inc. All Rights Reserved.
+%% Copyright 2011-2013 Opscode, Inc. All Rights Reserved.
 %%
 %% This file is provided to you under the Apache License,
 %% Version 2.0 (the "License"); you may not use this file
@@ -25,62 +25,188 @@
 
 -define(x_ops_requester_id, "X-Ops-Requesting-Actor-Id").
 -define(x_ops_user, {"X-Ops-User-Id", "front-end-service"}).
+-define(IBROWSE_OPTIONS, [{response_format, binary}]).
+-define(REQ_ID_HEADER, "X-Request-Id").
 
 -type http_body() :: binary() | pid() | [].
 -type requestor_id() :: binary().
 -type http_method() :: atom().
--type http_path() :: list() | binary().
-
+-type http_path() :: string() | binary().
+-type req_id() :: string() | binary() | undefined.
+-type new_client_fun() :: fun((pid(), string(), non_neg_integer()) -> any()).
 
 -export([ping/0,
-         request/5]).
+         request/5,
+         request/6,
+         request/7,
+         request/8,
+         with_new_http_client/1,
+         with_new_http_client/3]).
 
 %
 % Handle raw interaction with authz; set up the headers as expected.
 %
--spec request(http_path(), http_method(), any(), http_body(), requestor_id()) -> term() | {error, any()}.
+-spec request(http_path(),
+              http_method(),
+              [{string(), string()}],
+              http_body(),
+              requestor_id()) -> ok | {ok, _} | {error, _}.
 request(Path, Method, Headers, Body, RequestorId) ->
-    FullHeaders = [{?x_ops_requester_id, binary_to_list(RequestorId)},
-                   ?x_ops_user,
-                   {"Accept", "application/json"},
-                   {"Content-Type", "application/json"} | Headers
-                  ],
-    {ok, AuthzHost} = application:get_env(oc_chef_authz, authz_root_url),
-    Url = AuthzHost ++ "/" ++ Path,
-    case ibrowse:send_req(Url, FullHeaders, Method, Body) of
-        {ok, "200", _ResponseHeaders, ResponseBody} = _Response ->
-            case ResponseBody of
-                "{}" -> ok;
-                Raw -> Json = jiffy:decode(Raw),
-                       {ok, Json}
+    request(undefined, no_pid, Path, Method, Headers, Body, RequestorId).
+
+-spec request(req_id(),
+              http_path(),
+              http_method(),
+              [{string(), string()}],
+              http_body(),
+              requestor_id()) -> ok | {ok, _} | {error, _}.
+request(ReqId, Path, Method, Headers, Body, RequestorId) ->
+    request(ReqId, no_pid, Path, Method, Headers, Body, RequestorId).
+
+-spec request(req_id(),
+              pid() | 'no_pid',
+              http_path(),
+              http_method(),
+              [{string(), string()}],
+              http_body(),
+              requestor_id()) -> ok | {ok, _} | {error, _}.
+request(ReqId, Pid, Path, Method, Headers, Body, RequestorId) ->
+    FullHeaders = full_headers(ReqId, RequestorId, Headers),
+    {ok, AuthzConfig} = application:get_env(oc_chef_authz, authz_service),
+    AuthzHost = proplists:get_value(root_url, AuthzConfig),
+    Timeout = proplists:get_value(timeout, AuthzConfig),
+    Url = AuthzHost ++ "/" ++ to_str(Path),
+    Response = ibrowse_request(Pid, Url, FullHeaders, Method, Body,
+                               ?IBROWSE_OPTIONS, Timeout),
+    handle_ibrowse_response(Response).
+
+%% @doc Execute request on the HTTP connection managed by `Pid'. Since `Pid' encapsulates
+%% the host and port, the `Path' is used as-is. When making a series of requests against the
+%% same connection, this avoids querying for the root URL and timeout repeatedly.
+-spec request(req_id(),
+              pid(),
+              http_path(),
+              http_method(),
+              [{string(), string()}],
+              http_body(),
+              requestor_id(),
+              non_neg_integer()) -> ok | {ok, _} | {error, _}.
+request(ReqId, Pid, Path, Method, Headers, Body, RequestorId, Timeout) when is_pid(Pid) ->
+    FullHeaders = full_headers(ReqId, RequestorId, Headers),
+    Response = ibrowse_request(Pid, to_str(Path), FullHeaders, Method, Body,
+                               ?IBROWSE_OPTIONS, Timeout),
+    handle_ibrowse_response(Response).
+
+ibrowse_request(no_pid, Url, Headers, Method, Body, Options, Timeout) ->
+    ibrowse:send_req(Url, Headers, Method, Body, Options, Timeout);
+ibrowse_request(Pid, Url, Headers, Method, Body, Options, Timeout) ->
+    ibrowse:send_req_direct(Pid, Url, Headers, Method, Body, Options, Timeout).
+
+
+handle_ibrowse_response({ok, Status, _, ResponseBody}) when Status =:= "200";
+                                                            Status =:= "201" ->
+    handle_response_body(ResponseBody);
+handle_ibrowse_response({ok, "403", _H, _B}) ->
+    {error, forbidden};
+handle_ibrowse_response({ok, "404", _H, _B}) ->
+    {error, not_found};
+handle_ibrowse_response({ok, "500", _H, _B}) ->
+    {error, server_error};
+handle_ibrowse_response({ok, Status, _H, _B}) ->
+    {error, Status};
+handle_ibrowse_response({error, _} = Error) ->
+    Error.
+
+handle_response_body("{}") ->
+    ok;
+handle_response_body(<<"{}">>) ->
+    ok;
+handle_response_body(Body) ->
+    {ok, jiffy:decode(Body)}.
+
+full_headers(ReqId, RequestorId, Headers) ->
+    [{?x_ops_requester_id, binary_to_list(RequestorId)},
+     ?x_ops_user,
+     {"Accept", "application/json"},
+     {"Content-Type", "application/json"} | add_req_id_header(ReqId, Headers)].
+
+add_req_id_header(undefined, Headers) ->
+    Headers;
+add_req_id_header(ReqId, Headers) ->
+    [{?REQ_ID_HEADER, to_str(ReqId)} | Headers].
+
+to_str(S) when is_list(S) ->
+    S;
+to_str(B) when is_binary(B) ->
+    binary_to_list(B).
+
+%% @doc Create a new HTTP connection to the authz service specified in config and execute
+%% `Fun'. See {@link with_new_http_client/3} for details.
+-spec with_new_http_client(new_client_fun()) -> any().
+with_new_http_client(Fun) ->
+    {RootUrl, Timeout} = authz_url_and_timeout(),
+    with_new_http_client(RootUrl, Timeout, Fun).
+
+%% Create a new HTTP connection (backed by `ibrowse_http_client') to the authz service at
+%% `RootUrl' and execute `Fun'. The function `Fun' will be passed three arguments: the `Pid'
+%% of the HTTP connection, `RootUrl', and `Timeout'. The HTTP connection will be closed and
+%% the result of `Fun' returned.
+-spec with_new_http_client(string(), non_neg_integer(), new_client_fun()) -> any().
+with_new_http_client(RootUrl, Timeout, Fun) ->
+    case spawn_http_client(RootUrl) of
+        {ok, Pid} ->
+            try
+                Fun(Pid, RootUrl, Timeout)
+            after
+                stop_http_client(Pid)
             end;
-        {ok, "201", _ResponseHeaders, ResponseBody} = _Response ->
-            case ResponseBody of
-                "{}" -> ok;
-                Raw -> Json = jiffy:decode(Raw),
-                       {ok, Json}
-            end;
-        {ok, "403", _H, _B} -> {error, forbidden};
-        {ok, "404", _H, _B} -> {error, not_found};
-        {ok, "500", _H, _B} -> {error, server_error};
-        {ok, ResponseValue, _ResponseHeaders, _ResponseBody} = _Response ->
-            {error, ResponseValue};
-        {error, Reason} ->
-            {error, Reason}
+        {error, Why} ->
+            {error, Why}
+    end.
+
+%% @doc Create a new HTTP connection to the upstream authz service located at
+%% `RootUrl'. When finished making requests, call {@link stop_http_client/1} to cleanup.
+spawn_http_client(RootUrl) ->
+    ibrowse_http_client:start_link(RootUrl).
+
+%% @doc Close and stop an HTTP connection started via {@link spawn_http_client/0}. Allows
+%% 200 ms for a clean shutdown before sending a kill message.
+stop_http_client(Pid) ->
+    case catch gen_server:call(Pid, stop, 200) of
+        {'EXIT', {timeout, _}} ->
+            exit(Pid, kill),
+            ok;
+        _ ->
+            ok
     end.
 
 -spec ping() -> pong | pang.
 ping() ->
+    {AuthzUrl, Timeout} = authz_url_and_timeout(),
+    with_new_http_client(AuthzUrl, Timeout, fun ping/3).
+
+ping(Pid, AuthzUrl, Timeout) ->
     try
-        {ok, AuthzUrl} = application:get_env(oc_chef_authz, authz_root_url),
         Url = AuthzUrl ++ "/_ping",
         Headers = [{"Accept", "application/json"}],
-        case ibrowse:send_req(Url, Headers, get) of
-            {ok, "200", _H, _B} -> pong;
-            _Else -> pang
+        case ibrowse_request(Pid, Url, Headers, get, [], ?IBROWSE_OPTIONS, Timeout) of
+            {ok, "200", _H, _B} ->
+                pong;
+            {ok, Status, _H, _B} ->
+                error_logger:error_report({oc_chef_authz_http_ping, pang, Status}),
+                pang;
+            {error, Why} ->
+                error_logger:error_report({oc_chef_authz_http_ping, pang, Why}),
+                pang
         end
     catch
-        How:Why ->
-            error_logger:error_report({oc_chef_authz_http, ping, How, Why}),
+        _How:Error ->
+            error_logger:error_report({oc_chef_authz_http_ping, pang, Error}),
             pang
     end.
+
+authz_url_and_timeout() ->
+    {ok, Config} = application:get_env(oc_chef_authz, authz_service),
+    Url = proplists:get_value(root_url, Config),
+    Timeout = proplists:get_value(timeout, Config),
+    {Url, Timeout}.
