4c6f54c97da0bde718409e0e7c3b31792fc29e65
Seth Chisamore <schisamo@opscode.com>
2013-06-27 14:23:42 -0400

    switch to community `runit` cookbook! \o/
    * pull in the cookbook from the community site at package build time
      using Berkshelf.
    * render chef-solo config and json files in software definition

--- config/software/private-chef-cookbooks.rb
+++ config/software/private-chef-cookbooks.rb
@@ -1,11 +1,32 @@
 name "private-chef-cookbooks"
 
-dependency "rsync"
+dependency "berkshelf"
 
-source :path => File.expand_path("files/private-chef-cookbooks", Omnibus.project_root)
+project_name = project.name
+
+source :path => File.expand_path("files/#{project_name}-cookbooks/#{project_name}", Omnibus.project_root)
 
 build do
   gem "install uuidtools --no-rdoc --no-ri -v 2.1.3"
   command "mkdir -p #{install_dir}/embedded/cookbooks"
-  command "#{install_dir}/embedded/bin/rsync --delete -a ./ #{install_dir}/embedded/cookbooks/"
+  command "#{install_dir}/bin/berks install -c ./Berksfile --path=#{install_dir}/embedded/cookbooks",
+          :env => { "RUBYOPT"         => nil,
+                    "BUNDLE_BIN_PATH" => nil,
+                    "BUNDLE_GEMFILE"  => nil,
+                    "GEM_PATH"        => nil,
+                    "GEM_HOME"        => nil }
+  block do
+    File.open("#{install_dir}/embedded/cookbooks/dna.json", "w") do |f|
+      f.puts "{\"run_list\": [ \"recipe[#{project_name}]\" ]}"
+    end
+    File.open("#{install_dir}/embedded/cookbooks/show-config.json", "w") do |f|
+      f.puts "{\"run_list\": [ \"recipe[#{project_name}::show_config]\" ]}"
+    end
+    File.open("#{install_dir}/embedded/cookbooks/solo.rb", "w") do |f|
+      f.puts "CURRENT_PATH = File.expand_path(File.dirname(__FILE__))"
+      f.puts "file_cache_path \"\#\{CURRENT_PATH\}/cache\""
+      f.puts "cookbook_path CURRENT_PATH"
+      f.puts "verbose_logging true"
+    end
+  end
 end
--- files/private-chef-cookbooks/private-chef/metadata.rb
+++ files/private-chef-cookbooks/private-chef/metadata.rb
@@ -1,3 +1,4 @@
+name "private-chef"
 maintainer        "Opscode, Inc."
 maintainer_email  "cookbooks@opscode.com"
 license           "Apache 2.0"
