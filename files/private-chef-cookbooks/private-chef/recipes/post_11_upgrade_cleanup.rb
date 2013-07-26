e0e8d6dbe483430c9f8a2b03aea4a321ddfda58e
Christopher Maier <cm@opscode.com>
2013-07-26 16:14:54 -0400

    Clean up after Nagios & Co.

--- files/private-chef-cookbooks/private-chef/recipes/post_11_upgrade_cleanup.rb
+++ files/private-chef-cookbooks/private-chef/recipes/post_11_upgrade_cleanup.rb
@@ -16,3 +16,36 @@ private_chef_package_cleaner "opscode-authz" do
   files ["/var/opt/opscode/couchdb/db/authorization.couch",
          "/var/opt/opscode/couchdb/db/authorization_design_documents.couch"]
 end
+
+private_chef_package_cleaner "nagios" do
+  directories ["/opt/opscode/embedded/nagios",
+               "/var/opt/opscode/nagios",
+               "/var/log/opscode/nagios"]
+  users ["opscode-nagios",
+         "opscode-nagios-cmd"]
+  groups ["opscode-nagios",
+          "opscode-nagios-cmd"]
+  files ["/var/log/opscode/nginx/nagios.access.log",
+         "/var/log/opscode/nginx/nagios.error.log",
+         "/etc/opscode/logrotate.d/nagios"]
+end
+
+private_chef_package_cleaner "nrpe" do
+  directories ["/var/opt/opscode/nrpe",
+               "/var/log/opscode/nrpe"]
+end
+
+private_chef_package_cleaner "fastcgi" do
+  # There isn't really a "service", per se for this
+  is_service false
+  files ["/opt/opscode/embedded/conf/fastcgi.conf",
+         "/var/opt/opscode/nginx/etc/fastcgi.conf"]
+end
+
+private_chef_package_cleaner "fcgiwrap" do
+  directories ["/var/log/opscode/fcgiwrap"]
+end
+
+private_chef_package_cleaner "php-fpm" do
+  directories ["/var/log/opscode/php-fpm"]
+end
