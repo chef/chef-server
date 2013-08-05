20f37057f665b34c6b2ed3fefa11eaa1f9e59bf8
Christopher Maier <cm@opscode.com>
2013-08-05 07:16:19 -0400

    Automated script for generating tech-preview wiki page
    This automates the generation of timed-expiration S3 links to various
    tech preview packages for all our products.  Given AWS credentials, it
    will generate the complete Confluence wiki markup to replace the
    entire contents of
    http://wiki.corp.opscode.com/display/CORP/Private+Chef+11+Preview+Builds
    with new links that are good for one week.
    
    It is designed to be simple to maintain and use, since doing this
    stuff by hand is a special kind of hell.
    
    Although this deals with packages other than just Private Chef,
    putting this code in this repository seemed better than creating a new
    repository for it (too heavy-weight) or sticking it in a Gist (easy to
    "lose").

---
+++ package_links/generate_wiki_page.rb
@@ -0,0 +1,185 @@
+#!/usr/bin/env ruby
+
+require 'aws'
+require 'erubis'
+
+module OCTechPreview
+
+  class WikiPage
+    def initialize(preview_info)
+      @preview_info = preview_info
+    end
+
+    def text
+      t = Erubis::Eruby.new(template)
+      data = {:info => @preview_info.as_hash, :days_valid => @preview_info.days_valid}
+      t.result(data)
+    end
+
+    def template
+      File.read("confluence_page.erb")
+    end
+  end
+
+  class URLGenerator
+    attr_reader :days_valid
+
+    def initialize(id, secret, bucket, days_valid=7)
+      @id, @secret, @bucket, @days_valid = id, secret, bucket, days_valid
+      @config = AWS.config(:access_key_id => @id, :secret_access_key => @secret, :region => 'us-west-2')
+      @s3 = AWS::S3.new
+    end
+
+    def url_for(package)
+      o = @s3.buckets[@bucket].objects[package]
+      o.url_for(:get, :expires => (60 * 60 * 24 * @days_valid))
+    end
+  end
+
+  class PreviewInfo
+    def initialize(packages, url_generator)
+      @packages = packages
+      @url_generator = url_generator
+    end
+
+    def days_valid
+      @url_generator.days_valid
+    end
+
+    def as_hash
+      @hash ||= begin
+                  @packages.inject({}) do |hash, package|
+                    info = PackageInfo.new(package)
+                    url = @url_generator.url_for(package)
+                    entry = {info.product => [[info.label, url]]}
+                    hash.merge!(entry) do |key, oldval, newval|
+                      oldval << newval.first
+                    end
+                    hash
+                  end
+                end
+    end
+  end
+
+  class PackageInfo
+    attr_reader :package
+
+    def initialize(package)
+      @package = package
+    end
+
+    def label
+      l = platform
+      l << " (#{architecture})" if architecture
+      l
+    end
+
+    def platform
+      case @package
+      when /\.el5\./
+        "CentOS 5"
+      when /\.el6\./
+        "Centos 6"
+      when /ubuntu\.10\.04/
+        "Ubuntu 10.04"
+      when /ubuntu\.11\.04/
+        "Ubuntu 11.04"
+      when /windows\.msi/
+        "Windows"
+      when /\.gem/
+        "All Platforms"
+      else
+        raise "Unrecognized package format for '#{@package}; could not determine platform!"
+      end
+    end
+
+    # @returns [String, nil] nil if no architecture is detected (this
+    # happens with e.g., Windows and Gem packages, and is not an
+    # error)
+    def architecture
+      case @package
+      when /x86_64/, /amd64/
+        "64-bit"
+      when /i686/, /i386/
+        "32-bit"
+      end
+    end
+
+    def product
+      case @package
+      when /private-chef/
+        :private_chef
+      when /opscode-reporting/
+        :reporting
+      when /knife-reporting/
+        :knife_reporting
+      when /opscode-push-jobs-server/
+        :push_jobs_server
+      when /opscode-push-jobs-client/
+        :push_jobs_client
+      when /knife-pushy/
+        :knife_push_jobs
+      when /opscode-webui/
+        :webui
+      else
+        raise "Unrecognized package format for '#{@package}; could not determine product!"
+      end
+    end
+
+  end
+end
+
+if __FILE__ == $0
+
+  BUCKET_NAME = "opc11-tech-preview"
+  DAYS_VALID  = 7
+
+  id     = ENV['AWS_ACCESS_KEY_ID']     || raise("No AWS_ACCESS_KEY_ID environment variable set!")
+  secret = ENV['AWS_SECRET_ACCESS_KEY'] || raise("No AWS_SECRET_ACCESS_KEY environment variable set!")
+
+  packages = [
+              # Private Chef
+              "private-chef-11.0.0_tech.preview.2+20130624205025-1.el5.x86_64.rpm",
+              "private-chef-11.0.0_tech.preview.2+20130624205025-1.el6.x86_64.rpm",
+              "private-chef_11.0.0-tech.preview.2+20130624205025-1.ubuntu.10.04_amd64.deb",
+              "private-chef_11.0.0-tech.preview.2+20130624205025-1.ubuntu.11.04_amd64.deb",
+
+              # Reporting
+              "opscode-reporting-0.2.0_tech.preview.2-1.el5.x86_64.rpm",
+              "opscode-reporting-0.2.0_tech.preview.2-1.el6.x86_64.rpm",
+              "opscode-reporting_0.2.0-tech.preview.2-1.ubuntu.10.04_amd64.deb",
+              "opscode-reporting_0.2.0-tech.preview.2-1.ubuntu.11.04_amd64.deb",
+
+              "knife-reporting-0.1.0.gem",
+
+              # Pushy
+              "opscode-push-jobs-server-0.9.0_tech.preview.1-1.el5.x86_64.rpm",
+              "opscode-push-jobs-server-0.9.0_tech.preview.1-1.el6.x86_64.rpm",
+              "opscode-push-jobs-server_0.9.0-tech.preview.1-1.ubuntu.10.04_amd64.deb",
+              "opscode-push-jobs-server_0.9.0-tech.preview.1-1.ubuntu.11.04_amd64.deb",
+
+              "opscode-push-jobs-client-0.0.1+20130307153525.git.98.c04f587-1.windows.msi",
+              "opscode-push-jobs-client-0.9.0_tech.preview.1+20130621231152.git.1.b4a8992-1.el5.i686.rpm",
+              "opscode-push-jobs-client-0.9.0_tech.preview.1+20130621231152.git.1.b4a8992-1.el5.x86_64.rpm",
+              "opscode-push-jobs-client-0.9.0_tech.preview.1+20130621231152.git.1.b4a8992-1.el6.i686.rpm",
+              "opscode-push-jobs-client-0.9.0_tech.preview.1+20130621231152.git.1.b4a8992-1.el6.x86_64.rpm",
+              "opscode-push-jobs-client_0.9.0-tech.preview.1+20130621231152.git.1.b4a8992-1.ubuntu.10.04_i386.deb",
+              "opscode-push-jobs-client_0.9.0-tech.preview.1+20130621231152.git.1.b4a8992-1.ubuntu.10.04_amd64.deb",
+              "opscode-push-jobs-client_0.9.0-tech.preview.1+20130621231152.git.1.b4a8992-1.ubuntu.11.04_i386.deb",
+              "opscode-push-jobs-client_0.9.0-tech.preview.1+20130621231152.git.1.b4a8992-1.ubuntu.11.04_amd64.deb",
+
+              "knife-pushy-0.1.gem",
+
+              # Web UI
+              "opscode-webui-0.1.0_tech.preview.3-1.el5.x86_64.rpm",
+              "opscode-webui-0.1.0_tech.preview.3-1.el6.x86_64.rpm",
+              "opscode-webui_0.1.0-tech.preview.3-1.ubuntu.10.04_amd64.deb",
+              "opscode-webui_0.1.0-tech.preview.3-1.ubuntu.11.04_amd64.deb"
+             ]
+
+  url_generator = OCTechPreview::URLGenerator.new(id, secret, BUCKET_NAME, DAYS_VALID)
+  preview_info  = OCTechPreview::PreviewInfo.new(packages, url_generator)
+  wiki_page     = OCTechPreview::WikiPage.new(preview_info)
+
+  puts wiki_page.text
+end
