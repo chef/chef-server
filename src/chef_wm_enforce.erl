8177df8b593c3f90cbe70f4d53f3e6a475061e3b
Seth Falcon <seth@opscode.com>
2013-08-29 10:35:08 -0700

    Add standard header and a small bit of docs for chef_wm_enforce

--- src/chef_wm_enforce.erl
+++ src/chef_wm_enforce.erl
@@ -1,3 +1,29 @@
+%% -*- erlang-indent-level: 4;indent-tabs-mode: nil; fill-column: 92-*-
+%% ex: ts=4 sw=4 et
+%% Copyright 2013 Opscode, Inc. All Rights Reserved.
+%%
+%% This file is provided to you under the Apache License,
+%% Version 2.0 (the "License"); you may not use this file
+%% except in compliance with the License.  You may obtain
+%% a copy of the License at
+%%
+%%   http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing,
+%% software distributed under the License is distributed on an
+%% "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+%% KIND, either express or implied.  See the License for the
+%% specific language governing permissions and limitations
+%% under the License.
+%%
+%% @doc A helper module for enforcing constraints on wm
+%% requests. Currently, the only constraint is on request body
+%% size. This gets called by {@link
+%% chef_wm_base:malformed_request/2}. Constraint failures are
+%% communicated to callers via `throw' and are otherwise expected to
+%% return a possibly modified request data item (`#wm_reqdata{}').
+%%
+%% @end
 -module(chef_wm_enforce).
 
 -export([
