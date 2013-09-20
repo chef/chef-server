30d18a3925867d99223e0e31a4565924fde43154
Seth Falcon <seth@opscode.com>
2013-09-20 14:53:43 -0700

    Add more functions to the chef_object behavior
    This covers:
    
    * authz_id
    * ejson_for_indexing
    * update_from_ejson
    * set_created
    * set_updated
    
    And new callbacks for chef_db integration that name the prepared query
    for the object:
    
    * create_query
    * update_query
    * delete_query
    * find_query
    * list_query
    * bulk_get_query
    
    These will allow us to share boilerplate code for interacting with the
    db, but keep the db code and this code decoupled in terms of being
    direct dependencies.

--- src/chef_object.erl
+++ src/chef_object.erl
@@ -24,28 +24,57 @@
 -include("chef_types.hrl").
 -include_lib("ej/include/ej.hrl").
 
+-type object_rec() :: tuple().
+
+-callback authz_id(object_rec()) -> object_id().
+-callback ejson_for_indexing(object_rec(), ejson_term()) -> ejson_term().
+-callback update_from_ejson(object_rec(), any()) -> object_rec().
+
+-callback set_created(object_rec(), object_id()) -> object_rec().
+-callback set_updated(object_rec(), object_id()) -> object_rec().
+
+-callback create_query(object_rec()) -> atom().
+-callback update_query(object_rec()) -> atom().
+-callback delete_query(object_rec()) -> atom().
+-callback find_query(object_rec()) -> atom().
+-callback list_query(object_rec()) -> atom().
+-callback bulk_get_query(object_rec()) -> atom().
+
 -callback new_record(OrgId :: object_id(),
                      AuthzId :: object_id() | unset,
                      ObjectEjson :: ejson_term() |
                                     binary() |
                                     {binary(), ejson_term()} |
                                     {ejson_term(), _}) ->
-    tuple().
+    object_rec().
 
--callback name(tuple()) ->
+-callback name(object_rec()) ->
     binary() | {binary(), binary()}.
 
--callback id(tuple()) ->
+-callback id(object_rec()) ->
     object_id().
 
--callback type_name(tuple()) ->
+-callback type_name(object_rec()) ->
     atom().
 
 -export([
+         authz_id/1,
+         set_created/2,
+         set_updated/2,
+         ejson_for_indexing/2,
+         update_from_ejson/2,
          new_record/4,
          name/1,
          id/1,
-         type_name/1
+         type_name/1,
+
+         bulk_get_query/1,
+         create_query/1,
+         delete_query/1,
+         find_query/1,
+         list_query/1,
+         update_query/1
+
         ]).
 
 -spec new_record(RecType :: atom(),
@@ -55,22 +84,71 @@
                                 binary() |
                                 {binary(), ejson_term()} |
                                 {ejson_term(), _}) ->
-                        tuple().
+                        object_rec().
 new_record(RecType, OrgId, AuthzId, ObjectEjson) ->
     RecType:new_record(OrgId, AuthzId, ObjectEjson).
 
--spec name(tuple()) -> binary() | {binary(), binary()}.
+-spec name(object_rec()) -> binary() | {binary(), binary()}.
 name(Rec) ->
     call(Rec, name).
 
--spec id(tuple()) -> object_id().
+-spec id(object_rec()) -> object_id().
 id(Rec) ->
     call(Rec, id).
 
--spec type_name(tuple()) -> atom().
+-spec type_name(object_rec()) -> atom().
 type_name(Rec) ->
     call(Rec, type_name).
 
+-spec authz_id(object_rec()) -> object_id().
+authz_id(Rec) ->
+    call(Rec, authz_id).
+
+-spec ejson_for_indexing(object_rec(), ejson_term()) -> ejson_term().
+ejson_for_indexing(Rec, Data) ->
+    Mod = callback_mod(Rec),
+    Mod:ejson_for_indexing(Rec, Data).
+
+-spec update_from_ejson(object_rec(), any()) -> object_rec().
+update_from_ejson(Rec, Data) ->
+    Mod = callback_mod(Rec),
+    Mod:update_from_ejson(Rec, Data).
+
+-spec set_updated(object_rec(), object_id()) -> object_rec().
+set_updated(Rec, ActorId) ->
+    Mod = callback_mod(Rec),
+    Mod:set_updated(Rec, ActorId).
+
+-spec set_created(object_rec(), object_id()) -> object_rec().
+set_created(Rec, ActorId) ->
+    Mod = callback_mod(Rec),
+    Mod:set_created(Rec, ActorId).
+
+create_query(Rec) ->
+    call(Rec, create_query).
+
+update_query(Rec) ->
+    call(Rec, update_query).
+
+delete_query(Rec) ->
+    call(Rec, delete_query).
+
+find_query(Rec) ->
+    call(Rec, find_query).
+
+list_query(Rec) ->
+    call(Rec, list_query).
+
+bulk_get_query(Rec) ->
+    call(Rec, bulk_get_query).
+
+%% Return the callback module for a given object record type. We're putting the abstraction
+%% in place in case we need to do something other than the identity mapping of record name
+%% to callback module name that we're doing there. If we needed to swap in something else,
+%% we could do that mapping here.
+callback_mod(Rec) ->
+    element(1, Rec).
+
 call(Rec, Fun) ->
-    Mod = element(1, Rec),
+    Mod = callback_mod(Rec),
     Mod:Fun(Rec).
