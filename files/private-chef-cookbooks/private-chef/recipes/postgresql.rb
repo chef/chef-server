cff2615250ec8ce875c8ad96e2c6004fff98f5ca
Christopher Maier <cm@opscode.com>
2013-09-25 20:57:05 -0400

    How do methods work?

--- files/private-chef-cookbooks/private-chef/recipes/postgresql.rb
+++ files/private-chef-cookbooks/private-chef/recipes/postgresql.rb
@@ -117,7 +117,7 @@ end
 # upgrades for new releases of Enterprise Chef.  As a result, we can't
 # just do a check against node['private_chef']['bootstrap']['enable'],
 # which would only run them one time.
-if OmnibusHelper.is_data_master?
+if OmnibusHelper.is_data_master?(node)
   include_recipe "private-chef::erchef_database"
   include_recipe "private-chef::bifrost_database"
 end
