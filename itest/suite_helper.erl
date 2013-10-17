2bb4bd6e7df97f7591bca38e20d2e1336ecccea4
Oliver Ferrigni <oliver@opscode.com>
2013-10-17 11:45:19 -0500

    Move schema to common directory

--- itest/suite_helper.erl
+++ itest/suite_helper.erl
@@ -71,7 +71,7 @@ random_bogus_port() ->
 
 init_pg_db(Config) ->
     Dir = ?config(priv_dir, Config),
-    Schema = filename:join([?config(data_dir, Config), "schema.sql"]),
+    Schema = filename:join([?config(data_dir, Config), "../common/schema.sql"]),
     PgData = filename:join(Dir, "pg_data"),
     PgLog = filename:join(Dir, "pg.log"),
     Port = random_bogus_port(),
