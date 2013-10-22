a2d7e2840968dacf178fe036ac5624d2e234f77a
Christopher Maier <cm@opscode.com>
2013-10-22 16:20:03 -0400

    Whitespace

--- files/private-chef-cookbooks/private-chef/recipes/keepalived.rb
+++ files/private-chef-cookbooks/private-chef/recipes/keepalived.rb
@@ -11,22 +11,22 @@ keepalived_bin_dir = File.join(keepalived_dir, "bin")
 keepalived_log_dir = node['private_chef']['keepalived']['log_directory']
 
 [ keepalived_dir, keepalived_etc_dir, keepalived_bin_dir, keepalived_log_dir ].each do |dir|
-	directory dir do
-		recursive true
-		mode "0755"
-	end
+  directory dir do
+    recursive true
+    mode "0755"
+  end
 end
 
 template File.join(keepalived_etc_dir, "keepalived.conf") do
-	source "keepalived.conf.erb"
-	mode "0644"
+  source "keepalived.conf.erb"
+  mode "0644"
   variables(node['private_chef']['keepalived'].to_hash)
-	notifies :restart, 'runit_service[keepalived]' if OmnibusHelper.should_notify?("kepalived")
+  notifies :restart, 'runit_service[keepalived]' if OmnibusHelper.should_notify?("kepalived")
 end
 
 template File.join(keepalived_bin_dir, "cluster.sh") do
-	source "cluster.sh.erb"
-	mode "0755"
+  source "cluster.sh.erb"
+  mode "0755"
   variables(node['private_chef']['keepalived'].to_hash)
   notifies :restart, 'runit_service[keepalived]' if OmnibusHelper.should_notify?("kepalived")
 end
