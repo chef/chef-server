f87bee8bc5ab115570493da572de93219a023f49
sdelano <stephen@opscode.com>
2013-10-23 23:18:55 -0700

    itest framework for containers

---
+++ itest/mocks/oc_chef_authz.erl
@@ -0,0 +1,33 @@
+%% -*- erlang-indent-level: 4;indent-tabs-mode: nil; fill-column: 92 -*-
+%% ex: ts=4 sw=4 et
+%% @author Stephen Delano <stephen@opscode.com>
+%% Copyright 2013 Opscode, Inc. All Rights Reserved.
+
+-module(oc_chef_authz).
+
+-include("oc_chef_authz/include/oc_chef_authz.hrl").
+
+-export([
+         create_entity_if_authorized/4,
+         delete_resource/3,
+         get_container_aid_for_object/3,
+         is_authorized_on_resource/6,
+         make_context/2
+        ]).
+
+create_entity_if_authorized(_Context, _OrgId, _Creator, _ObjectType) ->
+    <<RandomInt:128>> = crypto:rand_bytes(16),
+    RandomId = iolist_to_binary(io_lib:format("~32.16.0b", [RandomInt])),
+    {ok, RandomId}.
+
+delete_resource(_RequestorId, _ResourceType, _Id) ->
+    ok.
+
+get_container_aid_for_object(_Context, _OrgId, _ObjectType) ->
+    <<"00000000000000000000000000000000">>.
+
+is_authorized_on_resource(_ReqestorId, _ResourceType, _ResourceId, _ActorType, _ActorId, _AccessMethod) ->
+    true.
+
+make_context(_ReqId, _Darklaunch) ->
+    {mock, tuple}.
