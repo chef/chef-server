70ccb322917d99dc3edc74a87ad6c3717b0cd3e8
Christopher Maier <cm@opscode.com>
2013-10-24 15:30:45 -0400

    Remove should_notify?, check_status methods

--- files/private-chef-cookbooks/private-chef/libraries/helper.rb
+++ files/private-chef-cookbooks/private-chef/libraries/helper.rb
@@ -17,16 +17,6 @@ class OmnibusHelper
     File.exists?(bootstrap_sentinel_file)
   end
 
-  def self.should_notify?(service_name)
-    File.symlink?("/opt/opscode/service/#{service_name}") && check_status(service_name)
-  end
-
-  def self.check_status(service_name)
-    o = Mixlib::ShellOut.new("/opt/opscode/bin/private-chef-ctl status #{service_name}")
-    o.run_command
-    o.exitstatus == 0 ? true : false
-  end
-
   # generate a certificate signed by the opscode ca key
   #
   # === Returns
