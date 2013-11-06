885075870733cefd50dc3e1fda88cac6020f683b
Grant Hudgens <grant.hudgens@gmail.com>
2013-11-06 13:53:27 -0600

    Adding a quick sleep before starting the migrate script to give time for all mover services to be running.

--- files/private-chef-upgrades/001/010_chef_mover.rb
+++ files/private-chef-upgrades/001/010_chef_mover.rb
@@ -47,6 +47,7 @@ define_upgrade do
 
     # Restart chef-mover for the duration of the migration
     run_command("private-chef-ctl restart opscode-chef-mover")
+    sleep(60)
 
     # Perform the actual migration
     run_command("/opt/opscode/embedded/bin/escript /opt/opscode/embedded/service/opscode-chef-mover/scripts/migrate")
