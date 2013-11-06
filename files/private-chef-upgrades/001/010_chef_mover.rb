1190809158c26f43249d30a0573e633ef48c1624
Grant Hudgens <grant.hudgens@gmail.com>
2013-11-06 13:50:15 -0600

    Shifting up Mover log clean up in case the upgrade needs to be restarted.

--- files/private-chef-upgrades/001/010_chef_mover.rb
+++ files/private-chef-upgrades/001/010_chef_mover.rb
@@ -38,19 +38,17 @@ define_upgrade do
     # Shut down everything but couch & postgres
     down_services.each{|s| run_command("private-chef-ctl stop #{s}")}
 
+    # Remove any mover log files from a previous run, if they exist.
+    # The log message parser requires a "clean slate".
+    mover_log_file_glob    = "/var/log/opscode/opscode-chef-mover/console.log*"
+    parsed_log_output = "/var/log/opscode/opscode-chef-mover/parsed_console.log"
+    run_command("rm -f #{mover_log_file_glob}")
+    run_command("rm -f #{parsed_log_output}")
+
     # Start chef-mover for the duration of the migration
     run_command("private-chef-ctl start opscode-chef-mover")
 
     # Perform the actual migration
-
-    mover_log_file_glob    = "/var/log/opscode/opscode-chef-mover/console.log*"
-    parsed_log_output = "/var/log/opscode/opscode-chef-mover/parsed_console.log"
-
-    # Remove any mover log files from a previous run, if they exist.
-    # The log message parser requires a "clean slate".
-    run_command("rm -f #{mover_log_file_glob}")
-    run_command("rm -f #{parsed_log_output}")
-
     run_command("/opt/opscode/embedded/bin/escript /opt/opscode/embedded/service/opscode-chef-mover/scripts/migrate")
 
     run_command("./check_logs.rb #{parsed_log_output} #{mover_log_file_glob}",
