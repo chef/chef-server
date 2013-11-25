4d7b422eebbce601f4a0c5e7d3a5fca2b3389df9
Marc Paradise <marc@opscode.com>
2013-11-25 16:49:18 -0500

    only require dual ipv6/4 bindings if this is an ipv6 configuration

--- files/private-chef-cookbooks/private-chef/recipes/sysctl-updates.rb
+++ files/private-chef-cookbooks/private-chef/recipes/sysctl-updates.rb
@@ -4,10 +4,13 @@
 #
 # All Rights Reserved
 
+
 # Typically speaking supported systems will default to this value
 # but we'll want to ensure that it's correct. Many of our services currently rely on
 # automatic dual-binding of listening ports to both ipv6 and ipv4.  This directive ensures
 # the behavior.
+# net.ipv6.bindv6only = 0
+
 
 execute "sysctl-reload" do
   command "/sbin/sysctl -p /etc/sysctl.conf"
@@ -25,4 +28,5 @@ bash "dual ip4/ip6 portbind" do
   EOF
   notifies :run, 'execute[sysctl-reload]', :immediately
   not_if "egrep '^net\.ipv6\.bindv6only = 0' /etc/sysctl.conf"
+  only_if { PrivateChef["use_ipv6"] == true }
 end
