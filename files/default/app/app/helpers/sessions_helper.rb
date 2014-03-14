f4177bd8403e3bf44e8162a25e1c1d1c01f2a5ae
Christian Nunciato <cnunciato@opscode.com>
2014-03-13 21:55:57 -0700

    Session-delete fix

--- files/default/app/app/helpers/sessions_helper.rb
+++ files/default/app/app/helpers/sessions_helper.rb
@@ -23,7 +23,7 @@ module SessionsHelper
 
   def redirect_back_or(default)
     redirect_to(session[:return_to] || default)
-    session.delete[:return_to]
+    session.delete(:return_to)
   end
 
 end
