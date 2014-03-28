chef-boneyard/opscode-omnibus

Protect logfiles against root umask other than 0022

files/private-chef-cookbooks/private-chef/recipes/default.rb
@@ -139,6 +139,13 @@ directory "/var/opt/opscode" do
   action :create
 end
 
+directory "/var/log/opscode" do
+  owner "root"
+  group "root"
+  mode "0755"
+  action :nothing
+end.run_action(:create)
+
 include_recipe "enterprise::runit"
 include_recipe "private-chef::sysctl-updates"
 

chef/chef-server

files/private-chef-cookbooks/private-chef/recipes/default.rb
@@ -139,6 +139,13 @@ directory "/var/opt/opscode" do
   action :create
 end
 
+directory "/var/log/opscode" do
+  owner "opscode"
+  group "opscode"
+  mode "0755"
+  action :create
+end
+
 include_recipe "enterprise::runit"
 include_recipe "private-chef::sysctl-updates"
 