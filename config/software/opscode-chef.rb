d014e63ebcf74fbedb9bafd6f9a8842b533b3f85
Seth Chisamore <schisamo@getchef.com>
2014-04-08 15:00:01 -0400

    Add `libffi` so `ffi` gem stops linking against system lib

--- config/software/opscode-chef.rb
+++ config/software/opscode-chef.rb
@@ -8,14 +8,20 @@ dependencies ["ruby",
               "libxslt",
               "postgresql", # TODO: how do we install just the client library?
               "curl",
-              "rsync"]
+              "rsync",
+              "libffi"]
 
 source :git => "git@github.com:opscode/opscode-chef"
 
 relative_path "opscode-chef"
 
+env = {
+  "CFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
+  "LDFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include"
+}
+
 build do
-  bundle "install --without mysql integration_test dev --path=/opt/opscode/embedded/service/gem"
+  bundle "install --without mysql integration_test dev --path=/opt/opscode/embedded/service/gem", :env => env
   command "mkdir -p #{install_dir}/embedded/service/opscode-chef"
   command "#{install_dir}/embedded/bin/rsync -a --delete --delete-excluded --exclude=.git/*** --exclude=.gitignore --exclude=chef-server-webui/mockups/*** ./ #{install_dir}/embedded/service/opscode-chef/"
 end
