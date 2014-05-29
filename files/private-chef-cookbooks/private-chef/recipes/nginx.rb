2bdae48bdc53251efa9f6c6c7d3c47bb8aa43484
Ho-Sheng Hsiao <hosh@getchef.com>
2014-05-29 16:50:31 -0700

    Added default orgname

--- files/private-chef-cookbooks/private-chef/recipes/nginx.rb
+++ files/private-chef-cookbooks/private-chef/recipes/nginx.rb
@@ -109,6 +109,7 @@ nginx_vars = nginx_vars.merge({ :helper => NginxErb.new(node) })
 lbconf = node['private_chef']['lb'].to_hash.merge(nginx_vars).merge({
   :redis_host => node['private_chef']['redis_lb']['vip'],
   :redis_port => node['private_chef']['redis_lb']['port'],
+  :default_orgname => node['private_chef']['default_orgname'],
   :omnihelper => OmnibusHelper.new(node)
 })
 
