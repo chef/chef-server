d3c2ae09479a1ce3736e1ad7271cf2ce36939cb1
John Keiser <jkeiser@opscode.com>
2014-06-10 11:52:22 -0700

    Don't use $1 in ALTER USER, PGconn doesn't understand it

--- files/private-chef-cookbooks/private-chef/providers/pg_user.rb
+++ files/private-chef-cookbooks/private-chef/providers/pg_user.rb
@@ -15,36 +15,32 @@ action :create do
       user_info = user_info[0]
       changes = [ "Update Postgres user #{new_resource.username}" ]
       sql = ''
-      sql_params = []
       if user_info['usesuper'] != (new_resource.superuser ? 't' : 'f')
         changes << "  Set superuser to #{!!new_resource.superuser}"
         sql << (new_resource.superuser ? ' SUPERUSER' : ' NOSUPERUSER')
       end
       if new_resource.password && user_info['passwd'] != ::PGconn.encrypt_password(new_resource.password, new_resource.username)
         changes << '  Update password'
-        sql << ' ENCRYPTED PASSWORD $1'
-        sql_params << new_resource.password
+        sql << " ENCRYPTED PASSWORD '#{connection.escape(new_resource.password)}'"
       end
       if changes.size > 1
         converge_by changes do
-          connection.exec("ALTER USER #{new_resource.username}#{sql}", sql_params)
+          connection.exec("ALTER USER #{new_resource.username}#{sql}")
         end
       end
     else
       changes = [ "Create Postgres user #{new_resource.username}" ]
       sql = ''
-      sql_params = []
       if new_resource.superuser
         changes << "  Set superuser to #{!!new_resource.superuser}"
         sql << (new_resource.superuser ? ' SUPERUSER' : ' NOSUPERUSER')
       end
       if new_resource.password
         changes << '  Update password'
-        sql << ' ENCRYPTED PASSWORD $1'
-        sql_params << new_resource.password
+        sql << " ENCRYPTED PASSWORD '#{connection.escape(new_resource.password)}'"
       end
       converge_by changes do
-        connection.exec("CREATE USER #{new_resource.username}#{sql}", sql_params)
+        connection.exec("CREATE USER #{new_resource.username}#{sql}")
       end
     end
   end
