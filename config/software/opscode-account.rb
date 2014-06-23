5f0b30a115beef2941a706f1d1ac8cea80cedbd4
Oliver Ferrigni <oliver@opscode.com>
2014-06-23 15:34:34 -0500

    cleanup .git directories for projects using `bundle install`
    Conflicts:
    	config/software/opscode-account.rb
    	config/software/opscode-platform-debug.rb
    	config/software/opscode-test.rb

--- config/software/opscode-account.rb
+++ config/software/opscode-account.rb
@@ -10,8 +10,21 @@ source :git => "git@github.com:opscode/opscode-account"
 
 relative_path "opscode-account"
 
+# Since this project pulls in the pg gem (or depends on something that
+# does) we need to have the pg_config binary on the PATH so the
+# correct library and header locations can be found
+env = {
+  'PATH' => "#{install_dir}/embedded/bin:#{ENV['PATH']}"
+}
+
+bundle_path = "#{install_dir}/embedded/service/gem"
+
 build do
-  bundle "install --without mysql test --path=/opt/opscode/embedded/service/gem"
+  bundle "install --without mysql test --path=#{bundle_path}", :env => env
   command "mkdir -p #{install_dir}/embedded/service/opscode-account"
   command "#{install_dir}/embedded/bin/rsync -a --delete --exclude=.git/*** --exclude=.gitignore ./ #{install_dir}/embedded/service/opscode-account/"
+
+  # cleanup the .git directories in the bundle path before commiting
+  # them as submodules to the git cache
+  command "find #{bundle_path} -type d -name .git | xargs rm -rf"
 end
