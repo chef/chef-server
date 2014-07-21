d565121b78a94deca963f773963da68bc5c29880
Oliver Ferrigni <oliver@opscode.com>
2014-07-21 18:47:49 -0500

    Test to prove eredis client code is executed

---
+++ test/mover_eredis_sup_test.erl
@@ -0,0 +1,16 @@
+-module(mover_eredis_sup_test).
+
+-include_lib("hoax/include/hoax.hrl").
+-include_lib("eunit/include/eunit.hrl").
+
+-compile(export_all).
+
+?HOAX_FIXTURE.
+
+%% This test is verifying that the eredis_client start_link -> init code path
+%% is traversed.  Previously, there was api drift in eredis that prevented
+%% the start_link path from being followed.
+start_link_should_start_client() ->
+    process_flag(trap_exit, true),
+    ?assertEqual({error, {connection_error, {connection_error,nxdomain}}}, mover_eredis_sup:eredis_start_link(host, port)).
+            
