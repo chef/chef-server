086f2c52221e4fdb8cc2a0c160097e14e0161dbc
Seth Vargo <sethvargo@gmail.com>
2014-08-20 16:06:25 -0400

    Update bookshelf to use the new Omnibusisms

--- config/software/bookshelf.rb
+++ config/software/bookshelf.rb
@@ -1,24 +1,33 @@
+#
+# Copyright 2012-2014 Chef Software, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 name "bookshelf"
 default_version "1.1.4"
 
+source git: "git://github.com/opscode/bookshelf.git"
+
 dependency "erlang"
 dependency "rebar"
-dependency "rsync"
-
-source :git => "git://github.com/opscode/bookshelf.git"
-
-relative_path "bookshelf"
-
-env = {
-  "PATH" => "#{install_dir}/embedded/bin:#{ENV["PATH"]}",
-  "LDFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
-  "LD_RUN_PATH" => "#{install_dir}/embedded/lib"
-}
 
 build do
-  command "make distclean", :env => env
-  command "make rel", :env => env
-  command "mkdir -p #{install_dir}/embedded/service/bookshelf"
-  command "#{install_dir}/embedded/bin/rsync -a --delete ./rel/bookshelf/ #{install_dir}/embedded/service/bookshelf/"
-  command "rm -rf #{install_dir}/embedded/service/bookshelf/log"
+  env = with_standard_compiler_flags(with_embedded_path)
+
+  make "distclean", env: env
+  make "rel", env: env
+
+  sync "#{project_dir}/rel/bookshelf/", "#{install_dir}/embedded/service/bookshelf/"
+  delete "#{install_dir}/embedded/service/bookshelf/log"
 end
