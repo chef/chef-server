99586716904a3e0ed531a70c3d9a1b5baeaa6998
mmzyk <mmzyk@opscode.com>
2014-08-21 14:23:41 -0400

    Add partybus migration update db schema to 2.4.0
    This adds org association and org user tables.
    It also adds the OSC hash password types to the password_hash_type enum.
    This is in support of upgrades from OSC.

---
+++ files/private-chef-upgrades/001/016_add_org_tables_osc_hash_types.rb
@@ -0,0 +1,14 @@
+define_upgrade do
+
+  if Partybus.config.bootstrap_server
+
+    must_be_data_master
+
+    # run 2.4.0 migrations to update db - includes adding org association/user tables
+    # and adding the OSC password hash types to the password_hash_type_enum
+    run_command("make deploy",
+                :cwd => "/opt/opscode/embedded/service/enterprise-chef-server-schema",
+                :env => {"EC_TARGET" => "@2.4.0", "OSC_TARGET" => "@1.0.4", "DB_USER" => "opscode-pgsql"}
+                )
+  end
+end
