6391376769b151399abe4a1a3f2213aa35aab952
mmzyk <mmzyk@opscode.com>
2014-08-21 14:52:33 -0400

    Make sure postgres is running when doing the db migration

--- files/private-chef-upgrades/001/016_add_org_tables_osc_hash_types.rb
+++ files/private-chef-upgrades/001/016_add_org_tables_osc_hash_types.rb
@@ -4,11 +4,15 @@ define_upgrade do
 
     must_be_data_master
 
+    start_service('postgresql')
+
     # run 2.4.0 migrations to update db - includes adding org association/user tables
     # and adding the OSC password hash types to the password_hash_type_enum
     run_command("make deploy",
                 :cwd => "/opt/opscode/embedded/service/enterprise-chef-server-schema",
                 :env => {"EC_TARGET" => "@2.4.0", "OSC_TARGET" => "@1.0.4", "DB_USER" => "opscode-pgsql"}
                 )
+
+    stop_service('postgresql')
   end
 end
