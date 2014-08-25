584ef5cbdc35e0418ad92fcf3c013bb46e8f91bc
Douglas Triggs <doug@opscode.com>
2014-08-25 11:22:37 -0600

    Initial non-working changes

--- files/private-chef-cookbooks/private-chef/recipes/bootstrap.rb
+++ files/private-chef-cookbooks/private-chef/recipes/bootstrap.rb
@@ -6,7 +6,6 @@
 #
 
 require 'securerandom'
-
 opscode_test_dir = "/opt/opscode/embedded/service/opscode-test"
 opscode_test_config_dir = "/opt/opscode/embedded/service/opscode-test/bootstrapper-config"
 
@@ -60,3 +59,63 @@ file OmnibusHelper.bootstrap_sentinel_file do
   mode "0600"
   content "You've been bootstrapped, punk. Delete me if you feel lucky. Do ya, Punk?"
 end
+
+# TODO: way to override versions?
+packages = [["opscode-analytics", "1.0.0-1"],
+  ["opscode-push-jobs-server", "1.1.2-1"],
+  ["opscode-reporting", "1.1.5-1"],
+  ["opscode-manage", "1.5.4-1"]]
+
+case node['platform_family']
+when 'debian'
+
+  packages.each do |package_params|
+
+    name = package_params[0]
+    file = "#{name}_#{package_params[1]}_amd64.deb"
+
+    # TODO: file installation fallback?
+    # TODO: file installation directory parameter?
+    apt_repository name do
+      # TODO: is there a better URI for this?
+      uri "https://packagecloud.io/chef/test-stable/#{file}"
+#      key 'http://apt.opscode.com/packages@opscode.com.gpg.key'
+      distribution node['lsb']['codename']
+#      deb_src true
+      trusted true
+      components %w( main )
+      not_if { OmnibusHelper.has_been_bootstrapped? }
+    end
+  end
+
+  # Performs an apt-get update
+  include_recipe 'apt::default'
+ 
+when 'rhel'
+ 
+  packages.each do |package_params|
+
+    name = package_params[0]
+    # TODO: replace el5 with correct version:
+    file = "#{name}-#{package_params[1]}.el5.x86_64.rpm"
+
+    # TODO: file installation fallback?
+    # TODO: file installation directory parameter?
+    yum_repository name do
+      description name
+      baseurl 'https://packagecloud.io/chef/test-stable/#{file}'
+#      gpgkey 'http://apt.opscode.com/packages@opscode.com.gpg.key'
+      sslverify true
+#      gpgcheck true
+      action :create
+      not_if { OmnibusHelper.has_been_bootstrapped? }
+    end
+  end
+
+else
+  # TODO: probably don't want to fail?  Say, on any platform where this would have
+  # to be done manually.
+  raise "I don't know how to install addons for platform family: #{node['platform_family']}"
+end
+ 
+package 'opscode-manage'
