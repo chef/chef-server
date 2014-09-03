b83932379abc6f57371f8e700e2b665913246c31
mmzyk <mmzyk@opscode.com>
2014-09-03 17:32:28 -0400

    Add org name validation to standalone data transform upgrade command

--- files/private-chef-ctl-commands/chef12_upgrade_data_transform.rb
+++ files/private-chef-ctl-commands/chef12_upgrade_data_transform.rb
@@ -51,7 +51,11 @@ add_command "chef12-upgrade-data-transform", "Transfrom data from an open source
   key_file = "#{chef11_data_dir}/key_dump.json"
 
   chef11_upgrade = OpenSourceChef11Upgrade.new(@options, self)
+
+  org_name, org_full_name = chef11_upgrade.determine_org_name
+  chef11_upgrade.validate_org_names(org_name, org_full_name)
+
   chef12_data_dir = chef11_upgrade.determine_chef12_data_dir
-  chef11_upgrade.transform_chef11_data(chef11_data_dir, key_file, chef12_data_dir)
+  chef11_upgrade.transform_chef11_data(chef11_data_dir, key_file, chef12_data_dir, org_name, org_full_name)
 
 end
