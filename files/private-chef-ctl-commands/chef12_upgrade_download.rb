b77b94da9d9b2378c5caa558638827b886d53dba
mmzyk <mmzyk@opscode.com>
2014-09-02 21:27:23 -0400

    Add user, key & updated url options to upgrade download command
    This adds the ability to specify the Chef 11 user and key to use for
    data downloads to the individual upgrade download command.
    
    This also include the updated Chef 11 url option

--- files/private-chef-ctl-commands/chef12_upgrade_download.rb
+++ files/private-chef-ctl-commands/chef12_upgrade_download.rb
@@ -8,13 +8,15 @@ require "/opt/opscode/embedded/service/omnibus-ctl/open_source_chef12_upgrade"
 require 'optparse'
 require 'ostruct'
 
-add_command "chef12-upgrade-download", "Download data from a ope source Chef 11 server.", 2 do
+add_command "chef12-upgrade-download", "Download data from a open source Chef 11 server.", 2 do
 
    def parse(args)
     @options = OpenStruct.new
 
     # Define defaults
-    @options.chef_server_url = "https://localhost"
+    @options.chef11_server_url = "https://localhost"
+    @options.chef11_admin_client_name = "admin"
+    @options.chef11_admin_client_key = "/etc/chef-server/admin.pem"
 
     opt_parser = OptionParser.new do |opts|
       opts.banner = "Usage: private-chef-ctl chef12-upgrade-download [options]"
@@ -23,8 +25,19 @@ add_command "chef12-upgrade-download", "Download data from a ope source Chef 11 
         @options.chef11_data_dir = chef11_dir
       end
 
-      opts.on("-s", "--chef-server-url [url]", String, "The url of the open source Chef 11 server.  Defaults to #{@options.chef_server_url}") do |u|
-         @options.chef_server_url = u
+      # This option matches the knife -s option
+      opts.on("-s", "--chef11-server-url [url]", String, "The url of the open source Chef 11 server.  Defaults to #{@options.chef11_server_url}") do |url|
+         @options.chef11_server_url = url
+      end
+
+      # This option matches the knife -u option
+      opts.on("-u", "--user", String, "Chef 11 API client user. This is the admin user who will be used to download the Chef 11 data. Should match with the key specified.") do |user|
+        @options.chef11_admin_client_name = user
+      end
+
+      # This option matches the knife -k option
+      opts.on("-k", "--key", String, "Chef 11 API client key. This is the admin key that will be sued to download the Chef 11 data. Should match with the user specified.") do |key|
+        @options.chef11_admin_client_key = key
       end
 
       opts.on("-h", "--help", "Show this message") do
