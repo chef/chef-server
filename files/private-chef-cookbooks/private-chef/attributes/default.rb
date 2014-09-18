451709afd8a8f959caa368ae54a0b9155dc68d9f
Douglas Triggs <doug@opscode.com>
2014-09-18 11:20:26 -0600

    Added push jobs back to packages since dependencies fixed

--- files/private-chef-cookbooks/private-chef/attributes/default.rb
+++ files/private-chef-cookbooks/private-chef/attributes/default.rb
@@ -44,7 +44,7 @@ default['private_chef']['default_orgname'] = nil
 default['private_chef']['addons']['install'] = false
 default['private_chef']['addons']['path'] = nil
 default['private_chef']['addons']['packages'] =
-  %w{opscode-reporting opscode-manage opscode-analytics}
+  %w{opscode-reporting opscode-manage opscode-analytics opscode-push-jobs-server}
 # TODO: push jobs server currently failing dependency check; replace above with
 # below once that's fixed and a new version is available
 #  %w{opscode-push-jobs-server opscode-reporting opscode-manage opscode-analytics}
