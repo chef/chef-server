f88deea47f0e5dced8324a2cd5b2695d47c765a8
Seth Chisamore <schisamo@getchef.com>
2014-09-22 17:09:06 -0400

    Update postgresql92 to use the new Omnibusisms

--- config/software/postgresql92.rb
+++ config/software/postgresql92.rb
@@ -1,6 +1,5 @@
 #
-# Copyright:: Copyright (c) 2012-2013 Opscode, Inc.
-# License:: Apache License, Version 2.0
+# Copyright 2012-2014 Chef Software, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -18,43 +17,39 @@
 name "postgresql92"
 default_version "9.2.9"
 
+source url: "http://ftp.postgresql.org/pub/source/v9.2.9/postgresql-9.2.9.tar.bz2",
+       md5: "38b0937c86d537d5044c599273066cfc"
+
 dependency "zlib"
 dependency "openssl"
 dependency "libedit"
 dependency "ncurses"
 dependency "libossp-uuid"
 
-source :url => "http://ftp.postgresql.org/pub/source/v9.2.9/postgresql-9.2.9.tar.bz2",
-       :md5 => "38b0937c86d537d5044c599273066cfc"
-
 relative_path "postgresql-9.2.9"
 
-configure_env = {
-  "LDFLAGS" => "-Wl,-rpath,#{install_dir}/embedded/lib,-rpath,#{install_dir}/embedded/postgresql/9.2/lib -L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
-  "CFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
-  "LD_RUN_PATH" => "#{install_dir}/embedded/lib"
-}
-
 build do
-  command ["./configure",
-           "--prefix=#{install_dir}/embedded/postgresql/9.2",
-           "--with-libedit-preferred",
-           "--with-openssl",
-           "--with-ossp-uuid",
-           "--with-includes=#{install_dir}/embedded/include",
-           "--with-libraries=#{install_dir}/embedded/lib"].join(" "), :env => configure_env
-  command "make world -j #{max_build_jobs}", :env => {"LD_RUN_PATH" => "#{install_dir}/embedded/lib"}
-  command "make install-world"
+  env = with_standard_compiler_flags(with_embedded_path)
+
+  command "./configure" \
+          " --prefix=#{install_dir}/embedded/postgresql/9.2" \
+          " --with-libedit-preferred" \
+          " --with-openssl" \
+          " --with-ossp-uuid" \
+          " --with-includes=#{install_dir}/embedded/include" \
+          " --with-libraries=#{install_dir}/embedded/lib", env: env
+
+  make "world -j #{workers}", env: env
+  make "install-world -j #{workers}", env: env
+
 
   # Postgres 9.2 is our "real" Postgres installation (prior versions
   # that are installed are solely to facilitate upgrades).  As a
   # result, we need to have the binaries for this version available
-  # with the other binaries used by Private Chef.  This one-liner is
-  # probably the easiest way to do that.
-  #
-  # NOTE: The single-quotes wrapping the inner command are required;
-  # otherwise, we end up getting some kind of weird Inception-style
-  # variable interpolation, such that the value of $BIN in each
-  # iteration of the loop is the last file in the directory.
- command "sh -c 'for BIN in #{install_dir}/embedded/postgresql/9.2/bin/*; do ln -s ${BIN} #{install_dir}/embedded/bin/$(basename ${BIN}); done'"
+  # with the other binaries used by Private Chef.
+  block do
+    Dir.glob("#{install_dir}/embedded/postgresql/9.2/bin/*").sort.each do |bin|
+      link bin, "#{install_dir}/embedded/bin/#{File.basename(bin)}"
+    end
+  end
 end
