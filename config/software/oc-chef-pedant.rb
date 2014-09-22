af7a7a779922fa069c7be365c6691c718af72fc6
Seth Chisamore <schisamo@getchef.com>
2014-09-22 17:07:19 -0400

    Update oc-chef-pedant to use the new Omnibusisms

--- config/software/oc-chef-pedant.rb
+++ config/software/oc-chef-pedant.rb
@@ -1,37 +1,33 @@
+#
+# Copyright 2012-2014 Chef Software, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 name "oc-chef-pedant"
 default_version "1.0.68"
 
+source git: "git@github.com:opscode/oc-chef-pedant.git"
+
 dependency "ruby"
 dependency "bundler"
-dependency "rsync"
-
-source :git => "git@github.com:opscode/oc-chef-pedant.git"
-
-relative_path "oc-chef-pedant"
-
-required_files = [
-                  'HEAD',
-                  'description',
-                  'hooks',
-                  'info',
-                  'objects',
-                  'refs',
-                  ]
-
-bundle_path = "#{install_dir}/embedded/service/gem"
 
 build do
-  bundle "install --path=#{bundle_path}"
-  command "mkdir -p #{install_dir}/embedded/service/oc-chef-pedant"
-  command "#{install_dir}/embedded/bin/rsync -a --delete --exclude=.git/*** --exclude=.gitignore ./ #{install_dir}/embedded/service/oc-chef-pedant/"
+  env = with_standard_compiler_flags(with_embedded_path)
 
-  # cleanup the .git directories in the bundle path before commiting
-  # them as submodules to the git cache
-  Dir.glob("#{install_dir}/**/config").reject{ |path|
-    required_files.any? { |required_file|
-      !File.exists? File.join(File.dirname(path), required_file)
-    }
-  }.each { |path|
-    FileUtils.rm_rf File.dirname(path)
-  }
+  bundle "install --path=#{install_dir}/embedded/service/gem", env: env
+
+  command "mkdir -p #{install_dir}/embedded/service/oc-chef-pedant"
+
+  sync project_dir, "#{install_dir}/embedded/service/oc-chef-pedant/"
 end
