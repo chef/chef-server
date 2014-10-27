caffc54d0577329ad16cb008dc94041a774cee24
Douglas Triggs <doug@opscode.com>
2014-10-27 13:05:51 -0600

    Changes to default ubuntu distribution to lucid when distro not found

--- files/private-chef-cookbooks/private-chef/attributes/default.rb
+++ files/private-chef-cookbooks/private-chef/attributes/default.rb
@@ -45,9 +45,11 @@ default['private_chef']['addons']['install'] = false
 default['private_chef']['addons']['path'] = nil
 default['private_chef']['addons']['packages'] =
   %w{opscode-reporting opscode-manage opscode-analytics opscode-push-jobs-server}
-# TODO: push jobs server currently failing dependency check; replace above with
-# below once that's fixed and a new version is available
-#  %w{opscode-push-jobs-server opscode-reporting opscode-manage opscode-analytics}
+default['private_chef']['addons']['ubuntu_supported_codenames'] =
+  %w{lucid natty precise}
+default['private_chef']['addons']['ubuntu_distribution'] =
+  node['private_chef']['addons']['ubuntu_supported_codenames'].include?(node['lsb']['codename']) ?
+  node['lsb']['codename'] : 'lucid'
 
 ####
 # The Chef User that services run as
