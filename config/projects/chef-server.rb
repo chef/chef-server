cb978347a1861bc6d14dd62345520eafc230eade
tylercloke <tylercloke@gmail.com>
2015-01-15 15:43:02 -0800

    Pull in ruby 2.1.4.
    This is what client currently uses for nix and ohai needs >= 2.

--- config/projects/chef-server.rb
+++ config/projects/chef-server.rb
@@ -30,6 +30,7 @@ override :rebar, version: "2.0.0"
 override :berkshelf2, version: "2.0.18"
 override :rabbitmq, version: "3.3.4"
 override :erlang, version: "R16B03-1"
+override :ruby, version: "2.1.4"
 override :'omnibus-ctl', version: "0.3.1"
 override :'chef-gem', version: "12.0.3"
 
