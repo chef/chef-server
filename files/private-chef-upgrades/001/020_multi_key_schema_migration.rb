791e4680327a4302b9750e35d71e72a233628da7
Steven Danna <steve@opscode.com>
2015-01-19 18:53:00 +0000

    Run the migrate_keys procedure after schema migration.

--- files/private-chef-upgrades/001/020_multi_key_schema_migration.rb
+++ files/private-chef-upgrades/001/020_multi_key_schema_migration.rb
@@ -1,10 +1,10 @@
 define_upgrade do
-
   if Partybus.config.bootstrap_server
     must_be_data_master
     run_command("make deploy",
                 :cwd => "/opt/opscode/embedded/service/enterprise-chef-server-schema",
                 :env => {"EC_TARGET" => "@2.5.0", "OSC_TARGET" => "@1.0.4", "DB_USER" => Partybus.config.database_unix_user}
                 )
+    run_command("sudo -u #{Partybus.config.database_unix_user} /opt/opscode/embedded/bin/psql opscode_chef -c 'select migrate_keys();'")
   end
 end
