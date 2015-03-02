27098e7b83269d8f82831eb865bee9ad2b303694
danielsdeleo <dan@getchef.com>
2015-03-02 10:09:44 -0800

    Add TODO for policy container ACLs for clients

--- apps/oc_chef_authz/src/oc_chef_authz_org_creator.erl
+++ apps/oc_chef_authz/src/oc_chef_authz_org_creator.erl
@@ -102,6 +102,8 @@
            {add_acl, [{container, sandboxes}], [create], [{group, users}]},
 
            %% clients
+
+           %% TODO: add read for policies for clients.
            {add_acl, [{container, nodes}], [read, create], [{group, clients}]},
            {add_acl, [{container, data}], [read], [{group, clients}]},
            {add_acl, mk_tl(container, [cookbooks, environments, roles]), [read] , [{group, clients}]}
