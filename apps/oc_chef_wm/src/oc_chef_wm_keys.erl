29c307740eae173f7642283590d8f18ab5ad9982
Marc Paradise <marc@getchef.com>
2015-03-11 17:20:06 -0400

    add named key delete endpoint behavior

--- apps/oc_chef_wm/src/oc_chef_wm_keys.erl
+++ apps/oc_chef_wm/src/oc_chef_wm_keys.erl
@@ -61,16 +61,17 @@ request_type() ->
 allowed_methods(Req, State) ->
     {['GET', 'POST'], Req, State}.
 
-validate_request('GET', Req, #base_state{resource_args = ObjectType, chef_db_context = Ctx, organization_guid = OrgId} = State) ->
-    ObjectName = chef_wm_util:object_name(ObjectType, Req),
-    ResourceState = make_resource_state_for_object(Ctx, ObjectType, ObjectName, OrgId),
-    {Req, State#base_state{resource_state = ResourceState}};
+%% This export is mixed into oc_chef_wm_named_key
 validate_request('POST', Req, #base_state{resource_args = ObjectType, chef_db_context = Ctx, organization_guid = OrgId} = State) ->
     EJ = chef_key:parse_binary_json(wrq:req_body(Req), undefined),
     ObjectName = chef_wm_util:object_name(ObjectType, Req),
     ResourceState = make_resource_state_for_object(Ctx, ObjectType, ObjectName, OrgId),
     ResourceState1 = ResourceState#key_state{key_data = EJ},
-    {Req, State#base_state{resource_state = ResourceState1 }}.
+    {Req, State#base_state{resource_state = ResourceState1 }};
+validate_request(_Method, Req, #base_state{resource_args = ObjectType, chef_db_context = Ctx, organization_guid = OrgId} = State) ->
+    ObjectName = chef_wm_util:object_name(ObjectType, Req),
+    ResourceState = make_resource_state_for_object(Ctx, ObjectType, ObjectName, OrgId),
+    {Req, State#base_state{resource_state = ResourceState}}.
 
 make_resource_state_for_object(DbContext, client, Name, OrgId) ->
     make_resource_state_for_object(client, chef_db:fetch(#chef_client{name = Name, org_id = OrgId}, DbContext));
@@ -85,13 +86,15 @@ make_resource_state_for_object(Type, Object) ->
                parent_authz_id = chef_object:authz_id(Object), parent_id = chef_object:id(Object)}.
 
 %% Permissions:
-%% The permissions model is the same between oc_chef_wm_keys and oc_chef_wm_named_key
+%% The permissions model is the same between oc_chef_wm_keys and oc_chef_wm_named_key, and
+%% these functions are mixed into oc_chef_wm_named_key
 %% Keys are considered an attribute of the object that owns them:
 %%   * target object (user or client) must exist
 %%   * In order to view a target's keys, requestor will need read access to that target. (handled in default is_authorized)
 %%   * In order to update/delete/add a key for a target, requestor will need update access to that target.
 %%   * If requestor is a user and target is a client, user must in the same org as the client
 %%   * If requestor is a user and target is self, requestor gets full permissions without specific acl checks.
+%%     * behavior above is removed. ACLs should take care of this without special-casing ^
 %%
 %% Before proceeding to authorize, first ensure that the owning actor (client or user) specified in the url exists.
 auth_info(Req, #base_state{resource_args = TargetType, resource_state = #key_state{parent_id = undefined}} = State) ->
@@ -99,10 +102,6 @@ auth_info(Req, #base_state{resource_args = TargetType, resource_state = #key_sta
     Message = chef_wm_util:not_found_message(TargetType, Name),
     Req1 = chef_wm_util:set_json_body(Req, Message),
     {{halt, 404}, Req1, State#base_state{log_msg = {TargetType, not_found}}};
-%% If requestor is a user and target is self, requestor is authorized
-auth_info(Req, #base_state{ requestor_id = RequestorAuthzId,
-                            resource_state = #key_state{parent_authz_id = RequestorAuthzId}} = State) ->
-    {authorized, Req, State};
 auth_info(Req, #base_state{ resource_mod = Mod } = State) ->
     Mod:auth_info(wrq:method(Req), Req, State).
 
--- apps/oc_chef_wm/src/oc_chef_wm_named_key.erl
+++ apps/oc_chef_wm/src/oc_chef_wm_named_key.erl
@@ -1,6 +1,9 @@
 %% -*- erlang-indent-level: 4;indent-tabs-mode: nil; fill-column: 80 -*-
 %% ex: ts=4 sw=4 et
+%%
 %% @author Tyler Cloke <tyler@chef.io>
+%% @author Marc Paradise <marc@chef.io>
+%%
 %% Copyright 2015 Chef Software, Inc. All Rights Reserved.
 %%
 %% This file is provided to you under the Apache License,
@@ -31,12 +34,8 @@
                            is_authorized/2,
                            service_available/2]}]).
 
--mixin([{oc_chef_wm_keys, [
-                           auth_info/2,
-                           auth_info/3,
-                           validate_request/3
-                          ]
-        }]).
+-mixin([{oc_chef_wm_keys, [auth_info/2,
+                           validate_request/3]}]).
 
 %% chef_wm behavior callbacks
 -behavior(chef_wm).
@@ -47,6 +46,7 @@
 
 -export([allowed_methods/2,
          delete_resource/2,
+         auth_info/3,
          from_json/2,
          to_json/2]).
 
@@ -60,37 +60,56 @@ request_type() ->
     "keys".
 
 allowed_methods(Req, State) ->
-    {['GET'], Req, State}.
+    {['GET', 'DELETE'], Req, State}.
 
-to_json(Req, #base_state{chef_db_context = DbContext,
-                         resource_state = #key_state{parent_id = Id,
-                                                     full_type = FullType} = KeyState}= State) ->
-    PathInfo = wrq:path_info(Req),
-    case FullType of
-        client_key ->
-            [{_,_},{key_name,Name},{_,_}] = PathInfo;
-        user_key ->
-            [{key_name,Name},{_,_}] = PathInfo
-    end,
-    case chef_db:fetch(#chef_key{id = Id, key_name = Name}, DbContext) of
-        not_found ->
-            Message = chef_wm_util:not_found_message(environment, Name),
-            Req1 = chef_wm_util:set_json_body(Req, Message),
-            {{halt, 404}, Req1, State#base_state{log_msg = environment_not_found}};
-        Key ->
-            KeyState1 = KeyState#key_state{chef_key = Key},
-            State1 = State#base_state{resource_state = KeyState1},
-            Ejson = chef_key:ejson_from_find(Key),
-            {chef_json:encode(Ejson), Req, State1}
+% auth requirements are shared between this module and oc_chef_wm_key,
+% but we additionally need to verify that the user isn't attempting to remove the
+% key currently used for authentication, and that the requested key record exists.
+% before we attempt any operation on it.
+auth_info(Method, Req, #base_state{chef_db_context = Ctx,
+                                   requestor = #chef_requestor{id = RequestorId, key_name = AuthenticatedKeyName},
+                                   resource_state = #key_state{parent_id = ParentId} = ResourceState} = State) ->
+
+    Name = chef_wm_util:object_name(key, Req),
+    ct:pal("~p ~p ~p~n", [Name, RequestorId, Method]),
+    case {Name, RequestorId, Method} of
+        {AuthenticatedKeyName, ParentId, 'DELETE'} ->
+            key_in_use_response(Name, Req, State);
+        _ ->
+            case chef_db:fetch(#chef_key{id = ParentId, key_name = Name}, Ctx) of
+                not_found ->
+                    key_not_found_response(Name, Req, State);
+                #chef_key{} = Key ->
+                    ResourceState1 = ResourceState#key_state{chef_key = Key},
+                    oc_chef_wm_keys:auth_info(Method, Req, State#base_state{resource_state = ResourceState1})
+            end
     end.
+
+key_in_use_response(Name, Req, #base_state{resource_state = #key_state{parent_name = ParentName}} = State) ->
+    % This is an action forbidden by policy, but the usual "you don't have permissions to X" doesn't apply -
+    % use a custom message.
+    Message = iolist_to_binary(["The key '", Name, "' was used to authenticate this request and cannot be modified or deleted."]),
+    {{halt, 403, Message}, Req, State#base_state{log_msg={key_in_use_for_request, {ParentName, Name}}}}.
+
+key_not_found_response(Name, Req, #base_state{resource_state = #key_state{parent_name = ParentName}} = State) ->
+    Message = chef_wm_util:not_found_message(key, {ParentName, Name}),
+    Req1 = chef_wm_util:set_json_body(Req, Message),
+    {{halt, 404}, Req1, State#base_state{log_msg={key_not_found, {ParentName, Name}}}}.
+
+to_json(Req, #base_state{resource_state = #key_state{chef_key = Key}} = State) ->
+    EJ = chef_key:ejson_from_key(Key),
+    {chef_json:encode(EJ), Req, State}.
 
 %% TODO: needed for PUT
 from_json(_Req, #base_state{}) ->
     error(not_implemented).
 
-%% TODO: needed for DELETE
-delete_resource(_Req, #base_state{}) ->
-    error(not_implemented).
+delete_resource(Req, #base_state{requestor_id = RequestorId, chef_db_context = Ctx,
+                                 resource_state = #key_state{chef_key = Key}} = State) ->
+    ok = oc_chef_wm_base:delete_object(Ctx, Key, RequestorId),
+    EJ = chef_key:ejson_from_key(Key),
+    Req1 = chef_wm_util:set_json_body(Req, EJ),
+    {true, Req1, State}.
 
 malformed_request_message(Any, _Req, _state) ->
     error({unexpected_malformed_request_message, Any}).
