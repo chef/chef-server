f79ebd1e83a76db4889c9b86c757eff0e9c7647a
Josh Black <raskchanky@gmail.com>
2015-03-25 13:03:15 -0700

    refactor delete buttons into a helper instead of a partial
    also, use the correct rails helper method instead of manually creating a form
    so that the styles don't get jacked up

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,4 +11,17 @@ module ApplicationHelper
 
     classes[flash_type.to_sym] || flash_type.to_s
   end
+
+  def delete_button(text, url)
+    button_to(
+      text,
+      url,
+      :class => 'button',
+      :method => :delete,
+      :data => {
+        'abide' => '',
+        'confirm' => 'Are you sure?'
+      }
+    )
+  end
 end
