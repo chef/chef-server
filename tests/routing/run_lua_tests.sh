828cfeb4b8f7b577c6576e29408983ad3ee4d3b3
tylercloke <tylercloke@gmail.com>
2015-04-08 12:52:22 -0700

    Updated routing tests so that they can be run locally and in travis.
    ChangeLog-Entry: Updated lua routing tests to run on local dev machines and in travis.

--- tests/routing/run_lua_tests.sh
+++ tests/routing/run_lua_tests.sh
@@ -5,7 +5,9 @@ if ! lua -v >/dev/null 2>&1; then
     exit
 fi
 
-LUALIB="/opt/opscode/embedded/lualib"
+# If $LUALIB is not set externally, set it to a default that works on a server install
+${LUALIB:="/opt/opscode/embedded/lualib"}
+echo $LUALIB
 
 if [ ! -f lpeg.so ]; then
   ln -s $LUALIB/lpeg.so
@@ -27,21 +29,42 @@ if [ $? -ne 0 ]; then
   exit
 fi
 
-SCRIPT_DIR="/var/opt/opscode/nginx/etc/scripts"
+# If $USE_OMNIBUS_FILES is not set externally, default it to false
+${USE_OMNIBUS_FILES:=1}
+echo $USE_OMNIBUS_FILES
 
-if [ ! -f resolver.lua ]; then
-  ln -s $SCRIPT_DIR/resolver.lua
-fi
+# If we are testing locally, use omnibus files
+if [ $USE_OMNIBUS_FILES -eq 0 ]
+then
+  SCRIPT_DIR="../../files/private-chef-cookbooks/private-chef/templates/default/nginx/scripts"
+  if [ ! -f resolver.lua ]; then
+    ln -s $SCRIPT_DIR/resolver.lua.erb ./resolver.lua
+  fi
 
-if [ ! -f routes.lua ]; then
-  ln -s $SCRIPT_DIR/routes.lua
-fi
+  if [ ! -f routes.lua ]; then
+    ln -s $SCRIPT_DIR/routes.lua.erb ./routes.lua
+  fi
 
-if [ ! -f route_checks.lua ]; then
-  ln -s $SCRIPT_DIR/route_checks.lua
+  if [ ! -f route_checks.lua ]; then
+    ln -s $SCRIPT_DIR/route_checks.lua.erb ./route_checks.lua
+  fi
+else # Else, we are on a server install, use sane default
+  SCRIPT_DIR="/var/opt/opscode/nginx/etc/scripts"
+  if [ ! -f resolver.lua ]; then
+    ln -s $SCRIPT_DIR/resolver.lua
+  fi
+
+  if [ ! -f routes.lua ]; then
+    ln -s $SCRIPT_DIR/routes.lua
+  fi
+
+  if [ ! -f route_checks.lua ]; then
+    ln -s $SCRIPT_DIR/route_checks.lua
+  fi
 fi
 
 echo "Running tests"
 echo "-------------"
 
 lua run_tests.lua
+exit $?
