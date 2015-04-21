67947e26c4903d0f58221df5c3c1515968f7e084
Irving Popovetsky <irving@getchef.com>
2015-04-21 10:20:09 -0700

    Update Java JRE to 8u31 from omnibus-software
    ChangeLog-Entry: Update embedded Java JRE to 8u31

--- config/projects/chef-server.rb
+++ config/projects/chef-server.rb
@@ -32,7 +32,6 @@ override :berkshelf2, version: "2.0.18"
 override :rabbitmq, version: "3.3.4"
 override :erlang, version: "R16B03-1"
 override :ruby, version: "2.1.4"
-override :'server-jre', version: "7u25"
 
 # creates required build directories
 dependency "preparation"
