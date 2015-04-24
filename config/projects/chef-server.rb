e94e2d9a4d83aacd8eb79d359c92ba6158af04da
Steven Danna <steve@opscode.com>
2015-04-24 09:33:58 +0100

    Bump version to 12.0.9 for development

--- config/projects/chef-server.rb
+++ config/projects/chef-server.rb
@@ -22,8 +22,8 @@ package_name    "chef-server-core"
 replace         "private-chef"
 conflict        "private-chef"
 install_dir     "/opt/opscode"
-# last released version is 12.0.7
-build_version   "12.0.8"
+# last released version is 12.0.8
+build_version   "12.0.9"
 build_iteration 1
 
 override :cacerts, version: '2014.08.20'
