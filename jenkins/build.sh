4ea99d11a22325aec8aad98cbc64c38459172194
Marc Paradise <marc@chef.io>
2015-04-27 15:56:56 -0400

    jenkiiiiiiiins!

--- jenkins/build.sh
+++ jenkins/build.sh
@@ -5,28 +5,26 @@
 # PROJ_NAME and the make command.
 
 PROJ_NAME=oc_erchef
-
-export PATH=/opt/ruby1.9/bin:$PATH:/usr/local/bin
-jenkins/builder_info.rb
-source machine_info
-ARTIFACT_BASE=opscode-ci/artifacts/$os/$machine/$PROJ_NAME
-
-make distclean rel || exit 1
+export PATH=/opt/ruby1.9/bin:/opt/erlang/bin:$PATH:/usr/local/bin
+git clean -fdX
+MAKEFLAGS="-j 4" USE_SYSTEM_GECODE=1 make distclean rel || exit 1
 
 # If the string is null, then the git command returned false
 if git describe --tags --match='[0-9]*.[0-9]*.[0-9]*' --exact-match
 then
     VERSION=$(git describe --tags --exact-match --match='[0-9]*.[0-9]*.[0-9]*')
     PACKAGE=${PROJ_NAME}-${VERSION}.tar.gz
-    cd rel
-    tar zcf $PACKAGE $PROJ_NAME/
-    s3cmd put $PACKAGE s3://$ARTIFACT_BASE/$PACKAGE
+    cd _rel
 else
     REL_VERSION=`cat rel/reltool.config|grep '{rel,.*"oc_erchef"'|cut -d ',' -f 3|sed 's/"//g'`
     GIT_SHA=`git rev-parse --short HEAD`
     VERSION=${REL_VERSION}-${GIT_SHA}
     PACKAGE=${PROJ_NAME}-${VERSION}.tar.gz
-    cd rel
-    tar zcf $PACKAGE $PROJ_NAME/
-    s3cmd put $PACKAGE s3://$ARTIFACT_BASE/$PACKAGE
+    cd _rel
 fi
+
+# Yep, hard-coding platform and version. We're building ubuntu only, 10.04 only,
+# but need to distribute to both.
+tar zcf $PACKAGE $PROJ_NAME/
+s3cmd put $PACKAGE s3://opscode-ci/artifacts/ubuntu-10.04/x86_64/$PROJ_NAME
+s3cmd put $PACKAGE s3://opscode-ci/artifacts/ubuntu-12.04/x86_64/$PROJ_NAME
