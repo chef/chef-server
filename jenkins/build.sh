552e664d2050818142c1b763e72e870b56715992
Marc Paradise <marc@chef.io>
2015-04-29 10:54:42 -0400

    make it jenkins-buildable again

--- jenkins/build.sh
+++ jenkins/build.sh
@@ -1,32 +1,28 @@
 #!/bin/bash -xe
 
-# Build oc_erchef. If building a release tag, create a tarball and upload to
+# Build the project. If building a release tag, create a tarball and upload to
 # s3. To customize for another project, you should only need to edit
 # PROJ_NAME and the make command.
 
 PROJ_NAME=mover
 
-export PATH=$PATH:/usr/local/bin
-jenkins/builder_info.rb
-source machine_info
-ARTIFACT_BASE=opscode-ci/artifacts/$os/$machine/chef-mover
+export PATH=/opt/ruby1.9/bin:/opt/erlang/bin:$PATH:/usr/local/bin
+git clean -fdX
+USE_SYSTEM_GECODE=1 make distclean rel || exit 1
 
-make distclean rel || exit 1
-
-# If the string is null, then the git command returned false
 if git describe --tags --match='[0-9]*.[0-9]*.[0-9]*' --exact-match
 then
     VERSION=$(git describe --tags --exact-match --match='[0-9]*.[0-9]*.[0-9]*')
-    PACKAGE=${PROJ_NAME}_${VERSION}.tar.gz
-    cd rel
-    tar zcf $PACKAGE $PROJ_NAME/
-    s3cmd put $PACKAGE s3://$ARTIFACT_BASE/$PACKAGE
+    PACKAGE=${PROJ_NAME}-${VERSION}.tar.gz
 else
-    REL_VERSION=`cat rel/reltool.config|grep '{rel,.*"mover"'|cut -d ',' -f 3|sed 's/"//g'|sed 's/ //g'`
+    REL_VERSION=`cat rel/reltool.config|grep '{rel,.*"mover"'|cut -d ',' -f 3|sed 's/"//g'`
     GIT_SHA=`git rev-parse --short HEAD`
     VERSION=${REL_VERSION}-${GIT_SHA}
-    PACKAGE=${PROJ_NAME}_${VERSION}.tar.gz
-    cd rel
-    tar zcf $PACKAGE $PROJ_NAME/
-    s3cmd put $PACKAGE s3://$ARTIFACT_BASE/$PACKAGE
+    PACKAGE=${PROJ_NAME}-${VERSION}.tar.gz
 fi
+
+
+cd rel
+tar zcf $PACKAGE $PROJ_NAME/
+s3cmd put $PACKAGE s3://opscode-ci/artifacts/ubuntu-10.04/x86_64/$PROJ_NAME
+s3cmd put $PACKAGE s3://opscode-ci/artifacts/ubuntu-12.04/x86_64/$PROJ_NAME
