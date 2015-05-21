6bc89ee9cd8d5e612a48ee8e8107831634c9950f
Marc Paradise <marc@chef.io>
2015-05-21 16:25:37 -0400

    [oc_erchef] reindex to ask erchef for correct api version instead of hard-coding

--- src/oc_erchef/src/oc_erchef_app.erl
+++ src/oc_erchef/src/oc_erchef_app.erl
@@ -4,6 +4,8 @@
 
 %% Application callbacks
 -export([start/2, stop/1]).
+-export([server_api_version/1]).
+-include("include/server_api_version.hrl").
 
 %% ===================================================================
 %% Application callbacks
@@ -19,3 +21,12 @@ start(_StartType, _StartArgs) ->
 
 stop(_State) ->
     ok.
+
+-spec server_api_version(min|max|deprecated) -> {ok, api_version()}.
+server_api_version(min) ->
+    {ok, ?API_MIN_VER};
+server_api_version(max) ->
+    {ok, ?API_MAX_VER};
+server_api_version(deprecated) ->
+    {ok, ?API_DEPRECATED_VER}.
+
