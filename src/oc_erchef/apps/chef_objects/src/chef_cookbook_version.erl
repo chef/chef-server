75c7a4b925aeaf826f17d1adce8b4aaddea3ce5d
Noah Kantrowitz <noah@coderanger.net>
2015-05-30 15:14:09 -0700

    Allow specificity to be null.
    I think?

--- src/oc_erchef/apps/chef_objects/src/chef_cookbook_version.erl
+++ src/oc_erchef/apps/chef_objects/src/chef_cookbook_version.erl
@@ -275,7 +275,7 @@ file_list_spec() ->
     {array_map, {[{<<"name">>, string},
                   {<<"path">>, string},
                   {<<"checksum">>, string},
-                  {<<"specificity">>, string}
+                  {<<"specificity">>, {any_of, {string, null}}}
                  ]}}.
 
 -define(COOKBOOK_SEGMENTS, [<<"attributes">>,
