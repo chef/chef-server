a3b0b0b86095e41314e2f6edcf05d8878e9834da
Marc Paradise <marc@chef.io>
2015-06-03 14:23:25 -0700

    update cache mtime to force an casher update when rsync finds something

--- cache-push.sh
+++ cache-push.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
+
+regex=".*transferred: 0"
 while read f; do
-  rsync -azrR --delete --stats $HOME/$f $HOME/.realcache
+  results=`rsync -azrR --delete --stats $HOME/$f $HOME/.realcache | grep "files transferred: 0"`
+  if [ "$results" == "" ]
+  then
+    echo "Update to cache due to change in $HOME/$f"
+    touch $HOME/.realcache # Ensure timestamp is updated so cacher knows we've changed.
+  fi
 done < $HOME/build/chef/chef-server/.cache-files
