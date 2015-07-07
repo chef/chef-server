b8b6f23207562cd2232ce87d413c092cc0b0c989
Joe DeVivo <joe@devivo.com>
2015-07-07 08:15:18 -0700

    Backporting Jenkins fixes from master

--- src/oc_bifrost/jenkins/build.sh
+++ src/oc_bifrost/jenkins/build.sh
@@ -6,10 +6,9 @@
 
 PROJ_NAME=oc_bifrost
 
-export PATH=$PATH:/usr/local/bin
+export PATH=/opt/erlang/bin:$PATH:/usr/local/bin
 jenkins/builder_info.rb
 source machine_info
-ARTIFACT_BASE=opscode-ci/artifacts/$os/$machine/$PROJ_NAME
 
 make distclean rel || exit 1
 
@@ -18,15 +17,17 @@ if git describe --tags --match='[0-9]*.[0-9]*.[0-9]*' --exact-match
 then
     VERSION=$(git describe --tags --exact-match --match='[0-9]*.[0-9]*.[0-9]*')
     PACKAGE=${PROJ_NAME_}${VERSION}.tar.gz
-    cd rel
-    tar zcvf $PACKAGE $PROJ_NAME/
-    s3cmd put --progress $PACKAGE s3://$ARTIFACT_BASE/$PACKAGE
 else
-    REL_VERSION=`cat rel/reltool.config|grep '{rel,.*"oc_bifrost"'|cut -d ',' -f 3|sed 's/"//g'`
+    REL_VERSION=`cat relx.config|grep '{release,' |cut -d ',' -f 3|sed 's/"//g'|sed 's/}//g'`
     GIT_SHA=`git rev-parse --short HEAD`
     VERSION=${REL_VERSION}-${GIT_SHA}
     PACKAGE=${PROJ_NAME_}${VERSION}.tar.gz
-    cd rel
-    tar zcvf $PACKAGE $PROJ_NAME/
-    s3cmd put --progress $PACKAGE s3://$ARTIFACT_BASE/$PACKAGE
 fi
+
+cd _rel
+
+# Yep, hard-coding platform and version. We're building ubuntu only, 10.04 only,
+# but need to distribute to both.
+tar zcf $PACKAGE $PROJ_NAME/
+s3cmd put $PACKAGE s3://opscode-ci/artifacts/ubuntu-10.04/x86_64/$PROJ_NAME/$PACKAGE
+s3cmd put $PACKAGE s3://opscode-ci/artifacts/ubuntu-12.04/x86_64/$PROJ_NAME/$PACKAGE
