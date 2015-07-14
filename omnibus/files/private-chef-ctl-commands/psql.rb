81ccb1a9e45890b6fa5028bb8afb4818e61b7da2
andy-dufour <adufour@chef.io>
2015-07-14 10:07:52 -0400

    Adding apache v2 license. Removing commented pry line.

--- omnibus/files/private-chef-ctl-commands/psql.rb
+++ omnibus/files/private-chef-ctl-commands/psql.rb
@@ -1,5 +1,17 @@
-# Copyright (c) 2014 Chef Software, Inc.
-# All Rights Reserved
+#
+# Copyright:: Copyright (c) 2015 Chef Software, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 require 'json'
 
@@ -32,7 +44,6 @@ add_command_under_category "psql", "Database", "Launches an interactive psql ses
 
   if File.exists?("/etc/opscode-push-jobs-server/opscode-push-jobs-server-running.json")
     db_config.merge!(JSON.parse(File.read("/etc/opscode-push-jobs-server/opscode-push-jobs-server-running.json")))
-      #require 'pry'; binding.pry
   end
   if File.exists?("/etc/opscode-reporting/opscode-reporting-running.json")
     db_config.merge!(JSON.parse(File.read("/etc/opscode-reporting/opscode-reporting-running.json")))
