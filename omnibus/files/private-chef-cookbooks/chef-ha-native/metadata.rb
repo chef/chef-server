9bff17233f7f628cac8afc01863746d66cb7796f
Steven Danna <steve@chef.io>
2015-07-21 12:45:28 +0100

    Add keepalived scaffolding to start work on per-service clustering

---
+++ omnibus/files/private-chef-cookbooks/chef-ha-native/metadata.rb
@@ -0,0 +1,11 @@
+name "chef-ha-native"
+maintainer        "Chef Software, Inc."
+maintainer_email  "cookbooks@chef.io"
+license           "Apache 2.0"
+description       "Sets up UNRELEASED per-service replication for Chef Server"
+long_description  "Sets up UNRELEASED per-service replication for Chef Server"
+version           "0.1.0"
+
+%w{ ubuntu debian redhat centos oracle scientific fedora amazon }.each do |os|
+  supports os
+end
