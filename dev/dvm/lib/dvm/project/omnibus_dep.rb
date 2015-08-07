d3f4b215d52bca0ddf4ab6163b9e50d414cd6916
Marc Paradise <marc@chef.io>
2015-08-07 15:57:55 -0400

    [dvm] perform bundle install when loading bundled omnibus components

--- dev/dvm/lib/dvm/project/omnibus_dep.rb
+++ dev/dvm/lib/dvm/project/omnibus_dep.rb
@@ -1,6 +1,6 @@
 module DVM
   class OmnibusDep < Dep
-    attr_reader :source_path, :dest_path, :reconfigure_on_load
+    attr_reader :source_path, :dest_path, :reconfigure_on_load, :bundler
     # Needed for now to make tools mixin behave:
     attr_reader :path
     def initialize(base_dir, name, config)
@@ -10,6 +10,7 @@ module DVM
       @path = @source_path
       @reconfigure_on_load = config['reconfigure_on_load']
       @available = true
+      @bundler = config['bundler']
     end
     def unload
       unmount(source_path)
@@ -18,10 +19,14 @@ module DVM
       end
     end
     def load(opts)
+      if bundler
+        run_command("rm -rf .bundle/config && bundle install --path /opt/opscode/embedded/service/gem --no-binstubs", "Installing in-place...", cwd: @dest_path)
+      end
       bind_mount(source_path, dest_path)
       if reconfigure_on_load and not opts[:no_build]
         run_command("chef-server-ctl reconfigure", "Reconfiguring chef server to pick up the changes")
       end
+
     end
     def loaded?
       path_mounted? source_path
