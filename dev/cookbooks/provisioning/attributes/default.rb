8d5fe4c5f8f7ef05316ddf918fc63935404d9ba4
Marc Paradise <marc@chef.io>
2015-12-04 13:11:42 -0500

    wip that I don't wnat to have only on my local machine

--- dev/cookbooks/provisioning/attributes/default.rb
+++ dev/cookbooks/provisioning/attributes/default.rb
@@ -1,3 +1,5 @@
 
 default['provisioning']['chef-server-config'] = {}
+default['provisioning']['postgresql']['chef-server']['external'] = false
+default['provisioning']['postgresql']['reporting']['external'] = false
 
