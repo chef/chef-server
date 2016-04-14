dd03bfa6aa4920f99a0aa7c9d5873b9bca932acc
Steven Danna <steve@chef.io>
2016-04-14 20:44:24 +0100

    [omnibus] Add rb-readline to the build
    Omnibus-software no longer compiles ruby with readline support because
    of various compatibility issues. Adding rb-readline ensures that irb
    and other irb or pry-based REPLs have readline support.

--- omnibus/config/projects/chef-server.rb
+++ omnibus/config/projects/chef-server.rb
@@ -42,6 +42,7 @@ dependency "private-chef-scripts" # assorted scripts used by installed instance
 dependency "private-chef-ctl" # additional project-specific private-chef-ctl subcommands
 dependency "ctl-man" # install man page
 dependency "openresty"
+dependency "rb-readline"
 dependency "redis-gem" # gem for interacting with redis
 dependency "openresty-lpeg"  # lua-based routing
 dependency "runit"
