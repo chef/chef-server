a845fd25d49e3f3742f0d50483aac83a47cc7556
Steven Danna <steve@chef.io>
2016-10-05 22:42:21 +0100

    Remove nodejs from the final package
    Encouraged by rails-coffee and uglifier, nodejs used up 51MB of disk
    space!  In retaliation rm and rf teamed up at tribal council and voted
    her off the island.
    
    We've moved the two gems that require a javascript runtime into a new
    group in the Gemfile.  They will still be installed by default, but
    `rake` and `rails server` won't require them by default when starting
    up.
    
    Signed-off-by: Steven Danna <steve@chef.io>

--- omnibus/config/software/cleanup.rb
+++ omnibus/config/software/cleanup.rb
@@ -7,6 +7,8 @@ build do
   # Delete cached .gem files and git checkouts
   delete "#{install_dir}/embedded/lib/ruby/gems/2.2.0/cache/*.gem"
   delete "#{install_dir}/embedded/service/gem/ruby/2.2.0/cache/*.gem"
+  # Remove nodejs
+  delete "#{install_dir}/embedded/nodejs"
   # strip gecode shared object files related to gecode installs
   command "strip #{install_dir}/embedded/lib/libgecode*.so.32.0"
   command "strip #{install_dir}/embedded/lib/ruby/gems/2.2.0/gems/dep-selector-libgecode-*/lib/dep-selector-libgecode/vendored-gecode/lib/*.so"
