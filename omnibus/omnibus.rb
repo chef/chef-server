677dcf3cdad94aff0603d1c9d3e76379537bac62
Marc Paradise <marc@chef.io>
2016-12-15 10:30:26 -0500

    Not for merge. Disable license checks for local opscode-ci testing
    Signed-off-by: Marc Paradise <marc@chef.io>

--- omnibus/omnibus.rb
+++ omnibus/omnibus.rb
@@ -19,4 +19,4 @@ fetcher_read_timeout 120
 # software_gems ['omnibus-software', 'my-company-software']
 # local_software_dirs ['/path/to/local/software']
 
-fatal_transitive_dependency_licensing_warnings true
+fatal_transitive_dependency_licensing_warnings false
