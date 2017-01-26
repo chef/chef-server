e61623722ec3b8b689b4287dc08cc7305711e30c
Mark Anderson <mark@chef.io>
2017-01-26 15:58:04 -0800

    Add gproc monitoring

--- src/sky/apps/sky/src/sky_client_sup.erl
+++ src/sky/apps/sky/src/sky_client_sup.erl
@@ -4,21 +4,23 @@
 -export([start_link/0, init/1, start_client/4, spawn_all_clients/5]).
 
 start_link() ->
-  supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+    supervisor:start_link({local, ?MODULE}, ?MODULE, []).
 
 init([]) ->
-  SupFlags = #{strategy => simple_one_for_one},
-  ChildSpecs = [#{id => sky_client,
-                  start => {sky_client, start_link, []},
-                  shutdown => brutal_kill}],
-  {ok, {SupFlags, ChildSpecs}}.
+    gproc:reg({a, l, connected_clients}, undefined),
+
+    SupFlags = #{strategy => simple_one_for_one},
+    ChildSpecs = [#{id => sky_client,
+                    start => {sky_client, start_link, []},
+                    shutdown => brutal_kill}],
+    {ok, {SupFlags, ChildSpecs}}.
 
 start_client(Host, Port, Org, Name) ->
-	supervisor:start_child(?MODULE, [Host, Port, Org, Name]).
+    supervisor:start_child(?MODULE, [Host, Port, Org, Name]).
 
 spawn_all_clients(Host, Port, Org, Name, Count) ->
-  ClientNames = [ io_lib:format("~s~4..0B", [Name, I]) || I <- lists:seq(1, Count)],
-  [ start_client(Host, Port, Org, NameI) || NameI <- ClientNames ].
+    ClientNames = [ io_lib:format("~s~4..0B", [Name, I]) || I <- lists:seq(1, Count)],
+    [ start_client(Host, Port, Org, NameI) || NameI <- ClientNames ].
 
 
 
--- src/sky/apps/sky/src/sky_websocket_handler.erl
+++ src/sky/apps/sky/src/sky_websocket_handler.erl
@@ -16,20 +16,26 @@ init(Req, _Opts) ->
     OrgName = cowboy_req:binding(orgname, Req),
     ClientName = cowboy_req:binding(clientname, Req),
     State = #state{ orgname = OrgName, clientname = ClientName },
-    gproc:reg({n, l, {OrgName, ClientName}),
+    lager:info("Starting listener ~p for ~p ~p", [self(), OrgName, ClientName]),
     % Note: Req goes away after this; must extract all the things we need first.
     {cowboy_websocket, Req, State, ?DISCONNECT_TIMEOUT}.
 
 %% Notes: We most likely will want to hibernate connections to save
 %% memory (there's a CPU/latency cost, but we don't need that snappy
 %% of a protocol)
-websocket_init(State) ->
+websocket_init(State = #state{orgname=OrgName, clientname=ClientName}) ->
+    Key = make_client_gproc_key(State),
+    gproc:reg({n, l, Key}),
+    gproc:reg({c, l, connected_clients}, 1),
+
+    lager:info("Registering listener ~p for ~p ~p", [self(), OrgName, ClientName]),
     erlang:start_timer(1000, self(), <<"SERVER_HEARTBEAT">>),
     ServerHelloMsg = make_server_hello(State),
     {reply, {text, ServerHelloMsg}, State}.
 
-websocket_handle({text, Msg}, State) ->
-    {reply, {text, <<"That's what she said! ", Msg/binary >>}, State};
+websocket_handle({text, Msg}, State = #state{orgname=OrgName, clientname=ClientName}) ->
+    lager:info("Handling message ~p for ~p ~p: ~p", [self(), OrgName, ClientName, Msg]),
+    {ok, State};
 websocket_handle(_Data, State) ->
     {ok, State}.
 
@@ -44,3 +50,7 @@ websocket_info(_Info, State) ->
 %% Server HELLO should send checking interval (in seconds) and the GUID of the client.
 make_server_hello(_State) ->
     <<"CONFIG:60:DEADBEEF">>.
+
+
+make_client_gproc_key(#state{orgname=OrgName, clientname=ClientName}) ->
+    {client_connection, {OrgName, ClientName}}.
