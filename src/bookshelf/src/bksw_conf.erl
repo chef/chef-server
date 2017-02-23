544623e3b93fb7f3bc7c92471bfb4674d8d7693c
Stephan Renatus <srenatus@chef.io>
2017-02-23 13:06:43 +0100

    bookshelf: wire chef_secrets into it
    * omnibus sys.config updates for erchef and bookshelf
    * wire in chef_secrets for looking up s3 creds
    * wrap bksw_conf convenience-exports in -ifdef(TEST)
    * fix config/app.config so this can run in ./rebar3 shell
    
    Signed-off-by: Stephan Renatus <srenatus@chef.io>

--- src/bookshelf/src/bksw_conf.erl
+++ src/bookshelf/src/bksw_conf.erl
@@ -32,7 +32,9 @@
          summarize_config/0]).
 
 %% Exported for common test
+-ifdef(TEST).
 -export([port/0, ip/0, keys/0]).
+-endif.
 
 -include("internal.hrl").
 
@@ -143,13 +145,9 @@ port() ->
     envy:get(bookshelf, port, 4321, positive_integer).
 
 keys() ->
-    case envy:get(bookshelf, keys, undefined, any) of
-        undefined ->
-            error({missing_config, {bookshelf, keys}});
-        {AWSAccessKey, SecretKey} ->
-            {bksw_util:to_binary(AWSAccessKey),
-             bksw_util:to_binary(SecretKey)}
-    end.
+    {ok, AWSAccessKey} = chef_secrets:get(<<"bookshelf">>, <<"access_key_id">>),
+    {ok, SecretKey} = chef_secrets:get(<<"bookshelf">>, <<"secret_access_key">>),
+    {AWSAccessKey, SecretKey}.
 
 log_dir() ->
     envy:get(bookshelf, log_dir, code:priv_dir(bookshelf), any).
