bcbbf8e193658adc7ce2f4b6c7af24d35c6c2caa
Stephan Renatus <srenatus@chef.io>
2017-02-23 15:45:35 +0100

    oc_bifrost: use chef_secrets
    also remove superfluous upgrade/0
    
    Signed-off-by: Stephan Renatus <srenatus@chef.io>
    
    Signed-off-by: Stephan Renatus <srenatus@chef.io>

--- src/oc_bifrost/apps/bifrost/src/bifrost_sup.erl
+++ src/oc_bifrost/apps/bifrost/src/bifrost_sup.erl
@@ -4,8 +4,7 @@
 
 %% External exports
 -export([
-         start_link/0,
-         upgrade/0
+         start_link/0
         ]).
 
 %% supervisor callbacks
@@ -15,25 +14,6 @@
 
 start_link() ->
     supervisor:start_link({local, ?MODULE}, ?MODULE, []).
-
-%% NOTE: This is the same as found in chef_wm_sup
-upgrade() ->
-    {ok, {_, Specs}} = init([]),
-
-    Old = sets:from_list(
-            [Name || {Name, _, _, _} <- supervisor:which_children(?MODULE)]),
-    New = sets:from_list([Name || {Name, _, _, _, _, _} <- Specs]),
-    Kill = sets:subtract(Old, New),
-
-    sets:fold(fun (Id, ok) ->
-                      %%% We wish to continue even if a child isn't found.
-                      _ = supervisor:terminate_child(?MODULE, Id),
-                      _ = supervisor:delete_child(?MODULE, Id),
-                      ok
-              end, ok, Kill),
-
-    [supervisor:start_child(?MODULE, Spec) || Spec <- Specs],
-    ok.
 
 init([]) ->
 
