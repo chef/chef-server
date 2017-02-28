134fe1b61a7334cd4ba2b4477d3f8425bb6eb9ad
Stephan Renatus <srenatus@chef.io>
2017-02-28 10:33:00 +0100

    omnibus: use erlang 18.3, update omnibus-software and license_scout
    For erlang without hipe on ppc64le. License_scout was by-catch.
    
    Signed-off-by: Stephan Renatus <srenatus@chef.io>

--- omnibus/config/projects/chef-server.rb
+++ omnibus/config/projects/chef-server.rb
@@ -28,7 +28,7 @@ build_version   "12.13.1"
 build_iteration 1
 
 override :rabbitmq, version: "3.3.4"
-override :erlang, version: "17.5"
+override :erlang, version: "18.3"
 override :lua, version: "5.1.5"
 override :'omnibus-ctl', version: "master"
 override :chef, version: "v12.17.44"
