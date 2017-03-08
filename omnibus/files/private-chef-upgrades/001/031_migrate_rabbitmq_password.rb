eef329653dba3601093ab0047aa8d637ddb5f11d
Stephan Renatus <srenatus@chef.io>
2017-03-08 13:50:52 +0100

    add partybus migration for rabbitmq password rehashing
    Signed-off-by: Stephan Renatus <srenatus@chef.io>

---
+++ omnibus/files/private-chef-upgrades/001/031_migrate_rabbitmq_password.rb
@@ -0,0 +1,14 @@
+define_upgrade do
+  if Partybus.config.bootstrap_server # TODO: do we want this?
+
+    rmq = Partybus.config.running_server["private_chef"]["rabbitmq"]
+    [
+      [rmq["user"], "password"],
+      [rmq["actions_user"], "actions_password"],
+      [rmq["management_user"], "management_password"]
+    ].each do |name, passname|
+      pass = Partybus.config.secrets['rabbitmq'][passname].value
+      run_command("/opt/opscode/embedded/bin/rabbitmqctl change_password #{name} #{pass}")
+    end
+  end
+end
