63559941737ac5312bb9ebbabbc2e08ff39f1f0b
Steven Danna <steve@chef.io>
2017-03-22 15:49:01 +0000

    [oc-id] Don't write application configuration to disk by default
    The user-configured applications previously resulted in files written
    to disk with the oauth2 secret for that application.  These files are
    how users currently integrate supermarket, compliance, and other
    applications with oc-id.
    
    To replace them, we've provided an oc-id-show-app ctl command that
    will emit the same JSON that would have been in the file.
    
    Signed-off-by: Steven Danna <steve@chef.io>

---
+++ omnibus/files/private-chef-ctl-commands/oc_id_show_app.rb
@@ -0,0 +1,27 @@
+add_command_under_category "oc-id-show-app", "Secrets Management", "Show configuration for oc-id applications", 2 do
+  require 'mixlib/shellout'
+  require 'json'
+
+  app_name = ARGV[3]
+  if !app_name
+    STDERR.puts "No app name provided"
+    exit(1)
+  end
+
+  rails_script = <<EOF
+app = Doorkeeper::Application.find_by(:name => "#{app_name}");
+puts app.to_json
+EOF
+
+  cmd = Mixlib::ShellOut.new("bin/rails runner -e production '#{rails_script}'",
+                          :cwd => '/opt/opscode/embedded/service/oc_id')
+  cmd.run_command
+  json = cmd.stdout.lines.last.chomp
+  if json == "null"
+    STDERR.puts "Could not find app #{app_name}"
+    exit(1)
+  end
+
+  hash = JSON.parse(json).delete_if { |key| %w[ id created_at updated_at].include? key }
+  puts JSON.pretty_generate(hash)
+end
