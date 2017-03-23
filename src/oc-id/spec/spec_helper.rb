e77bc556a5c612bf0e02c80b797741e61a669f7c
Steven Danna <steve@chef.io>
2017-03-23 15:00:11 +0000

    Add spec_helper code to initialize secrets file if necessary
    Signed-off-by: Steven Danna <steve@chef.io>

--- src/oc-id/spec/spec_helper.rb
+++ src/oc-id/spec/spec_helper.rb
@@ -1,4 +1,44 @@
 ENV["RAILS_ENV"] ||= 'test'
+
+def setup_chef_secrets
+  f = File.open(File.expand_path("../../config/private-chef-secrets.json", __FILE__), "r")
+  puts "Using development secrets file at FD #{f.to_i}"
+  ENV['CHEF_SECRETS_FD'] = f.to_i.to_s
+  f
+end
+
+#
+# Holy cow what is this?
+#
+# Our secrets library expect to be passed credentials via a file
+# descriptor. However, depending on how rspec was launched, someone
+# may have already consumed the secrets and closed the file, so here
+# we check if the passed FD is valid or create a new one.
+#
+f = if ENV['CHEF_SECRETS_FD'].nil?
+      puts "No CHEF_SECRETS_FD set"
+      setup_chef_secrets
+    else
+      begin
+        puts "CHEF_SECRETS_FD is set to #{ENV['CHEF_SECRETS_FD']}"
+        f = IO.new(ENV['CHEF_SECRETS_FD'].to_i, "r")
+        if f.closed?
+          puts "File is alrady closed. Likely running from rake."
+          setup_chef_secrets
+        end
+        f
+      rescue Errno::EBADF, Errno::EINVAL
+        puts "CHEF_SECRETS_FD has already been read. Likely running from rake."
+        setup_chef_secrets
+      end
+    end
+
+at_exit do
+  # Ensure f stays open until the at_exit handler runs
+  # Note: it will be closed by veil
+  f
+end
+
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 
