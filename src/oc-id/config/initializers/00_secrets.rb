6451c565a8903f0ef086557813d85d60cca97cdf
Steven Danna <steve@chef.io>
2017-03-24 01:15:26 +0000

    Use chef-secrets-env during rspec tests
    We've been hitting a problem with the FD provider in travis even
    though it works locally on OS X and locally in an Ubuntu VM on the
    same version of ruby being used in travis.
    
    To avoid this for now, we use the environment provider.
    
    Signed-off-by: Steven Danna <steve@chef.io>

--- src/oc-id/config/initializers/00_secrets.rb
+++ src/oc-id/config/initializers/00_secrets.rb
@@ -7,7 +7,17 @@ class Secrets
   end
 
   def initialize
-    @veil = Veil::CredentialCollection.from_config(provider: 'chef-secrets-fd')
+    # TODO(ssd) 2017-03-24: This should maybe go in chef-secrets itself
+    provider = if ENV['CHEF_SECRETS_DATA']
+                 puts "CHEF_SECRETS_DATA is set. Using that directly"
+                 'chef-secrets-env'
+               elsif ENV['CHEF_SECRETS_FD']
+                 puts "CHEF_SECRETS_FD=#{ENV['CHEF_SECRETS_FD']}"
+                 'chef-secrets-fd'
+               else
+                 raise "No CHEF_SECRETS initialization data in the environment."
+               end
+    @veil = Veil::CredentialCollection.from_config(provider: provider)
   end
 
   def get(group, name)
