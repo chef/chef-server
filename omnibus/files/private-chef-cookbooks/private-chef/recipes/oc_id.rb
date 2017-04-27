chef/chef-server

commit 6824d90681a87ee18fb64f826507998978742d40

Update chef to version 12
Signed-off-by: Salim Afiune <afiune@chef.io>

src/oc-id/Gemfile
@@ -1,7 +1,7 @@
 source 'https://rubygems.org'
 
 gem 'rails', '= 4.2.5.2'
-gem 'chef', '~> 11.14'
+gem 'chef', '~> 12'
 gem 'jbuilder', '~> 1.2'
 gem 'jquery-rails'
 gem 'jwt' # For Zendesk SSO

commit 202bd46107af177e53ea0e6d686a3d3b6f9d946b

Fix Chef 12 oc_id regression
The upgrade of oc_id to Chef 12 in commit

6824d90681a87ee18fb64f826507998978742d40

introduced 2 issues:

(1) Incorrect arguments to ServrAPI.new
(2) A change in ssl verification behavior

This commit

- Fixes issue (1),
- Make the ssl_verify_mode configurable
- Updates omniauth-chef to a version that contains similar fixes.

* SSL Verify Mode

The ssl verify mode has been set to verify_none. This isn't a behavior
change from previous versions since this was the default in the old
version of Chef we were previously pinned to.

Because we want to talk to oc_id via 'localhost' to avoid cross-node
requests, changing this to verify_peer is problematic since, even if
we did the work to add /var/opt/opscode/nginx/ca (or the user-provided
cert location) to the trusted_cert_dir, the cert would likely still
mismatch since in the common case such certs ar for the `api_fqdn` and
not "localhost".

Signed-off-by: Steven Danna <steve@chef.io>

omnibus/files/private-chef-cookbooks/private-chef/recipes/oc_id.rb
@@ -27,7 +27,16 @@ end
 app_settings = {
   'chef' => {
     'endpoint' => "https://#{node['private_chef']['lb_internal']['vip']}",
-    'superuser' => 'pivotal'
+    'superuser' => 'pivotal',
+    #
+    # Why is this verify_none?
+    #
+    # Since we set the endpoint to 'localhost', even if we set the
+    # trusted_cert_dir to include the user-provided or self-signed
+    # cert in use by nginx, we will likely fail verification since
+    # those are certs for the api_fqdn and not localhost.
+    #
+    'ssl_verify_mode' => 'verify_none'
   },
   'doorkeeper' => {
     'administrators' => node['private_chef']['oc_id']['administrators'] || []

src/oc-id/Gemfile
@@ -13,14 +13,14 @@ gem 'unicorn-rails', '~> 1.1.0'
 gem 'omniauth', '~> 1.3.2'
 gem 'nokogiri', '>= 1.7.1'
 gem 'pg'
-gem 'mixlib-authentication', '~> 1.3.0'
+gem 'mixlib-authentication', '~> 1.3'
 gem 'sentry-raven'
 gem 'responders', '~> 2.0'
 gem 'newrelic_rpm'
 gem 'doorkeeper', '~> 1.4.0'
 
 gem 'veil', git: 'https://github.com/chef/chef_secrets'
-gem 'omniauth-chef', git: 'https://github.com/chef/omniauth-chef'
+gem 'omniauth-chef', git: 'https://github.com/chef/omniauth-chef', branch: 'ssd/POOL-581'
 gem 'chef-web-core', git: 'https://github.com/chef/chef-web-core.git'
 
 #

src/oc-id/lib/chef_resource.rb
@@ -10,7 +10,8 @@ module ChefResource
   end
 
   def chef
-    ::Chef::ServerAPI.new endpoint, Settings.chef.superuser, nil, parameters
+    ::Chef::Config.ssl_verify_mode Settings.chef.ssl_verify_mode.to_sym || :verify_peer
+    ::Chef::ServerAPI.new endpoint, parameters
   end
 
   private
@@ -28,6 +29,9 @@ module ChefResource
   end
 
   def parameters
-    { headers: headers, raw_key: key }
+    { headers: headers,
+      client_name: Settings.chef.superuser,
+      client_key: nil,
+      raw_key: key }
   end
 end