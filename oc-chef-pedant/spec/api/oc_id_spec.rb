e6d6d1423723186669c034d85413b5653fe02799
Steven Danna <steve@chef.io>
2017-05-05 18:04:32 +0100

    [pedant] Assert key is valid in oc_id tests
    Signed-off-by: Steven Danna <steve@chef.io>

--- oc-chef-pedant/spec/api/oc_id_spec.rb
+++ oc-chef-pedant/spec/api/oc_id_spec.rb
@@ -1,4 +1,5 @@
 require 'pedant/rspec/common'
+require 'openssl'
 
 describe "oc_id API", :oc_id do
   context "status endpoint" do
@@ -45,10 +46,16 @@ describe "oc_id API", :oc_id do
 
     let(:request_url) { "#{platform.server}/id/profile/regen_key" }
     let(:request_body) { "authenticity_token=#{CGI.escape(csrf[:token])}&commit=Get+a+New+Key" }
+    let(:response) { post(request_url, platform.superuser, headers: request_headers, payload: request_body) }
     it "returns a non-zero file" do
-      response = post(request_url, platform.superuser, headers: request_headers, payload: request_body)
       expect(response.code).to eq(200)
       expect(response.body.length).to_not be(0)
+    end
+
+    it "returns a valid key" do
+      expect(response.code).to eq(200)
+      # This will raise if it isn't a valid key
+      expect(OpenSSL::PKey::RSA.new(response.body).class).to eq(OpenSSL::PKey::RSA)
     end
   end
 
