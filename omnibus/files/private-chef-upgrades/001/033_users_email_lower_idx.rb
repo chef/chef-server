cecacc9cb33e9dea72056d6785800fa5b4a62adb
Stephan Renatus <srenatus@chef.io>
2017-05-30 15:33:27 +0200

    add sqitch tag and partybus migration
    Signed-off-by: Stephan Renatus <srenatus@chef.io>

---
+++ omnibus/files/private-chef-upgrades/001/033_users_email_lower_idx.rb
@@ -0,0 +1,10 @@
+define_upgrade do
+
+  if Partybus.config.bootstrap_server
+    must_be_data_master
+
+    # schema update for adding a functional index for index-supported queries
+    # using `WHERE lower(email) = lower("USER@FOO.com")`.
+    run_sqitch('@users_email_functional_index', 'oc_erchef')
+  end
+end
