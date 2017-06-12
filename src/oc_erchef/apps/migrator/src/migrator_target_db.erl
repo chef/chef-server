46a34db3c309d10327bcc307f5de0fcbb92d6c2c
Marc A. Paradise <marc.paradise@gmail.com>
2017-06-12 11:15:17 -0400

    More WIP
    Signed-off-by: Marc A. Paradise <marc.paradise@gmail.com>

--- src/oc_erchef/apps/migrator/src/migrator_target_db.erl
+++ src/oc_erchef/apps/migrator/src/migrator_target_db.erl
@@ -1,18 +1,36 @@
+%% -*- erlang-indent-level: 4;indent-tabs-mode: nil -*-
+%% ex: ts=4 sw=4 et
+%% @author Marc Paradise <marc@chef.io>
+%% Copyright 2017 Chef Software, Inc. All Rights Reserved.
+%%
+%% This file is provided to you under the Apache License,
+%% Version 2.0 (the "License"); you may not use this file
+%% except in compliance with the License.  You may obtain
+%% a copy of the License at
+%%
+%%   http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing,
+%% software distributed under the License is distributed on an
+%% "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+%% KIND, either express or implied.  See the License for the
+%% specific language governing permissions and limitations
+%% under the License.
+%%
+
 %% This applies SQL updates to the target system directly. It maintains its own connection.
 %% We may look at having multiple instances of these (one per DB or more) down the road.
 -module(migrator_target_db).
 -behaviour(gen_server).
 
 
-bin_cache_get(TXTerm) ->
-
-
-
-% When you invoke apply/1:
+% When you invoke execute/1:
 %
 % This module maintains a two-level cache:
-% 1. statements converted to binary strings with placeholders, suitable for preparing
+% 1. statements converted to binary strings with placeholders, suitable for preparing;
+%    and
 % 2. per-postgres-connection prepared statement references
+%    The per-connection cache is specific to this process and it's pg connection.
 %
 %
 %  - check the per-connection cache for the statement. Return that reference if it exists, otherwise:
@@ -28,5 +46,58 @@ bin_cache_get(TXTerm) ->
 %
 %  The prepared statement reference cache is keyed to {ConnId, phash2(InputTxTerm)}
 %
-%  Any connection that uses this service is monitored; if it terminates,
-%  the associated prepared statement cache will be destroyed
+-define(SERVER, ?MODULE).
+
+-export([%% API functions
+         start_link/1,
+         execute/1,
+         %% gen_server behaviour:
+         init/1,
+         handle_call/3,
+         handle_cast/2,
+         terminate/2,
+         handle_info/2,
+         code_change/3]).
+
+
+%% API
+start_link(Config) ->
+  gen_server:start_link({local, ?SERVER}, ?MODULE, [], Config).
+execute(TXTerm) ->
+  gen_server:call(?SERVER, {execute, TXTerm}).
+
+%% gen_server functions
+init(_Args) ->
+  Conn = connect(),
+  {ok, #{}}.
+
+handle_call({execute, TXTerm}, _From, State) ->
+  {reply, Result, State}
+  {reply, ok, State}.
+handle_call(_Request, _From, State) ->
+  {reply, ok, State}.
+
+handle_cast(_Msg, State) ->
+  {noreply, State}.
+
+handle_info(_Info, State) ->
+  {noreply, State};
+
+terminate(_Reason, _State) ->
+  ok.
+
+code_change(_OldVsn, State, _Extra) ->
+  {ok, State}.
+
+%% internal
+%%
+connect() ->
+  %% Target is currently hard-coded to a local copy of opscode_chef.  Prior to running,
+  %% nuke and recreate it since this POC conveniently avoids the question of initial sync.
+  %%
+  %{ok, Conn} = epgsql:connect("127.0.0.1", "migration_user", "password", [{database, "opscode_chef_target"}]),
+  ok.
+
+
+
+
