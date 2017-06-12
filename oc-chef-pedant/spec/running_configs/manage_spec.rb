36b66fec44c72e1bf742919f69ee0d4714843dc3
Stephan Renatus <srenatus@chef.io>
2017-06-12 16:55:33 +0200

    Add running configs checks to pedant
    So far, this includes checking for values expected by Manage, Reporting,
    and Pushy Server. Some tests are there twice, but then this is because the
    values are used in two places (and thus kind of independent).
    
    Signed-off-by: Stephan Renatus <srenatus@chef.io>

---
+++ oc-chef-pedant/spec/running_configs/manage_spec.rb
@@ -0,0 +1,42 @@
+require 'json'
+require 'pedant/rspec/common'
+
+describe "running configs required by Manages", :config do
+  let (:config) { JSON.parse(IO.read("/etc/opscode/chef-server-running.json"))['private_chef'] }
+
+  it "lb/api_fqdn" do
+    expect(config['lb']['api_fqdn'].to_s).to_not eq ''
+  end
+
+  it "nginx/ssl_port" do
+    expect(config['nginx']['ssl_port'].to_i).to_not eq 0
+  end
+
+  it "nginx/ssl_protocols" do
+    expect(config['nginx']['ssl_protocols'].to_s).to_not eq ''
+  end
+
+  it "nginx/ssl_ciphers" do
+    expect(config['nginx']['ssl_ciphers'].to_s).to_not eq ''
+  end
+
+  it "nginx/ssl_certificate" do
+    expect(File.exists?(config['nginx']['ssl_certificate'])).to be true
+  end
+
+  it "nginx/ssl_certificate_key" do
+    expect(File.exists?(config['nginx']['ssl_certificate_key'])).to be true
+  end
+
+  it "nginx/dir" do
+    expect(File.exists?(File.join(config['nginx']['dir'], "etc/addon.d"))).to be true
+  end
+
+  it "opscode-erchef/strict_search_result_acls" do
+    expect(config['opscode-erchef']['strict_search_result_acls']).to be(true).or be(false)
+  end
+
+  it "ldap is a hash" do
+    expect(config['ldap'].class).to eq(Hash)
+  end
+end
---
+++ oc-chef-pedant/spec/running_configs/reporting_spec.rb
@@ -0,0 +1,198 @@
+require 'json'
+require 'pedant/rspec/common'
+
+describe "running configs required by Reporting", :config do
+  let (:config) { JSON.parse(IO.read("/etc/opscode/chef-server-running.json"))['private_chef'] }
+
+  it "role" do
+    expect(config['role'].to_s).to eq("standalone").or eq("backend").or eq("frontend")
+  end
+
+  it "topology" do
+    expect(config['topology'].to_s).to eq("tier").or eq("ha").or eq("standalone")
+  end
+
+  context "opscode-solr" do
+
+    it "opscode-solr4/external" do
+      expect(config['opscode-solr4']['external']).to be(true).or be(false)
+    end
+
+    it "opscode-solr4/external_url" do
+      if config['opscode-solr4']['external']
+        expect(config['opscode-solr4']['external_url'].to_s).not_to eq ''
+      else
+        skip "not using external solr"
+      end
+    end
+
+    it "opscode-solr4/vip" do
+      expect(config['opscode-solr4']['vip'].to_s).not_to eq ''
+    end
+
+    it "opscode-solr4/port" do
+      expect(config['opscode-solr4']['port'].to_i).not_to eq 0
+    end
+  end
+
+  context "postgresql" do
+
+    it "postgresql/vip" do
+      expect(config['postgresql']['vip'].to_s).not_to eq ''
+    end
+
+    it "postgresql/port" do
+      expect(config['postgresql']['port'].to_i).not_to eq 0
+    end
+
+    it "postgresql/username" do
+      expect(config['postgresql']['username'].to_s).not_to eq ''
+    end
+
+    it "postgresql/db_superuser" do
+      expect(config['postgresql']['db_superuser'].to_s).not_to eq ''
+    end
+
+    it "postgresql/vip or backend_vips/ipaddress" do
+      if config['role'] == 'standalone'
+        expect(config['postgresql']['vip'].to_s).not_to eq ''
+      else
+        expect(config['backend_vips']['ipaddress'].to_s).not_to eq ''
+      end
+    end
+  end
+
+  context "oc-reporting-pedant" do
+
+    it "api_version" do
+      expect(config['api_version'].to_s).not_to eq ''
+    end
+
+    it "nginx/ssl_protocols" do
+      expect(config['nginx']['ssl_protocols'].to_s).to_not eq ''
+    end
+  end
+
+  context "nginx" do
+
+    it "nginx/dir" do
+      expect(File.exists?(File.join(config['nginx']['dir'], "etc/addon.d"))).to be true
+    end
+  end
+
+  context "providers/pg_upgrade" do
+
+    it "postgresql/data_dir" do
+      expect(File.exists?(config['postgresql']['data_dir'])).to be true
+    end
+
+    it "postgresql/username" do
+      expect(config['postgresql']['username'].to_s).to_not eq ''
+    end
+  end
+
+  context "rabbitmq" do
+
+    it "user/username" do
+      expect(config['user']['username'].to_s).to_not eq ''
+    end
+
+    it "rabbitmq/actions_vhost" do
+      expect(config['rabbitmq']['actions_vhost'].to_s).to_not eq ''
+    end
+  end
+
+  context "pedant-config" do
+
+    it "lb/api_fqdn" do
+      expect(config['lb']['api_fqdn'].to_s).to_not eq ''
+    end
+
+    it "opscode-erchef/search_provider" do
+      expect(config['opscode-erchef']['search_provider'].to_s).to eq('solr').or eq('elasticsearch')
+    end
+
+    it "opscode-erchef/listen" do
+      expect(config['opscode-erchef']['listen'].to_s).not_to eq ''
+    end
+
+    it "opscode-erchef/port" do
+      expect(config['opscode-erchef']['port'].to_i).not_to eq 0
+    end
+  end
+
+  context "oc-reporting-config" do
+
+    it "oc_bifrost/vip" do
+      expect(config['oc_bifrost']['vip'].to_s).not_to eq ''
+    end
+
+    it "oc_bifrost/port" do
+      expect(config['oc_bifrost']['port'].to_i).not_to eq 0
+    end
+
+    it "couchdb/vip" do
+      expect(config['couchdb']['vip'].to_s).not_to eq ''
+    end
+
+    it "couchdb/port" do
+      expect(config['couchdb']['port'].to_i).not_to eq 0
+    end
+
+    it "nginx/enable_ipv6" do
+      expect(config['nginx']['enable_ipv6']).to be(true).or be(false)
+    end
+
+    it "opscode-erchef/search_provider" do
+      expect(config['opscode-erchef']['search_provider'].to_s).to eq('solr').or eq('elasticsearch')
+    end
+
+    it "opscode-erchef/solr_timeout" do
+      expect(config['opscode-erchef']['solr_timeout'].to_i).not_to eq 0
+    end
+
+    it "opscode-erchef/solr_http_init_count" do
+      expect(config['opscode-erchef']['solr_http_init_count'].to_i).not_to eq 0
+    end
+
+    it "opscode-erchef/solr_http_max_count" do
+      expect(config['opscode-erchef']['solr_http_max_count'].to_i).not_to eq 0
+    end
+
+    it "opscode-erchef/solr_http_cull_interval" do
+      expect(config['opscode-erchef']['solr_http_cull_interval'].to_s).not_to eq ''
+    end
+
+    it "opscode-erchef/solr_http_max_age" do
+      expect(config['opscode-erchef']['solr_http_max_age'].to_s).not_to eq ''
+    end
+
+    it "opscode-erchef/solr_http_max_connection_duration" do
+      expect(config['opscode-erchef']['solr_http_max_connection_duration'].to_s).not_to eq ''
+    end
+
+    it "opscode-erchef/solr_ibrowse_options" do
+      expect(config['opscode-erchef']['solr_ibrowse_options'].to_s).not_to eq ''
+    end
+
+    it "lb/api_fqdn" do
+      expect(config['lb']['api_fqdn'].to_s).not_to eq ''
+    end
+
+    it "lb/vip" do
+      expect(config['lb']['vip'].to_s).not_to eq ''
+    end
+
+    it "lb_internal/chef_port" do
+      expect(config['lb_internal']['chef_port'].to_i).not_to eq 0
+    end
+
+    it "estatsd/vip" do
+      expect(config['estatsd']['vip'].to_s).not_to eq ''
+    end
+
+    it "estatsd/port" do
+      expect(config['estatsd']['port'].to_i).not_to eq 0
+    end
+  end
+end
