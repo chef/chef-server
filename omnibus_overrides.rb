125e729d92e2594aaddf1f7534451fb1742a09eb
Stephan Renatus <srenatus@chef.io>
2017-06-30 09:09:24 +0200

    [omnibus] bump ohai and chef to 13.1.*
    Signed-off-by: Stephan Renatus <srenatus@chef.io>

--- omnibus_overrides.rb
+++ omnibus_overrides.rb
@@ -4,8 +4,8 @@ build_iteration 1
 override :erlang, version: "18.3"
 override :lua, version: "5.1.5"
 override :'omnibus-ctl', version: "master"
-override :chef, version: "v12.19.36"
-override :ohai, version: "v8.23.0"
+override :chef, version: "v13.1.31"
+override :ohai, version: "v13.1.0"
 override :ruby, version: "2.4.1"
 override :rubygems, version: "2.6.8"
 # This SHA is the last commit before the 6.0 release
