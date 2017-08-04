Ashish055/chef_repo

Refactor chef_wm_rabbitmq_management to be able to deal with multiple vips
The current chef server configuration allows people to point actions at
a rabbitmq that is not the same as the chef_index one. This change will
allow us to check the health of each one.

src/oc_erchef/apps/oc_chef_wm/itest/chef_wm_actions_queue_monitoring_SUITE.erl
@@ -35,7 +35,7 @@
 -define(ACTIONS_EXCHANGE, <<"actions">>).
 
 -define(Q_SETTING(K, V),
-        chef_wm_rabbitmq_management:set_rabbit_queue_monitor_setting(K,V)).
+        oc_chef_action_queue:set_rabbit_queue_monitor_setting(K,V)).
 
 init_per_suite(InitialConfig) ->
     UseFakeRabbit =
@@ -134,7 +134,7 @@ basic_queue_monitor(Config) ->
 
 
     % don't drop messages from now on
-    chef_wm_rabbitmq_management:set_rabbit_queue_monitor_setting(drop_on_full_capacity, false),
+    oc_chef_action_queue:set_rabbit_queue_monitor_setting(drop_on_full_capacity, false),
 
     make_data_bag(?CLIENT_NAME, 13),
     make_data_bag(?CLIENT_NAME, 14),
@@ -172,7 +172,7 @@ queue_full_dont_start(Config) ->
     case ?config(use_fake_rabbit, Config) of
         true ->
             meck:expect(chef_wm_rabbitmq_management, sync_check_queue_at_capacity,
-                        fun(_Vhost, _Queue) ->
+                        fun(_PoolNameAtom, _Vhost, _Queue) ->
                                 {MaxLength, MaxLength, true}
                         end);
         false -> ok
@@ -347,12 +347,12 @@ setup_rabbit_fake(Config) ->
 
 
     meck:expect(chef_wm_rabbitmq_management, get_max_length,
-                fun(_Vhost) ->
+                fun(_PoolNameAtom, _Vhost) ->
                         ?config(max_length, Config)
                 end),
 
     meck:expect(chef_wm_rabbitmq_management, get_current_length,
-                fun(_Vhost, _Queue) ->
+                fun(_PoolNameAtom, _Vhost, _Queue) ->
                         fake_rabbit_current_length()
                 end),
     ok.

src/oc_erchef/apps/oc_chef_wm/src/chef_wm_actions_queue_monitoring.erl
@@ -88,7 +88,7 @@
 -define(QUEUE, <<"alaska">>).
 -define(GEN_SERVER_TIMEOUT_MILLIS, 5000).
 -define(QUEUE_MONITOR_RUN_EVERY_MILLIS, 60000).
--define(QUEUE_MONITOR_SETTING(Key, Default), chef_wm_rabbitmq_management:get_rabbit_queue_monitor_setting(Key, Default)).
+-define(QUEUE_MONITOR_SETTING(Key, Default), oc_chef_action_queue:get_rabbit_queue_monitor_setting(Key, Default)).
 
 -record(queue_monitor_state, {
                 % name of the rabbitmq vhost to monitor, as a string
@@ -306,9 +306,11 @@ handle_info(status_ping, #queue_monitor_state{
     Pid = spawn_link(
             fun () ->
                     Result =
-                      chef_wm_rabbitmq_management:check_current_queue_state(Vhost,
-                                                                            Queue,
-                                                                            Dropped),
+                      chef_wm_rabbitmq_management:check_current_queue_state(
+                        oc_chef_action_queue:get_rabbit_management_pool_name(),
+                        Vhost,
+                        Queue,
+                        Dropped),
                     ParentPid ! Result
             end),
     {noreply, State#queue_monitor_state{worker_process=Pid}};

src/oc_erchef/apps/oc_chef_wm/src/chef_wm_status.erl
@@ -61,7 +61,7 @@ check_health() ->
     Status = overall_status(Pings),
 
     QueueMonStatus =
-        case chef_wm_rabbitmq_management:get_rabbit_queue_monitor_setting(queue_length_monitor_enabled, false) of
+        case oc_chef_action_queue:get_rabbit_queue_monitor_setting(queue_length_monitor_enabled, false) of
             false -> % chef_wm_actions_queue_monitoring isn't running, skip it
                     [];
             true -> AnalyticsQ = chef_wm_actions_queue_monitoring:status(),
@@ -92,7 +92,7 @@ overall_status(Pings) ->
 
 -spec is_analytics_queue_at_capacity() -> boolean().
 is_analytics_queue_at_capacity() ->
-    case chef_wm_rabbitmq_management:get_rabbit_queue_monitor_setting(queue_length_monitor_enabled, false) of
+    case oc_chef_action_queue:get_rabbit_queue_monitor_setting(queue_length_monitor_enabled, false) of
         % don't try to connect to the queue monitor if it isn't running
         true -> chef_wm_actions_queue_monitoring:is_queue_at_capacity();
         false -> false
@@ -100,7 +100,7 @@ is_analytics_queue_at_capacity() ->
 
 -spec queue_at_capacity_affects_overall_status() -> boolean().
 queue_at_capacity_affects_overall_status() ->
-    chef_wm_rabbitmq_management:get_rabbit_queue_monitor_setting(queue_at_capacity_affects_overall_status, false).
+    oc_chef_action_queue:get_rabbit_queue_monitor_setting(queue_at_capacity_affects_overall_status, false).
 
 
 -spec log_failure(fail | pong, [{binary(), <<_:32>>}], list()) -> ok.

src/oc_erchef/apps/oc_chef_wm/src/oc_chef_action.erl
@@ -174,7 +174,7 @@ maybe_add_remote_request_id(Msg, RemoteRequestId) ->
               Msg :: binary()) -> ok.
 publish(RoutingKey, Msg)->
     QueueMonitorEnabled =
-      chef_wm_rabbitmq_management:get_rabbit_queue_monitor_setting(queue_length_monitor_enabled, false),
+      oc_chef_action_queue:get_rabbit_queue_monitor_setting(queue_length_monitor_enabled, false),
     publish(RoutingKey, Msg, QueueMonitorEnabled).
 
 
@@ -185,7 +185,7 @@ publish(RoutingKey, Msg, false) ->
       oc_chef_action_queue:publish(RoutingKey, Msg);
 publish(RoutingKey, Msg, true) ->
     DropOnCapacity =
-      chef_wm_rabbitmq_management:get_rabbit_queue_monitor_setting(drop_on_full_capacity, true),
+      oc_chef_action_queue:get_rabbit_queue_monitor_setting(drop_on_full_capacity, true),
     case DropOnCapacity andalso chef_wm_actions_queue_monitoring:is_queue_at_capacity() of
         true ->
             chef_wm_actions_queue_monitoring:message_dropped();
@@ -399,7 +399,8 @@ req_header(Name, Req) ->
 -spec ping() -> pong | pang.
 ping() ->
     VHost = envy:get(oc_chef_wm, actions_vhost, binary),
-    case chef_wm_rabbitmq_management:check_aliveness(binary_to_list(VHost)) of
+    case chef_wm_rabbitmq_management:check_aliveness(
+           oc_chef_action_queue:get_rabbit_management_pool_name(), binary_to_list(VHost)) of
         true -> pong;
         _ -> pang
     end.

src/oc_erchef/apps/oc_chef_wm/test/chef_wm_actions_queue_monitoring_tests.erl
@@ -332,8 +332,8 @@ queue_length_test_() ->
       {"check_publish_not_at_capacity",
        fun() ->
             % ensure the publish function is called and no messages are dropped
-            chef_wm_rabbitmq_management:set_rabbit_queue_monitor_setting(queue_length_monitor_enabled, true),
-            chef_wm_rabbitmq_management:set_rabbit_queue_monitor_setting(drop_on_full_capacity, true),
+            oc_chef_action_queue:set_rabbit_queue_monitor_setting(queue_length_monitor_enabled, true),
+            oc_chef_action_queue:set_rabbit_queue_monitor_setting(drop_on_full_capacity, true),
 
             meck:new(oc_chef_action_queue),
             meck:expect(oc_chef_action_queue, publish, fun (_, _) -> ok end),
@@ -350,8 +350,8 @@ queue_length_test_() ->
        fun() ->
             %% ensure the publish function is called and 1 message is dropped
             %% due to queue being at capacity
-          chef_wm_rabbitmq_management:set_rabbit_queue_monitor_setting(queue_length_monitor_enabled, true),
-            chef_wm_rabbitmq_management:set_rabbit_queue_monitor_setting(drop_on_full_capacity, true),
+            oc_chef_action_queue:set_rabbit_queue_monitor_setting(queue_length_monitor_enabled, true),
+            oc_chef_action_queue:set_rabbit_queue_monitor_setting(drop_on_full_capacity, true),
 
             meck_response("200", max_length_json(), "200", at_capacity_json()),
             %% ensure that the queue is at capacity before calling
@@ -382,8 +382,8 @@ queue_length_test_() ->
             %% due to queue being at capacity, reset queue length to 0
             %% and 1 message should be published
 
-            chef_wm_rabbitmq_management:set_rabbit_queue_monitor_setting(queue_length_monitor_enabled, true),
-            chef_wm_rabbitmq_management:set_rabbit_queue_monitor_setting(drop_on_full_capacity, true),
+            oc_chef_action_queue:set_rabbit_queue_monitor_setting(queue_length_monitor_enabled, true),
+            oc_chef_action_queue:set_rabbit_queue_monitor_setting(drop_on_full_capacity, true),
 
             meck_response("200", max_length_json(), "200", at_capacity_json()),
             %% ensure that the queue is at capacity before calling
@@ -425,8 +425,8 @@ queue_length_test_() ->
       {"check_publish_at_capacity_no_drop",
        fun() ->
           % queue is at capacity, but don't drop messages due to configuration
-          chef_wm_rabbitmq_management:set_rabbit_queue_monitor_setting(queue_length_monitor_enabled, true),
-          chef_wm_rabbitmq_management:set_rabbit_queue_monitor_setting(drop_on_full_capacity, false),
+          oc_chef_action_queue:set_rabbit_queue_monitor_setting(queue_length_monitor_enabled, true),
+          oc_chef_action_queue:set_rabbit_queue_monitor_setting(drop_on_full_capacity, false),
 
           meck_response("200", max_length_json(), "200", at_capacity_json()),
           %% ensure that the queue is at capacity before calling
@@ -451,8 +451,8 @@ queue_length_test_() ->
           % queue length monitor is disabled
           catch(chef_wm_actions_queue_monitoring:stop()),
 
-          chef_wm_rabbitmq_management:set_rabbit_queue_monitor_setting(queue_length_monitor_enabled, false),
-          chef_wm_rabbitmq_management:set_rabbit_queue_monitor_setting(drop_on_full_capacity, false),
+          oc_chef_action_queue:set_rabbit_queue_monitor_setting(queue_length_monitor_enabled, false),
+          oc_chef_action_queue:set_rabbit_queue_monitor_setting(drop_on_full_capacity, false),
 
           undefined = whereis(chef_wm_actions_queue_monitoring),
           meck:new(oc_chef_action_queue),
@@ -470,7 +470,7 @@ queue_length_test_() ->
        fun() ->
             % set oc_httpc to sleep for 100 millis, but have is_queue_at_capacity timeout
             % after 10 millis.
-            chef_wm_rabbitmq_management:set_rabbit_queue_monitor_setting(queue_length_monitor_timeout_millis, 0),
+            oc_chef_action_queue:set_rabbit_queue_monitor_setting(queue_length_monitor_timeout_millis, 0),
 
             ?assertEqual(true, chef_wm_actions_queue_monitoring:is_queue_at_capacity())
        end

src/oc_erchef/apps/oc_chef_wm/test/chef_wm_status_tests.erl
@@ -44,7 +44,7 @@ setup_env() ->
     application:set_env(oc_chef_wm, health_ping_timeout, ?PING_TIMEOUT),
     application:set_env(oc_chef_wm, health_ping_modules, ?CHECK_MODS),
     application:set_env(oc_chef_wm, rabbitmq, default_config()),
-    chef_wm_rabbitmq_management:set_rabbit_queue_monitor_setting(queue_length_monitor_enabled, false),
+    oc_chef_action_queue:set_rabbit_queue_monitor_setting(queue_length_monitor_enabled, false),
     ok.
 
 cleanup_env() ->
@@ -222,7 +222,7 @@ check_queue_mon_affects_overall_status_test_() ->
       fun() ->
               % overall_status will be fail as the queue is at capacity as
               % queue_at_capacity_affects_overall_status is true
-              chef_wm_rabbitmq_management:set_rabbit_queue_monitor_setting(queue_at_capacity_affects_overall_status, true),
+              oc_chef_action_queue:set_rabbit_queue_monitor_setting(queue_at_capacity_affects_overall_status, true),
               {Status, Json} = chef_wm_status:check_health(),
               ?assertEqual(fail, Status),
               Ejson = chef_json:decode(Json),
@@ -234,7 +234,7 @@ check_queue_mon_affects_overall_status_test_() ->
       fun() ->
               % overall_status will be pong as the queue is at capacity, but
               % queue_at_capacity_affects_overall_status is false
-              chef_wm_rabbitmq_management:set_rabbit_queue_monitor_setting(queue_at_capacity_affects_overall_status, false),
+              oc_chef_action_queue:set_rabbit_queue_monitor_setting(queue_at_capacity_affects_overall_status, false),
               {Status, Json} = chef_wm_status:check_health(),
               ?assertEqual(pong, Status),
               Ejson = chef_json:decode(Json),
@@ -246,8 +246,8 @@ check_queue_mon_affects_overall_status_test_() ->
       fun() ->
               % overall_status will be pong as the queue is at capacity, but the queue monitor is
               % disabled
-              chef_wm_rabbitmq_management:set_rabbit_queue_monitor_setting(queue_at_capacity_affects_overall_status, false),
-              chef_wm_rabbitmq_management:set_rabbit_queue_monitor_setting(queue_length_monitor_enabled, false),
+              oc_chef_action_queue:set_rabbit_queue_monitor_setting(queue_at_capacity_affects_overall_status, false),
+              oc_chef_action_queue:set_rabbit_queue_monitor_setting(queue_length_monitor_enabled, false),
               {Status, Json} = chef_wm_status:check_health(),
               ?assertEqual(pong, Status),
               Ejson = chef_json:decode(Json),
@@ -258,8 +258,8 @@ check_queue_mon_affects_overall_status_test_() ->
       end,
       fun() ->
               % overall_status not affected as the queue monitor is disabled
-              chef_wm_rabbitmq_management:set_rabbit_queue_monitor_setting(queue_at_capacity_affects_overall_status, true),
-              chef_wm_rabbitmq_management:set_rabbit_queue_monitor_setting(queue_length_monitor_enabled, false),
+              oc_chef_action_queue:set_rabbit_queue_monitor_setting(queue_at_capacity_affects_overall_status, true),
+              oc_chef_action_queue:set_rabbit_queue_monitor_setting(queue_length_monitor_enabled, false),
               {Status, Json} = chef_wm_status:check_health(),
               ?assertEqual(pong, Status),
               Ejson = chef_json:decode(Json),

chef/chef-server

Signed-off-by: Jay Mundrawala <jmundrawala@chef.io>

src/oc_erchef/apps/oc_chef_wm/itest/chef_wm_actions_queue_monitoring_SUITE.erl
@@ -35,7 +35,7 @@
 -define(ACTIONS_EXCHANGE, <<"actions">>).
 
 -define(Q_SETTING(K, V),
-        chef_wm_rabbitmq_management:set_rabbit_queue_monitor_setting(K,V)).
+        oc_chef_action_queue_config:set_rabbit_queue_monitor_setting(K,V)).
 
 init_per_suite(InitialConfig) ->
     UseFakeRabbit =
@@ -134,7 +134,7 @@ basic_queue_monitor(Config) ->
 
 
     % don't drop messages from now on
-    chef_wm_rabbitmq_management:set_rabbit_queue_monitor_setting(drop_on_full_capacity, false),
+    oc_chef_action_queue_config:set_rabbit_queue_monitor_setting(drop_on_full_capacity, false),
 
     make_data_bag(?CLIENT_NAME, 13),
     make_data_bag(?CLIENT_NAME, 14),
@@ -172,7 +172,7 @@ queue_full_dont_start(Config) ->
     case ?config(use_fake_rabbit, Config) of
         true ->
             meck:expect(chef_wm_rabbitmq_management, sync_check_queue_at_capacity,
-                        fun(_Vhost, _Queue) ->
+                        fun(_PoolNameAtom, _Vhost, _Queue) ->
                                 {MaxLength, MaxLength, true}
                         end);
         false -> ok
@@ -347,12 +347,12 @@ setup_rabbit_fake(Config) ->
 
 
     meck:expect(chef_wm_rabbitmq_management, get_max_length,
-                fun(_Vhost) ->
+                fun(_PoolNameAtom, _Vhost) ->
                         ?config(max_length, Config)
                 end),
 
     meck:expect(chef_wm_rabbitmq_management, get_current_length,
-                fun(_Vhost, _Queue) ->
+                fun(_PoolNameAtom, _Vhost, _Queue) ->
                         fake_rabbit_current_length()
                 end),
     ok.

src/oc_erchef/apps/oc_chef_wm/src/chef_wm_actions_queue_monitoring.erl
@@ -88,7 +88,7 @@
 -define(QUEUE, <<"alaska">>).
 -define(GEN_SERVER_TIMEOUT_MILLIS, 5000).
 -define(QUEUE_MONITOR_RUN_EVERY_MILLIS, 60000).
--define(QUEUE_MONITOR_SETTING(Key, Default), chef_wm_rabbitmq_management:get_rabbit_queue_monitor_setting(Key, Default)).
+-define(QUEUE_MONITOR_SETTING(Key, Default), oc_chef_action_queue_config:get_rabbit_queue_monitor_setting(Key, Default)).
 
 -record(queue_monitor_state, {
                 % name of the rabbitmq vhost to monitor, as a string
@@ -306,9 +306,11 @@ handle_info(status_ping, #queue_monitor_state{
     Pid = spawn_link(
             fun () ->
                     Result =
-                      chef_wm_rabbitmq_management:check_current_queue_state(Vhost,
-                                                                            Queue,
-                                                                            Dropped),
+                      chef_wm_rabbitmq_management:check_current_queue_state(
+                        oc_chef_action_queue_config:get_rabbit_management_pool_name(),
+                        Vhost,
+                        Queue,
+                        Dropped),
                     ParentPid ! Result
             end),
     {noreply, State#queue_monitor_state{worker_process=Pid}};

src/oc_erchef/apps/oc_chef_wm/src/chef_wm_status.erl
@@ -61,7 +61,7 @@ check_health() ->
     Status = overall_status(Pings),
 
     QueueMonStatus =
-        case chef_wm_rabbitmq_management:get_rabbit_queue_monitor_setting(queue_length_monitor_enabled, false) of
+        case oc_chef_action_queue_config:get_rabbit_queue_monitor_setting(queue_length_monitor_enabled, false) of
             false -> % chef_wm_actions_queue_monitoring isn't running, skip it
                     [];
             true -> AnalyticsQ = chef_wm_actions_queue_monitoring:status(),
@@ -92,7 +92,7 @@ overall_status(Pings) ->
 
 -spec is_analytics_queue_at_capacity() -> boolean().
 is_analytics_queue_at_capacity() ->
-    case chef_wm_rabbitmq_management:get_rabbit_queue_monitor_setting(queue_length_monitor_enabled, false) of
+    case oc_chef_action_queue_config:get_rabbit_queue_monitor_setting(queue_length_monitor_enabled, false) of
         % don't try to connect to the queue monitor if it isn't running
         true -> chef_wm_actions_queue_monitoring:is_queue_at_capacity();
         false -> false
@@ -100,7 +100,7 @@ is_analytics_queue_at_capacity() ->
 
 -spec queue_at_capacity_affects_overall_status() -> boolean().
 queue_at_capacity_affects_overall_status() ->
-    chef_wm_rabbitmq_management:get_rabbit_queue_monitor_setting(queue_at_capacity_affects_overall_status, false).
+    oc_chef_action_queue_config:get_rabbit_queue_monitor_setting(queue_at_capacity_affects_overall_status, false).
 
 
 -spec log_failure(fail | pong, [{binary(), <<_:32>>}], list()) -> ok.

src/oc_erchef/apps/oc_chef_wm/src/oc_chef_action.erl
@@ -174,7 +174,7 @@ maybe_add_remote_request_id(Msg, RemoteRequestId) ->
               Msg :: binary()) -> ok.
 publish(RoutingKey, Msg)->
     QueueMonitorEnabled =
-      chef_wm_rabbitmq_management:get_rabbit_queue_monitor_setting(queue_length_monitor_enabled, false),
+      oc_chef_action_queue_config:get_rabbit_queue_monitor_setting(queue_length_monitor_enabled, false),
     publish(RoutingKey, Msg, QueueMonitorEnabled).
 
 
@@ -185,7 +185,7 @@ publish(RoutingKey, Msg, false) ->
       oc_chef_action_queue:publish(RoutingKey, Msg);
 publish(RoutingKey, Msg, true) ->
     DropOnCapacity =
-      chef_wm_rabbitmq_management:get_rabbit_queue_monitor_setting(drop_on_full_capacity, true),
+      oc_chef_action_queue_config:get_rabbit_queue_monitor_setting(drop_on_full_capacity, true),
     case DropOnCapacity andalso chef_wm_actions_queue_monitoring:is_queue_at_capacity() of
         true ->
             chef_wm_actions_queue_monitoring:message_dropped();
@@ -399,7 +399,8 @@ req_header(Name, Req) ->
 -spec ping() -> pong | pang.
 ping() ->
     VHost = envy:get(oc_chef_wm, actions_vhost, binary),
-    case chef_wm_rabbitmq_management:check_aliveness(binary_to_list(VHost)) of
+    case chef_wm_rabbitmq_management:check_aliveness(
+           oc_chef_action_queue_config:get_rabbit_management_pool_name(), binary_to_list(VHost)) of
         true -> pong;
         _ -> pang
     end.

src/oc_erchef/apps/oc_chef_wm/test/chef_wm_actions_queue_monitoring_tests.erl
@@ -332,8 +332,8 @@ queue_length_test_() ->
       {"check_publish_not_at_capacity",
        fun() ->
             % ensure the publish function is called and no messages are dropped
-            chef_wm_rabbitmq_management:set_rabbit_queue_monitor_setting(queue_length_monitor_enabled, true),
-            chef_wm_rabbitmq_management:set_rabbit_queue_monitor_setting(drop_on_full_capacity, true),
+            oc_chef_action_queue_config:set_rabbit_queue_monitor_setting(queue_length_monitor_enabled, true),
+            oc_chef_action_queue_config:set_rabbit_queue_monitor_setting(drop_on_full_capacity, true),
 
             meck:new(oc_chef_action_queue),
             meck:expect(oc_chef_action_queue, publish, fun (_, _) -> ok end),
@@ -350,8 +350,8 @@ queue_length_test_() ->
        fun() ->
             %% ensure the publish function is called and 1 message is dropped
             %% due to queue being at capacity
-          chef_wm_rabbitmq_management:set_rabbit_queue_monitor_setting(queue_length_monitor_enabled, true),
-            chef_wm_rabbitmq_management:set_rabbit_queue_monitor_setting(drop_on_full_capacity, true),
+            oc_chef_action_queue_config:set_rabbit_queue_monitor_setting(queue_length_monitor_enabled, true),
+            oc_chef_action_queue_config:set_rabbit_queue_monitor_setting(drop_on_full_capacity, true),
 
             meck_response("200", max_length_json(), "200", at_capacity_json()),
             %% ensure that the queue is at capacity before calling
@@ -382,8 +382,8 @@ queue_length_test_() ->
             %% due to queue being at capacity, reset queue length to 0
             %% and 1 message should be published
 
-            chef_wm_rabbitmq_management:set_rabbit_queue_monitor_setting(queue_length_monitor_enabled, true),
-            chef_wm_rabbitmq_management:set_rabbit_queue_monitor_setting(drop_on_full_capacity, true),
+            oc_chef_action_queue_config:set_rabbit_queue_monitor_setting(queue_length_monitor_enabled, true),
+            oc_chef_action_queue_config:set_rabbit_queue_monitor_setting(drop_on_full_capacity, true),
 
             meck_response("200", max_length_json(), "200", at_capacity_json()),
             %% ensure that the queue is at capacity before calling
@@ -425,8 +425,8 @@ queue_length_test_() ->
       {"check_publish_at_capacity_no_drop",
        fun() ->
           % queue is at capacity, but don't drop messages due to configuration
-          chef_wm_rabbitmq_management:set_rabbit_queue_monitor_setting(queue_length_monitor_enabled, true),
-          chef_wm_rabbitmq_management:set_rabbit_queue_monitor_setting(drop_on_full_capacity, false),
+          oc_chef_action_queue_config:set_rabbit_queue_monitor_setting(queue_length_monitor_enabled, true),
+          oc_chef_action_queue_config:set_rabbit_queue_monitor_setting(drop_on_full_capacity, false),
 
           meck_response("200", max_length_json(), "200", at_capacity_json()),
           %% ensure that the queue is at capacity before calling
@@ -451,8 +451,8 @@ queue_length_test_() ->
           % queue length monitor is disabled
           catch(chef_wm_actions_queue_monitoring:stop()),
 
-          chef_wm_rabbitmq_management:set_rabbit_queue_monitor_setting(queue_length_monitor_enabled, false),
-          chef_wm_rabbitmq_management:set_rabbit_queue_monitor_setting(drop_on_full_capacity, false),
+          oc_chef_action_queue_config:set_rabbit_queue_monitor_setting(queue_length_monitor_enabled, false),
+          oc_chef_action_queue_config:set_rabbit_queue_monitor_setting(drop_on_full_capacity, false),
 
           undefined = whereis(chef_wm_actions_queue_monitoring),
           meck:new(oc_chef_action_queue),
@@ -470,7 +470,7 @@ queue_length_test_() ->
        fun() ->
             % set oc_httpc to sleep for 100 millis, but have is_queue_at_capacity timeout
             % after 10 millis.
-            chef_wm_rabbitmq_management:set_rabbit_queue_monitor_setting(queue_length_monitor_timeout_millis, 0),
+            oc_chef_action_queue_config:set_rabbit_queue_monitor_setting(queue_length_monitor_timeout_millis, 0),
 
             ?assertEqual(true, chef_wm_actions_queue_monitoring:is_queue_at_capacity())
        end

src/oc_erchef/apps/oc_chef_wm/test/chef_wm_status_tests.erl
@@ -44,7 +44,7 @@ setup_env() ->
     application:set_env(oc_chef_wm, health_ping_timeout, ?PING_TIMEOUT),
     application:set_env(oc_chef_wm, health_ping_modules, ?CHECK_MODS),
     application:set_env(oc_chef_wm, rabbitmq, default_config()),
-    chef_wm_rabbitmq_management:set_rabbit_queue_monitor_setting(queue_length_monitor_enabled, false),
+    oc_chef_action_queue_config:set_rabbit_queue_monitor_setting(queue_length_monitor_enabled, false),
     ok.
 
 cleanup_env() ->
@@ -222,7 +222,7 @@ check_queue_mon_affects_overall_status_test_() ->
       fun() ->
               % overall_status will be fail as the queue is at capacity as
               % queue_at_capacity_affects_overall_status is true
-              chef_wm_rabbitmq_management:set_rabbit_queue_monitor_setting(queue_at_capacity_affects_overall_status, true),
+              oc_chef_action_queue_config:set_rabbit_queue_monitor_setting(queue_at_capacity_affects_overall_status, true),
               {Status, Json} = chef_wm_status:check_health(),
               ?assertEqual(fail, Status),
               Ejson = chef_json:decode(Json),
@@ -234,7 +234,7 @@ check_queue_mon_affects_overall_status_test_() ->
       fun() ->
               % overall_status will be pong as the queue is at capacity, but
               % queue_at_capacity_affects_overall_status is false
-              chef_wm_rabbitmq_management:set_rabbit_queue_monitor_setting(queue_at_capacity_affects_overall_status, false),
+              oc_chef_action_queue_config:set_rabbit_queue_monitor_setting(queue_at_capacity_affects_overall_status, false),
               {Status, Json} = chef_wm_status:check_health(),
               ?assertEqual(pong, Status),
               Ejson = chef_json:decode(Json),
@@ -246,8 +246,8 @@ check_queue_mon_affects_overall_status_test_() ->
       fun() ->
               % overall_status will be pong as the queue is at capacity, but the queue monitor is
               % disabled
-              chef_wm_rabbitmq_management:set_rabbit_queue_monitor_setting(queue_at_capacity_affects_overall_status, false),
-              chef_wm_rabbitmq_management:set_rabbit_queue_monitor_setting(queue_length_monitor_enabled, false),
+              oc_chef_action_queue_config:set_rabbit_queue_monitor_setting(queue_at_capacity_affects_overall_status, false),
+              oc_chef_action_queue_config:set_rabbit_queue_monitor_setting(queue_length_monitor_enabled, false),
               {Status, Json} = chef_wm_status:check_health(),
               ?assertEqual(pong, Status),
               Ejson = chef_json:decode(Json),
@@ -258,8 +258,8 @@ check_queue_mon_affects_overall_status_test_() ->
       end,
       fun() ->
               % overall_status not affected as the queue monitor is disabled
-              chef_wm_rabbitmq_management:set_rabbit_queue_monitor_setting(queue_at_capacity_affects_overall_status, true),
-              chef_wm_rabbitmq_management:set_rabbit_queue_monitor_setting(queue_length_monitor_enabled, false),
+              oc_chef_action_queue_config:set_rabbit_queue_monitor_setting(queue_at_capacity_affects_overall_status, true),
+              oc_chef_action_queue_config:set_rabbit_queue_monitor_setting(queue_length_monitor_enabled, false),
               {Status, Json} = chef_wm_status:check_health(),
               ?assertEqual(pong, Status),
               Ejson = chef_json:decode(Json),