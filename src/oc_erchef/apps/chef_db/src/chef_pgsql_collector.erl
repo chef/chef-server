ba4b83958bcf5e0f7be5d6ce3b08d190d68d73cc
Jay Mundrawala <jdmundrawala@gmail.com>
2017-08-24 17:50:12 -0500

    Added pgsql metrics
    Example below:
    
    ```
      # TYPE pg_stat_seq_scan counter
      # HELP pg_stat_seq_scan Number of sequential scans initiated
      pg_stat_seq_scan 5047095
      # TYPE pg_stat_seq_tup_read counter
      # HELP pg_stat_seq_tup_read Number of live rows fetched by sequential scans
      pg_stat_seq_tup_read 415028001
      # TYPE pg_stat_idx_scan counter
      # HELP pg_stat_idx_scan Number of index scans initiated
      pg_stat_idx_scan 23751732
      # TYPE pg_stat_tup_fetch counter
      # HELP pg_stat_tup_fetch Number of live rows fetched by index scans
      pg_stat_tup_fetch 25953870
      # TYPE pg_stat_n_tup_ins counter
      # HELP pg_stat_n_tup_ins Number of rows inserted
      pg_stat_n_tup_ins 52452
      # TYPE pg_stat_n_tup_upd counter
      # HELP pg_stat_n_tup_upd Number of rows updated
      pg_stat_n_tup_upd 73461
      # TYPE pg_stat_n_tup_del counter
      # HELP pg_stat_n_tup_del Number of rows deleted
      pg_stat_n_tup_del 31020
      # TYPE pg_stat_n_live_tup gauge
      # HELP pg_stat_n_live_tup Estimated number of live rows
      pg_stat_n_live_tup 92355
      # TYPE pg_stat_n_dead_tup gauge
      # HELP pg_stat_n_dead_tup Estimated number of dead rows
      pg_stat_n_dead_tup 29046
      # TYPE pg_stat_heap_blocks_read counter
      # HELP pg_stat_heap_blocks_read Number of disk blocks read
      pg_stat_heap_blocks_read 69231
      # TYPE pg_stat_heap_blocks_hit counter
      # HELP pg_stat_heap_blocks_hit Number of buffer hits
      pg_stat_heap_blocks_hit 38834925
      # TYPE pg_stat_idx_blks_read counter
      # HELP pg_stat_idx_blks_read Number of disk blocks read from all indexes
      pg_stat_idx_blks_read 49632
      # TYPE pg_stat_idx_blks_hit counter
      # HELP pg_stat_idx_blks_hit Number of buffer hits in all indexes
      pg_stat_idx_blks_hit 35615613
      # TYPE pg_stat_toast_blks_read counter
      # HELP pg_stat_toast_blks_read Number of disk blocks read from TOAST tables
      pg_stat_toast_blks_read 1410
      # TYPE pg_stat_toast_blks_hit counter
      # HELP pg_stat_toast_blks_hit Number of buffer hits in TOAST tables
      pg_stat_toast_blks_hit 2397
      # TYPE pg_stat_tidx_blks_read counter
      # HELP pg_stat_tidx_blks_read Number of disk blocks read from TOAST tables
      pg_stat_tidx_blks_read 1410
      # TYPE pg_stat_tidx_blks_hit counter
      # HELP pg_stat_tidx_blks_hit Number of buffer hits in TOAST table indexes
      pg_stat_tidx_blks_hit 3948
    ```

---
+++ src/oc_erchef/apps/chef_db/src/chef_pgsql_collector.erl
@@ -0,0 +1,89 @@
+-module(chef_pgsql_collector).
+
+-export([deregister_cleanup/1,
+         collect_mf/2,
+         collect_metrics/2]).
+
+-import(prometheus_model_helpers, [create_mf/5,
+                                   label_pairs/1,
+                                   gauge_metrics/1,
+                                   gauge_metric/1,
+                                   gauge_metric/2,
+                                   counter_metric/1,
+                                   counter_metric/2]).
+
+-behaviour(prometheus_collector).
+
+-define(METRICS, [
+                  {<<"seq_scan">>, {pg_stat_seq_scan, counter, "Number of sequential scans initiated", fun erlang:binary_to_integer/1}},
+                  {<<"seq_tup_read">>, {pg_stat_seq_tup_read, counter, "Number of live rows fetched by sequential scans", fun erlang:binary_to_integer/1}},
+                  {<<"idx_scan">>, {pg_stat_idx_scan, counter, "Number of index scans initiated", fun erlang:binary_to_integer/1}},
+                  {<<"idx_tup_fetch">>, {pg_stat_tup_fetch, counter, "Number of live rows fetched by index scans", fun erlang:binary_to_integer/1}},
+                  {<<"n_tup_ins">>, {pg_stat_n_tup_ins, counter, "Number of rows inserted", fun erlang:binary_to_integer/1}},
+                  {<<"n_tup_upd">>, {pg_stat_n_tup_upd, counter, "Number of rows updated", fun erlang:binary_to_integer/1}},
+                  {<<"n_tup_del">>, {pg_stat_n_tup_del, counter, "Number of rows deleted", fun erlang:binary_to_integer/1}},
+                  {<<"n_live_tup">>, {pg_stat_n_live_tup, gauge, "Estimated number of live rows", fun erlang:binary_to_integer/1}},
+                  {<<"n_dead_tup">>, {pg_stat_n_dead_tup, gauge, "Estimated number of dead rows", fun erlang:binary_to_integer/1}},
+                  {<<"heap_blks_read">>, {pg_stat_heap_blocks_read, counter, "Number of disk blocks read", fun erlang:binary_to_integer/1}},
+                  {<<"heap_blks_hit">>, {pg_stat_heap_blocks_hit, counter, "Number of buffer hits", fun erlang:binary_to_integer/1}},
+                  {<<"idx_blks_read">>, {pg_stat_idx_blks_read, counter, "Number of disk blocks read from all indexes", fun erlang:binary_to_integer/1}},
+                  {<<"idx_blks_hit">>, {pg_stat_idx_blks_hit, counter, "Number of buffer hits in all indexes", fun erlang:binary_to_integer/1}},
+                  {<<"toast_blks_read">>, {pg_stat_toast_blks_read, counter, "Number of disk blocks read from TOAST tables", fun erlang:binary_to_integer/1}},
+                  {<<"toast_blks_hit">>, {pg_stat_toast_blks_hit, counter, "Number of buffer hits in TOAST tables", fun erlang:binary_to_integer/1}},
+                  {<<"tidx_blks_read">>, {pg_stat_tidx_blks_read, counter, "Number of disk blocks read from TOAST tables", fun erlang:binary_to_integer/1}},
+                  {<<"tidx_blks_hit">>, {pg_stat_tidx_blks_hit, counter, "Number of buffer hits in TOAST table indexes", fun erlang:binary_to_integer/1}}
+                 ]).
+
+%%====================================================================
+%% Collector API
+%%====================================================================
+
+deregister_cleanup(_) -> ok.
+
+
+collect_mf(_Registry, Callback) ->
+
+    Stats = stats(),
+
+    lists:foreach(fun({ColName, {StatName, StatType, StatHelp, Transform}}) ->
+                          case proplists:get_value(ColName, Stats) of
+                              undefined ->
+                                  ok;
+                              Value ->
+                                  Callback(create_metric(StatName,
+                                                         StatHelp,
+                                                         StatType,
+                                                         {StatType, Transform(Value)}))
+                          end
+                  end, ?METRICS),
+    ok.
+
+collect_metrics(_StatName, {counter, Value}) ->
+    counter_metric(Value);
+collect_metrics(_StatName, {gauge, Value}) ->
+    gauge_metric(Value).
+
+
+create_metric(Name, Help, Type, Data) ->
+    create_mf(Name, Help, Type, ?MODULE, Data).
+
+
+%%====================================================================
+%% Private functions
+%%====================================================================
+
+-spec stats() -> [{binary(), binary()}].
+stats() ->
+    try
+        case sqerl:select(stats, [], first, []) of
+            {ok, Stats} -> Stats;
+            _Else -> throw(error)
+        end
+    catch
+        How:Why ->
+            error_logger:error_report({chef_sql, stats, How, Why}),
+            %% We don't want to propagate errors here. Doing so would mean
+            %% an error would prevent us from getting any metrics.
+            []
+    end.
+
