ac515c0b7cecc05bc8abf9913c6c63df8efb33bb
Steven Danna <steve@chef.io>
2017-09-14 18:05:24 +0100

    Update pins to the latest ruby and rubygems
    Signed-off-by: Steven Danna <steve@chef.io>

--- omnibus_overrides.rb
+++ omnibus_overrides.rb
@@ -3,7 +3,7 @@ override :lua, version: "5.1.5"
 override :'omnibus-ctl', version: "master"
 override :chef, version: "v12.19.36"
 override :ohai, version: "v8.23.0"
-override :ruby, version: "2.2.6"
-override :rubygems, version: "2.6.8"
+override :ruby, version: "2.2.8"
+override :rubygems, version: "2.6.13"
 # This SHA is the last commit before the 6.0 release
 override :'berkshelf-no-depselector', version: '6016ca10b2f46508b1b107264228668776f505d9'
