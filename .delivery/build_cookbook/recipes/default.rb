090a265ade750b56c2b893dd6260c86717eeaedd
Steven Danna <steve@chef.io>
2017-09-20 17:02:37 +0100

    Try a different keyserver
    Signed-off-by: Steven Danna <steve@chef.io>

--- .delivery/build_cookbook/recipes/default.rb
+++ .delivery/build_cookbook/recipes/default.rb
@@ -16,6 +16,10 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
 
+# NOTE(ssd) 2017-09-20: Keyservers are not very reliable. Automate has
+# more luck with MIT, but it fails constantly for me so I'm changing
+# it here.  We have to use override because it is set explicitly in
+# the expeditor-build cookbook at the normal level.
+node.override['chef-apt-docker']['keyserver'] = 'na.pool.sks-keyservers.net'
 include_recipe 'expeditor-build::default'
