6af05ca00f58fdaaf7dc48232d364f880bc0e834
Seth Chisamore <schisamo@chef.io>
2017-11-03 16:50:44 -0400

    audit cookbook is now applied via an optional recipe
    See the following change for details: chef/cd-infrastructure#97
    
    Signed-off-by: Seth Chisamore <schisamo@chef.io>

--- .delivery/build_cookbook/metadata.rb
+++ .delivery/build_cookbook/metadata.rb
@@ -2,7 +2,7 @@ name 'build_cookbook'
 maintainer 'Chef Software Inc Engineering'
 maintainer_email 'engineering@chef.io'
 license 'Apache 2.0'
-version '0.1.1'
+version '0.1.2'
 
 chef_version '>= 12.19'
 
