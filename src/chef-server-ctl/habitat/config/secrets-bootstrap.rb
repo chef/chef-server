def03809ee9b7ee69d9e1b922e4192f04823a044
Irving Popovetsky <irving@chef.io>
2017-11-06 16:55:44 -0800

    Fix TOML and secrets walking/updating logic so now it stabilizes after the initial run

--- src/chef-server-ctl/habitat/config/secrets-bootstrap.rb
+++ src/chef-server-ctl/habitat/config/secrets-bootstrap.rb
@@ -5,15 +5,16 @@ require 'openssl'
 require 'securerandom'
 
 secrets = TOML.load_file('{{pkg.svc_config_path}}/hab-secrets-config.toml')
-new_secrets = {}
-secrets.each do |service, value|
-  new_secrets[service] = {}
+new_secrets = secrets.dup
+new_secrets['secrets'].each do |service, value|
   value.each do |name, _pass|
-    name.each do |k, _v|
-      new_secrets[service][k] = k.end_with?('_key') ? OpenSSL::PKey::RSA.generate(2048).to_s : SecureRandom.hex(32)
+    puts "[DEBUG] service #{service} valname #{name} valpass #{_pass}"
+    if _pass.empty?
+      new_secrets['secrets'][service][name] = name.end_with?('_key') ? OpenSSL::PKey::RSA.generate(2048).to_s : SecureRandom.hex(32)
+      puts "[DEBUG] DID: #{new_secrets['secrets'][service][name]}"
     end
   end
 end
 
-File.write('{{pkg.svc_data_path}}/veil-secrets.toml', TOML::Generator.new(new_secrets).body)
-system "hab config apply oc_erchef.default #{Time.now.getutc.to_i} {{pkg.svc_data_path}}/veil-secrets.toml"
+File.write('{{pkg.svc_data_path}}/hab-secrets-modified.toml', TOML::Generator.new(new_secrets).body)
+system "hab config apply chef-server-ctl.default #{Time.now.getutc.to_i} {{pkg.svc_data_path}}/hab-secrets-modified.toml"
