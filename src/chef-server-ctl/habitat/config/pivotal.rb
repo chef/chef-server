d31ecc4bcc9b8e3ffb682f789eb842a951e1d15f
Irving Popovetsky <irving@chef.io>
2017-11-15 14:49:49 -0800

    Update the pivotal config in the same way as the 'pedant' config
    Signed-off-by: Irving Popovetsky <irving@chef.io>

--- src/chef-server-ctl/habitat/config/pivotal.rb
+++ src/chef-server-ctl/habitat/config/pivotal.rb
@@ -1,6 +1,6 @@
 node_name "pivotal"
-chef_server_url "https://chef-server-nginx:443"
-chef_server_root "https://chef-server-nginx:443"
+chef_server_url "https://172.18.0.1"
+chef_server_root "https://172.18.0.1"
 no_proxy "127.0.0.1"
 client_key "{{pkg.svc_config_path}}/pivotal.pem"
 ssl_verify_mode :verify_none
