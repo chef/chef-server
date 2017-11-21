96257a8a634eec8e8be42393ec57ac49e8718a1c
Mark Anderson <mark@chef.io>
2017-11-20 16:38:01 -0800

    Fix spelling error (pedant)

--- src/chef-server-ctl/habitat/config/oc-chef-pedant.sh
+++ src/chef-server-ctl/habitat/config/oc-chef-pedant.sh
@@ -4,4 +4,4 @@ export SUPERUSER_KEY=`sed ':a;N;$!ba;s/\n/\\n/g' {{pkg.svc_config_path}}/pivotal
 export WEBUI_KEY=`sed ':a;N;$!ba;s/\n/\\n/g' {{pkg.svc_config_path}}/webui_priv.pem`
 
 cd {{pkg.path}}/oc-chef-pedant
-$(hab pkg path "core/bundler")/bin/bundle exec bin/oc-chef-pedant --log-file /dev/null -c {{pkg.svc_config_path}}/pendant_config.rb --focus smoke
+$(hab pkg path "core/bundler")/bin/bundle exec bin/oc-chef-pedant --log-file /dev/null -c {{pkg.svc_config_path}}/pedant_config.rb --focus smoke
