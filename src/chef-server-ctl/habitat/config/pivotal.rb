86220827b4e4df11ba80fc33f622896fd84c55cd
Mark Anderson <mark@chef.io>
2017-12-08 20:40:05 -0800

    Make port configurable for chef-server-ctl

--- src/chef-server-ctl/habitat/config/pivotal.rb
+++ src/chef-server-ctl/habitat/config/pivotal.rb
@@ -1,6 +1,6 @@
 node_name "pivotal"
-chef_server_url "https://{{cfg.chef_server_api.ip}}"
-chef_server_root "https://{{cfg.chef_server_api.ip}}"
+chef_server_url "https://{{cfg.chef_server_api.ip}}:{{cfg.chef_server_api.ssl_port}}"
+chef_server_root "https://{{cfg.chef_server_api.ip}}:{{cfg.chef_server_api.ssl_port}}"
 no_proxy "127.0.0.1"
 client_key "{{pkg.svc_config_path}}/pivotal.pem"
 ssl_verify_mode :verify_none
