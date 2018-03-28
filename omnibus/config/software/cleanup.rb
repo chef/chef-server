5031cd072f9ce8eb18188b0773f0a849f590017c
Mark Anderson <mark@chef.io>
2018-03-27 18:45:23 -0700

    Bulk update Gemfile.lock files.
    We have a lot of stale dependencies in various corners, this is a
    simple refresh of those to clean things up.
    
    Signed-off-by: Mark Anderson <mark@chef.io>

--- omnibus/config/software/cleanup.rb
+++ omnibus/config/software/cleanup.rb
@@ -7,6 +7,10 @@ build do
   # Delete cached .gem files and git checkouts
   delete "#{install_dir}/embedded/lib/ruby/gems/2.2.0/cache/*.gem"
   delete "#{install_dir}/embedded/service/gem/ruby/2.2.0/cache/*.gem"
+  delete "#{install_dir}/embedded/lib/ruby/gems/2.3.0/cache/*.gem"
+  delete "#{install_dir}/embedded/service/gem/ruby/2.3.0/cache/*.gem"
+  delete "#{install_dir}/embedded/lib/ruby/gems/2.4.0/cache/*.gem"
+  delete "#{install_dir}/embedded/service/gem/ruby/2.4.0/cache/*.gem"
   # Remove nodejs
   delete "#{install_dir}/embedded/nodejs"
   # strip shared object files related to gecode installs
