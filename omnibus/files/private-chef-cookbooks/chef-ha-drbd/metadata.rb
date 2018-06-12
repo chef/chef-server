c4a3ed622c262e575d78e558d23cddc6df2bba9e
Tim Smith <tsmith@chef.io>
2018-06-12 15:53:12 -0700

    Use a SPDX compliant license string
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- omnibus/files/private-chef-cookbooks/chef-ha-drbd/metadata.rb
+++ omnibus/files/private-chef-cookbooks/chef-ha-drbd/metadata.rb
@@ -1,7 +1,7 @@
 name              "chef-ha-drbd"
 maintainer        "Chef Software, Inc."
 maintainer_email  "cookbooks@chef.io"
-license           "Apache 2.0"
+license           "Apache-2.0"
 description       "Sets up HA DRDB for the chef server"
 long_description  "Sets up HA DRDB for the chef server"
 version           "0.1.0"
