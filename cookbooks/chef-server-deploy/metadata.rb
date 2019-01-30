fcc692c3283b725043ff5f2bbc229b987bc1e5c6
Seth Chisamore <schisamo@chef.io>
2019-01-30 14:20:55 -0500

    Disable data-collector in accceptance
    We are currently having issues in the upstream A1 acceptance instance.
    Until this is fixed we'll stop sending traffic to the A1 instance as it
    causes the Chef Server instance to fail status checks.
    
    Signed-off-by: Seth Chisamore <schisamo@chef.io>

--- cookbooks/chef-server-deploy/metadata.rb
+++ cookbooks/chef-server-deploy/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'engineering@chef.io'
 license 'Apache 2.0'
 description 'Installs/Configures Chef Server in ACC'
 long_description 'Installs/Configures Chef Server in ACC'
-version '0.1.11'
+version '0.1.12'
 
 chef_version '>= 12.1' if respond_to?(:chef_version)
 
