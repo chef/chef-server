0019b9420461cb7bfce76398c835f0bf6e883e2e
Zane Geiger <zane.geiger@relativity.com>
2019-02-26 13:55:32 -0600

    Bump private-chef cookbook version
    Signed-off-by: Zane Geiger <zane.geiger@relativity.com>

--- omnibus/files/private-chef-cookbooks/private-chef/metadata.rb
+++ omnibus/files/private-chef-cookbooks/private-chef/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email  "cookbooks@chef.io"
 license           "Apache-2.0"
 description       "Installs and configures Chef Server from Omnibus"
 long_description  "Installs and configures Chef Server from Omnibus"
-version           "0.1.1"
+version           "0.2.0"
 recipe            "chef-server", "Configures the Chef Server from Omnibus"
 
 %w{ ubuntu debian redhat centos oracle scientific fedora amazon }.each do |os|
