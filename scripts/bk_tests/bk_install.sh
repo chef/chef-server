210ccc43f1d6e2f674a3fcdfdcaf2b6ab8f15f99
Jaymala Sinha <jsinha@chef.io>
2019-03-08 14:33:24 -0500

    Revert "Fix env for erchef"
    This reverts commit bc02d4a5e1f074941115da506f776b41fc64eb74.

--- scripts/bk_tests/bk_install.sh
+++ scripts/bk_tests/bk_install.sh
@@ -3,7 +3,6 @@ apt-get update -y && apt-get install -y lua5.1 luarocks postgresql-9.6
 cp /workdir/scripts/bk_tests/pb_hba.conf /etc/postgresql/9.6/main/pg_hba.conf
 asdf global erlang 18.3
 gem install bundler --version '1.17.3' --no-document
-gem uninstall bundler -v '>= 2.0.1' -a  # we uninstall bundler 2.0.1 as asdf defaults to it
 export LUALIB=~/.luarocks/lib/lua/5.2
 luarocks install --local lpeg
 luarocks install --local lua-cjson
