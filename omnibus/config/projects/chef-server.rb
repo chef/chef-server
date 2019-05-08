4633bb1d13235b4857e9b707b942acbced3527cf
Bryan McLellan <btm@loftninjas.org>
2019-05-08 12:08:44 -0400

    Update the omnibus build license to the Chef EULA
    The resulting distribuions are licensed under the Chef EULA.
    
    Signed-off-by: Bryan McLellan <btm@loftninjas.org>

--- omnibus/config/projects/chef-server.rb
+++ omnibus/config/projects/chef-server.rb
@@ -17,8 +17,8 @@
 name "chef-server"
 maintainer "Chef Software, Inc. <maintainers@chef.io>"
 homepage   "https://www.chef.io"
-license "Apache-2.0"
-license_file "LICENSE"
+license "Chef EULA"
+license_file "https://www.chef.io/end-user-license-agreement/"
 
 package_name    "chef-server-core"
 replace         "private-chef"
