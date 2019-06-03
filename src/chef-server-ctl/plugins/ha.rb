293fd3d38cf156b738c5e0155536e36a57138000
Mark Anderson <mark@chef.io>
2019-06-02 22:33:30 -0700

    [chef-server-ctl] Remove chef ha commands and warn when used.
    A part of the deprecation of the drbd/keepalived based HA commands.
    
    Remove the ha manipulation commands, and warn if they're used.
    
    Remove HA commands from gather-logs
    
    Signed-off-by: Mark Anderson <mark@chef.io>

--- src/chef-server-ctl/plugins/ha.rb
+++ src/chef-server-ctl/plugins/ha.rb
@@ -1,15 +1,17 @@
 #
-# Copyright:: Copyright (c) 2012 Opscode, Inc.
+# Copyright:: Copyright (c) 2012-2019 Chef Software, Inc.
 #
 # All Rights Reserved
 #
 
 add_command_under_category "master-recover", "high-availability", "Set this server to HA master state, ignoring VRRP.", 1 do
-  exec "/var/opt/opscode/keepalived/bin/cluster.sh master"
+  puts err_CONFIG_DRBD_HA_REMOVED("master-recover")
+  exit 1
 end
 
 add_command_under_category "backup-recover", "high-availability", "Set this server to HA backup state, ignoring VRRP", 1 do
-  exec "/var/opt/opscode/keepalived/bin/cluster.sh backup"
+  puts err_CONFIG_DRBD_HA_REMOVED("backup-recover")
+  exit 1
 end
 
 #
@@ -22,116 +24,34 @@ end
 #
 
 add_command_under_category "ha-status", "high-availability", "Show the status of high availability services.", 1 do
-  if running_config.nil?
-    puts "[ERROR] cannot check HA status if you haven't completed a reconfigure"
-    exit 1
-  end
-
-  error_exit = 0
-
-  if running_config['private_chef']['keepalived']['enable']
-    puts "[OK] keepalived HA services enabled."
-  else
-    puts "[ERROR] keepalived HA services not enabled, please configure keepalived according to documentation."
-    exit 1
-  end
+  puts err_CONFIG_DRBD_HA_REMOVED("ha-status")
+  exit 1
+end
 
 
-  requested_cluster_status = File.read("/var/opt/opscode/keepalived/requested_cluster_status").chomp
-  current_cluster_status = File.read("/var/opt/opscode/keepalived/current_cluster_status").chomp
+def err_CONFIG_DRBD_HA_REMOVED(command)
+  <<EOM
+DRBD_HA_001: HA Command #{command} no longer supported.
 
-  if requested_cluster_status != current_cluster_status
-    puts "[WARN] inconsistent cluster status, requested: #{requested_cluster_status}, current: #{current_cluster_status}, failover running?"
-    error_exit = 2
-  else
-    puts "[OK] cluster status = #{current_cluster_status}"
-  end
+The DRBD/keepalived based HA subsystem was deprecated as of Chef Server
+12.9, and officially reached end of life on 2019-03-31. It has been
+disabled in Chef Server 13.
 
-  vrrp_instance_ipaddress = running_config['private_chef']['keepalived']['vrrp_instance_ipaddress']
-  vrrp_instance_ipaddress_dev = running_config['private_chef']['keepalived']['vrrp_instance_ipaddress_dev']
-  has_vrrp_addr = `ip a show dev #{vrrp_instance_ipaddress_dev}` =~ /inet6? #{Regexp.escape(vrrp_instance_ipaddress)}/
+See this post for more details:
+https://blog.chef.io/2018/10/02/end-of-life-announcement-for-drbd-based-ha-support-in-chef-server/
 
-  if has_vrrp_addr
-    if current_cluster_status == "master"
-      puts "[OK] found VIP IP address and I am master"
-    else
-      puts "[ERROR] found VRRP address and I am not master"
-      error_exit = 3
-    end
-  else
-    if current_cluster_status == "master"
-      puts "[ERROR] did not find VIP IP address and I am master"
-      error_exit = 3
-    else
-      puts "[OK] did not find VIP IP address and I am not master"
-    end
-  end
+What are my options?
 
-  vrrp_instance_interface = running_config['private_chef']['keepalived']['vrrp_instance_interface']
-  has_vrrp_instance_interface = system "ip a show dev #{vrrp_instance_interface} >/dev/null 2>&1"
+Chef Backend was announced over two years ago and is the recommended solution
+for all customers. It is a licensed product and available under the terms
+of a Chef Infra License.
 
-  if has_vrrp_instance_interface
-    puts "[OK] found VRRP communications interface #{vrrp_instance_interface}"
-  else
-    puts "[ERROR] VRRP communications interface #{vrrp_instance_interface} not found."
-    error_exit = 8
-  end
+For more information on migrating from DRBD HA to Chef Backend or other HA, see this blog
+post and webinar: Best Practices for Migrating your Chef Server at
+https://blog.chef.io/2018/04/06/best-practices-for-migrating-your-chef-server/
 
-  custom_backend = "/var/opt/opscode/keepalived/bin/custom_backend_"
-  ['storage', 'ip'].each do |t|
-    if File.executable?("#{custom_backend}#{t}")
-      if system("#{custom_backend}#{t} status #{current_cluster_status}")
-        puts "[OK] #{t} backend status is OK"
-      else
-        puts "[ERROR] #{t} backend status is not OK"
-        error_exit = 4
-      end
-    end
-  end
+Customers in cloud environments are also encouraged to look at AWS OpsWorks
+and the Chef Automate Managed Service for Azure.
 
-  # HA services are those that have a 'keepalive_me' file in their
-  # runit directories
-  ha_services = Dir.chdir(running_config['runit']['sv_dir']){Dir.glob('**/keepalive_me').map{|f| File.dirname(f)}}.sort
-
-  get_all_services.sort.each do |service_name|
-    # opscode-chef-mover service is only used during an upgrade, and does not need to be running all of the time
-    # do not consider any hidden services when checking ha status
-    next if hidden_services.include?(service_name)
-    
-    if service_enabled?(service_name)
-      status = run_command("/opt/opscode/init/#{service_name} status >/dev/null 2>&1")
-      if ha_services.include?(service_name)
-        if current_cluster_status == "master"
-          if status.success?
-            puts "[OK] #{service_name} is running correctly, and I am master."
-          else
-            puts "[ERROR] #{service_name} is not running correctly, and I am master. "
-            error_exit = 7
-          end
-        else
-          if status.success?
-            puts "[ERROR] #{service_name} is running, and I am not master."
-            error_exit = 7
-          else
-            puts "[OK] #{service_name} is not running, and I am not master."
-          end
-        end
-      else
-        if status.success?
-          puts "[OK] #{service_name} is running."
-        else
-          puts "[ERROR] #{service_name} is not running."
-          error_exit = 7
-        end
-      end
-    end
-  end
-
-  if error_exit != 0
-    puts "\n[ERROR] ERRORS WERE DETECTED.\n\n"
-  else
-    puts "\n[OK] all checks passed.\n\n"
-  end
-
-  exit error_exit
+EOM
 end
