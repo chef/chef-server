dcdc068e3df4911ff09672277cbecc2d43649ebe
Lincoln Baker <lbaker@chef.io>
2019-08-01 13:48:06 -0500

    turn on ssl option in provision-postgres.sh; comment cleanup

--- dev/scripts/provision-postgres.sh
+++ dev/scripts/provision-postgres.sh
@@ -5,25 +5,13 @@ apt-get update
 apt-get install postgresql-9.6 -y
 echo "host    all             all             $1/32         md5" >> /etc/postgresql/9.6/main/pg_hba.conf
 echo "listen_addresses='*'" >> /etc/postgresql/9.6/main/postgresql.conf
-########################################################################
-# lincoln: incorporating prajakta's edits
-#   https://github.com/chef/chef-server/compare/zanecodes_ssl?expand=1#diff-eee84728cb317f4bee8d7dfac54ebe69R8
-#   not sure how this will work with my setup.
-#
 # To turn ssl on in the external postgresql setup uncomment the following line.
-#   lincoln: what about the existing ssl=off? is that somehow overwritten?
-#echo "ssl=on" >> /etc/postgresql/9.6/main/postgresql.conf
-
+echo "ssl=on" >> /etc/postgresql/9.6/main/postgresql.conf
 # For the dev setup you can use the nginx certs.
 # Copy them to /var/opt/opscode/postgresql/9.6/ and chown opscode-pgsql.
-#   lincoln: will also need to chmod 0600 these files.
-#   might be better to put these in postgres's default data directory,
-#   which means we can eliminate the ssl_cert/key_file settings.
-#  no `echo <whatever> >> <file>` below?
-#  nginx certs
+# Will also need to chmod 0600 these files.
 #ssl_cert_file = '/var/opt/opscode/postgresql/9.6/api.chef-server.dev.crt'
-#ssl_key_file = '/var/opt/opscode/postgresql/9.6/api.chef-server.dev.key'
-########################################################################
+#ssl_key_file  = '/var/opt/opscode/postgresql/9.6/api.chef-server.dev.key'
 service postgresql restart
 export PATH=/usr/lib/postgresql/9.6/bin:$PATH
 sudo -u postgres psql -c "CREATE USER bofh SUPERUSER ENCRYPTED PASSWORD 'i1uvd3v0ps';"
