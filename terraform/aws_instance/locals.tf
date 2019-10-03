isabella232/chef-server-1

Initial refactor of Terraform for on-demand integration testing
Signed-off-by: Christopher A. Snapp <csnapp@chef.io>

terraform/aws_instance/locals.tf
@@ -1,0 +1,18 @@
+# identify external ipv4 address of the system where terraform is being run from
+data "http" "workstation-ipv4" {
+  url = "http://ipv4.icanhazip.com"
+}
+
+locals {
+  workstation-ipv4-cidr = "${chomp(data.http.workstation-ipv4.body)}/32"
+
+  vpc_name = "${var.aws_vpc_name != "" ? var.aws_vpc_name : "${var.aws_ssh_key_id}-chef_server-test"}"
+
+  ami_ids = {
+    rhel-6       = "${data.aws_ami.rhel_6.id}"
+    rhel-7       = "${data.aws_ami.rhel_7.id}"
+    rhel-8       = "${data.aws_ami.rhel_8.id}"
+    ubuntu-16.04 = "${data.aws_ami.ubuntu_1604.id}"
+    ubuntu-18.04 = "${data.aws_ami.ubuntu_1804.id}"
+  }
+}

terraform/aws_instance/main.tf
@@ -1,0 +1,75 @@
+provider "aws" {
+  profile = "${var.aws_profile}"
+  region  = "${var.aws_region}"
+}
+
+data "aws_vpc" "chef_vpc" {
+  tags = {
+    Name = "${local.vpc_name}"
+  }
+}
+
+data "aws_subnet" "chef_subnet" {
+  tags = {
+    Name = "${local.vpc_name}"
+  }
+}
+
+resource "aws_security_group" "default" {
+  name_prefix = "${local.vpc_name}"
+  vpc_id      = "${data.aws_vpc.chef_vpc.id}"
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["${cidrsubnet(data.aws_vpc.chef_vpc.cidr_block, 4, 1)}", "${local.workstation-ipv4-cidr}"]
+  }
+
+  ingress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    ipv6_cidr_blocks = ["${cidrsubnet(data.aws_vpc.chef_vpc.ipv6_cidr_block, 8, 1)}"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "${var.name}-${local.vpc_name}"
+  }
+}
+
+resource "aws_instance" "default" {
+  ami           = "${local.ami_ids[var.platform]}"
+  instance_type = "${var.aws_instance_type}"
+  key_name      = "${var.aws_ssh_key_id}"
+
+  root_block_device {
+    volume_size = 50
+  }
+
+  subnet_id = "${data.aws_subnet.chef_subnet.id}"
+
+  vpc_security_group_ids = [
+    "${aws_security_group.default.id}",
+  ]
+
+  tags = {
+    Name      = "${var.name}-${local.vpc_name}"
+    X-Contact = "${var.aws_ssh_key_id}"
+    X-Dept    = "Eng"
+  }
+}

terraform/aws_vpc/locals.tf
@@ -1,0 +1,3 @@
+locals {
+  vpc_name = "${var.aws_vpc_name != "" ? var.aws_vpc_name : "${var.aws_ssh_key_id}-chef_server-test"}"
+}

terraform/scenarios/omnibus-standalone-upgrade-from-stable/variables.tf
@@ -1,0 +1,49 @@
+#########################################################################
+# AWS
+#########################################################################
+variable "aws_profile" {
+  type        = "string"
+  description = "Name of the AWS profile used for authentication (e.g. chef-engineering)."
+}
+
+variable "aws_region" {
+  type        = "string"
+  description = "Name of the AWS region to create instances in (e.g. us-west-2)."
+  default     = "us-west-1"
+}
+
+variable "aws_vpc_name" {
+  type        = "string"
+  description = "Name of the AWS virtual private cloud where tests will be run."
+  default     = ""
+}
+
+variable "aws_ssh_key_id" {
+  type        = "string"
+  description = "AWS ID of the SSH key used to access the instance (e.g. csnapp)."
+}
+
+variable "aws_instance_type" {
+  type        = "string"
+  description = "Name of the AWS instance type used to determine size of instances (e.g. t3.medium)."
+  default     = "t3.medium"
+}
+
+variable "platform" {
+  type        = "string"
+  description = "Operating System of the instance to be created."
+  default     = "rhel-7"
+}
+
+#########################################################################
+# Chef Server
+#########################################################################
+variable "build_version" {
+  type        = "string"
+  description = "The version of chef server build artifact to install (e.g. 13.0.38+20190904060033)"
+}
+
+variable "stable_version" {
+  type        = "string"
+  description = "The stable version of chef server to install (e.g. 13.0.17)"
+}

terraform/scenarios/omnibus-tiered-fresh-install/variables.tf
@@ -1,0 +1,44 @@
+#########################################################################
+# AWS
+#########################################################################
+variable "aws_profile" {
+  type        = "string"
+  description = "Name of the AWS profile used for authentication (e.g. chef-engineering)."
+}
+
+variable "aws_region" {
+  type        = "string"
+  description = "Name of the AWS region to create instances in (e.g. us-west-2)."
+  default     = "us-west-1"
+}
+
+variable "aws_vpc_name" {
+  type        = "string"
+  description = "Name of the AWS virtual private cloud where tests will be run."
+  default     = ""
+}
+
+variable "aws_ssh_key_id" {
+  type        = "string"
+  description = "AWS ID of the SSH key used to access the instance (e.g. csnapp)."
+}
+
+variable "aws_instance_type" {
+  type        = "string"
+  description = "Name of the AWS instance type used to determine size of instances (e.g. t3.medium)."
+  default     = "t3.medium"
+}
+
+variable "platform" {
+  type        = "string"
+  description = "Operating System of the instance to be created."
+  default     = "ubuntu-16.04"
+}
+
+#########################################################################
+# Chef Server
+#########################################################################
+variable "build_version" {
+  type        = "string"
+  description = "The version of chef server build artifact to install (e.g. 13.0.38+20190904060033)"
+}

chef/chef-server

terraform/aws_instance/locals.tf
@@ -1,0 +1,18 @@
+# identify external ipv4 address of the system where terraform is being run from
+data "http" "workstation-ipv4" {
+  url = "http://ipv4.icanhazip.com"
+}
+
+locals {
+  workstation-ipv4-cidr = "${chomp(data.http.workstation-ipv4.body)}/32"
+
+  vpc_name = "${var.aws_vpc_name != "" ? var.aws_vpc_name : "${var.aws_contact}-chef_server-test"}"
+
+  ami_ids = {
+    rhel-6       = "${data.aws_ami.rhel_6.id}"
+    rhel-7       = "${data.aws_ami.rhel_7.id}"
+    rhel-8       = "${data.aws_ami.rhel_8.id}"
+    ubuntu-16.04 = "${data.aws_ami.ubuntu_1604.id}"
+    ubuntu-18.04 = "${data.aws_ami.ubuntu_1804.id}"
+  }
+}

terraform/aws_instance/main.tf
@@ -1,0 +1,81 @@
+provider "aws" {
+  profile = "${var.aws_profile}"
+  region  = "${var.aws_region}"
+}
+
+data "aws_vpc" "chef_vpc" {
+  tags = {
+    Name      = "${local.vpc_name}"
+    X-Dept    = "${var.aws_department}"
+    X-Contact = "${var.aws_contact}"
+  }
+}
+
+data "aws_subnet" "chef_subnet" {
+  tags = {
+    Name      = "${local.vpc_name}"
+    X-Dept    = "${var.aws_department}"
+    X-Contact = "${var.aws_contact}"
+  }
+}
+
+resource "aws_security_group" "default" {
+  name_prefix = "${local.vpc_name}"
+  vpc_id      = "${data.aws_vpc.chef_vpc.id}"
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["${cidrsubnet(data.aws_vpc.chef_vpc.cidr_block, 4, 1)}", "${local.workstation-ipv4-cidr}"]
+  }
+
+  ingress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    ipv6_cidr_blocks = ["${cidrsubnet(data.aws_vpc.chef_vpc.ipv6_cidr_block, 8, 1)}"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name      = "${var.name}-${local.vpc_name}"
+    X-Dept    = "${var.aws_department}"
+    X-Contact = "${var.aws_contact}"
+  }
+}
+
+resource "aws_instance" "default" {
+  ami           = "${local.ami_ids[var.platform]}"
+  instance_type = "${var.aws_instance_type}"
+  key_name      = "${var.aws_ssh_key_id}"
+
+  root_block_device {
+    volume_size = 50
+  }
+
+  subnet_id = "${data.aws_subnet.chef_subnet.id}"
+
+  vpc_security_group_ids = [
+    "${aws_security_group.default.id}",
+  ]
+
+  tags = {
+    Name      = "${var.name}-${local.vpc_name}"
+    X-Dept    = "${var.aws_department}"
+    X-Contact = "${var.aws_contact}"
+  }
+}

terraform/aws_vpc/locals.tf
@@ -1,0 +1,3 @@
+locals {
+  vpc_name = "${var.aws_vpc_name != "" ? var.aws_vpc_name : "${var.aws_contact}-chef_server-test"}"
+}

terraform/scenarios/omnibus-standalone-upgrade-from-stable/variables.tf
@@ -1,0 +1,60 @@
+#########################################################################
+# AWS
+#########################################################################
+variable "aws_profile" {
+  type        = "string"
+  description = "Name of the AWS profile used for authentication (e.g. chef-engineering)."
+  default     = "chef-engineering"
+}
+
+variable "aws_region" {
+  type        = "string"
+  description = "Name of the AWS region to create instances in (e.g. us-west-2)."
+  default     = "us-west-1"
+}
+
+variable "aws_vpc_name" {
+  type        = "string"
+  description = "Name of the AWS virtual private cloud where tests will be run."
+  default     = ""
+}
+
+variable "aws_department" {
+  type        = "string"
+  description = "Department that owns the resources should be one of: EngServ, Operations, Eng, Training, Solutions, Sales, BD, Success or Partner"
+}
+
+variable "aws_contact" {
+  type        = "string"
+  description = "The primary contact for the resources, this should be the IAM username and must be able to receive email by appending @chef.io to it (this person can explain what/why, might not be the business owner)."
+}
+
+variable "aws_ssh_key_id" {
+  type        = "string"
+  description = "AWS ID of the SSH key used to access the instance (e.g. csnapp)."
+}
+
+variable "aws_instance_type" {
+  type        = "string"
+  description = "Name of the AWS instance type used to determine size of instances (e.g. t3.medium)."
+  default     = "t3.medium"
+}
+
+variable "platform" {
+  type        = "string"
+  description = "Operating System of the instance to be created."
+  default     = "rhel-7"
+}
+
+#########################################################################
+# Chef Server
+#########################################################################
+variable "unstable_version" {
+  type        = "string"
+  description = "The version of chef server build artifact to install (e.g. 13.0.38+20190904060033)"
+}
+
+variable "stable_version" {
+  type        = "string"
+  description = "The stable version of chef server to install (e.g. 13.0.17)"
+}

terraform/scenarios/omnibus-tiered-fresh-install/variables.tf
@@ -1,0 +1,55 @@
+#########################################################################
+# AWS
+#########################################################################
+variable "aws_profile" {
+  type        = "string"
+  description = "Name of the AWS profile used for authentication (e.g. chef-engineering)."
+  default     = "chef-engineering"
+}
+
+variable "aws_region" {
+  type        = "string"
+  description = "Name of the AWS region to create instances in (e.g. us-west-2)."
+  default     = "us-west-1"
+}
+
+variable "aws_vpc_name" {
+  type        = "string"
+  description = "Name of the AWS virtual private cloud where tests will be run."
+  default     = ""
+}
+
+variable "aws_department" {
+  type        = "string"
+  description = "Department that owns the resources should be one of: EngServ, Operations, Eng, Training, Solutions, Sales, BD, Success or Partner"
+}
+
+variable "aws_contact" {
+  type        = "string"
+  description = "The primary contact for the resources, this should be the IAM username and must be able to receive email by appending @chef.io to it (this person can explain what/why, might not be the business owner)."
+}
+
+variable "aws_ssh_key_id" {
+  type        = "string"
+  description = "AWS ID of the SSH key used to access the instance (e.g. csnapp)."
+}
+
+variable "aws_instance_type" {
+  type        = "string"
+  description = "Name of the AWS instance type used to determine size of instances (e.g. t3.medium)."
+  default     = "t3.medium"
+}
+
+variable "platform" {
+  type        = "string"
+  description = "Operating System of the instance to be created."
+  default     = "ubuntu-16.04"
+}
+
+#########################################################################
+# Chef Server
+#########################################################################
+variable "unstable_version" {
+  type        = "string"
+  description = "The version of chef server build artifact to install (e.g. 13.0.38+20190904060033)"
+}