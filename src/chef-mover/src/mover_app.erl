78c6a54a54d85d74c3a1a6be493f676eb1209711
Lincoln Baker <lbaker@chef.io>
2019-10-23 16:26:20 -0500

    fix SIGTERM breakage for erlang 20.x
    Changes to SIGTERM handling in Erlang 19.x caused runit failures in
    Chef Server. This was originally fixed when we upgraded to Erlang 19
    by passing a startup flag to the Erlang VM on bootup.  But the startup
    flag was removed in Erlang 20, so another fix had to be employed.
    
    This change effectively reverts Erlang's SIGTERM handling to previous
    (pre-19) behavior.
    
    REF: https://github.com/erlang/otp/pull/1315
    
    Signed-off-by: Lincoln Baker <lbaker@chef.io>

--- src/chef-mover/src/mover_app.erl
+++ src/chef-mover/src/mover_app.erl
@@ -1,7 +1,7 @@
 %% -*- erlang-indent-level: 4;indent-tabs-mode: nil;fill-column: 92 -*-
 %% ex: ts=4 sw=4 et
 %% @author Kevin Smith <kevin@chef.io>
-%% @copyright 2011 Opscode, Inc.
+%% @copyright 2019 Opscode, Inc.
 -module(mover_app).
 
 -behaviour(application).
@@ -11,6 +11,10 @@
          stop/1]).
 
 start(_StartType, _StartArgs) ->
+    %% erlang 19.3.x SIGTERM changes caused runit failures.
+    %% this reverts to previous working SIGTERM behavior.
+    os:set_signal(sigterm, default),
+
     { ok, AppList } =  application:get_key(mover, included_applications),
     [ application:ensure_all_started(App, permanent) || App <- AppList ],
     mover_sup:start_link().
