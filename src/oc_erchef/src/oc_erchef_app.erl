d4a5aaa6b62699620fff53177e3084d68731ee01
Lincoln Baker <lbaker@chef.io>
2019-10-23 16:26:20 -0500

    fix SIGTERM breakage for erlang20x
    send notification for erl_signal_server on sigterm sent from runit
    
    Runit sends sigterm to the erlang application. Erlang 19.3 onwards
    erlang can be notified of some of the os signals.
    REF: https://github.com/erlang/otp/pull/1315
    
    Signed-off-by: Lincoln Baker <lbaker@chef.io>

--- src/oc_erchef/src/oc_erchef_app.erl
+++ src/oc_erchef/src/oc_erchef_app.erl
@@ -12,6 +12,8 @@
 %% ===================================================================
 
 start(_StartType, _StartArgs) ->
+    os:set_signal(sigterm, default),
+
     %% When start is invoked, any non-included apps are already started.
     %% This means that chef_secrets is started - so it's safe to start
     %% pooler and any other included apps that needed to wait for pooler.
