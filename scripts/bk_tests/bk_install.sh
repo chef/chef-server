f4bde0d5c704145fc4f2ef97cfd61499cff10dcc
Prajakta Purohit <prajakta@chef.io>
2019-10-28 14:08:17 -0700

    Log the version of erlang and the env currently being used for testing.

--- scripts/bk_tests/bk_install.sh
+++ scripts/bk_tests/bk_install.sh
@@ -3,6 +3,7 @@ apt-get update -y && apt-get install -y lua5.1 luarocks postgresql-9.6
 cp /workdir/scripts/bk_tests/pb_hba.conf /etc/postgresql/9.6/main/pg_hba.conf
 asdf install erlang 19.3
 asdf local erlang 19.3
+erl -eval 'erlang:display(erlang:system_info(otp_release)), halt().' -noshell
 asdf local ruby 2.5.6
 gem install bundler --version '~> 1.17' --no-document
 export LUALIB=~/.luarocks/lib/lua/5.2
@@ -11,4 +12,5 @@ luarocks install --local lua-cjson
 eval $(luarocks path)
 cpanm --notest --quiet --local-lib=$HOME/perl5 local::lib && eval $(perl -I ~/perl5/lib/perl5/ -Mlocal::lib)
 cpanm --notest --quiet App::Sqitch
+env
 service postgresql restart
