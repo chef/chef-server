68f708bfd00547d5be023e71b7c2b3709560da8c
Lincoln Baker <lbaker@chef.io>
2019-10-30 16:01:35 -0500

    homogenized oc_chef_types.hrl for side-by-side comparability and maintainability purposes.  also corrected dialyzer issues.
    added spacing/formatting to aid in side-by-side comparability for:
    
    src/chef-mover/_checkouts/oc_erchef/include/oc_chef_types.hrl
    src/oc_erchef/include/oc_chef_types.hrl
    
    Signed-off-by: Lincoln Baker <lbaker@chef.io>

--- src/chef-mover/_checkouts/oc_erchef/include/oc_chef_types.hrl
+++ src/chef-mover/_checkouts/oc_erchef/include/oc_chef_types.hrl
@@ -4,7 +4,13 @@
 %% Copyright 2013 Opscode, Inc. All Rights Reserved.
 %% Copyright 2014 Chef, Inc. All Rights Reserved.
 
+
+
+
+
+
 -record(oc_chef_container, {
+
           id,
           authz_id,
           org_id,
@@ -15,6 +21,7 @@
          }).
 
 -record(oc_chef_group, {
+
           id,
           for_requestor_id,
           authz_id,
@@ -30,7 +37,87 @@
           auth_side_groups = []
           }).
 
+%% NOTE: this is not the end of the file.  there is space here to preserve side-by-side
+%% comparability and maintainability with oc_erchef/include/oc_chef_types.hrl.
+%% scroll further down for more code.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 -record(oc_chef_organization, {
+
           id,
           authz_id,
           name,
@@ -42,6 +129,7 @@
          }).
 
 -record(oc_chef_org_user_association, {
+
           org_id,
           user_id,
           user_name, % Not part of the table but retrieved via join
@@ -51,6 +139,7 @@
          }).
 
 -record(oc_chef_org_user_invite, {
+
           id,
           org_id,
           org_name,  % Not  part of table - retrieved via join
