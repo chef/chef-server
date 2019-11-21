8bb429a58ad967aefaf192cd82d60f9ee7e24f59
Christopher A. Snapp <csnapp@chef.io>
2019-11-21 11:16:18 -0700

    Speed up Terraform Push Jobs tests
    Signed-off-by: Christopher A. Snapp <csnapp@chef.io>

--- terraform/common/files/install_addon_push_jobs.sh
+++ terraform/common/files/install_addon_push_jobs.sh
@@ -5,6 +5,8 @@ set -evx
 echo -e '\nBEGIN INSTALL PUSH JOBS ADDON\n'
 
 sudo chef-server-ctl install opscode-push-jobs-server
+sudo mkdir -p /etc/opscode-push-jobs-server
+printf -- "opscode_pushy_server['heartbeat_interval'] = 1000" | sudo tee /etc/opscode-push-jobs-server/opscode-push-jobs-server.rb
 sudo chef-server-ctl reconfigure --chef-license=accept
 sleep 30
 sudo opscode-push-jobs-server-ctl reconfigure
