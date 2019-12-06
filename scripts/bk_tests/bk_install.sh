ed425bca4a7ad2911d5aa1da358013c445552da5
Lincoln Baker <lbaker@chef.io>
2019-12-06 16:35:57 -0600

    update erlang to version <whatever>
    Signed-off-by: Lincoln Baker <lbaker@chef.io>

--- scripts/bk_tests/bk_install.sh
+++ scripts/bk_tests/bk_install.sh
@@ -1,7 +1,10 @@
 apt-get --purge remove -y postgresql-9.3
 apt-get update -y && apt-get install -y lua5.1 luarocks postgresql-9.6
 cp /workdir/scripts/bk_tests/pb_hba.conf /etc/postgresql/9.6/main/pg_hba.conf
-asdf local erlang 20.3
+# this is needed until the erlang version is installed in the docker container
+# then it can be removed
+asdf install erlang 21.1
+asdf local erlang 21.1
 erl -eval 'erlang:display(erlang:system_info(otp_release)), halt().' -noshell
 gem install bundler --version '~> 1.17' --no-document
 export LUALIB=~/.luarocks/lib/lua/5.2
