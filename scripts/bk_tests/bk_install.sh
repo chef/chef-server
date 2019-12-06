4407c4a48737850de26dd28d9f91e4e3d9434677
Lincoln Baker <lbaker@chef.io>
2019-12-06 16:35:57 -0600

    update erlang to version 22.x
    Signed-off-by: Lincoln Baker <lbaker@chef.io>

--- scripts/bk_tests/bk_install.sh
+++ scripts/bk_tests/bk_install.sh
@@ -31,10 +31,12 @@ apt-get install -y lua5.1 luarocks postgresql-9.6 libsqlite3-dev
 
 echo "Configuring postgresql"
 cp /workdir/scripts/bk_tests/pb_hba.conf /etc/postgresql/9.6/main/pg_hba.conf
+
+# erlang software definition lives in: /omnibus-software/config/software/erlang.rb
 # this is needed until the erlang version is installed in the docker container
-# then it can be removed
-#asdf install erlang 21.3.8.11
-asdf local erlang 21.3.8.11
+echo "Installing erlang"
+asdf install erlang 22.1.8
+asdf local erlang 22.1.8
 erl -eval 'erlang:display(erlang:system_info(otp_release)), halt().' -noshell
 
 echo "Installing Bundler"
