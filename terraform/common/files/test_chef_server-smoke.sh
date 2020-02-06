f935b746e5a82947ec6b367ebe1e04a8dc93d0a7
Christopher A. Snapp <csnapp@chef.io>
2020-02-06 12:46:57 -0700

    Fix integration test check for FIPS being enabled
    Signed-off-by: Christopher A. Snapp <csnapp@chef.io>

--- terraform/common/files/test_chef_server-smoke.sh
+++ terraform/common/files/test_chef_server-smoke.sh
@@ -4,7 +4,7 @@ set -evx
 
 echo -e '\nBEGIN SMOKE TEST\n'
 
-if grep -q 'fips true' /etc/opscode/chef-server.rb; then
+if sudo grep -q 'fips true' /etc/opscode/chef-server.rb; then
     sudo chef-server-ctl test -J pedant-fips.xml --smoke
 else
     sudo chef-server-ctl test
