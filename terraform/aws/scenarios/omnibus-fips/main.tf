c5df435e66c14606edc3ffb1d1424bf3e42acc0f
Christopher A. Snapp <csnapp@chef.io>
2020-03-12 13:11:02 -0600

    Fix fips enabled status check in omnibus-fips
    Signed-off-by: Christopher A. Snapp <csnapp@chef.io>

--- terraform/aws/scenarios/omnibus-fips/main.tf
+++ terraform/aws/scenarios/omnibus-fips/main.tf
@@ -65,7 +65,7 @@ resource "null_resource" "chef_server_config" {
     inline = [
       "set -evx",
       "echo -e '\nCHECKING FIPS STATUS:\n'",
-      "if sudo sysctl crypto.fips_enabled | grep -q 0; then echo 'SUCCESS: FIPS mode is enabled!'; else echo 'FAIL: FIPS mode is not enabled!; exit 1; fi",
+      "if sudo sysctl crypto.fips_enabled | grep -q 1; then echo 'SUCCESS: FIPS mode is enabled!'; else echo 'FAIL: FIPS mode is not enabled!'; exit 1; fi",
       "echo -e '\nBEGIN INSTALL CHEF SERVER\n'",
       "curl -vo /tmp/${replace(var.upgrade_version_url, "/^.*\\//", "")} ${var.upgrade_version_url}",
       "sudo ${replace(var.upgrade_version_url, "rpm", "") != var.upgrade_version_url ? "rpm -U" : "dpkg -iEG"} /tmp/${replace(var.upgrade_version_url, "/^.*\\//", "")}",
