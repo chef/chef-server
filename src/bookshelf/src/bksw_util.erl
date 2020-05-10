7e860d108b9168396fc6707de55a98de1f3f0c09
Lincoln Baker <lbaker@chef.io>
2020-05-10 00:31:29 -0600

    add instrumentation to bookshelf
    Signed-off-by: Lincoln Baker <lbaker@chef.io>

--- src/bookshelf/src/bksw_util.erl
+++ src/bookshelf/src/bksw_util.erl
@@ -72,5 +72,6 @@ get_object_and_bucket(Rq0) ->
              <<"">>};
         [Bucket | Path] ->
             {ok, bksw_io_names:decode(bksw_util:to_binary(Bucket)),
-             bksw_util:to_binary(filename:join(Path))}
+             bksw_util:to_binary(filename:join(Path))};
+_ -> io:format("~nbksw_util:get_object_and_bucket failed !!!!!!!!!!!")
     end.
