27b43cebe19a46c108c1328db6024595ae3364c8
Lincoln Baker <lbaker@chef.io>
2020-05-12 02:47:55 -0600

    wip fix calculation of signature in standard authorization contd
    Signed-off-by: Lincoln Baker <lbaker@chef.io>

--- src/bookshelf/src/bksw_sec.erl
+++ src/bookshelf/src/bksw_sec.erl
@@ -150,7 +150,9 @@ try
             QueryParams = wrq:req_qs(Req0),
             io:format("~nQueryParams: ~p", [QueryParams]),
             %SigV4Headers = erlcloud_aws:sign_v4(list_to_atom(Method), Url, Config, Headers, Payload, Region, "s3", QueryParams, Date),
-            SigV4Headers = erlcloud_aws:sign_v4(list_to_atom(Method), Url, Config, SignedHeaders, "UNSIGNED-PAYLOAD", Region, "s3", QueryParams, XAmzDate),
+            %SigV4Headers = erlcloud_aws:sign_v4(list_to_atom(Method), Url, Config, SignedHeaders, "UNSIGNED-PAYLOAD", Region, "s3", QueryParams, XAmzDate),
+            % removed payload (replaced with <<>>), signedheaders = host: api, changed Url for Path
+            SigV4Headers = erlcloud_aws:sign_v4(list_to_atom(Method), Path, Config, [{"host", "api"}], <<>>, Region, "s3", QueryParams, XAmzDate),
             io:format("~nsigv4headers: ~p", [SigV4Headers]),
 
             Sig1 = IncomingSignature,
