ff6167bfd89d9dce10672ee88383f75f888b1c6b
Seth Chisamore <schisamo@chef.io>
2020-05-18 16:34:13 -0400

    Use Expeditor built-in to purge Fastly
    Signed-off-by: Seth Chisamore <schisamo@chef.io>

--- .expeditor/purge_cdn.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -eou pipefail
-
-target_channel="${EXPEDITOR_CHANNEL:-unstable}"
-
-echo "Purging '${target_channel}/chef-server/latest' Surrogate Key group from Fastly"
-curl -X POST -H "Fastly-Key: $FASTLY_API_TOKEN" "https://api.fastly.com/service/1ga2Kt6KclvVvCeUYJ3MRp/purge/${target_channel}/chef-server/latest"
