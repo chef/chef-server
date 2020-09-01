d3341bb117714cb43ba5e06e3d0682cff2f53a2b
Lincoln Baker <lbaker@chef.io>
2020-09-01 06:17:26 -0500

    Miscellaneous
    Signed-off-by: Lincoln Baker <lbaker@chef.io>

--- src/oc_erchef/apps/chef_objects/src/chef_s3_ops.erl
+++ src/oc_erchef/apps/chef_objects/src/chef_s3_ops.erl
@@ -168,7 +168,6 @@ check_file(OrgId, AwsConfig, Bucket, Checksum, AttemptsLeft) ->
                      TimeoutMsgTemplate :: string()) ->
                             bulk_op_return().
 s3_checksum_op(OrgId, Checksums, Fun, TimeoutMsgTemplate) ->
-
     Bucket = chef_s3:bucket(),
     AwsConfig = chef_s3:get_internal_config(),
     Timeout = chef_config:config_option(chef_objects, s3_parallel_ops_timeout, pos_integer),
@@ -185,7 +184,6 @@ s3_checksum_op(OrgId, Checksums, Fun, TimeoutMsgTemplate) ->
                      end,
 
     Results = chef_parallel:parallelize_all_with_timeout(Checksums, RequestFun, Fanout, Timeout, TimeoutHandler),
-
     %% Now we need to consolidate our results based on:
     %%
     %%   Did the operation succeed?
