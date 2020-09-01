8f6b5d43aa9df539a4f6df50861a968bd6a5aecd
Lincoln Baker <lbaker@chef.io>
2020-09-01 06:17:01 -0500

    Replace AWS sigv2 authentication with sigv4.
    These changes are part of a broader effort to replace AWS sigv2 with sigv4,
    encompassing modifications to oc_erchef, bookshelf, mini_s3, erlcloud,
    pedant clients, and other systems. In brief, the erlcloud library was wired-in
    to oc_erchef and bookshelf via mini_s3. oc-erchef was modified to generate
    sigv4 requests. bookshelf was modified to accept sigv4 requests. A host of
    issues were solved pertaining to host headers and ports, expiration windows,
    ipv6, etc. Finally, new unit tests exercising various aspects of newly-added
    functionality were added.
    
    NOTE: If the signature computation and comparison within bksw_sec fails,
    bksw_sec attempts an alternative signature computation/comparison
    which adds or removes the port from the host header depending on whether
    the port is present or absent. At the time this was put in, there were
    problems with signature failures due to inconsistent treatment of host
    headers by various chef clients (present or missing ports).  Determining
    whether this capability is still necessary would require some investigation
    and testing.
    
    A brief list of some of the changes:
    
    add sigv4 presigned url request verification
    
    add unit tests for bksw_sec.erl
    
    add 2nd method for sigv4 url request verification, and add associated tests
    
    add authorization header request
    
    parse and validate sigv4 request elements
    
    fix chef_s3_tests.erl:
        if i understood the tests correctly, and understood what was being
        tested, there was some impedance mismatch between how the old mini_s3
        did things and how the new erlcloud and mini_s3 wrapper does things. old
        mini_s3 config apparently stores a whole url, either with port
        (scheme://whatever.com:port) or without port (scheme://whatever.com).
        erlcloud config stores the url in separate pieces (scheme, uri, port,
        etc), and it always stores a port. the issue with these tests was,
        the test result wanted the url which may or may not have originally
        included a port. this was fine for old mini_s3, as it just stored the
        whole url (which either included or didn't include the port), and could
        produce this url on demand. but this wasn't fine for erlcloud, because
        it always has a port stored, and defaults to a port if there isn't one.
        this fix attempted to address this issue.set cache-control header
    
    bksw_sec: dont trigger new sha256 calculation in erlcloud sigv4 signing
    
    change test dealing with url-encoded bucket
    
    patch in alternate computation/evaluation of host header
    
    fix pedant test expecting a 503 but getting a 500
    
    Signed-off-by: Lincoln Baker <lbaker@chef.io>
    
    src/bookshelf/src/bksw_wm_base.erl
    
    Signed-off-by: Lincoln Baker <lbaker@chef.io>

--- src/bookshelf/src/bksw_sec.erl
+++ src/bookshelf/src/bksw_sec.erl
@@ -1,7 +1,8 @@
 %% -*- erlang-indent-level: 4;indent-tabs-mode: nil; fill-column: 92 -*-
 %% ex: ts=4 sw=4 et
 %% @author Eric B Merritt <ericbmerritt@gmail.com>
-%% Copyright 2012 Opscode, Inc. All Rights Reserved.
+%% @author Lincoln Baker  <lbaker@chef.io>
+%% Copyright 2020 Chef Software, Inc.
 %%
 %% This file is provided to you under the Apache License,
 %% Version 2.0 (the "License"); you may not use this file
@@ -20,124 +21,151 @@
 
 -module(bksw_sec).
 
--export([is_authorized/2]).
+-export([encode_access_denied_error_response/3]).
+-export([is_authorized/2                      ]).
+-export([parse_authorization/1                ]).
 
--define(SECONDS_AT_EPOCH, 62167219200).
+-ifdef(TEST).
+-compile([export_all, nowarn_export_all       ]).
+-endif.
+
+% is this necessary?  try removing.
 -include("internal.hrl").
+
+-include_lib("erlcloud/include/erlcloud_aws.hrl").
 
 %%===================================================================
 %% API functions
 %%===================================================================
-is_authorized(Req0, #context{auth_check_disabled=true} = Context) ->
-    {true, Req0, Context};
-is_authorized(Req0, #context{} = Context) ->
-    Headers = mochiweb_headers:to_list(wrq:req_headers(Req0)),
-    {RequestId, Req1} = bksw_req:with_amz_request_id(Req0),
-    case proplists:get_value('Authorization', Headers, undefined) of
-        undefined ->
-            do_signed_url_authorization(RequestId, Req1, Context);
-        IncomingAuth ->
-            do_standard_authorization(RequestId, IncomingAuth, Req1, Context)
-    end.
 
-do_signed_url_authorization(RequestId, Req0, #context{reqid = ReqId} = Context) ->
-    AWSAccessKeyId = wrq:get_qs_value("AWSAccessKeyId", Req0),
-    Expires = wrq:get_qs_value("Expires", Req0),
-    IncomingSignature = wrq:get_qs_value("Signature", Req0),
-    RawMethod = wrq:method(Req0),
-    Method = string:to_lower(erlang:atom_to_list(RawMethod)),
-    Headers = mochiweb_headers:to_list(wrq:req_headers(Req0)),
-    Path  = wrq:path(Req0),
-    AccessKey = bksw_conf:access_key_id(Context),
-    SecretKey = bksw_conf:secret_access_key(Context),
-    ExpireDiff = expire_diff(Expires),
-    case make_signed_url_authorization(SecretKey,
-                                       Method,
-                                       Path,
-                                       Expires,
-                                       Headers) of
-        {StringToSign, Signature} ->
-            case ExpireDiff =< 0 of
-                true ->
-                    ?LOG_DEBUG("req_id=~p expired signature (~p) for ~p",
-                               [ReqId, Expires, Path]),
-                    encode_access_denied_error_response(RequestId, Req0, Context);
-                false ->
-                    case ((erlang:iolist_to_binary(AWSAccessKeyId) ==
-                               erlang:iolist_to_binary(AccessKey)) andalso
-                          erlang:iolist_to_binary(Signature) ==
-                              erlang:iolist_to_binary(IncomingSignature)) of
-                        true ->
-                            MaxAge = "max-age=" ++ integer_to_list(ExpireDiff),
-                            Req1 = wrq:set_resp_header("Cache-Control", MaxAge, Req0),
-                            {true, Req1, Context};
-                        false ->
-                            ?LOG_DEBUG("req_id=~p signing error for ~p", [ReqId, Path]),
-                            encode_sign_error_response(AWSAccessKeyId, IncomingSignature, RequestId,
-                                                       StringToSign, Req0, Context)
-                    end
-                end;
-        error ->
-            encode_access_denied_error_response(RequestId, Req0, Context)
-    end.
+is_authorized(Req0, #context{auth_check_disabled = true          } = Context) -> {true, Req0, Context};
+is_authorized(Req0, #context{auth_type           = presigned_url,
+                             date                = Date,
+                             incoming_sig        = IncomingSignature,
+                             signed_headers      = SignedHeaders,
+                             x_amz_expires_int   = XAmzExpiresInt} = Context) ->
+    Auth               = auth_init(Req0, Context, SignedHeaders),
+    {Bucketname, Key } = get_bucket_key(path(Auth)),
+    ComparisonURL      = mini_s3:s3_url(method(Auth), Bucketname, Key, XAmzExpiresInt, SignedHeaders, Date, config(Auth)),
+    IncomingSig        = list_to_binary(IncomingSignature),
+    [_, ComparisonSig] = string:split(ComparisonURL, "&X-Amz-Signature=", trailing),
 
-make_signed_url_authorization(SecretKey, Method, Path, Expires, Headers) ->
-    try
-        mini_s3:make_signed_url_authorization(SecretKey,
-                                              erlang:list_to_existing_atom(Method),
-                                              Path,
-                                              Expires,
-                                              Headers)
-    catch
-        _:Why ->
-            error_logger:error_report({error, {{mini_s3, make_signed_url_authorization},
-                                               [<<"SECRETKEY">>, Method, Path, Expires, Headers],
-                                               Why}}),
-            error
-    end.
+    % If the signature computation and comparison fails, this
+    % implementation attempts an alternative signature computation/comparison
+    % which adds or removes the port from the host header depending on whether
+    % the port is present or absent. At the time this was put in, there were
+    % problems with signature failures due to inconsistent treatment of host
+    % headers by various chef clients (present or missing ports).  Determining
+    % whether this capability is still necessary would require some investigation
+    % and testing.
 
-do_standard_authorization(RequestId, IncomingAuth, Req0, Context) ->
-    Headers = mochiweb_headers:to_list(wrq:req_headers(Req0)),
-    AmzHeaders = amz_headers(Headers),
-    RawMethod = wrq:method(Req0),
-    Method = string:to_lower(erlang:atom_to_list(RawMethod)),
-    ContentMD5 = proplists:get_value('Content-Md5', Headers, ""),
-    ContentType = proplists:get_value('Content-Type', Headers, ""),
-    Date = proplists:get_value('Date', Headers, ""),
-    %% get_object_and_bucket decodes the bucket, but the request will have been signed with
-    %% the encoded bucket.
-    {ok, Bucket0, Resource} = bksw_util:get_object_and_bucket(Req0),
-    Bucket = bksw_io_names:encode(Bucket0),
-    AccessKey = bksw_conf:access_key_id(Context),
-    SecretKey = bksw_conf:secret_access_key(Context),
+    CalculatedSig =
+        case IncomingSig of
+            ComparisonSig ->
+                %AltComparisonSig = "not computed",
+                IncomingSig;
+            _ ->
+                AltComparisonURL      = mini_s3:s3_url(method(Auth), Bucketname, Key, XAmzExpiresInt, alt_signed_headers(Auth), Date, config(Auth)),
+                [_, AltComparisonSig] = string:split(AltComparisonURL, "&X-Amz-Signature=", all),
+                AltComparisonSig
+        end,
+    auth_finish(Auth, Context, ComparisonURL, IncomingSig, CalculatedSig);
+is_authorized(Req0, #context{auth_type           = auth_header,
+                             date                = Date,
+                             incoming_sig        = IncomingSignature,
+                             region              = Region,
+                             signed_headers      = SignedHeaders} = Context) ->
+    Auth              = auth_init(Req0, Context, SignedHeaders),
+    ComparisonURL     = "not-applicable",
+    QueryParams       = wrq:req_qs(req(Auth)),
+    SigV4Headers      = erlcloud_aws:sign_v4(method(Auth), path(Auth), config(Auth), SignedHeaders, <<>>, Region, "s3", QueryParams, Date),
+    IncomingSig       = IncomingSignature,
+    ComparisonSig     = parseauth_or_throw(proplists:get_value("Authorization", SigV4Headers, ""), {reqid(Auth), req(Auth), Context}),
 
-    {StringToSign, RawCheckedAuth} =
-        mini_s3:make_authorization(AccessKey, SecretKey,
-                                   erlang:list_to_existing_atom(Method),
-                                   bksw_util:to_string(ContentMD5),
-                                   bksw_util:to_string(ContentType),
-                                   bksw_util:to_string(Date),
-                                   AmzHeaders, bksw_util:to_string(Bucket),
-                                   "/" ++ bksw_util:to_string(Resource),
-                                   ""),
-    CheckedAuth = erlang:iolist_to_binary(RawCheckedAuth),
-    [AccessKeyId, Signature] = split_authorization(IncomingAuth),
-    case erlang:iolist_to_binary(IncomingAuth) of
-        CheckedAuth ->
-            {true, Req0, Context};
+    % If the signature computation and comparison fails, this
+    % implementation attempts an alternative signature computation/comparison
+    % which adds or removes the port from the host header depending on whether
+    % the port is present or absent. At the time this was put in, there were
+    % problems with signature failures due to inconsistent treatment of host
+    % headers by various chef clients (present or missing ports).  Determining
+    % whether this capability is still necessary would require some investigation
+    % and testing.
+
+    CalculatedSig =
+        case IncomingSig of
+            ComparisonSig ->
+                %AltComparisonSig = "not computed",
+                IncomingSig;
+            _ ->
+                AltSigV4Headers   = erlcloud_aws:sign_v4(method(Auth), path(Auth), config(Auth), alt_signed_headers(Auth), <<>>, Region, "s3", QueryParams, Date),
+                _AltComparisonSig = parseauth_or_throw(proplists:get_value("Authorization", AltSigV4Headers, ""), {reqid(Auth), req(Auth), Context})
+        end,
+    auth_finish(Auth, Context, ComparisonURL, IncomingSig, CalculatedSig).
+
+% @doc split authorization header into component parts
+% https://docs.aws.amazon.com/AmazonS3/latest/API/sigv4-auth-using-authorization-header.html
+-spec parse_authorization(string()) -> {ok, [string()]} | {error, parse_authorization}.
+parse_authorization(Authorization) ->
+    case string:split(Authorization, " ", all) of
+        ["AWS4-HMAC-SHA256", "Credential="++Cred, "SignedHeaders="++SigHead, "Signature="++Signature] ->
+            {ok, [string:trim(Cred, trailing, ","), string:trim(SigHead, trailing, ","), Signature]};
         _ ->
-            encode_sign_error_response(AccessKeyId, Signature,
-                                       RequestId, StringToSign, Req0, Context)
+            {error, parse_authorization}
     end.
 
--spec expire_diff(undefined | binary()) -> integer().
-expire_diff(undefined) -> 1;
-expire_diff(Expires) ->
-    Now = calendar:datetime_to_gregorian_seconds(erlang:universaltime()),
-    bksw_util:to_integer(Expires) - (Now - ?SECONDS_AT_EPOCH).
+encode_access_denied_error_response(RequestId, Req0, Context) ->
+    Req1 = bksw_req:with_amz_id_2(Req0),
+    Body = bksw_xml:access_denied_error(RequestId),
+    Req2 = wrq:set_resp_body(Body, Req1),
+    {{halt, 403}, Req2, Context}.
 
-%% get_bucket([Bucket, _, _]) ->
-%%     Bucket.
+%%===================================================================
+% local functions, helpers, etc.
+%%===================================================================
+
+% common setup, init
+-spec auth_init(any(), tuple(), string()) -> map().
+auth_init(Req0, Context, SignedHeaders) ->
+    AccessKey            =  bksw_conf:access_key_id(Context),
+    {RequestId, Req1}    =  bksw_req:with_amz_request_id(Req0),
+    Config               =  mini_s3:new(AccessKey, bksw_conf:secret_access_key(Context), host(Req1)),
+    #{accesskey          => AccessKey,
+      config             => Config,
+      alt_signed_headers => [case {K, V} of {"host", _} -> {"host", get_host_toggleport(host(Req1), Config)}; _ -> {K, V} end || {K, V} <- SignedHeaders],
+      method             => list_to_atom(string:to_lower(erlang:atom_to_list(wrq:method(Req1)))),
+      path               => wrq:path(Req1),
+      req                => Req1,
+      reqid              => RequestId}.
+
+% TODO: spec
+auth_finish(Auth, #context{
+                     aws_access_key_id = AWSAccessKeyId,
+                     date              = Date,
+                     reqid             = ReqId,
+                     x_amz_expires_int = XAmzExpiresInt,
+                     x_amz_expires_str = XAmzExpiresString
+                  } = Context, ComparisonURL, IncomingSig, CalculatedSig) ->
+    case IncomingSig of
+        CalculatedSig ->
+            case is_expired(Date, XAmzExpiresInt) of
+                true ->
+                    ?LOG_DEBUG("req_id=~p expired signature (~p) for ~p", [ReqId, XAmzExpiresInt, path(Auth)]),
+                    encode_access_denied_error_response(reqid(Auth), req(Auth), Context);
+                false ->
+                    case erlang:iolist_to_binary(AWSAccessKeyId) == erlang:iolist_to_binary(accesskey(Auth)) of
+                        true ->
+                            MaxAge = "max-age=" ++ XAmzExpiresString,
+                            Req2   = wrq:set_resp_header("Cache-Control", MaxAge, req(Auth)),
+                            {true, Req2, Context};
+                        false ->
+                            ?LOG_DEBUG("req_id=~p signing error for ~p", [ReqId, path(Auth)]),
+                            encode_sign_error_response(AWSAccessKeyId, IncomingSig, reqid(Auth),
+                                                       ComparisonURL, req(Auth), Context)
+                    end
+            end;
+        _ ->
+            encode_access_denied_error_response(reqid(Auth), req(Auth), Context)
+    end.
 
 encode_sign_error_response(AccessKeyId, Signature,
                            RequestId, StringToSign, Req0,
@@ -150,26 +178,83 @@ encode_sign_error_response(AccessKeyId, Signature,
     Req2 = wrq:set_resp_body(Body, Req1),
     {{halt, 403}, Req2, Context}.
 
-encode_access_denied_error_response(RequestId, Req0, Context) ->
-    Req1 = bksw_req:with_amz_id_2(Req0),
-    Body = bksw_xml:access_denied_error(RequestId),
-    Req2 = wrq:set_resp_body(Body, Req1),
-    {{halt, 403}, Req2, Context}.
+% split  "<bucketname>/<key>" (possibly leading and/or trailing /) into {"bucketname", "key"}
+% Path = "<bucketname>/<key>"
+-spec get_bucket_key(Path::string()) -> {string(), string()}.
+get_bucket_key(Path) ->
+    case string:lexemes(Path, "/") of
+        [            ] -> {"",     ""};
+        [Bucket      ] -> {Bucket, ""};
+        [Bucket | Key] -> {Bucket, filename:join(Key)}
+    end.
 
-split_authorization([$A, $W, $S, $\s, $: | Rest]) ->
-    [<<>>, Rest];
-split_authorization([$A, $W, $S, $\s  | Rest]) ->
-    string:tokens(Rest, ":").
+% get host and toggle the port (add port or remove it)
+-spec get_host_toggleport(string(), aws_config()) -> string().
+get_host_toggleport(Host, Config) ->
+    case string:split(Host, ":", trailing) of
+        [Host      ] ->
+            Port = integer_to_list(Config#aws_config.s3_port),
+            string:join([Host, Port], ":");
+        ["http",  _] ->
+            Port = integer_to_list(Config#aws_config.s3_port),
+            string:join([Host, Port], ":");
+        ["https", _] ->
+            Port = integer_to_list(Config#aws_config.s3_port),
+            string:join([Host, Port], ":");
+        [H,       _] ->
+            H
+    end.
 
-is_amz([$X, $-, $A, $m, $z, $-, $A, $c, $l | _]) ->
-    true;
-is_amz([$x, $-, $a, $m, $z, $-, $a, $c, $l | _]) ->
-    true;
-is_amz(_) ->
-    false.
+-spec host(tuple()) -> list().
+host(Req0) ->
+    wrq:get_req_header("Host", Req0).
 
-amz_headers(Headers) ->
-    [{process_header(K), V} || {K,V} <- Headers, is_amz(K)].
+% most ways of getting the date/time seem problematic.  for instance, docs for
+% calendar:universal_time() and erlang:universaltime() say: 'Returns local time
+% if universal time is unavailable.'
+% since it is unknown which time would be used, we could use local time and
+% convert to universal.  however, local time could be an 'illegal' time wrt
+% universal time if switching to/from daylight savings time.
+%
+% note that we are neither measuring an elapsed time, nor determining an order
+% of events, nor creating a unique name, so time correction should not be an
+% issue.
+% https://erlang.org/doc/apps/erts/time_correction.html
+% 1 =< ExpiresSec =< 604800
+-spec is_expired(DateTimeString::string(), ExpiresSec::integer()) -> boolean().
+is_expired(DateTimeString, ExpiresSec) ->
+    [Y1, Y2, Y3, Y4, M1, M2, D1, D2, _, H1, H2, N1, N2, S1, S2, _] = DateTimeString,
+    Year    = list_to_integer([Y1, Y2, Y3, Y4]),
+    Month   = list_to_integer([M1, M2        ]),
+    Day     = list_to_integer([D1, D2        ]),
+    Hour    = list_to_integer([H1, H2        ]),
+    Min     = list_to_integer([N1, N2        ]),
+    Sec     = list_to_integer([S1, S2        ]),
 
-process_header(Key) ->
-    string:to_lower(bksw_util:to_string(Key)).
+    % this could be used to check if the date constructed is valid
+    % calendar:valid_date({{Year, Month, Day}, {Hour, Min, Sec}}),
+
+    DateSeconds      = calendar:datetime_to_gregorian_seconds({{Year, Month, Day}, {Hour, Min, Sec}}),
+    UniversalTimeSec = calendar:datetime_to_gregorian_seconds(calendar:now_to_universal_time(os:timestamp())),
+    DateSeconds + ExpiresSec < UniversalTimeSec.
+
+% TODO: export and test this
+-spec parseauth_or_throw(string(), tuple()) -> string() | no_return().
+parseauth_or_throw(Auth, Throw) ->
+    try
+        case parse_authorization(Auth) of
+            {ok, [_, _, Sig]} -> Sig;
+            _                 -> throw(Throw)
+        end
+    catch
+        throw:{ReqId, Req, Context} -> encode_access_denied_error_response(ReqId, Req, Context)
+    end.
+
+% accessors
+accesskey(Auth)          -> maps:get(accesskey,          Auth).
+alt_signed_headers(Auth) -> maps:get(alt_signed_headers, Auth).
+config(Auth)             -> maps:get(config,             Auth).
+method(Auth)             -> maps:get(method,             Auth).
+path(Auth)               -> maps:get(path,               Auth).
+req(Auth)                -> maps:get(req,                Auth).
+reqid(Auth)              -> maps:get(reqid,              Auth).
