bf857b4775542caf191ba4accda5902dbbe740cb
Steven Danna <steve@chef.io>
2020-09-16 10:25:39 +0100

    Update openresty to 1.17.8.2
    Signed-off-by: Prajakta Purohit <prajakta@chef.io>

--- omnibus_overrides.rb
+++ omnibus_overrides.rb
@@ -17,10 +17,4 @@ override :perl, version: "5.18.1"
 # This SHA is the last commit before the 6.0 release
 override :'berkshelf-no-depselector', version: '6016ca10b2f46508b1b107264228668776f505d9'
 
-# Note 2018/02/01 (sr): This is related to issue #1417:
-# 1.11.2.1 was the last version that supports --with-lua51=PATH, which allows us to
-# build it with lua on ppc64[le] and s390x. Those platforms are not supported
-# in mainline luajit. There's forks for ppc64, and s390x, but going forward with
-# those was so far blocked by ppc64 not being supported even with the PPC64
-# fork.
-override :openresty, version: "1.15.8.1"
+override :openresty, version: "1.17.8.2"
