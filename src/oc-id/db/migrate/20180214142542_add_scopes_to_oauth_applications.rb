9242a8e7ebe45b5437de24a40781a695f0c13ad9
antima-gupta <agupta@msystechnologies.com>
2020-09-23 16:00:35 +0530

    Added ActiveRecord Migration version 6.0
    Signed-off-by: antima-gupta <agupta@msystechnologies.com>

--- src/oc-id/db/migrate/20180214142542_add_scopes_to_oauth_applications.rb
+++ src/oc-id/db/migrate/20180214142542_add_scopes_to_oauth_applications.rb
@@ -1,4 +1,4 @@
-class AddScopesToOauthApplications < ActiveRecord::Migration
+class AddScopesToOauthApplications < ActiveRecord::Migration[6.0]
   def change
     add_column :oauth_applications, :scopes, :string, null: false, default: ''
   end
