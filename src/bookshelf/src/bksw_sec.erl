aa7cf16fc9228cbf32e4405557a80d86be58a76a
Lincoln Baker <lbaker@chef.io>
2020-09-27 23:09:03 -0500

    consolidate throws into auth_finish
    Signed-off-by: Lincoln Baker <lbaker@chef.io>

--- src/bookshelf/src/bksw_sec.erl
+++ src/bookshelf/src/bksw_sec.erl
@@ -51,61 +51,53 @@ is_authorized(Req0, #context{auth_type           = presigned_url,
                              incoming_sig        = IncomingSignature,
                              signed_headers      = SignedHeaders,
                              x_amz_expires_int   = XAmzExpiresInt} = Context) ->
-    {RequestId, Req1}      = bksw_req:with_amz_request_id(Req0),
-    try
-        Auth               = auth_init(Req1, Context, SignedHeaders),
-        {Bucketname, Key } = get_bucket_key(?PATH(Auth)),
-        ComparisonURL      = mini_s3:s3_url(?METHOD(Auth), Bucketname, Key, XAmzExpiresInt, SignedHeaders, Date, ?CONFIG(Auth)),
-        IncomingSig        = list_to_binary(IncomingSignature),
-        [_, ComparisonSig] = string:split(ComparisonURL, "&X-Amz-Signature=", trailing),
+    {RequestId, Req1}  = bksw_req:with_amz_request_id(Req0),
+    Auth               = auth_init(Req1, Context, SignedHeaders),
+    {Bucketname, Key } = get_bucket_key(?PATH(Auth)),
+    ComparisonURL      = mini_s3:s3_url(?METHOD(Auth), Bucketname, Key, XAmzExpiresInt, SignedHeaders, Date, ?CONFIG(Auth)),
+    IncomingSig        = list_to_binary(IncomingSignature),
+    [_, ComparisonSig] = string:split(ComparisonURL, "&X-Amz-Signature=", trailing),
 
-        % TODO: try to remove alt sig computation and see what happens
-        % NOTE: this was tried, and caused compilation and test failures.
+    % TODO: try to remove alt sig computation and see what happens
+    % NOTE: this was tried, and caused compilation and test failures.
 
-        CalculatedSig =
-            case IncomingSig of
-                ComparisonSig ->
-                    %AltComparisonSig = "not computed",
-                    IncomingSig;
-                _ ->
-                    AltComparisonURL = mini_s3:s3_url(?METHOD(Auth), Bucketname, Key, XAmzExpiresInt, ?ALT_SIGNED_HEADERS(Auth), Date, ?CONFIG(Auth)),
-                    [_, AltComparisonSig] = string:split(AltComparisonURL, "&X-Amz-Signature=", all),
-                    AltComparisonSig
-            end,
-    auth_finish(RequestId, Req1, Context, Auth, ComparisonURL, IncomingSig, CalculatedSig)
-    catch
-        throw:{RequestId, Req, Context} -> encode_access_denied_error_response(RequestId, Req, Context)
-    end;
+    CalculatedSig =
+        case IncomingSig of
+            ComparisonSig ->
+                %AltComparisonSig = "not computed",
+                IncomingSig;
+            _ ->
+                AltComparisonURL = mini_s3:s3_url(?METHOD(Auth), Bucketname, Key, XAmzExpiresInt, ?ALT_SIGNED_HEADERS(Auth), Date, ?CONFIG(Auth)),
+                [_, AltComparisonSig] = string:split(AltComparisonURL, "&X-Amz-Signature=", all),
+                AltComparisonSig
+        end,
+    auth_finish(RequestId, Req1, Context, Auth, ComparisonURL, IncomingSig, CalculatedSig);
 is_authorized(Req0, #context{auth_type           = auth_header,
                              date                = Date,
                              incoming_sig        = IncomingSignature,
                              region              = Region,
                              signed_headers      = SignedHeaders} = Context) ->
     {RequestId, Req1} = bksw_req:with_amz_request_id(Req0),
-    try
-        Auth          = auth_init(Req1, Context, SignedHeaders),
-        ComparisonURL = "not-applicable",
-        QueryParams   = wrq:req_qs(Req1),
-        SigV4Headers  = erlcloud_aws:sign_v4(?METHOD(Auth), ?PATH(Auth), ?CONFIG(Auth), SignedHeaders, <<>>, Region, "s3", QueryParams, Date),
-        IncomingSig   = IncomingSignature,
-        ComparisonSig = parseauth_or_throw(proplists:get_value("Authorization", SigV4Headers, ""), {RequestId, Req1, Context}),
+    Auth              = auth_init(Req1, Context, SignedHeaders),
+    ComparisonURL     = "not-applicable",
+    QueryParams       = wrq:req_qs(Req1),
+    SigV4Headers      = erlcloud_aws:sign_v4(?METHOD(Auth), ?PATH(Auth), ?CONFIG(Auth), SignedHeaders, <<>>, Region, "s3", QueryParams, Date),
+    IncomingSig       = IncomingSignature,
+    ComparisonSig     = parseauth_or_throw(proplists:get_value("Authorization", SigV4Headers, ""), {RequestId, Req1, Context}),
 
-        % TODO: try to remove alt sig computation and see what happens
-        % NOTE: this was tried, and caused compilation and test failures.
+    % TODO: try to remove alt sig computation and see what happens
+    % NOTE: this was tried, and caused compilation and test failures.
 
-        CalculatedSig =
-            case IncomingSig of
-                ComparisonSig ->
-                    %AltComparisonSig = "not computed",
-                    IncomingSig;
-                _ ->
-                    AltSigV4Headers   = erlcloud_aws:sign_v4(?METHOD(Auth), ?PATH(Auth), ?CONFIG(Auth), ?ALT_SIGNED_HEADERS(Auth), <<>>, Region, "s3", QueryParams, Date),
-                    _AltComparisonSig = parseauth_or_throw(proplists:get_value("Authorization", AltSigV4Headers, ""), {RequestId, Req1, Context})
-            end,
-    auth_finish(RequestId, Req1, Context, Auth, ComparisonURL, IncomingSig, CalculatedSig)
-    catch
-        throw:{RequestId, Req, Context} -> encode_access_denied_error_response(RequestId, Req, Context)
-    end.
+    CalculatedSig =
+        case IncomingSig of
+            ComparisonSig ->
+                %AltComparisonSig = "not computed",
+                IncomingSig;
+            _ ->
+                AltSigV4Headers   = erlcloud_aws:sign_v4(?METHOD(Auth), ?PATH(Auth), ?CONFIG(Auth), ?ALT_SIGNED_HEADERS(Auth), <<>>, Region, "s3", QueryParams, Date),
+                _AltComparisonSig = parseauth_or_throw(proplists:get_value("Authorization", AltSigV4Headers, ""), {RequestId, Req1, Context})
+        end,
+    auth_finish(RequestId, Req1, Context, Auth, ComparisonURL, IncomingSig, CalculatedSig).
 
 % @doc split authorization header into component parts
 % https://docs.aws.amazon.com/AmazonS3/latest/API/sigv4-auth-using-authorization-header.html
@@ -241,9 +233,14 @@ is_expired(DateTimeString, ExpiresSec) ->
     UniversalTimeSec = calendar:datetime_to_gregorian_seconds(calendar:now_to_universal_time(os:timestamp())),
     DateSeconds + ExpiresSec < UniversalTimeSec.
 
+% TODO: export and test this
 -spec parseauth_or_throw(string(), tuple()) -> string() | no_return().
 parseauth_or_throw(Auth, Throw) ->
-    case parse_authorization(Auth) of
-        {ok, [_, _, Sig]} -> Sig;
-        _                 -> throw(Throw)
+    try
+        case parse_authorization(Auth) of
+            {ok, [_, _, Sig]} -> Sig;
+            _                 -> throw(Throw)
+        end
+    catch
+        throw:{RequestId, Req, Context} -> encode_access_denied_error_response(RequestId, Req, Context)
     end.
