fd0c3fd5cc652247cabdf9d09bc0645b9d4e74c2
Timothy Ehlers <tehlers@expedia.com>
2020-10-27 13:55:36 -0500

    fix max elasticsearch memory
    Signed-off-by: Timothy Ehlers <tehlers@expedia.com>

--- omnibus/files/private-chef-cookbooks/private-chef/metadata.rb
+++ omnibus/files/private-chef-cookbooks/private-chef/metadata.rb
@@ -3,7 +3,7 @@ maintainer        'Chef Software, Inc.'
 maintainer_email  'cookbooks@chef.io'
 license           'Apache-2.0'
 description       'Installs and configures Chef Server from Omnibus'
-version           '0.1.1'
+version           '0.1.2'
 
 %w( ubuntu debian redhat centos oracle scientific fedora amazon ).each do |os|
   supports os
