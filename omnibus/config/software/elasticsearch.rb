d9163814fbd52c43ab5d07b7ccd4eea72aa54f7e
Lincoln Baker <lbaker@chef.io>
2020-12-03 02:47:18 -0600

    Add elasticsearch 7.9.1 to omnibus/config/software/elasticsearch.rb
    Signed-off-by: Lincoln Baker <lbaker@chef.io>

--- omnibus/config/software/elasticsearch.rb
+++ omnibus/config/software/elasticsearch.rb
@@ -30,6 +30,11 @@ version "6.8.12" do
          sha512: "9f6179ee49baa48b49c5328b88ddf2f0ef868f49c1f04d77975622120749725c48ac09cd565c05f6033eb227eaff905aff9f881a85efcf2fa75cc586cb8c45cb"
 end
 
+version "7.9.1" do
+  source url: "https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-#{version}-linux-x86_64.tar.gz",
+         sha512: "5c159bdf0d6e140a2bee5fbb1c379fbe23b0ea39b01d715564f02e4674b444b065a8abfda86440229c4b70defa175722c479b60009b7eef7b3de66e2339aacea"
+end
+
 target_path = "#{install_dir}/embedded/elasticsearch"
 
 build do
