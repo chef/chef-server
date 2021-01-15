ec9426cfc826fb439780481782efb919ed27377e
Prajakta Purohit <prajakta@chef.io>
2021-01-15 07:54:37 -0800

    The version of ruby on the builder was updated from 2.6.0 to 2.7.0. Updating the monkeypatch for this to later version.
    Signed-off-by: Prajakta Purohit <prajakta@chef.io>

--- src/oc-id/spec/support/monkeypatch.rb
+++ src/oc-id/spec/support/monkeypatch.rb
@@ -1,18 +1,24 @@
-# This patch is only required when using rails 4.2 with ruby 2.6
+# This patch is only required when using rails 4.2 with ruby 2.6 or greater
 #
-# https://github.com/rails/rails/issues/34790
+# https://github.com/rails/rails/issues/34790#issuecomment-681034561
 
-if RUBY_VERSION>='2.6.0'
-  if Rails.version < '5'
+# a workaround to avoid MonitorMixin double-initialize error
+# https://github.com/rails/rails/issues/34790#issuecomment-681034561
+if Gem::Version.new(RUBY_VERSION) >= Gem::Version.new('2.6.0')
+  if Gem::Version.new(Rails.version) < Gem::Version.new('5.0.0')
     class ActionController::TestResponse < ActionDispatch::TestResponse
       def recycle!
-        # hack to avoid MonitorMixin double-initialize error:
-        @mon_mutex_owner_object_id = nil
-        @mon_mutex = nil
-        initialize
+        if Gem::Version.new(RUBY_VERSION) >= Gem::Version.new('2.7.0')
+          @mon_data = nil
+          @mon_data_owner_object_id = nil
+        else
+          @mon_mutex = nil
+          @mon_mutex_owner_object_id = nil
+        end
+         initialize
       end
     end
   else
-    puts "Monkeypatch for ActionController::TestResponse no longer needed"
+    $stderr.puts "Monkeypatch for ActionController::TestResponse is no longer needed"
   end
 end
