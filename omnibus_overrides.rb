53a871174492fbef996557c639b41cdcac54cd64
Prajakta Purohit <prajakta@chef.io>
2021-01-21 20:20:47 -0800

    Point omnibus_overrides to the fixed branch
    Signed-off-by: Prajakta Purohit <prajakta@chef.io>

--- omnibus_overrides.rb
+++ omnibus_overrides.rb
@@ -6,7 +6,7 @@ override :erlang, version: "22.2"
 override :rubygems, version: "3.0.3"
 override :bundler, version: "1.17.2"  # pin to avoid double bundle error
 override :'omnibus-ctl', version: "master"
-override :chef, version: "v15.12.22"
+override :chef, version: "chef-15"
 override :ohai, version: "v15.12.0"
 override :ruby, version: "2.6.6"
 override :perl, version: "5.18.1"
