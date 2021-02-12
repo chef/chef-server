f0fe56483adb65c757537aa00af540311bafefe5
Lincoln Baker <lbaker@chef.io>
2021-02-12 08:28:35 -0600

    dev/scripts/provision-postgres.sh
    Signed-off-by: Lincoln Baker <lbaker@chef.io>

--- dev/scripts/provision-postgres.sh
+++ dev/scripts/provision-postgres.sh
@@ -2,9 +2,9 @@ echo "deb http://apt.postgresql.org/pub/repos/apt/ xenial-pgdg main" > /etc/apt/
 wget --quiet https://www.postgresql.org/media/keys/ACCC4CF8.asc
 apt-key add ACCC4CF8.asc
 apt-get update
-apt-get install postgresql-9.6 -y
-echo "host    all             all            $1/32         md5" >> /etc/postgresql/9.6/main/pg_hba.conf
-echo "listen_addresses='*'" >> /etc/postgresql/9.6/main/postgresql.conf
+apt-get install postgresql-12.5 -y
+echo "host    all             all            $1/32         md5" >> /etc/postgresql/12.5/main/pg_hba.conf
+echo "listen_addresses='*'" >> /etc/postgresql/12.5/main/postgresql.conf
 service postgresql restart
-export PATH=/usr/lib/postgresql/9.6/bin:$PATH
+export PATH=/usr/lib/postgresql/12.5/bin:$PATH
 sudo -u postgres psql -c "CREATE USER bofh SUPERUSER ENCRYPTED PASSWORD 'i1uvd3v0ps';"
