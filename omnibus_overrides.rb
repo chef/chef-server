a1bf09d91a040470144320833ba14ea0c8f731bf
Christopher A. Snapp <chris.snapp@progress.com>
2021-03-30 17:14:55 -0600

    Update to use sqitch 0.9999
    Signed-off-by: Christopher A. Snapp <chris.snapp@progress.com>

--- omnibus_overrides.rb
+++ omnibus_overrides.rb
@@ -8,6 +8,7 @@ override :chef, version: "v16.17.4"
 override :ohai, version: "v16.17.0"
 override :ruby, version: "2.7.5"
 override :perl, version: "5.30.0"
+override :sqitch, version: "0.9999"
 override :redis, version: "5.0.14"
 
 override :cpanminus, version: "1.7004" # 1.9019 breaks installs currently
