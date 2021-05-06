32686a1012731c76693ddc2734bb6e6ba4cbeb5a
Tim Smith <tsmith@chef.io>
2021-05-05 22:05:07 -0700

    Pin logrotate
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- omnibus_overrides.rb
+++ omnibus_overrides.rb
@@ -10,5 +10,6 @@ override :ruby, version: "2.6.7"
 override :perl, version: "5.18.1"
 
 override :cpanminus, version: "1.7004" # 1.9019 breaks installs currently
+override :logrotate, version: "3.9.2" # 3.18.0 patches fail
 
 override :openresty, version: "1.19.3.1"
