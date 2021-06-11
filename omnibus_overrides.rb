2494d3d8cdc7fb4f7c8d0ec39a9ebf6a7a5cae69
Tim Smith <tsmith@chef.io>
2021-06-11 15:13:45 -0700

    Bump Perl to 5.34
    Get onto modern perl releases so we can build on RHEL 8
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- omnibus_overrides.rb
+++ omnibus_overrides.rb
@@ -7,7 +7,7 @@ override :'omnibus-ctl', version: "master"
 override :chef, version: "v15.17.4"
 override :ohai, version: "v15.12.0"
 override :ruby, version: "2.6.7"
-override :perl, version: "5.18.1"
+override :perl, version: "5.34.0"
 
 override :cpanminus, version: "1.7004" # 1.9019 breaks installs currently
 override :logrotate, version: "3.9.2" # 3.18.0 patches fail
