329ffca3de8f5a682e84efbdca30f6545f04c8d3
Lincoln Baker <lbaker@chef.io>
2021-06-22 21:48:03 -0500

    convert references to 12 to 13
    Signed-off-by: Lincoln Baker <lbaker@chef.io>

--- scripts/bk_tests/bk_install.sh
+++ scripts/bk_tests/bk_install.sh
@@ -27,10 +27,10 @@ rm /etc/apt/sources.list.d/microsoft-prod.list
 
 echo  "Installing test dependencies"
 apt-get update -y
-apt-get install -y lua5.1 luarocks postgresql-12 libsqlite3-dev
+apt-get install -y lua5.1 luarocks postgresql-13 libsqlite3-dev
 
 echo "Configuring postgresql"
-cp /workdir/scripts/bk_tests/pb_hba.conf /etc/postgresql/12/main/pg_hba.conf
+cp /workdir/scripts/bk_tests/pb_hba.conf /etc/postgresql/13/main/pg_hba.conf
 
 # the erlang software definition lives in: /omnibus-software/config/software/erlang.rb
 
