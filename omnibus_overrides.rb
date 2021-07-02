9a4be1e3ec37762e795cd07bd7166611161249fb
Tim Smith <tsmith@chef.io>
2021-07-01 19:29:58 -0700

    Don't install chef in the omnibus build
    Rely on chef coming in as a dep in the ctl command instead.
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- omnibus_overrides.rb
+++ omnibus_overrides.rb
@@ -4,8 +4,6 @@
 #
 override :erlang, version: "22.2"
 override :'omnibus-ctl', version: "master"
-override :chef, version: "v15.17.4"
-override :ohai, version: "v15.12.0"
 override :ruby, version: "2.6.7"
 override :perl, version: "5.18.1"
 
