149e8d33c3875d1afe893b7c05d7c55df1616a36
Swati Keshari <skeshari@msystechnologies.com>
2021-07-21 15:00:50 +0530

    updating cpanminus version
    Signed-off-by: Swati Keshari <skeshari@msystechnologies.com>

--- omnibus_overrides.rb
+++ omnibus_overrides.rb
@@ -9,7 +9,7 @@ override :ohai, version: "v16.13.0"
 override :ruby, version: "2.7.4"
 override :perl, version: "5.18.1"
 
-override :cpanminus, version: "1.7004" # 1.9019 breaks installs currently
+override :cpanminus, version: "1.9011" # 1.9019 breaks installs currently
 override :logrotate, version: "3.9.2" # 3.18.0 patches fail
 
 override :openresty, version: "1.19.3.2"
