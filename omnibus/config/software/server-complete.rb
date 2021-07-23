1d53e0eece33b9b7d9419ac586e67af6c3451d53
Prajakta Purohit <prajakta@chef.io>
2021-07-23 09:33:40 -0700

    Smooth upgrade from 9.6 to 13.3
    Signed-off-by: Prajakta Purohit <prajakta@chef.io>

--- omnibus/config/software/server-complete.rb
+++ omnibus/config/software/server-complete.rb
@@ -29,6 +29,7 @@ dependency "runit"
 dependency "openssl-fips-config" if fips_mode?
 
 # the backend
+dependency "postgresql96-bin" # for upgrading 9.6 -> 13.3
 dependency "postgresql13"
 dependency "redis" # dynamic routing controls
 dependency "haproxy"
