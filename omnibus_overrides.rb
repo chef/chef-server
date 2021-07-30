15d62f66ee932f26ca2a2ca53f1d0862841872ac
Swati Keshari <skeshari@msystechnologies.com>
2021-07-30 19:43:51 +0530

    override perl version
    Signed-off-by: Swati Keshari <skeshari@msystechnologies.com>

--- omnibus_overrides.rb
+++ omnibus_overrides.rb
@@ -7,7 +7,7 @@ override :'omnibus-ctl', version: "master"
 override :chef, version: "v16.13.16"
 override :ohai, version: "v16.13.0"
 override :ruby, version: "2.7.4"
-override :perl, version: "5.18.1"
+override :perl, version: "5.30.0"
 
 override :cpanminus, version: "1.9011" # 1.9019 breaks installs currently
 override :logrotate, version: "3.9.2" # 3.18.0 patches fail
