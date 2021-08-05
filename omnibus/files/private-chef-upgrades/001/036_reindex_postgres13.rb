f924a24912019e7bb26047969132bd6c19ffa30c
Prajakta Purohit <prajakta@chef.io>
2021-08-05 09:12:51 -0700

    the sql_user key exists
    Signed-off-by: Prajakta Purohit <prajakta@chef.io>

--- omnibus/files/private-chef-upgrades/001/036_reindex_postgres13.rb
+++ omnibus/files/private-chef-upgrades/001/036_reindex_postgres13.rb
@@ -14,8 +14,7 @@
       #run_command("/opt/opscode/embedded/bin/reindexdb --all", :user => 'opscode-pgsql')
       running_config = JSON.parse(File.read("/etc/opscode/chef-server-running.json"))
       pc = running_config['private_chef']
-      keyname = pc['opscode-erchef'].has_key?('sql_user') ? 'opscode_erchef' : 'opscode-pgsql'
-      connection = ::PG::Connection.open('user' => pc[keyname]['sql_user'],
+      connection = ::PG::Connection.open('user' => pc['opscode-erchef']['sql_user'],
                                  'host' => pc['postgresql']['vip'],
                                  'password' => Partybus.config.secrets.get('opscode_erchef', 'sql_password'),
                                  'port' => pc['postgresql']['port'],
