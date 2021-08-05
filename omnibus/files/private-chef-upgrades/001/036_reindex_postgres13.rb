c545814a2b0fd3a127db626b1fee575ac166fb89
Prajakta Purohit <prajakta@chef.io>
2021-08-05 08:42:23 -0700

    Connect to db to run reindex
    Signed-off-by: Prajakta Purohit <prajakta@chef.io>

--- omnibus/files/private-chef-upgrades/001/036_reindex_postgres13.rb
+++ omnibus/files/private-chef-upgrades/001/036_reindex_postgres13.rb
@@ -9,10 +9,21 @@
 
       sleep 30
 
-      # Run reindex for postgresql 13
+       # Run reindex for postgresql 13
       log "Reindexing postgresql..."
-      cmd = Mixlib::ShellOut.new("/opt/opscode/embedded/bin/reindexdb --all", :user => 'postgres')
-      cmd.run_command
-
+      #run_command("/opt/opscode/embedded/bin/reindexdb --all", :user => 'opscode-pgsql')
+      running_config = JSON.parse(File.read("/etc/opscode/chef-server-running.json"))
+      pc = running_config['private_chef']
+      keyname = pc['opscode-erchef'].has_key?('sql_user') ? 'opscode_erchef' : 'opscode-pgsql'
+      connection = ::PG::Connection.open('user' => pc[keyname]['sql_user'],
+                                 'host' => pc['postgresql']['vip'],
+                                 'password' => Partybus.config.secrets.get('opscode_erchef', 'sql_password'),
+                                 'port' => pc['postgresql']['port'],
+                                 'dbname' => 'opscode_chef')
+      begin
+        connection.exec("REINDEX DATABASE opscode_chef")
+      ensure
+        connection.close
+      end
     end
   end
