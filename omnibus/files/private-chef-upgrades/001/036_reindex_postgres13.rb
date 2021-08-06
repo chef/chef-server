717400ed34724737af24b83cdc312fa5367f7558
Prajakta Purohit <prajakta@chef.io>
2021-08-06 14:56:39 -0700

    cookstyle fixes
    Signed-off-by: Prajakta Purohit <prajakta@chef.io>

--- omnibus/files/private-chef-upgrades/001/036_reindex_postgres13.rb
+++ omnibus/files/private-chef-upgrades/001/036_reindex_postgres13.rb
@@ -2,11 +2,11 @@ define_upgrade do
   if Partybus.config.bootstrap_server
     must_be_data_master
     # Make sure API is down
-    stop_services(["nginx", "opscode-erchef"])
-    start_services(["postgresql"])
+    stop_services(%w(nginx opscode-erchef))
+    start_services(['postgresql'])
     sleep 30
-     # Run reindex for postgresql 13
-    log "Reindexing postgreSQL..."
+    # Run reindex for postgresql 13
+    log 'Reindexing postgreSQL...'
     run_command("sudo -H -u  opscode-pgsql bash -c \'/opt/opscode/embedded/bin/reindexdb --all\'")
   end
 end
