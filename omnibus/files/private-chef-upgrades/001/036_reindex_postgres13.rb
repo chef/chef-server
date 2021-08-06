3915dae375bd719bd60a5f9865a0aa53c8459557
jan shahid shaik <jashaik@progress.com>
2021-08-06 20:35:28 +0530

    Added auto reindexing for pg 13.3
    Signed-off-by: jan shahid shaik <jashaik@progress.com>

---
+++ omnibus/files/private-chef-upgrades/001/036_reindex_postgres13.rb
@@ -0,0 +1,12 @@
+define_upgrade do
+  if Partybus.config.bootstrap_server
+    must_be_data_master
+    # Make sure API is down
+    stop_services(["nginx", "opscode-erchef"])
+    start_services(["postgresql"])
+    sleep 30
+     # Run reindex for postgresql 13
+    log "Reindexing postgreSQL..."
+    run_command("sudo -H -u  opscode-pgsql bash -c \'/opt/opscode/embedded/bin/reindexdb --all\'")
+  end
+end
