2831dbdbcc40eda0035e28ec1a15550f8babb8a1
Collin McNeese <cmcneese@chef.io>
2021-08-25 07:47:50 -0500

    add guard for fix_permissions only if the dir already exists for nginx
    Signed-off-by: Collin McNeese <cmcneese@chef.io>

--- omnibus/files/server-ctl-cookbooks/infra-server/recipes/fix_permissions.rb
+++ omnibus/files/server-ctl-cookbooks/infra-server/recipes/fix_permissions.rb
@@ -32,6 +32,9 @@ end
 ].each do |nginx_no_root_perms_fix_path|
   execute "find #{nginx_no_root_perms_fix_path} -user 'root' -exec chown #{node['private_chef']['user']['username']} {} \\;" do
     user 'root'
-    only_if { node['private_chef']['nginx']['nginx_no_root'] }
+    only_if do
+      node['private_chef']['nginx']['nginx_no_root'] &&
+        Dir.exist?(nginx_no_root_perms_fix_path)
+    end
   end
 end
