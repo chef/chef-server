29dde2fe8324723989e8eb4876c8886ec39c62fa
Prajakta Purohit <prajakta@chef.io>
2021-09-08 07:20:10 -0700

    Pint to the original version redis was pointed at: 5.0.7
    Signed-off-by: Prajakta Purohit <prajakta@chef.io>

--- omnibus_overrides.rb
+++ omnibus_overrides.rb
@@ -8,7 +8,7 @@ override :chef, version: "v16.13.16"
 override :ohai, version: "v16.13.0"
 override :ruby, version: "2.7.4"
 override :perl, version: "5.18.1"
-override :redis, version: "3.0.7"
+override :redis, version: "5.0.7"
 
 override :cpanminus, version: "1.7004" # 1.9019 breaks installs currently
 override :logrotate, version: "3.9.2" # 3.18.0 patches fail
