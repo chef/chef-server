8ba73146dae8de4d8ee6843ca4441fe42db9158d
Josh Gitlin <jgitlin@pinnacle21.com>
2021-09-09 13:19:54 -0400

    Wordmark replacements: chef-server-ctl/plugins/version (#1949)
    Signed-off-by: Josh Gitlin <jgitlin@pinnacle21.com>

--- src/chef-server-ctl/plugins/version.rb
+++ src/chef-server-ctl/plugins/version.rb
@@ -13,8 +13,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 require "json"
+require "chef-utils"
 
-add_command_under_category "version", "general", "Display current version of the Chef Infra Server.", 2 do
+add_command_under_category "version", "general", "Display current version of #{ChefUtils::Dist::Server::PRODUCT}.", 2 do
 
   # detect if running as a habitat service
   #
