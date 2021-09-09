569052a56657abbafc6f5d8236427644a4d2fd81
jan shahid shaik <jashaik@progress.com>
2021-09-09 15:43:33 +0530

    updated version
    Signed-off-by: jan shahid shaik <jashaik@progress.com>

--- omnibus/files/server-ctl-cookbooks/infra-server/libraries/preflight_postgres_validator.rb
+++ omnibus/files/server-ctl-cookbooks/infra-server/libraries/preflight_postgres_validator.rb
@@ -23,7 +23,7 @@ class PostgresqlPreflightValidator < PreflightValidator
   REQUIRED_VERSION  = Gem::Version.new(9.6)
 
   # supported PG version
-  SUPPORTED_VERSION = Gem::Version.new(13.3)
+  SUPPORTED_VERSION = Gem::Version.new(13.4)
 
   def run!
     warn_about_removed_attribute('checkpoint_segments')
