c89bf61b000c6a74eff9804579ef8220ff2d6a5c
Lincoln Baker <lbaker@chef.io>
2021-09-22 18:01:18 -0500

    comments
    Signed-off-by: Lincoln Baker <lbaker@chef.io>

--- omnibus/config/software/server-complete.rb
+++ omnibus/config/software/server-complete.rb
@@ -29,7 +29,7 @@ dependency "runit"
 dependency "openssl-fips-config" if fips_mode?
 
 # the backend
-dependency "postgresql96-bin" # for upgrading 9.6 -> 13.3
+dependency "postgresql96-bin"  # for upgrading 9.6  -> 13.3
 dependency "postgresql133-bin" # for upgrading 13.3 -> 13.4
 dependency "postgresql13"
 dependency "redis" # dynamic routing controls
