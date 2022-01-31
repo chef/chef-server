71cc1ad8efaf3174cfee062943aa20ca03f5b8da
skeshari12 <skeshari@msystechnologies.com>
2022-01-31 22:38:21 +0530

    load default cpanminus verison
    Signed-off-by: skeshari12 <skeshari@msystechnologies.com>

--- omnibus_overrides.rb
+++ omnibus_overrides.rb
@@ -9,8 +9,6 @@ override :ohai, version: "v16.17.0"
 override :ruby, version: "2.7.5"
 override :perl, version: "5.18.1"
 override :redis, version: "5.0.14"
-
-# override :cpanminus, version: "1.7045" # 1.9019 breaks installs currently
 override :logrotate, version: "3.9.2" # 3.18.0 patches fail
 
 override :openresty, version: "1.19.9.1"
