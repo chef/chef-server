0f815fed8b180e6d68ffa99d574f4e17d117eb33
antima-gupta <agupta@msystechnologies.com>
2022-02-14 15:16:05 +0530

    Updated the ActiveRecord::MigrationActiveRecord::Migration to 7.0 .
    Signed-off-by: antima-gupta <agupta@msystechnologies.com>

--- src/oc-id/db/migrate/20180214142542_add_scopes_to_oauth_applications.rb
+++ src/oc-id/db/migrate/20180214142542_add_scopes_to_oauth_applications.rb
@@ -1,4 +1,4 @@
-class AddScopesToOauthApplications < ActiveRecord::Migration[6.0]
+class AddScopesToOauthApplications < ActiveRecord::Migration[7.0]
   def change
     add_column :oauth_applications, :scopes, :string, null: false, default: ''
   end
