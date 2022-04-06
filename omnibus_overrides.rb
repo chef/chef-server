f2fa1f437239ec8ce6723bddc12dd8685fcde020
Vinay Satish <vinay.satish@progress.com>
2022-04-06 18:04:40 +0530

    updating perl to 5.34.0
    Signed-off-by: Vinay Satish <vinay.satish@progress.com>

--- omnibus_overrides.rb
+++ omnibus_overrides.rb
@@ -7,12 +7,13 @@ override :'omnibus-ctl', version: "main"
 override :chef, version: "v16.17.51"
 override :ohai, version: "v16.17.0"
 override :ruby, version: "2.7.5"
-override :perl, version: "5.18.1"
+override :perl, version: "5.34.0"
 override :redis, version: "5.0.14"
 override :runit, version: "2.1.1" #standalone upgrade is failing, Needs to be reverted to 2.1.2 after fixing the umbrella
 override :sqitch, version: "0.973"
 
-override :cpanminus, version: "1.7004" # 1.9019 breaks installs currently
+#removing the override for perl update
+#override :cpanminus, version: "1.7004" # 1.9019 breaks installs currently
 override :logrotate, version: "3.9.2" # 3.18.0 patches fail
 
 # update `src/openresty-noroot/habitat/plan.sh` when updating this version.
