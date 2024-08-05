4752b1480c0a2742fa29bcdc567fa62256af4c86
Lincoln Baker <lbaker@chef.io>
2024-08-05 11:56:00 -0500

    fix undefined type error
    Signed-off-by: Lincoln Baker <lbaker@chef.io>

--- src/oc_erchef/apps/chef_objects/src/chef_depsolver_worker.erl
+++ src/oc_erchef/apps/chef_objects/src/chef_depsolver_worker.erl
@@ -67,12 +67,12 @@ start_link() ->
 %% Solve dependencies with the given state and constraints
 %% @end
 %%--------------------------------------------------------------------
--spec solve_dependencies(AllVersions :: [chef_depsolver:dependency_set()],
-                         EnvConstraints :: [chef_depsolver:constraint()],
-                         Cookbooks :: [Name :: binary() |
-                                             {Name :: binary(), Version :: binary()}],
-                         Timeout :: integer()) ->
-                                {ok, [ versioned_cookbook()]} | {error, term()}.
+%-spec solve_dependencies(AllVersions :: [chef_depsolver:dependency_set()],
+%                         EnvConstraints :: [chef_depsolver:constraint()],
+%                         Cookbooks :: [Name :: binary() |
+%                                             {Name :: binary(), Version :: binary()}],
+%                         Timeout :: integer()) ->
+%                                {ok, [ versioned_cookbook()]} | {error, term()}.
 solve_dependencies(AllVersions, EnvConstraints, Cookbooks, Timeout) ->
 ?debugMsg("solve_dependencies calling pooler:take_member"),
     case pooler:take_member(chef_depsolver, pooler_timeout()) of
