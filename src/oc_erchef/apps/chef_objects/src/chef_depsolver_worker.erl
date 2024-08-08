5ecc9f32a794bb8c2a942e6b9de4da00047ee80e
Lincoln Baker <lbaker@chef.io>
2024-08-08 16:23:38 -0500

    Add {minor_version, 1} option to term_to_binary pursuant to Erlang 26 update.
    term_to_binary changed.  This option is now needed in order to maintain
    current behavior and avoid failures.
    
    Signed-off-by: Lincoln Baker <lbaker@chef.io>

--- src/oc_erchef/apps/chef_objects/src/chef_depsolver_worker.erl
+++ src/oc_erchef/apps/chef_objects/src/chef_depsolver_worker.erl
@@ -125,7 +125,7 @@ init([]) ->
     %% info on startup so that we can use it in the event of a timeout. Hard-killing the process
     %% handles the failure case where the Ruby process gets hung and can no longer respond to
     %% STDOUT closing, which would typically cause the process to exit.
-    Payload = term_to_binary({get_pid}),
+    Payload = term_to_binary({get_pid}, [{minor_version, 1}]),
     erlang:port_command(Port, Payload),
     Pid = receive
               {Port, {data, Data}} ->
@@ -154,7 +154,7 @@ handle_call({solve, AllVersions, EnvConstraints, Cookbooks, Timeout},
     Payload = term_to_binary({solve, [{environment_constraints, EnvConstraints},
                                       {all_versions, AllVersions},
                                       {run_list, Cookbooks},
-                                      {timeout_ms, Timeout}]}),
+                                      {timeout_ms, Timeout}]}, [{minor_version, 1}]),
     erlang:port_command(Port, Payload),
 
     %% The underlying ruby code has the potential to reach nearly 2x the
