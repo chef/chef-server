d476605e78d2d5bb7770aeeabe30745067842f5f
Lincoln Baker <lbaker@chef.io>
2024-08-20 22:17:53 -0500

    Fix license scout issue (skip_transitive_dependency_licensing true)
    Signed-off-by: Lincoln Baker <lbaker@chef.io>

--- omnibus/config/software/server-ctl-cookbooks.rb
+++ omnibus/config/software/server-ctl-cookbooks.rb
@@ -22,6 +22,8 @@ skip_transitive_dependency_licensing true
 
 license :project_license
 
+skip_transitive_dependency_licensing true
+
 build do
   env = with_standard_compiler_flags(with_embedded_path)
 
