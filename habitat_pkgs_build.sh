1c4f6238fa7937524d90daa0a74fe93aed239b39
talktovikas <vikas.yadav@progress.com>
2024-09-16 17:10:10 +0530

    test G
    Signed-off-by: talktovikas <vikas.yadav@progress.com>

--- habitat_pkgs_build.sh
+++ habitat_pkgs_build.sh
@@ -3,7 +3,7 @@
 # this script builds all the essential habitat packages for running Chef Server
 # additionaly, it exports them as a local docker image
 
-export CHEF_SERVER_SRC='/workdir/src'
+export CHEF_SERVER_SRC='/src/src'
 export ORIGIN=chefservertesting
 
 for dir in oc-id openresty-noroot nginx bookshelf chef-server-ctl oc_bifrost oc_erchef; do
