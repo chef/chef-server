5cde95c7ef0aaf4ec60a0ce91760c3ec58fc8db9
talktovikas <vikas.yadav@progress.com>
2024-09-17 18:53:28 +0530

    Initial commit for bookself.
    Signed-off-by: talktovikas <vikas.yadav@progress.com>

---
+++ .expeditor/habitat.sh
@@ -0,0 +1,26 @@
+export CHEF_SERVER_SRC='/workdir/src'
+export ORIGIN=chef
+export HAB_LICENSE=accept-no-persist
+export PACKAGE_NAME=$1
+
+
+curl https://raw.githubusercontent.com/habitat-sh/habitat/main/components/hab/install.sh | sudo bash
+
+export JOB_TEMP_ROOT
+JOB_TEMP_ROOT=$(mktemp -d /tmp/job-root-XXXXXX)
+export HAB_CACHE_KEY_PATH
+HAB_CACHE_KEY_PATH="$JOB_TEMP_ROOT/keys"
+
+echo "--- :key: Generating fake origin key"
+hab license accept
+hab origin key generate
+echo "The directory:" `pwd`
+echo "ls -l:" `ls -l`
+echo "whoami:" `whoami`
+# cd /workdir/src/bookshelf
+hab pkg build src/$PACKAGE_NAME
+pushd results
+pkg_name=$(ls -1t *.hart | head -1)
+popd
+echo pkg_name is $pkg_name
+buildkite-agent artifact upload results/$pkg_name
