f89a3a430ef56dc9c6185f15b7711012d6ad2a8f
talktovikas <vikas.yadav@progress.com>
2024-09-17 17:09:33 +0530

    test N
    Signed-off-by: talktovikas <vikas.yadav@progress.com>

--- .expeditor/habitat.sh
+++ .expeditor/habitat.sh
@@ -2,22 +2,28 @@
 
 # this script builds all the essential habitat packages for running Chef Server
 # additionaly, it exports them as a local docker image
-export CHEF_SERVER_SRC='/src/src'
-export ORIGIN=chefservertesting
+# export CHEF_SERVER_SRC='/src/src'
+export ORIGIN=chef
 export HAB_LICENSE=accept-no-persist
 
-for dir in oc-id openresty-noroot nginx bookshelf chef-server-ctl oc_bifrost oc_erchef; do
-  cd $CHEF_SERVER_SRC/$dir
-  echo "hab version is :" `hab --version`
-  echo "[STATUS] building $dir"
-  build > /var/log/build-${dir}-$(date +%s).log
-  if [[ $dir =~ dbdpg|openresty-noroot ]]; then continue; fi
-  echo "[STATUS] exporting $dir pkg to docker daemon"
-  hab pkg export docker -i "$ORIGIN/{{pkg_name}}" \
-    --no-push-image \
-    --no-tag-latest \
-    --no-tag-version \
-    --no-tag-version-release \
-    --tag-custom "localdev" \
-    $(ls -1t results/*.hart | head -1)
-done
+curl https://raw.githubusercontent.com/habitat-sh/habitat/main/components/hab/install.sh | sudo bash
+hab license accept
+hab origin key generate
+hab pkg build src/bookshelf
+
+
+# for dir in oc-id openresty-noroot nginx bookshelf chef-server-ctl oc_bifrost oc_erchef; do
+#   cd $CHEF_SERVER_SRC/$dir
+#   echo "hab version is :" `hab --version`
+#   echo "[STATUS] building $dir"
+#   build > /var/log/build-${dir}-$(date +%s).log
+#   if [[ $dir =~ dbdpg|openresty-noroot ]]; then continue; fi
+#   echo "[STATUS] exporting $dir pkg to docker daemon"
+#   hab pkg export docker -i "$ORIGIN/{{pkg_name}}" \
+#     --no-push-image \
+#     --no-tag-latest \
+#     --no-tag-version \
+#     --no-tag-version-release \
+#     --tag-custom "localdev" \
+#     $(ls -1t results/*.hart | head -1)
+# done
