883eb6d8f2d660c11aa14d285d2b89a42d6696c8
talktovikas <vikas.yadav@progress.com>
2024-09-20 15:36:15 +0530

    adding build for automate.
    Signed-off-by: talktovikas <vikas.yadav@progress.com>

---
+++ .expeditor/automate_build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -e
+
+git clone https://github.com/chef/automate.git
+cd automate
+git checkout vikas/cs-changes-for-pipeline
+pushd results
+buildkite-agent artifact download chef-*.hart
+popd
+
+echo "results directory contents" `ls -l results`
+./scripts/verify_build.sh
+
+tar -cvf results.tar results
+
+gzip results.tar
+
+buildkite-agent artifact upload results.tar.gz
