9abb7d119d6545723feb069e3aae33d521af02cc
sreepuramsudheer <ssudheer@progress.com>
2024-09-20 19:00:23 +0530

    corrected .sh file location.
    Signed-off-by: sreepuramsudheer <ssudheer@progress.com>

--- .expeditor/automate_build.sh
+++ .expeditor/automate_build.sh
@@ -5,7 +5,7 @@ git clone https://github.com/chef/automate.git
 cd automate
 git checkout vikas/cs-changes-for-pipeline
 pushd results
-buildkite-agent artifact download chef-*.hart
+buildkite-agent artifact download "chef-*.hart"
 popd
 
 echo "results directory contents" `ls -l results`
