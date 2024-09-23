08cf127be23b5ea50cbf251981dcf45477c59bef
sreepuramsudheer <ssudheer@progress.com>
2024-09-23 19:31:06 +0530

    Made few changes.
    Signed-off-by: sreepuramsudheer <ssudheer@progress.com>

--- .expeditor/automate_build.sh
+++ .expeditor/automate_build.sh
@@ -3,11 +3,14 @@
 git clone https://github.com/chef/automate.git
 cd automate
 git checkout vikas/cs-changes-for-pipeline
-# buildkite-agent artifact download "*.hart" ./results
+
+buildkite-agent artifact download "*.hart" ./results
 
 # echo "results directory contents" `ls -l results`
 
-./scripts/verify_build.sh
+hab studio run -D "source .studiorc; set -e; build components/automate-cs-bookshelf"
+
+sh -vx ./scripts/verify_build.sh
 
 tar -cvf results.tar results
 echo "results.tar created"
