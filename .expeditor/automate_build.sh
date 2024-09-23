57fcc22906c024146b27d0fba8d6696cef757f7a
talktovikas <vikas.yadav@progress.com>
2024-09-23 19:10:59 +0530

    more test
    Signed-off-by: talktovikas <vikas.yadav@progress.com>

--- .expeditor/automate_build.sh
+++ .expeditor/automate_build.sh
@@ -4,9 +4,9 @@ echo "inside script"
 git clone https://github.com/chef/automate.git
 cd automate
 git checkout vikas/cs-changes-for-pipeline
-# pushd results
-# buildkite-agent artifact download "*.hart" ./
-# popd
+pushd results
+buildkite-agent artifact download "*.hart" ./
+popd
 
 echo "results directory contents" `ls -l results`
 
