f1500becfa3216f7ac39d1c2596a3ddca23b76dd
talktovikas <vikas.yadav@progress.com>
2024-09-23 12:00:58 +0530

    trying search and replace.
    Signed-off-by: talktovikas <vikas.yadav@progress.com>

--- .expeditor/automate_build.sh
+++ .expeditor/automate_build.sh
@@ -9,6 +9,18 @@ buildkite-agent artifact download "*.hart" ./
 popd
 
 echo "results directory contents" `ls -l results`
+
+Bookself_hart_file=$(ls results/*bookchef*.hart)
+echo "Found hart file: $Bookself_hart_file"
+
+base_name=$(basename "$Bookself_hart_file") # Get just the filename
+IFS='-' read -r name version timestamp <<< "$base_name"
+formatted_output="${name#chef-}/$version/$timestamp"
+
+plan_file="components/automate-cs-bookshelf/habitat/plan.sh"
+sed -i "s|\${vendor_origin}/bookshelf|\${vendor_origin}/${formatted_output}|g" "$plan_file"
+echo "Replaced line in $plan_file"
+
 ./scripts/verify_build.sh
 
 tar -cvf results.tar results
