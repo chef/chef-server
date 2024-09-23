515bfd32ed9e3bde11173827029d10fc21428755
sreepuramsudheer <ssudheer@progress.com>
2024-09-23 20:00:43 +0530

    added env variables.
    Signed-off-by: sreepuramsudheer <ssudheer@progress.com>

--- .expeditor/automate_build.sh
+++ .expeditor/automate_build.sh
@@ -4,13 +4,22 @@ git clone https://github.com/chef/automate.git
 cd automate
 git checkout vikas/cs-changes-for-pipeline
 
-buildkite-agent artifact download "*.hart" ./results
+# buildkite-agent artifact download "*.hart" ./results
 
 # echo "results directory contents" `ls -l results`
+export HAB_NONINTERACTIVE=true
+export HAB_STUDIO_SECRET_HAB_NONINTERACTIVE=true
+export HAB_NOCOLORING=true
+export HAB_STUDIO_SECRET_HAB_FEAT_IGNORE_LOCAL=false
+export HAB_STUDIO_SECRET_HAB_FEAT_OFFLINE_INSTALL=true
+export HAB_LICENSE="accept-no-persist"
+export HAB_ORIGIN=chef
+export ALLOW_LOCAL_PACKAGES=true
 
 hab studio run -D "source .studiorc; set -e; build components/automate-cs-bookshelf"
 
-sh -vx ./scripts/verify_build.sh
+echo "after build" `ls -l results`
+./scripts/verify_build.sh
 
 tar -cvf results.tar results
 echo "results.tar created"
