42b2358e3bc7c53daf7fb3fadfeb29b5e47e5ef1
sreepuramsudheer <ssudheer@progress.com>
2024-09-23 20:07:46 +0530

    added hab environment setup step
    Signed-off-by: sreepuramsudheer <ssudheer@progress.com>

--- .expeditor/automate_build.sh
+++ .expeditor/automate_build.sh
@@ -6,6 +6,7 @@ git checkout vikas/cs-changes-for-pipeline
 
 # buildkite-agent artifact download "*.hart" ./results
 
+
 # echo "results directory contents" `ls -l results`
 export HAB_NONINTERACTIVE=true
 export HAB_STUDIO_SECRET_HAB_NONINTERACTIVE=true
@@ -15,6 +16,14 @@ export HAB_STUDIO_SECRET_HAB_FEAT_OFFLINE_INSTALL=true
 export HAB_LICENSE="accept-no-persist"
 export HAB_ORIGIN=chef
 export ALLOW_LOCAL_PACKAGES=true
+
+export JOB_TEMP_ROOT
+JOB_TEMP_ROOT=$(mktemp -d /tmp/job-root-XXXXXX)
+export HAB_CACHE_KEY_PATH
+HAB_CACHE_KEY_PATH="$JOB_TEMP_ROOT/keys"
+
+echo "--- :key: Generating fake origin key"
+hab license accept
 
 hab studio run -D "source .studiorc; set -e; build components/automate-cs-bookshelf"
 
