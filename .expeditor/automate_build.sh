2dc0e3da446e3ddc54a406a3a91dc7403ef5a5c0
Kallol Roy <kallol.roy@progress.com>
2024-09-24 18:37:53 +0530

    Trying to run verify_build
    Signed-off-by: Kallol Roy <kallol.roy@progress.com>

--- .expeditor/automate_build.sh
+++ .expeditor/automate_build.sh
@@ -7,36 +7,37 @@ git checkout vikas/cs-changes-for-pipeline
 # buildkite-agent artifact download "*.hart" ./results
 
 ../.expeditor/replace.sh
+./scripts/verify_build.sh
 
-# echo "results directory contents" `ls -l results`
-export HAB_NONINTERACTIVE=true
-export HAB_STUDIO_SECRET_HAB_NONINTERACTIVE=true
-export HAB_NOCOLORING=true
-export HAB_LICENSE="accept-no-persist"
-export HAB_ORIGIN=chef
-export HAB_STUDIO_SECRET_HAB_FEAT_IGNORE_LOCAL=true
-export HAB_STUDIO_SECRET_HAB_FEAT_OFFLINE_INSTALL=true
-export ALLOW_LOCAL_PACKAGES=true
+# # echo "results directory contents" `ls -l results`
+# export HAB_NONINTERACTIVE=true
+# export HAB_STUDIO_SECRET_HAB_NONINTERACTIVE=true
+# export HAB_NOCOLORING=true
+# export HAB_LICENSE="accept-no-persist"
+# export HAB_ORIGIN=chef
+# export HAB_STUDIO_SECRET_HAB_FEAT_IGNORE_LOCAL=true
+# export HAB_STUDIO_SECRET_HAB_FEAT_OFFLINE_INSTALL=true
+# export ALLOW_LOCAL_PACKAGES=true
 
-export JOB_TEMP_ROOT
-JOB_TEMP_ROOT=$(mktemp -d /tmp/job-root-XXXXXX)
-export HAB_CACHE_KEY_PATH
-HAB_CACHE_KEY_PATH="$JOB_TEMP_ROOT/keys"
+# export JOB_TEMP_ROOT
+# JOB_TEMP_ROOT=$(mktemp -d /tmp/job-root-XXXXXX)
+# export HAB_CACHE_KEY_PATH
+# HAB_CACHE_KEY_PATH="$JOB_TEMP_ROOT/keys"
 
-echo "--- :key: Generating fake origin key"
-hab license accept
-hab origin key generate
+# echo "--- :key: Generating fake origin key"
+# hab license accept
+# hab origin key generate
 
-hab pkg build "components/automate-cs-oc-erchef"
+# hab studio run -D "source .studiorc; set -e; build "components/automate-cs-oc-erchef"
 
-# hab studio run -D "source .studiorc; set -e; build components/automate-cs-bookshelf"
+# # hab studio run -D "source .studiorc; set -e; build components/automate-cs-bookshelf"
 
-# hab studio run -D "source .studiorc; set -e; build components/automate-cs-nginx"
+# # hab studio run -D "source .studiorc; set -e; build components/automate-cs-nginx"
 
-# hab studio run -D "source .studiorc; set -e; build components/automate-cs-oc-bifrost"
+# # hab studio run -D "source .studiorc; set -e; build components/automate-cs-oc-bifrost"
 
-# hab studio run -D "source .studiorc; set -e; build components/automate-cs-oc-erchef"
+# # hab studio run -D "source .studiorc; set -e; build components/automate-cs-oc-erchef"
 
-# hab studio run -D "source .studiorc; set -e; build components/automate-cs-ocid"
+# # hab studio run -D "source .studiorc; set -e; build components/automate-cs-ocid"
 
-echo "after build" `ls -l results`
+# echo "after build" `ls -l results`
