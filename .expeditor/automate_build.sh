6076feef188f8a5aa4c0b681de64077800640755
sreepuramsudheer <ssudheer@progress.com>
2024-09-25 19:09:48 +0530

    Added few environment variables.
    Signed-off-by: sreepuramsudheer <ssudheer@progress.com>

--- .expeditor/automate_build.sh
+++ .expeditor/automate_build.sh
@@ -1,8 +1,20 @@
 #!/bin/bash -e
 
-export ORIGIN=cheftest
-export HAB_ORIGIN=cheftest
+export ORIGIN=chef
+export HAB_ORIGIN=chef
 export HAB_LICENSE=accept-no-persist
+export OCTOKIT_ACCESS_TOKEN
+export HAB_LICENSE=accept
+export CHEF_LICENSE="accept-no-persist"
+export CI=true
+#export HAB_ORIGIN=cheftest
+export HAB_ORIGIN=chef
+#export HAB_ORIGIN_KEYS=cheftest
+export HAB_ORIGIN_KEYS=chef
+export HAB_STUDIO_SECRET_HAB_FEAT_IGNORE_LOCAL=false
+export HAB_FEAT_IGNORE_LOCAL=false
+export HAB_STUDIO_HOST_ARCH=x86_64-linux
+export HAB_FEAT_OFFLINE_INSTALL=true
 
 curl https://raw.githubusercontent.com/habitat-sh/habitat/main/components/hab/install.sh | sudo bash
 
@@ -15,7 +27,6 @@ echo "--- :key: Generating fake origin key"
 hab license accept
 hab origin key generate
 
-# cd /workdir/src/bookshelf
 for pkg_name in `echo "bookshelf openresty-noroot"`
 do
   echo "generating package for $pkg_name"
@@ -28,7 +39,7 @@ for pkg_name in `echo "nginx"`
 do
   echo "generating package for $pkg_name"
   openresty_hart=$(ls -1t results/chef-openresty*.hart | head -1)
-  hab pkg install $openresty_hart
+  HAB_FEAT_OFFLINE_INSTALL=true HAB_FEAT_IGNORE_LOCAL=false HAB_ORIGIN=chef HAB_CACHE_KEY_PATH="$JOB_TEMP_ROOT/keys" DO_CHECK=true HAB_BLDR_CHANNEL=dev hab studio run -D "set -e; hab pkg install $openresty_hart; hab pkg build src/$pkg_name"
   hab studio run -D "hab pkg install $openresty_hart; build components/automate-cs-bookshelf" "src/$pkg_name"
 done
 
