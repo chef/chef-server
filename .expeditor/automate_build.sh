1925d8252385367967e5430fc07e6a862790a6a9
talktovikas <vikas.yadav@progress.com>
2024-09-30 10:43:23 +0530

    changing automate branch to obtain build record in results.tar
    Signed-off-by: talktovikas <vikas.yadav@progress.com>

--- .expeditor/automate_build.sh
+++ .expeditor/automate_build.sh
@@ -37,7 +37,7 @@ HAB_FEAT_OFFLINE_INSTALL=true HAB_FEAT_IGNORE_LOCAL=false HAB_ORIGIN=chef HAB_CA
 
 git clone https://github.com/chef/automate.git
 cd automate
-git checkout vikas/cs-changes-for-pipeline
+git checkout kalroy/cs_plan_changes
 
 RESOLVED_RESULTS_DIR=$(realpath results/)
 export DO_CHECK=true
