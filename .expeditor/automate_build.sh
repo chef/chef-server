7f18a42dc44457a6d344e070b96377054edfd807
Kallol Roy <kallol.roy@progress.com>
2024-10-17 00:26:06 +0530

    Unsetting GOROOT
    Signed-off-by: Kallol Roy <kallol.roy@progress.com>

--- .expeditor/automate_build.sh
+++ .expeditor/automate_build.sh
@@ -66,7 +66,7 @@ erchef_hart=$(ls -1t results/chef-oc_erchef*.hart | head -1)
 
 #HAB_FEAT_OFFLINE_INSTALL=true HAB_FEAT_IGNORE_LOCAL=false HAB_ORIGIN=chef HAB_CACHE_KEY_PATH="$JOB_TEMP_ROOT/keys" DO_CHECK=true HAB_BLDR_CHANNEL=dev hab studio run -D "set -e; hab pkg install $bifrost_hart; hab pkg build components/automate-cs-oc-bifrost"
 
-HAB_FEAT_OFFLINE_INSTALL=true HAB_FEAT_IGNORE_LOCAL=false HAB_ORIGIN=chef HAB_CACHE_KEY_PATH="$JOB_TEMP_ROOT/keys" DO_CHECK=true HAB_BLDR_CHANNEL=dev GOPROXY="https://proxy.golang.org,direct " GOSUMDB="sum.golang.org" hab studio run -D "set -e; hab pkg install $erchef_hart; hab pkg build components/automate-cs-oc-erchef"
+HAB_FEAT_OFFLINE_INSTALL=true HAB_FEAT_IGNORE_LOCAL=false HAB_ORIGIN=chef HAB_CACHE_KEY_PATH="$JOB_TEMP_ROOT/keys" DO_CHECK=true HAB_BLDR_CHANNEL=dev GOPROXY="https://proxy.golang.org,direct " GOSUMDB="sum.golang.org" hab studio run -D "set -e; unset GOROOT; hab pkg install $erchef_hart; hab pkg build components/automate-cs-oc-erchef"
 
 #HAB_FEAT_OFFLINE_INSTALL=true HAB_FEAT_IGNORE_LOCAL=false HAB_ORIGIN=chef HAB_CACHE_KEY_PATH="$JOB_TEMP_ROOT/keys" DO_CHECK=true HAB_BLDR_CHANNEL=dev hab studio run -D "set -e; hab pkg install $oc_id; hab pkg build components/automate-cs-ocid"
 
