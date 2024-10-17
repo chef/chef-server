1c0c574434663e0580f7cd26a00d81ae27386c5e
Vikas Yadav <139783200+talktovikas@users.noreply.github.com>
2024-10-17 17:37:09 +0530

    Build tests for embedded Chef-Server in Automate (#3917)
    * Initial commit for .hart files.
    
    * Adding more cases for Habitat.
    
    * fixing build issue in pipeline.
    ---------
    
    Signed-off-by: talktovikas <vikas.yadav@progress.com>
    Signed-off-by: sreepuramsudheer <ssudheer@progress.com>
    Signed-off-by: Kallol Roy <kallol.roy@progress.com>
    Co-authored-by: sreepuramsudheer <ssudheer@progress.com>
    Co-authored-by: Kallol Roy <kallol.roy@progress.com>

---
+++ .expeditor/chef_server.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#===============================================================================
+#Downloading the automate repo
+#===============================================================================
+
+git clone https://github.com/chef/automate.git
+cd automate
+git checkout kalroy/cs_plan_changes
+
+#===============================================================================
+chmod +x ./integration/tests/chef_server.sh
+#running the chef_server.sh script from the automate repo
+integration/run_test integration/tests/chef_server.sh
---
+++ .expeditor/habitat.sh
@@ -0,0 +1,32 @@
+#!/bin/bash -e
+
+# this script builds all the essential habitat packages for running Chef Server
+# additionaly, it exports them as a local docker image
+
+export CHEF_SERVER_SRC='/workdir/src'
+export ORIGIN=chef
+export HAB_LICENSE=accept-no-persist
+# export PACKAGE_NAME=$1
+
+
+curl https://raw.githubusercontent.com/habitat-sh/habitat/main/components/hab/install.sh | sudo bash
+
+export JOB_TEMP_ROOT
+JOB_TEMP_ROOT=$(mktemp -d /tmp/job-root-XXXXXX)
+export HAB_CACHE_KEY_PATH
+HAB_CACHE_KEY_PATH="$JOB_TEMP_ROOT/keys"
+
+echo "--- :key: Generating fake origin key"
+hab license accept
+hab origin key generate
+
+# cd /workdir/src/bookshelf
+echo "generating package for $PACKAGE_NAME"
+hab pkg build "src/$PACKAGE_NAME"
+echo "which pushd " $(which pushd)
+
+pushd results
+pkg_name=$(ls -1t *.hart | head -1)
+popd
+echo pkg_name is $pkg_name
+buildkite-agent artifact upload results/$pkg_name
