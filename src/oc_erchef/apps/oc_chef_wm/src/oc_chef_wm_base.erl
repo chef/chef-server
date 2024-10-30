a8c46951f0d530b5f084b030d7bfcfd26b3cfee3
jan shahid shaik <jashaik@progress.com>
2024-10-30 08:31:27 +0530

    Updated chef-server-ctl to handle invalid license + Added grace period in X-Ops-License
    Signed-off-by: jan shahid shaik <jashaik@progress.com>

--- src/oc_erchef/apps/oc_chef_wm/src/oc_chef_wm_base.erl
+++ src/oc_erchef/apps/oc_chef_wm/src/oc_chef_wm_base.erl
@@ -93,12 +93,12 @@ service_available(Req, #base_state{reqid_header_name = HeaderName} = State) ->
                             {true, Req, State3};
                         {commercial_grace_period, _, _, ExpDate, LicWarnMsg} ->
                             XOps = binary_to_list(chef_json:encode({[{<<"licenseType">>, <<"commercial">>},{<<"expirationDateTime">>, list_to_binary(ExpDate)},
-                                {<<"warningMessage">>, list_to_binary(LicWarnMsg)}]})),
+                                {<<"warningMessage">>, list_to_binary(LicWarnMsg)}, {<<"gracePeriod">>, true}]})),
                             Req1 = wrq:set_resp_header("X-Ops-License", XOps, Req),
                             {true, Req1, State3};
                         {_, Type, _, ExpDate, LicWarnMsg} ->
                             XOps = binary_to_list(chef_json:encode({[{<<"licenseType">>, Type},{<<"expirationDateTime">>, list_to_binary(ExpDate)},
-                                {<<"warningMessage">>, list_to_binary(LicWarnMsg)}]})),
+                                {<<"warningMessage">>, list_to_binary(LicWarnMsg)}, {<<"gracePeriod">>, false}]})),
                             Req1 = wrq:set_resp_header("X-Ops-License", XOps, Req),
                             Req2 = chef_wm_util:set_json_body(Req1, {[{<<"error">>,  list_to_binary(LicWarnMsg)},
                                                     {<<"message">>, <<"invalid-license">>}]}),
