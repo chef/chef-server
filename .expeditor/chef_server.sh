474ef0aa2979beded2a0e2e813825bde42707801
sreepuramsudheer <ssudheer@progress.com>
2024-11-13 19:45:03 +0530

    testing is_automate.
    Signed-off-by: sreepuramsudheer <ssudheer@progress.com>

--- .expeditor/chef_server.sh
+++ .expeditor/chef_server.sh
@@ -8,6 +8,8 @@ git clone https://github.com/chef/automate.git
 cd automate
 git checkout kalroy/cs_plan_changes
 
+echo "printing is_automate"
+env | grep IS_AUTOMATE
 #===============================================================================
 chmod +x ./integration/tests/chef_server.sh
 #running the chef_server.sh script from the automate repo
