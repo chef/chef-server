df2d576c29226e3cbc82fd5643748928b37c4b81
sreepuramsudheer <ssudheer@progress.com>
2024-11-13 22:16:03 +0530

    Replaced hardcoded expired license with one received from vault.
    Signed-off-by: sreepuramsudheer <ssudheer@progress.com>

--- oc-chef-pedant/spec/api/server_license_spec.rb
+++ oc-chef-pedant/spec/api/server_license_spec.rb
@@ -26,8 +26,6 @@ describe "server license testing", :license do
 
     context "when having valid license" do
       it "can get all users", :smoke do
-        puts "inside valid license"
-        puts ENV['IS_AUTOMATE']
         get(request_url, platform.superuser).should look_like({
             :status => 200,
             :body => users_body
@@ -40,14 +38,14 @@ describe "server license testing", :license do
       before(:all) do
         puts "applying expired license"
         puts ENV['A2_EXPIRED_LICENSE']
-        system("chef-automate license apply -f \"eyJhbGciOiJFUzUxMiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImEwYmMzZmMxLWFiOGUtNDI4ZC1hODZiLTU5MTE4ZDM3ZjYzOSIsInZlcnNpb24iOiIxIiwidHlwZSI6InRyaWFsIiwiZ2VuZXJhdG9yIjoiY2hlZi9saWNlbnNlLTIuMC4wIiwia2V5X3NoYTI1NiI6ImUwZGYyOGM4YmM2ODE1MGVkYmZlZjk4Y2Q2YjdkYzQzOWMxZjgwYzdlN2VmNzQ3ODkzYTY4OTNhMmY3YjYwZjciLCJnZW5lcmF0aW9uX2RhdGUiOnsic2Vjb25kcyI6MTcxOTY2NzUxM30sImN1c3RvbWVyIjoiU3RhbmRhcmQgQmFuayBSZW5ld2FsIFN0YXJ0IERhdGUgMjAyNC0wNy0wMSIsImN1c3RvbWVyX2lkIjoiNTRBOTNGOUEtOEE2RC00RERGLUJCQTktRkE4MDE0MkQxNTI1IiwiY3VzdG9tZXJfaWRfdmVyc2lvbiI6IjEiLCJlbnRpdGxlbWVudHMiOlt7Im5hbWUiOiJDaGVmIEF1dG9tYXRlIiwibWVhc3VyZSI6Im5vZGVzIiwibGltaXQiOi0xLCJzdGFydCI6eyJzZWNvbmRzIjoxNzE5NjE5MjAwfSwiZW5kIjp7InNlY29uZHMiOjE3MjIyOTc1OTl9fV19.AErwHcnIs7TQoWPEj35Fj-_zPOPIHAXH1YBpm7pUItchNhAMJhdlajhIlS0byVhmS479jf9mn9uCGTos9vsy4w9eAZV2uUUlXB9uOzd6QWEv_FpX2_CsfrpNQfkRiaZ21IgKySXEfmoNbbLWBXPC9lOxhwg2HkbH4aB0lkBUVqTHwE9D\"")
-        system("sleep 100")
+        system("chef-automate license apply -f \"${A2_EXPIRED_LICENSE}\"")
+        system("sleep 50")
         puts "expired license applied"
         puts system("chef-automate license status")
       end
       after(:all) do
         system("chef-automate license apply \"$A2_LICENSE\"")
-        system("sleep 100")
+        system("sleep 50")
         puts "valid license applied"
         puts system("chef-automate license status")
       end
