73d41ea0ce31dc0ef82f82c3004df96b60eae13c
jan shahid shaik <jashaik@progress.com>
2024-12-03 10:20:26 +0530

    Added libxml2
    Signed-off-by: jan shahid shaik <jashaik@progress.com>

--- omnibus/config/software/postgresql13.rb
+++ omnibus/config/software/postgresql13.rb
@@ -28,6 +28,7 @@ dependency "libedit"
 dependency "ncurses"
 dependency "libossp-uuid"
 dependency "config_guess"
+dependency "Libxml2"
 
 source url: "https://ftp.postgresql.org/pub/source/v#{version}/postgresql-#{version}.tar.bz2"
 version("17.2") { source sha256: "82ef27c0af3751695d7f64e2d963583005fbb6a0c3df63d0e4b42211d7021164" }
