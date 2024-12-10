56f25dfef46cf23d736c0a9b9516f995a9abddfa
sreepuramsudheer <ssudheer@progress.com>
2024-12-10 11:55:48 +0530

    Incorporated review comments.
    Signed-off-by: sreepuramsudheer <ssudheer@progress.com>

--- .expeditor/chef_server.sh
+++ .expeditor/chef_server.sh
@@ -6,7 +6,10 @@
 
 git clone https://github.com/chef/automate.git
 cd automate
-git checkout ssudheer/ruby_update_3.1
+if [ "${AUTOMATE_BRANCH}" != "" ]
+then
+  git checkout "${AUTOMATE_BRANCH}"
+fi
 
 #===============================================================================
 chmod +x ./integration/tests/chef_server.sh
