5e919649cca2709ba2013941b3d15697ac504976
talktovikas <vikas.yadav@progress.com>
2025-01-28 13:37:42 +0530

    fixing refresh channel in Nginx.
    Signed-off-by: talktovikas <vikas.yadav@progress.com>

--- .expeditor/automate_build.sh
+++ .expeditor/automate_build.sh
@@ -42,7 +42,7 @@ done
 
 echo "generating package for nginx"
 openresty_hart=$(ls -1t results/*openresty*.hart | head -1)
-HAB_FEAT_OFFLINE_INSTALL=true HAB_FEAT_IGNORE_LOCAL=false HAB_ORIGIN=chef HAB_CACHE_KEY_PATH="$JOB_TEMP_ROOT/keys" DO_CHECK=true HAB_BLDR_CHANNEL=LTS-2024 hab studio run -D "set -e; hab pkg install $openresty_hart; hab pkg build src/nginx"
+HAB_FEAT_OFFLINE_INSTALL=true HAB_FEAT_IGNORE_LOCAL=false HAB_ORIGIN=chef HAB_CACHE_KEY_PATH="$JOB_TEMP_ROOT/keys" DO_CHECK=true HAB_BLDR_CHANNEL=LTS-2024 HAB_REFRESH_CHANNEL=LTS-2024 hab studio run -D "set -e; hab pkg install $openresty_hart; hab pkg build src/nginx"
 
 git clone https://github.com/chef/automate.git
 cd automate
