13372a19001b0da74e31a64879bbd437fe2cc599
sreepuramsudheer <106745516+sreepuramsudheer@users.noreply.github.com>
2025-07-17 11:07:19 +0530

    Changing hardcoded date. (#4024)
    Signed-off-by: sreepuramsudheer <ssudheer@progress.com>

--- oc-chef-pedant/spec/api/keys/client_keys_spec.rb
+++ oc-chef-pedant/spec/api/keys/client_keys_spec.rb
@@ -228,7 +228,8 @@ describe "Client keys endpoint", :keys, :client_keys do
   context "when a client's default key has an expiration date" do
     before(:each) do
       delete_client_key(org_name, client["name"], "default")
-      add_client_key(org_name, client["name"], :key, "default", expires: "2025-03-24T21:00:00Z").should look_like(status: 201)
+      future_time = Time.now.utc + 60 * 60 * 24 * 365 # 1 year from now
+      add_client_key(org_name, client["name"], :key, "default", expires: future_time.strftime("%Y-%m-%dT%H:%M:%SZ")).should look_like(status: 201)
     end
 
     context "and is updated via a PUT to /organizations/:org/clients/:client" do
@@ -247,7 +248,8 @@ describe "Client keys endpoint", :keys, :client_keys do
 
   context "when a client key has an expiration date and isn't expired" do
     before(:each) do
-      add_client_key(org_name, client["name"], :key, key_name, expires: "2025-03-24T21:00:00Z").should look_like(status: 201)
+      future_time = Time.now.utc + 60 * 60 * 24 * 365 # 1 year from now
+      add_client_key(org_name, client["name"], :key, key_name, expires: future_time.strftime("%Y-%m-%dT%H:%M:%SZ")).should look_like(status: 201)
     end
 
     it "should authenticate against the key" do
