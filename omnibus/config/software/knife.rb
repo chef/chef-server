3b940cbbe8040fb87ccf5c19ab7f8de420237ad3
Lincoln Baker <sabaker@progress.com>
2025-07-18 09:20:24 -0500

    Revert "upgrade to knife 18.7.10 (WIP)"
    This reverts commit e3aae0cee5e48520b5721291c6bd249a569c4875.

--- omnibus/config/software/knife.rb
+++ omnibus/config/software/knife.rb
@@ -15,7 +15,7 @@
 #
 
 name "knife"
-default_version "18.7.10"
+default_version "17.10.0"
 
 license "Apache-2.0"
 license_file "https://raw.githubusercontent.com/chef/chef/main/knife/LICENSE"
@@ -24,32 +24,17 @@ dependency "ruby"
 
 skip_transitive_dependency_licensing "true"
 
-# TODO: Switch back to gem installation when knife 18.7.10+ is published to RubyGems
-# Original gem installation method (commented out):
-# relative_path "knife-#{version}"
-# build do
-#     env = with_standard_compiler_flags(with_embedded_path)
-#     v_opts = "--version '#{version}'" unless version.nil?
-#     gem [
-#       "install knife",
-#       v_opts,
-#       "--no-document",
-#     ].compact.join(" "), env: env
-#     command "knife --version", env: env
-#     copy "#{install_dir}/embedded/bin/knife", "#{install_dir}/bin"
-# end
-
-# Temporary git source build until knife 18.7.10+ gem is published
-source git: "https://github.com/chef/chef.git"
-
-relative_path "chef"
+relative_path "knife-#{version}"
 
 build do
     env = with_standard_compiler_flags(with_embedded_path)
 
-    # Build knife from the knife subdirectory
-    command "cd knife && gem build knife.gemspec", env: env
-    command "cd knife && gem install knife-#{version}.gem --no-document", env: env
+    v_opts = "--version '#{version}'" unless version.nil?
+    gem [
+      "install knife",
+      v_opts,
+      "--no-document",
+    ].compact.join(" "), env: env
   
     # confirm the install was successful
     command "knife --version", env: env
