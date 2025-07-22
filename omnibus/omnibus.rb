b7c005d707158f1eb5a5bd5dab6a0c6e2ade5105
poorndm <poorndm@progress.com>
2025-07-22 17:12:37 +0530

    Update S3 Caching to Use Private Bucket
    Signed-off-by: poorndm <poorndm@progress.com>

--- omnibus/omnibus.rb
+++ omnibus/omnibus.rb
@@ -7,7 +7,7 @@
 use_s3_caching true
 s3_access_key  ENV['AWS_ACCESS_KEY_ID']
 s3_secret_key  ENV['AWS_SECRET_ACCESS_KEY']
-s3_bucket      'opscode-omnibus-cache'
+s3_bucket      'opscode-omnibus-cache-private'
 
 # Customize compiler bits
 # ------------------------------
