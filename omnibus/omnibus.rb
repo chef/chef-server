5535a5b9bd1dfafc4080e6f7925dd79162533a28
poorndm <poorndm@progress.com>
2025-07-23 20:57:29 +0530

    modified region
    Signed-off-by: poorndm <poorndm@progress.com>

--- omnibus/omnibus.rb
+++ omnibus/omnibus.rb
@@ -8,6 +8,7 @@ use_s3_caching true
 s3_access_key  ENV['AWS_ACCESS_KEY_ID']
 s3_secret_key  ENV['AWS_SECRET_ACCESS_KEY']
 s3_bucket      'opscode-omnibus-cache-private'
+s3_region 'us-west-2'
 
 # Customize compiler bits
 # ------------------------------
