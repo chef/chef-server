3e9df014d55675369c8ece65f2b9c381e0798127
jan shahid shaik <jashaik@progress.com>
2025-07-29 19:03:48 +0530

    openresty update
    Signed-off-by: jan shahid shaik <jashaik@progress.com>

--- omnibus_overrides.rb
+++ omnibus_overrides.rb
@@ -15,7 +15,7 @@ override :sqitch, version: "0.973"
 override :logrotate, version: "3.19.0"
 
 # update `src/openresty-noroot/habitat/plan.sh` when updating this version.
-override :openresty, version: "1.25.3.1"
+override :openresty, version: "1.25.8.1"
 
 override :nokogiri, version: "1.18.9"
 override :openssl, version: "3.2.4", fips_version: "3.1.2"
