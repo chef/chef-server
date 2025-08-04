735b026fda4af289286ee4e9e44e7ee13cf320bb
RoyShravani <shravani.roy@progress.com>
2025-08-04 12:50:53 +0530

    revert pedant rspec change
    Signed-off-by: RoyShravani <shravani.roy@progress.com>

--- src/oc_bifrost/oc-bifrost-pedant/lib/rspec-shared.rb
+++ src/oc_bifrost/oc-bifrost-pedant/lib/rspec-shared.rb
@@ -4,8 +4,3 @@
 require 'rspec/core'
 require 'rspec-shared/methods'
 RSpec.configuration.extend RSpecShared::Methods
-
-# Make shared method available in example groups
-RSpec.configure do |config|
-  config.extend RSpecShared::Methods
-end
