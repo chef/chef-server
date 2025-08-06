5ab06f7b45b7ffacbf70a80f6177c756f8febedf
Lincoln Baker <sabaker@progress.com>
2025-08-06 12:31:28 -0500

    move to -c pivotal.rb approach, stage 4
    Signed-off-by: Lincoln Baker <sabaker@progress.com>

--- src/chef-server-ctl/plugins/wrap-knife.rb
+++ src/chef-server-ctl/plugins/wrap-knife.rb
@@ -46,9 +46,9 @@ cmds.each do |cmd, args|
     
     # Build authentication arguments
     auth_args = []
-    auth_args << "--server-url" << server_url
+    # auth_args << "--server-url" << server_url
     auth_args << "-c" << knife_config
-    auth_args << "--config-option" << "ssl_verify_mode=verify_none"
+    # auth_args << "--config-option" << "ssl_verify_mode=verify_none"
     
     # Build command args - don't escape config options with = signs
     all_args = transformed_args + auth_args
