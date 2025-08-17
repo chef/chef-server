f40556c43bf2d75aa0066f3e895fe3224e7a83bf
Jan Shahid Shaik <jashaik@progress.com>
2025-08-18 01:18:43 +0700

    Fix verify 4
    Signed-off-by: Jan Shahid Shaik <jashaik@progress.com>

--- src/oc-id/spec/support/factories.rb
+++ src/oc-id/spec/support/factories.rb
@@ -2,12 +2,12 @@ require 'factory_bot'
 
 FactoryBot.define do
   factory :user do
-    username 'applejack'
-    password 'password'
-    first_name 'Apple'
-    last_name 'Jack'
-    email 'applejack@ponyville.com'
-    public_key %Q{-----BEGIN PUBLIC KEY-----
+    username { 'applejack' }
+    password { 'password' }
+    first_name { 'Apple' }
+    last_name { 'Jack' }
+    email { 'applejack@ponyville.com' }
+    public_key { %Q{-----BEGIN PUBLIC KEY-----
 MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAv8QZxdp5XQVGnpHgPdRn
 MeHghbzDW/D6oAcXT6spfqN+5T7W/TruYpJbL+9cDfrIoNW8YOvHhDp0yoHl/YNl
 ZX0bYltdgZer10/Yv9PoB2U4TAzajBcd3DF3TxiB1sBPxqLvcF30CkmPpq4lmsNs
@@ -15,16 +15,16 @@ n/L6OlcrGk26TMEhZwxw9tx8sl50DVlWm9GfefvVeZHfk1d1c5Yi/YfMiX688zRI
 SzQ2i3KSq450nfaX0p4dnRq5cq7/qW+Yr11lRByTIq6j8qEwPJNIXsUwIDWab8fr
 F6dutenFO3xjG+s12x8iU8MQLzsBMtFa9V1hr189xqUwAW0DBoiKBXjkQ20DKC/T
 SQIDAQAB
------END PUBLIC KEY-----}
+-----END PUBLIC KEY-----} }
   end
 
   factory :administrator, parent: :user do
-    username 'rainbowdash'
-    password 'password'
-    first_name 'Rainbow'
-    last_name 'Dash'
-    email 'rainbowdash@ponyville.com'
-    public_key %Q{-----BEGIN PUBLIC KEY-----
+    username { 'rainbowdash' }
+    password { 'password' }
+    first_name { 'Rainbow' }
+    last_name { 'Dash' }
+    email { 'rainbowdash@ponyville.com' }
+    public_key { %Q{-----BEGIN PUBLIC KEY-----
 MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAv8QZxdp5XQVGnpHgPdRn
 MeHghbzDW/D6oAcXT6spfqN+5T7W/TruYpJbL+9cDfrIoNW8YOvHhDp0yoHl/YNl
 ZX0bYltdgZer10/Yv9PoB2U4TAzajBcd3DF3TxiB1sBPxqLvcF30CkmPpq4lmsNs
@@ -32,6 +32,6 @@ n/L6OlcrGk26TMEhZwxw9tx8sl50DVlWm9GfefvVeZHfk1d1c5Yi/YfMiX688zRI
 SzQ2i3KSq450nfaX0p4dnRq5cq7/qW+Yr11lRByTIq6j8qEwPJNIXsUwIDWab8fr
 F6dutenFO3xjG+s12x8iU8MQLzsBMtFa9V1hr189xqUwAW0DBoiKBXjkQ20DKC/T
 SQIDAQAB
------END PUBLIC KEY-----}
+-----END PUBLIC KEY-----} }
   end
 end
