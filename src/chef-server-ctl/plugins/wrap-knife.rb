5ae9384c5b580e7f7e68b5d4e42197d49a279b44
lbakerchef <51833247+lbakerchef@users.noreply.github.com>
2025-08-21 23:57:30 -0500

    Knife-ec-backup issue in standalone fresh installs (#4072)
    * fix issue with knife-ec-backup not being installed
    
    Signed-off-by: Lincoln Baker <sabaker@progress.com>
    
    * remove previously accidentally-committed extraneous (empty) files
    
    Signed-off-by: Lincoln Baker <sabaker@progress.com>
    
    * add back original knife path as one of the path search options
    
    Signed-off-by: Lincoln Baker <sabaker@progress.com>
    
    * experimental knife path resolution change
    
    test if this works in both chef-server and automate
    
    Signed-off-by: Lincoln Baker <sabaker@progress.com>
    
    * Removed unwanted commented lines
    
    Signed-off-by: Jan Shahid Shaik <jashaik@progress.com>
    
    ---------
    
    Signed-off-by: Lincoln Baker <sabaker@progress.com>
    Signed-off-by: Jan Shahid Shaik <jashaik@progress.com>
    Co-authored-by: Jan Shahid Shaik <jashaik@progress.com>

--- src/chef-server-ctl/plugins/wrap-knife.rb
+++ src/chef-server-ctl/plugins/wrap-knife.rb
@@ -23,17 +23,11 @@ cmd_args     = ARGV[1..-1]
 # Determine knife binary path using precedence:
 # 1) CSC_KNIFE_BIN environment variable (for overriding in development/testing)
 # 2) Result of `which knife` command if available
-# 3) Default Chef Server knife path
+# 3) Original Chef Server knife path (/opt/opscode/bin/knife)
+# 4) Default embedded knife path
 def resolve_knife_bin
-  # Check environment variable first (must be non-empty)
-  return ENV["CSC_KNIFE_BIN"] if ENV["CSC_KNIFE_BIN"]&.!empty?
-  
-  # Try to find knife in PATH
-  which_result = `which knife 2>/dev/null`.strip
-  return which_result unless which_result.empty?
-  
-  # Fall back to default
-  "/opt/opscode/embedded/bin/knife"
+  # test if this works for both chef-server and automate
+  return ::ChefServerCtl::Config.knife_bin
 end
 
 knife_cmd = resolve_knife_bin
